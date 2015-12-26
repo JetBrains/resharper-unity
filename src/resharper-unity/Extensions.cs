088a0ae077fac512f2127867e8f6fd13bccd436b
Alexander Saratow <a@saratow.biz>
2015-12-26 22:40:51 +0100

    resolved issue #15 - suppressing ReSharper warnings about fields not being used or fields that can be made private on public fields in types annotated with System.SerializableAttribute, types derived from UnityEngine.UnityObject - private fields annotated with UnityEngine.SerializeField attribute are not marked as unused when they are located inside of a type annotated with System.SerializableAttribute or a type derived from UnityEngine.UnityObject

---
+++ src/resharper-unity/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+using JetBrains.Metadata.Reader.Impl;
+using JetBrains.ReSharper.Psi;
+
+namespace JetBrains.ReSharper.Plugins.Unity
+{
+    public static class Extensions
+    {
+        public static bool HasAttribute([NotNull] this IAttributesSet set, [NotNull] string attribute)
+        {
+            return set.HasAttributeInstance(new ClrTypeName(attribute), true);
+        }
+    }
+}
