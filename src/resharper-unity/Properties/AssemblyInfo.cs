531f2f70c5951052bb4e17f610da67dae71b4d62
Slava Trenogin <slava.trenogin@jetbrains.com>
2016-04-16 22:07:40 +0300

    ReSharper 2016.1 support

--- src/resharper-unity/Properties/AssemblyInfo.cs
+++ src/resharper-unity/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("resharper-unity")]
-[assembly: AssemblyCopyright("Copyright © JetBrains 2015")]
+[assembly: AssemblyCopyright("Copyright © JetBrains 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
-[assembly: AssemblyInformationalVersion("1.2.0")]
+[assembly: AssemblyVersion("1.2.1.0")]
+[assembly: AssemblyFileVersion("1.2.1.0")]
+[assembly: AssemblyInformationalVersion("1.2.1")]
