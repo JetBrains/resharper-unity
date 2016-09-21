110838f67b09c9357767a11809c687c25b35ae8a
Matt Ellis <m.t.ellis@gmail.com>
2016-09-21 16:25:54 +0100

    Navigate to Unity docs from QuickDoc
    There seems to be a bug somewhere, though, as sometimes clicking the "read more" link does nothing. See RSRP-460907

---
+++ src/resharper-unity/Help/ShowUnityHelp.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using JetBrains.Application;
+using JetBrains.Application.StdApplicationUI;
+using JetBrains.UI.Application;
+using JetBrains.Util;
+
+namespace JetBrains.ReSharper.Plugins.Unity.Help
+{
+    [ShellComponent]
+    public class ShowUnityHelp : IShowHelp
+    {
+        private readonly OpensUri myUriOpener;
+
+        public ShowUnityHelp(OpensUri uriOpener)
+        {
+            myUriOpener = uriOpener;
+        }
+
+        public bool ShowHelp(string keyword, HelpSystem.HelpKind kind)
+        {
+            if (kind != HelpSystem.HelpKind.Msdn) return false;
+            if (!IsUnityKeyword(keyword)) return false;
+
+            keyword = StripPrefix(keyword);
+
+            var uri = GetUri(keyword);
+            myUriOpener.OpenUri(uri);
+
+            return true;
+        }
+
+        // Must be less than `VsShowMsdnHelp` so we get a shot at handling it
+        public double Priority => 0;
+        public HelpSystem.HelpKind[] SupportedKinds => new[] {HelpSystem.HelpKind.Msdn};
+
+        private static bool IsUnityKeyword(string keyword)
+        {
+            return keyword.StartsWith("UnityEngine.", StringComparison.OrdinalIgnoreCase)
+                   || keyword.StartsWith("UnityEditor.", StringComparison.OrdinalIgnoreCase);
+        }
+
+        private static string StripPrefix(string keyword)
+        {
+            // We know the string starts with `UnityEngine.` or `UnityEditor.`
+            return keyword.Substring(12);
+        }
+
+        private static Uri GetUri(string keyword)
+        {
+            var path = $"ScriptReference/{keyword}.html";
+
+            return GetUri(GetProgramFiles(), path)
+                   ?? new Uri("https://docs.unity3d.com/" + path);
+        }
+
+        private static FileSystemPath GetProgramFiles()
+        {
+            // PlatformUtils.GetProgramFiles() will return the relevant folder for
+            // the current app, not the current system. So a 32 bit app on a 64 bit
+            // system will return the 32 bit Program Files. Force to get the system
+            // native Program Files folder
+            var environmentVariable = Environment.GetEnvironmentVariable("ProgramW6432");
+            return string.IsNullOrWhiteSpace(environmentVariable) ? FileSystemPath.Empty : FileSystemPath.TryParse(environmentVariable);
+        }
+
+        private static Uri GetUri(FileSystemPath programFiles, string htmlPath)
+        {
+            const string localPathRoot = "Unity/Editor/Data/Documentation/en";
+
+            if (programFiles.IsEmpty)
+                return null;
+
+            var fileSystemPath = programFiles/localPathRoot/htmlPath;
+            return fileSystemPath.ExistsFile ? fileSystemPath.ToUri() : null;
+        }
+    }
+}
