5d78db7a25767d37b951170ae819455f3c3b3e80
Matt Ellis <m.t.ellis@gmail.com>
2016-12-29 12:27:10 +0000

    Display Unity version in Properties toolwindow
    Only displayed when a Unity project is selected. Useful for validating which version of Unity is being targeted and used for API resolution.

---
+++ src/resharper-unity/VisualStudio/Property.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using JetBrains.Application;
+using JetBrains.DataFlow;
+using JetBrains.ProjectModel;
+using JetBrains.PsiFeatures.VisualStudio.Core.ProjectModel.PropertiesExtender;
+using JetBrains.VsIntegration.ProjectModel.PropertiesExtender;
+
+namespace JetBrains.ReSharper.Plugins.Unity.VisualStudio
+{
+    [SolutionComponent]
+    public class VsUnityVersionPropertiesExtenderProvider : IPropertiesExtenderProvider
+    {
+        private readonly Lifetime myLifetime;
+        private readonly IShellLocks myLocks;
+        private readonly UnityVersion myUnityVersion;
+
+        public VsUnityVersionPropertiesExtenderProvider(Lifetime lifetime, IShellLocks locks, UnityVersion unityVersion)
+        {
+            myLifetime = lifetime;
+            myLocks = locks;
+            myUnityVersion = unityVersion;
+        }
+
+        public bool CanExtend(IProjectItem projectItem)
+        {
+            var project = projectItem as IProject;
+            return project != null && project.IsUnityProject();
+        }
+
+        public IEnumerable<PropertyDescriptor> GetPropertyDescriptors(IProjectItem projectItem)
+        {
+            var project = (IProject)projectItem;
+
+            yield return new ReSharperPropertyDescriptor<string, IProject>(myLifetime, myLocks,
+              name: "UnityVersion",
+              defaultValue: "Unspecified",
+              displayName: "Unity Version",
+              description: "The version of Unity being targeted by the project. Used by ReSharper to validate APIs.",
+              projectItem: project,
+              getValueAction: p => myUnityVersion.Version.ToString(2),
+              setValueAction: (p, value) => { });
+        }
+    }
+}
