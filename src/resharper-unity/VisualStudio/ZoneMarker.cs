0a0d7de78b030d3ef32c987aaec880f4678d764e
Matt Ellis <m.t.ellis@gmail.com>
2016-12-29 12:13:03 +0000

    Update projects to 2016.3.1 SDK
    No real changes, trying to add VisualStudio components

---
+++ src/resharper-unity/VisualStudio/ZoneMarker.cs
@@ -0,0 +1,10 @@
+﻿using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.VsIntegration.Shell.Zones;
+
+namespace JetBrains.ReSharper.Plugins.Unity.VisualStudio
+{
+    [ZoneMarker]
+    public class ZoneMarker : IRequire<IVisualStudioZone>
+    {
+    }
+}
