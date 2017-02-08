6b01168a118e01a62ad94e166a08dffa5c42f8dd
Matt Ellis <m.t.ellis@gmail.com>
2017-02-08 01:14:09 +0000

    Add availability tests for convert to CompareTag

---
+++ test/data/intentions/QuickFixes/ConvertToCompareTag/Availability/Test01.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Foo
+{
+    public void Method(GameObject o)
+    {
+        if (o.t{caret}ag == "Whatever") { }
+    }
+}
---
+++ test/data/intentions/QuickFixes/ConvertToCompareTag/Availability/Test02.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Foo
+{
+    public void Method(GameObject o)
+    {
+        if ("Whatever" == o.t{caret}ag) { }
+    }
+}
