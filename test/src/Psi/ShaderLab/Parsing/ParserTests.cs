4b9ecccfbe3cac6edf4a500890789d9e2a4f2aec
Matt Ellis <m.t.ellis@gmail.com>
2017-02-25 01:00:30 +0000

    Test built in shaders parsing
    Requires built in shaders to be downloaded and placed in test/data/psi/shaderlab/parsing/external

--- test/src/Psi/ShaderLab/Parsing/ParserTests.cs
+++ test/src/Psi/ShaderLab/Parsing/ParserTests.cs
@@ -1,6 +1,17 @@
-﻿using JetBrains.ReSharper.Plugins.Unity.ProjectModel;
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Metadata.Reader.API;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Plugins.Unity.ProjectModel;
 using JetBrains.ReSharper.Plugins.Unity.Psi.ShaderLab;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.ExtensionsAPI;
+using JetBrains.ReSharper.Psi.Modules;
+using JetBrains.ReSharper.Psi.Tree;
 using JetBrains.ReSharper.TestFramework;
+using JetBrains.TestFramework.Utils;
+using JetBrains.Text;
+using JetBrains.Util;
 using NUnit.Framework;
 
 namespace JetBrains.ReSharper.Plugins.Unity.Tests.Psi.ShaderLab.Parsing
@@ -65,5 +76,60 @@ namespace JetBrains.ReSharper.Plugins.Unity.Tests.Psi.ShaderLab.Parsing
 
         [TestCase("LegacyBindChannels")]
         public void TestParser(string name) => DoOneTest(name);
+    }
+
+    [Explicit]
+    [TestUnity]
+    [TestFileExtension(ShaderLabProjectFileType.SHADER_EXTENSION)]
+    public class ParseBuiltinShaderTests : BaseTestWithSingleProject
+    {
+        // NOTE: Requires downloading the built in shaders from Unity
+        protected override string RelativeTestDataPath => @"psi\shaderLab\parsing\external\builtin_shaders-5.5.1f1";
+
+        [TestCaseSource(nameof(BuiltinShadersFolder))]
+        public void TestBuiltinShaders(string name) => DoOneTest(name);
+
+        private IEnumerable<string> BuiltinShadersFolder
+        {
+            get
+            {
+                TestUtil.SetHomeDir(GetType().Assembly);
+
+                var path = TestDataPath2;
+                foreach (var file in path.GetChildFiles("*.shader", PathSearchFlags.RecurseIntoSubdirectories))
+                    yield return file.MakeRelativeTo(TestDataPath2).ChangeExtension(string.Empty).FullPath;
+            }
+        }
+
+        protected override void DoTest(IProject testProject)
+        {
+            var projectFile = testProject.GetAllProjectFiles().FirstNotNull();
+
+            var text = projectFile.Location.ReadAllText2();
+            var buffer = new StringBuffer(text.Text);
+
+            var languageService = ShaderLabLanguage.Instance.LanguageService();
+            var lexer = languageService.GetPrimaryLexerFactory().CreateLexer(buffer);
+            var psiModule = Solution.PsiModules().GetPrimaryPsiModule(Project, TargetFrameworkId.Default);
+            var parser = languageService.CreateParser(lexer, psiModule, null);
+            var psiFile = parser.ParseFile();
+
+            if (DebugUtil.HasErrorElements(psiFile))
+            {
+                DebugUtil.DumpPsi(Console.Out, psiFile);
+                Assert.Fail("File contains error elements");
+            }
+
+            // Assert.AreEqual(buffer, psiFile.GetText(), "Reconstructed text mismatch");
+            //CheckRange(buffer, psiFile);
+        }
+
+        private static void CheckRange(string documentText, ITreeNode node)
+        {
+            Assert.AreEqual(node.GetText(), documentText.Substring(node.GetTreeStartOffset().Offset, node.GetTextLength()), "node range text mismatch");
+
+            for (var child = node.FirstChild; child != null; child = child.NextSibling)
+                CheckRange(documentText, child);
+        }
     }
 }
