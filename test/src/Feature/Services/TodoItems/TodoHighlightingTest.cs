0298cf833b06b638f45cde17444767d6f988dba4
Matt Ellis <m.t.ellis@gmail.com>
2017-02-27 19:15:58 +0000

    Find todo items in .shader files

---
+++ test/src/Feature/Services/TodoItems/TodoHighlightingTest.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.ReSharper.FeaturesTestFramework.Daemon;
+using JetBrains.ReSharper.Plugins.Unity.ProjectModel;
+using JetBrains.ReSharper.Plugins.Unity.Psi.ShaderLab;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.TestFramework;
+using NUnit.Framework;
+
+namespace JetBrains.ReSharper.Plugins.Unity.Tests.Feature.Services.TodoItems
+{
+    [TestUnity]
+    [TestFileExtension(ShaderLabProjectFileType.SHADER_EXTENSION)]
+    public class TodoHighlightingTest : ClrTodoHighlightingTestBase
+    {
+        protected override PsiLanguageType CompilerIdsLanguage => ShaderLabLanguage.Instance;
+
+        [Test] public void TestTodo01() { DoNamedTest2(); }
+    }
+}
