54ee9253892152c74aa0bc4a1cdcbde5a6ce09d3
Matt Ellis <m.t.ellis@gmail.com>
2017-05-06 00:33:36 +0100

    Fix compile errors with wave08-ide

--- test/src/Feature/Services/Descriptions/IdentifierTooltipTestBase.cs
+++ test/src/Feature/Services/Descriptions/IdentifierTooltipTestBase.cs
@@ -45,7 +45,12 @@ namespace JetBrains.ReSharper.Plugins.Unity.Tests.Feature.Services.Descriptions
                     var markupModel = Solution.GetComponent<IDocumentMarkupManager>().GetMarkupModel(document);
                     var highlighterTooltipProvider = DaemonUtil.GetHighlighterTooltipProvider(highlightingInfo.Highlighting, Solution);
                     var attributeId = HighlightingSettingsManager.Instance.GetAttributeId(highlightingInfo.Highlighting, psiSourceFile).NotNull();
-                    var highlighter = markupModel.AddHighlighter(new Key("test"), highlightingInfo.Range.TextRange, AreaType.EXACT_RANGE, 0, attributeId, new ErrorStripeAttributes(), highlighterTooltipProvider);
+#if WAVE08
+                    var key = new Key("test");
+#else
+                    var key = "test";
+#endif
+                    var highlighter = markupModel.AddHighlighter(key, highlightingInfo.Range.TextRange, AreaType.EXACT_RANGE, 0, attributeId, new ErrorStripeAttributes(), highlighterTooltipProvider);
                     ExecuteWithGold(writer => writer.WriteLine(highlighter.ToolTip));
                 }
             }
