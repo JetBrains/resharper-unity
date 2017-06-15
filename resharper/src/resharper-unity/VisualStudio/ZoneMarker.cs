2a31a3ae4d85d990a22b48846258c9d38eb3735c
Matt Ellis <m.t.ellis@gmail.com>
2017-06-15 09:10:18 +0100

    Disable VS ZoneMarker for Rider
    Code Cleanup uses XAML serialisers, which don't respect zones, so will try to load VS.Platform.Core to resolve `IVisualStudioZone`, and that causes issues in Rider.

--- resharper/src/resharper-unity/VisualStudio/ZoneMarker.cs
+++ resharper/src/resharper-unity/VisualStudio/ZoneMarker.cs
@@ -1,6 +1,13 @@
 ﻿using JetBrains.Application.BuildScript.Application.Zones;
 using JetBrains.VsIntegration.Shell.Zones;
 
+// It would be nice to use zones properly here, but
+// a) it's really hard :(
+// b) Code Cleanup uses XAML serialisers that don't respect zones and try
+//    to resolve all types causing problems for Rider, which doesn't have
+//    any VS libraries, such as VS.Platform.Core
+#if !RIDER
+
 namespace JetBrains.ReSharper.Plugins.Unity.VisualStudio
 {
     [ZoneMarker]
@@ -8,3 +15,5 @@ namespace JetBrains.ReSharper.Plugins.Unity.VisualStudio
     {
     }
 }
+
+#endif
