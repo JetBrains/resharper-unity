e72ecf564928bf18894c1a13bfc8cda9c1e80dd7
Matt Ellis <m.t.ellis@gmail.com>
2017-06-30 17:35:18 +0100

    Add commenting/uncommenting for shader files

---
+++ resharper/src/resharper-unity/Feature/Services/Comment/ShaderLabBlockCommentActionProvider.cs
@@ -0,0 +1,38 @@
+﻿using JetBrains.DocumentModel;
+using JetBrains.ReSharper.Feature.Services.Comment;
+using JetBrains.ReSharper.Plugins.Unity.Psi.ShaderLab;
+using JetBrains.ReSharper.Plugins.Unity.Psi.ShaderLab.Parsing;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.Parsing;
+using JetBrains.ReSharper.Psi.Tree;
+using JetBrains.Util;
+
+namespace JetBrains.ReSharper.Plugins.Unity.Feature.Services.Comment
+{
+    [Language(typeof(ShaderLabLanguage))]
+    public class ShaderLabBlockCommentActionProvider : IBlockCommentActionProvider
+    {
+        public TextRange GetBlockComment(CachingLexer lexer)
+        {
+            return lexer.TokenType == ShaderLabTokenType.MULTI_LINE_COMMENT
+                ? new TextRange(lexer.TokenStart, lexer.TokenEnd)
+                : TextRange.InvalidRange;
+        }
+
+        public int InsertBockCommentPosition(ILexer lexer, int position)
+        {
+            return position == lexer.TokenStart ? position : lexer.TokenEnd;
+        }
+
+        public bool IsAvailable(IFile file, DocumentRange range, out bool disableAllProviders)
+        {
+            disableAllProviders = false;
+            return true;
+        }
+
+        public string StartBlockCommentMarker => "/*";
+        public string EndBlockCommentMarker => "*/";
+        public string NestedStartBlockCommentMarker => null;
+        public string NestedEndBlockCommentMarker => "#";
+    }
+}
