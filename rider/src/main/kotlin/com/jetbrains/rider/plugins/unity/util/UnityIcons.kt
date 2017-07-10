0e5f0a6190962107c97787febab76fa68c3e7011
ivan.pashchenko <ivan.pashch@gmail.com>
2017-07-10 22:59:41 +0300

    logo fix

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/util/UnityIcons.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/util/UnityIcons.kt
@@ -5,7 +5,7 @@ import com.intellij.openapi.util.IconLoader
 class UnityIcons {
     companion object {
         // TODO: Proper icons!
-        val ShaderLabFile = IconLoader.getIcon("/rider/Logo/UnityLogo.png")
-        val AttachEditorDebugConfiguration = IconLoader.getIcon("/rider/Logo/UnityLogo.png")
+        val ShaderLabFile = IconLoader.getIcon("/resharper/Logo/UnityLogo.png")
+        val AttachEditorDebugConfiguration = IconLoader.getIcon("/resharper/Logo/UnityLogo.png")
     }
 }
