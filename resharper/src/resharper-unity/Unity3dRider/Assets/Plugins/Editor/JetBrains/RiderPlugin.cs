0419918f2c6b3db48e0b9b5f2905f1bd78806cb4
Matt Ellis <m.t.ellis@gmail.com>
2017-07-11 16:58:24 +0100

    Remove Open C# Project in Rider for Unity 2017

--- resharper/src/resharper-unity/Unity3dRider/Assets/Plugins/Editor/JetBrains/RiderPlugin.cs
+++ resharper/src/resharper-unity/Unity3dRider/Assets/Plugins/Editor/JetBrains/RiderPlugin.cs
@@ -407,6 +407,13 @@ namespace Plugins.Editor.JetBrains
       return process;
     }
 
+#if UNITY_4 || UNITY_5
+    // The default "Open C# Project" menu item will use the external script editor to load the .sln
+    // file, but unless Unity knows the external script editor can properly load solutions, it will
+    // also launch MonoDevelop (or the OS registered app for .sln files). This menu item side steps
+    // that issue, and opens the solution in Rider without opening MonoDevelop as well.
+    // Unity 2017.1 and later recognise Rider as an app that can load solutions, so this menu isn't
+    // needed in newer versions.
     [MenuItem("Assets/Open C# Project in Rider", false, 1000)]
     static void MenuOpenProject()
     {
@@ -422,6 +429,7 @@ namespace Plugins.Editor.JetBrains
     {
       return Enabled;
     }
+#endif
 
     /// <summary>
     /// Force Unity To Write Project File
