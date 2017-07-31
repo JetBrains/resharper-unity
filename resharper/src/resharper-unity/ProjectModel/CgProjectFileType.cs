8f4fe48878675d30b495cf96e9baba967374669d
Ivan Pashchenko <ivan.pashchenko@jetbrains.com>
2017-07-31 17:49:40 +0300

    CG project file type

---
+++ resharper/src/resharper-unity/ProjectModel/CgProjectFileType.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.ProjectModel;
+
+namespace JetBrains.ReSharper.Plugins.Unity.ProjectModel
+{
+    [ProjectFileTypeDefinition(Name)]
+    public class CgProjectFileType : KnownProjectFileType
+    {
+        public new const string Name = "CG";
+        public const string CG_EXTENSION = ".cginc";
+
+        public new static readonly CgProjectFileType Instance = null;
+
+        public CgProjectFileType()
+            : base(Name, "Cg", new[] {CG_EXTENSION})
+        {
+        }
+    }
+}
---
+++ resharper/test/src/ProjectModel/CgProjectFileTypeTests.cs
@@ -0,0 +1,27 @@
+﻿using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Plugins.Unity.ProjectModel;
+using JetBrains.ReSharper.Resources.Shell;
+using NUnit.Framework;
+
+namespace JetBrains.ReSharper.Plugins.Unity.Tests.ProjectModel
+{
+    [TestFixture]
+    public class CgProjectFileTypeTests
+    {
+        [Test]
+        public void ProjectFileTypeIsRegistered()
+        {
+            Assert.NotNull(CgProjectFileType.Instance);
+
+            var projectFileTypes = Shell.Instance.GetComponent<IProjectFileTypes>();
+            Assert.NotNull(projectFileTypes.GetFileType(CgProjectFileType.Name));
+        }
+
+        [Test]
+        public void ProjectFileTypeFromExtension()
+        {
+            var projectFileExtensions = Shell.Instance.GetComponent<IProjectFileExtensions>();
+            Assert.AreSame(CgProjectFileType.Instance, projectFileExtensions.GetFileType(CgProjectFileType.CG_EXTENSION));
+        }
+    }
+}
