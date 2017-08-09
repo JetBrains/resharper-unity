7bf778b551c5ab261d76e89fff3d3881df79b905
Ivan Pashchenko <ivan.pashchenko@jetbrains.com>
2017-08-09 21:05:02 +0300

    Fixes RIDER-5996 (allows to enable/disable hippie completion for .shader and .cginc files)

---
+++ resharper/src/resharper-unity/Rider/UnitySettingsSynchronizer.cs
@@ -0,0 +1,54 @@
+﻿#if RIDER
+using JetBrains.Application.Settings;
+using JetBrains.DataFlow;
+using JetBrains.ProjectModel;
+using JetBrains.ProjectModel.DataContext;
+using JetBrains.ReSharper.Host.Features;
+using JetBrains.ReSharper.Plugins.Unity.ProjectModel;
+using JetBrains.ReSharper.Plugins.Unity.Settings;
+using JetBrains.Rider.Model;
+using NuGet;
+
+namespace JetBrains.ReSharper.Plugins.Unity.Rider
+{
+    [SolutionComponent]
+    public class UnitySettingsSynchronizer : UnityReferencesTracker.IHandler
+    {
+        private readonly Lifetime myLifetime;
+        private readonly IContextBoundSettingsStoreLive myBoundStore;
+        private readonly SolutionModel mySolutionModel;
+
+        public UnitySettingsSynchronizer(
+            Lifetime lifetime,
+            ISolution solution,
+            SolutionModel solutionModel,
+            ISettingsStore settingsStore)
+        {
+            myLifetime = lifetime;
+            mySolutionModel = solutionModel;
+            myBoundStore = settingsStore.BindToContextLive(lifetime, ContextRange.Smart(solution.ToDataContext()));
+        }
+
+        public void OnReferenceAdded(IProject unityProject, Lifetime projectLifetime)
+        {
+        }
+
+        public void OnSolutionLoaded(UnityProjectsCollection solution)
+        {
+            var entry = myBoundStore.Schema.GetScalarEntry((UnitySettings s) => s.EnableShaderLabHippieCompletion);
+            myBoundStore.GetValueProperty<bool>(myLifetime, entry, null).Change.Advise(myLifetime, pcea =>
+            {
+                if (pcea.HasNew)
+                {
+                    if (mySolutionModel.HasCurrentSolution()) // in tests we don't have one
+                    {
+                        mySolutionModel.GetCurrentSolution()
+                            .CustomData
+                            .Data["UNITY_SETTINGS_EnableShaderLabHippieCompletion"] = pcea.New.ToString();
+                    }
+                }
+            });
+        }
+    }
+}
+#endif
