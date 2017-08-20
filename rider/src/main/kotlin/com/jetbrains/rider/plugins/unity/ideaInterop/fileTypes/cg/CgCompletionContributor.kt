85de2ea9dd891ab66abd43e81c930995b84d8af9
Ivan Pashchenko <ivan.pashchenko@jetbrains.com>
2017-08-20 21:03:18 +0300

    semi-focused behaviour for completion lookup
    (cherry picked from commit c2cdbbcd0bfc120d6cc146e6a174e17a3c0d5679)

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/ideaInterop/fileTypes/cg/CgCompletionContributor.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/ideaInterop/fileTypes/cg/CgCompletionContributor.kt
@@ -1,9 +1,10 @@
 package com.jetbrains.rider.plugins.unity.ideaInterop.fileTypes.cg
 
-import com.intellij.codeInsight.completion.CompletionParameters
-import com.intellij.codeInsight.completion.CompletionResultSet
-import com.intellij.codeInsight.completion.CompletionType
-import com.intellij.codeInsight.completion.WordCompletionContributor
+import com.intellij.codeInsight.completion.*
+import com.intellij.codeInsight.lookup.impl.LookupImpl
+import com.intellij.codeInsight.lookup.impl.LookupManagerImpl
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.util.Disposer
 import com.jetbrains.rider.projectView.solution
 
 class CgCompletionContributor : WordCompletionContributor() {
@@ -12,9 +13,20 @@ class CgCompletionContributor : WordCompletionContributor() {
         // TODO: this is temporary solution until we get proper completion up and running on backend
         val project = parameters.editor.project ?: return
         val isEnabled = project.solution.customData.data["UNITY_SETTINGS_EnableShaderLabHippieCompletion"] == "True"
+        if (!(isEnabled && (parameters.completionType == CompletionType.BASIC)))
+            return
 
-        if (parameters.completionType == CompletionType.BASIC && isEnabled) {
-            addWordCompletionVariants(result, parameters, emptySet<String>())
-        }
+        val completion = CompletionService.getCompletionService().currentCompletion
+
+        val indicatorDisposable = completion as? Disposable ?: return
+        if (Disposer.isDisposed(indicatorDisposable)) return
+
+        if (!completion.isAutopopupCompletion)
+            return
+
+        val lookup = LookupManagerImpl.getActiveLookup(parameters.editor) as? LookupImpl ?: return
+        lookup.focusDegree = LookupImpl.FocusDegree.SEMI_FOCUSED
+
+        addWordCompletionVariants(result, parameters, emptySet<String>())
     }
 }
