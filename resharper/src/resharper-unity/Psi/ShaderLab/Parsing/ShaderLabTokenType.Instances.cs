46a08a7a5c84aa988761c3c1e1933c9fab77d39f
Matt Ellis <m.t.ellis@gmail.com>
2017-08-25 11:02:19 +0100

    Improve support for property attribute values
    Fixes #187

--- resharper/src/resharper-unity/Psi/ShaderLab/Parsing/ShaderLabTokenType.Instances.cs
+++ resharper/src/resharper-unity/Psi/ShaderLab/Parsing/ShaderLabTokenType.Instances.cs
@@ -6,11 +6,12 @@ namespace JetBrains.ReSharper.Plugins.Unity.Psi.ShaderLab.Parsing
     {
         public const int IDENTIFIER_NODE_TYPE_INDEX = LAST_GENERATED_TOKEN_TYPE_INDEX + 1;
         public const int STRING_LITERAL_NODE_TYPE_INDEX = LAST_GENERATED_TOKEN_TYPE_INDEX + 2;
-        public const int NUMERIC_LITERAL_NODE_TYPE_INDEX = LAST_GENERATED_TOKEN_TYPE_INDEX + 3;
-        public const int CG_CONTENT_NODE_TYPE_INDEX = LAST_GENERATED_TOKEN_TYPE_INDEX + 4;
-        public const int PP_MESSAGE_NODE_TYPE_INDEX = LAST_GENERATED_TOKEN_TYPE_INDEX + 5;
-        public const int PP_DIGITS_NODE_TYPE_INDEX = LAST_GENERATED_TOKEN_TYPE_INDEX + 6;
-        public const int PP_SWALLOWED_NODE_TYPE_INDEX = LAST_GENERATED_TOKEN_TYPE_INDEX + 7;
+        public const int UNQUOTED_STRING_LITERAL_NODE_TYPE_INDEX = LAST_GENERATED_TOKEN_TYPE_INDEX + 3;
+        public const int NUMERIC_LITERAL_NODE_TYPE_INDEX = LAST_GENERATED_TOKEN_TYPE_INDEX + 4;
+        public const int CG_CONTENT_NODE_TYPE_INDEX = LAST_GENERATED_TOKEN_TYPE_INDEX + 5;
+        public const int PP_MESSAGE_NODE_TYPE_INDEX = LAST_GENERATED_TOKEN_TYPE_INDEX + 6;
+        public const int PP_DIGITS_NODE_TYPE_INDEX = LAST_GENERATED_TOKEN_TYPE_INDEX + 7;
+        public const int PP_SWALLOWED_NODE_TYPE_INDEX = LAST_GENERATED_TOKEN_TYPE_INDEX + 8;
 
         public static readonly TokenNodeType NEW_LINE = new NewLineNodeType(LAST_GENERATED_TOKEN_TYPE_INDEX + 10);
         public static readonly TokenNodeType WHITESPACE = new WhitespaceNodeType(LAST_GENERATED_TOKEN_TYPE_INDEX + 11);
@@ -23,6 +24,7 @@ namespace JetBrains.ReSharper.Plugins.Unity.Psi.ShaderLab.Parsing
 
         public static readonly TokenNodeType IDENTIFIER = new IdentifierNodeType(IDENTIFIER_NODE_TYPE_INDEX);
         public static readonly TokenNodeType STRING_LITERAL = new StringLiteralNodeType(STRING_LITERAL_NODE_TYPE_INDEX);
+        public static readonly TokenNodeType UNQUOTED_STRING_LITERAL = new UnquotedStringLiteralNodeType(UNQUOTED_STRING_LITERAL_NODE_TYPE_INDEX);
         public static readonly TokenNodeType NUMERIC_LITERAL = new NumericLiteralNodeType(NUMERIC_LITERAL_NODE_TYPE_INDEX);
         public static readonly TokenNodeType CG_CONTENT = new GenericTokenNodeType("CG_CONTENT", CG_CONTENT_NODE_TYPE_INDEX, "CG_CONTENT");
 
