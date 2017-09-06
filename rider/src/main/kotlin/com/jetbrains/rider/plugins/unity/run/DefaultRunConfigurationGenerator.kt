b043992375bcc67dc134500f0e7649ede11ecbdd
Ivan Pashchenko <ivan.pashchenko@jetbrains.com>
2017-09-06 17:18:39 +0300

    Updated after merge of 2017.2 IDEA to Rider

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/run/DefaultRunConfigurationGenerator.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/run/DefaultRunConfigurationGenerator.kt
@@ -1,12 +1,12 @@
 package com.jetbrains.rider.plugins.unity.run
 
-import com.intellij.execution.RunManagerEx
+import com.intellij.execution.RunManager
 import com.intellij.execution.configurations.ConfigurationTypeUtil
 import com.jetbrains.rider.UnityReferenceDiscoverer
 import com.jetbrains.rider.UnityReferenceListener
 import com.jetbrains.rider.plugins.unity.run.configurations.UnityDebugConfigurationType
 
-class DefaultRunConfigurationGenerator(unityReferenceDiscoverer: UnityReferenceDiscoverer, runManagerEx: RunManagerEx) {
+class DefaultRunConfigurationGenerator(unityReferenceDiscoverer: UnityReferenceDiscoverer, runManager: RunManager) {
 
     val CONFIGURATION_NAME = "Attach to Unity Editor"
 
@@ -16,23 +16,23 @@ class DefaultRunConfigurationGenerator(unityReferenceDiscoverer: UnityReferenceD
                 // Replace the "Default" run configuration that Rider creates. If there is more than one
                 // run configuration, the user has explicitly modified them. Don't create the Attach
                 // config, but do make sure there's something selected.
-                if (runManagerEx.allSettings.size == 1 && runManagerEx.allSettings[0].name == "Default") {
-                    runManagerEx.removeConfiguration(runManagerEx.allSettings[0])
+                if (runManager.allSettings.size == 1 && runManager.allSettings[0].name == "Default") {
+                    runManager.removeConfiguration(runManager.allSettings[0])
 
                     val configurationType = ConfigurationTypeUtil.findConfigurationType(UnityDebugConfigurationType::class.java)
-                    val runConfiguration = runManagerEx.createRunConfiguration(CONFIGURATION_NAME, configurationType.attachToEditorFactory)
+                    val runConfiguration = runManager.createRunConfiguration(CONFIGURATION_NAME, configurationType.attachToEditorFactory)
                     // Not shared, as that requires the entire team to have the plugin installed
                     // We'll create it if it doesn't exist, anyway
-                    runManagerEx.addConfiguration(runConfiguration, false)
-                    runManagerEx.selectedConfiguration = runConfiguration
+                    runManager.addConfiguration(runConfiguration, false)
+                    runManager.selectedConfiguration = runConfiguration
 
                     // TODO: Add another configuration "Attach to Unity Editor and Play"
                     // (runConfiguration.configuration as UnityAttachToEditorConfiguration).play = true
                 }
-                else if (runManagerEx.selectedConfiguration == null) {
-                    val runConfiguration = runManagerEx.findConfigurationByName(CONFIGURATION_NAME)
+                else if (runManager.selectedConfiguration == null) {
+                    val runConfiguration = runManager.findConfigurationByName(CONFIGURATION_NAME)
                     if (runConfiguration != null) {
-                        runManagerEx.selectedConfiguration = runConfiguration
+                        runManager.selectedConfiguration = runConfiguration
                     }
                 }
             }
