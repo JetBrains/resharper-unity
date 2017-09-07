2605fcfa3d8bb6881c17b74222607c08d8bf8a02
Ivan Pashchenko <ivan.pashchenko@jetbrains.com>
2017-09-07 17:25:31 +0300

    .gitignore updated, Library/Temp ignores restored

--- rider/src/main/kotlin/com/jetbrains/rider/UnityConfigurationImpl.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/UnityConfigurationImpl.kt
@@ -1,22 +1,32 @@
 package com.jetbrains.rider
 
 import com.intellij.openapi.project.Project
+import com.jetbrains.rider.projectView.indexing.contentModel.ContentModelExcludes
+import java.io.File
 
-class UnityConfigurationImpl(project: Project, unityReferenceDiscoverer: UnityReferenceDiscoverer) {
+class UnityConfigurationImpl(private val project: Project, unityReferenceDiscoverer: UnityReferenceDiscoverer, private val excludes: ContentModelExcludes) {
+
+    companion object {
+        private val ignoredDirectories = arrayOf("Library", "Temp")
+    }
 
     init {
+        val excludePaths = ignoredDirectories
+            .map { f -> GetChildAsFile(f) }
+            .filter { f -> f != null }
+            .map { f -> f!! }
+            .toHashSet()
         unityReferenceDiscoverer.addUnityReferenceListener(object : UnityReferenceListener {
             override fun HasUnityReference() {
-                ExcludeFolderFromContentStore(project, "Library")
-                ExcludeFolderFromContentStore(project, "Temp")
+                excludes.updateExcludes(this, excludePaths)
             }
         })
     }
 
-    private fun ExcludeFolderFromContentStore(project: Project, folderName: String) {
-//        val libraryFolder = project.baseDir.findChild(folderName)
-//        if (libraryFolder != null && !contentModelStore.hasExcludedFile(libraryFolder)) {
-//            contentModelStore.addExcludedFile(libraryFolder)
-//        }
+    private fun GetChildAsFile(directoryName: String) : File?
+    {
+        val libraryFolder = project.baseDir.findChild(directoryName)
+        val path = libraryFolder?.canonicalPath ?: return null
+        return File(path)
     }
 }
