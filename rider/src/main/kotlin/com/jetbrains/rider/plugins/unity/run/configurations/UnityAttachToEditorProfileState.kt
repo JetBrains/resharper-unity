4cd5a79c4eb9fded9f5cdc40a11e5f5be653a8bb
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2017-09-26 16:01:22 +0200

    notify backend for entering play mode

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/run/configurations/UnityAttachToEditorProfileState.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/run/configurations/UnityAttachToEditorProfileState.kt
@@ -4,18 +4,38 @@ import com.intellij.execution.ExecutionResult
 import com.intellij.execution.Executor
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.execution.runners.ProgramRunner
-import com.intellij.openapi.project.Project
 import com.jetbrains.rider.debugger.DebuggerWorkerProcessHandler
+import com.jetbrains.rider.projectView.solution
 import com.jetbrains.rider.run.configurations.remote.MonoConnectRemoteProfileState
+import com.jetbrains.rider.util.idea.getLogger
+import javax.swing.JOptionPane
 
-class UnityAttachToEditorProfileState(remoteConfiguration: UnityAttachToEditorConfiguration, executionEnvironment: ExecutionEnvironment)
+class UnityAttachToEditorProfileState(val remoteConfiguration: UnityAttachToEditorConfiguration, executionEnvironment: ExecutionEnvironment)
     : MonoConnectRemoteProfileState(remoteConfiguration, executionEnvironment) {
+    private val logger = getLogger<UnityAttachToEditorProfileState>()
 
     override fun execute(executor: Executor, runner: ProgramRunner<*>, workerProcessHandler: DebuggerWorkerProcessHandler): ExecutionResult {
         val result = super.execute(executor, runner, workerProcessHandler)
-        //if (remoteConfiguration.play) {
-            // TODO: Tell Unity Editor to switch to play mode
-        //}
+
+        notifyBackend("test")
         return result
     }
+
+    override fun execute(executor: Executor?, runner: ProgramRunner<*>): ExecutionResult? {
+        var result = super.execute(executor, runner)
+        notifyBackend("test2")
+        return result
+    }
+
+    private fun notifyBackend(text:String) {
+        if (remoteConfiguration.play) {
+            logger.info("Pass value to backend, which will push Unity to enter play mode."+text)
+            JOptionPane.showMessageDialog(null, text, "InfoBox: " + "", JOptionPane.INFORMATION_MESSAGE);
+
+            // pass value to backend, which will push Unity to enter play mode.
+            executionEnvironment.project.solution.customData.data["UNITY_AttachEditorAndRun"] = "true";
+        }
+    }
+
+
 }
