8ff3a62d8c783022de7be16f4f729497b5bbc56c
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2017-09-29 16:42:29 +0200

    merge UnityDispatcher into RiderProtocolController

--- resharper/src/resharper-unity/Unity3dRider/Assets/Plugins/Editor/JetBrains/RiderProtocolController.cs
+++ resharper/src/resharper-unity/Unity3dRider/Assets/Plugins/Editor/JetBrains/RiderProtocolController.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
-using System.Text;
-using JetBrains.Application.BuildScript.Application;
-using JetBrains.Application.Logging;
 using JetBrains.DataFlow;
 using JetBrains.Platform.RdFramework;
 using JetBrains.Platform.RdFramework.Base;
@@ -12,6 +9,7 @@ using JetBrains.Platform.Unity.Model;
 using JetBrains.Util;
 using JetBrains.Util.Logging;
 using UnityEditor;
+using System.Collections.Generic;
 
 namespace Plugins.Editor.JetBrains
 {
@@ -20,14 +18,15 @@ namespace Plugins.Editor.JetBrains
   {
     static RiderProtocolController()
     {
-      Start(Path.Combine(Path.GetTempPath(), "Unity3dRider", "Unity3dRider" + DateTime.Now.ToString("YYYY-MM-ddT-HH-mm-ss") + ".log"));
+      Start(Path.Combine(Path.GetTempPath(), "Unity3dRider",
+        "Unity3dRider" + DateTime.Now.ToString("YYYY-MM-ddT-HH-mm-ss") + ".log"));
     }
 
     private static void Start(string loggerPath)
-    {     
+    {
       var lifetimeDefinition = Lifetimes.Define(EternalLifetime.Instance);
       var lifetime = lifetimeDefinition.Lifetime;
-      
+
       ILogger myLogger = Logger.GetLogger("Core");
       var fileLogEventListener = new FileLogEventListener(loggerPath, false); // works in Unity mono 4.6
       //var fileLogEventListener = new FileLogEventListener(loggerPath); //fails in Unity mono 4.6
@@ -70,6 +69,134 @@ namespace Plugins.Editor.JetBrains
       catch (Exception ex)
       {
         myLogger.Error(ex);
+      }
+    }
+
+    /// <summary>
+    /// Editor Thread Dispatcher
+    /// Provides a means to execute a function on a Unity owned thread
+    /// </summary>
+    [InitializeOnLoad]
+    private class UnityDispatcher
+    {
+      private struct Task
+      {
+        public readonly Delegate Function;
+        public readonly object[] Arguments;
+
+        public Task(Delegate function, object[] arguments)
+        {
+          Function = function;
+          Arguments = arguments;
+        }
+      }
+
+      /// <summary>
+      /// The queue of tasks that are being requested for the next time DispatchTasks is called
+      /// </summary>
+      private static Queue<Task> mTaskQueue = new Queue<Task>();
+
+      /// <summary>
+      /// Indicates whether there are tasks available for dispatching
+      /// </summary>
+      /// <value>
+      /// <c>true</c> if there are tasks available for dispatching; otherwise, <c>false</c>.
+      /// </value>
+      private static bool AreTasksAvailable
+      {
+        get { return mTaskQueue.Count > 0; }
+      }
+
+      /// <summary>
+      /// Initializes all the required callbacks for this class to work properly
+      /// </summary>
+      static UnityDispatcher()
+      {
+#if UNITY_EDITOR
+        EditorApplication.update += DispatchTasks;
+#endif
+      }
+
+      /// <summary>
+      /// Dispatches the specified action delegate.
+      /// </summary>
+      /// <param name='function'>
+      /// The function delegate being requested
+      /// </param>
+      public static void Dispatch(Action function)
+      {
+        Dispatch(function, null);
+      }
+
+      /// <summary>
+      /// Dispatches the specified function delegate with the desired delegates
+      /// </summary>
+      /// <param name='function'>
+      /// The function delegate being requested
+      /// </param>
+      /// <param name='arguments'>
+      /// The arguments to be passed to the function delegate
+      /// </param>
+      /// <exception cref='System.NotSupportedException'>
+      /// Is thrown when this method is called from the Unity Player
+      /// </exception>
+      private static void Dispatch(Delegate function, params object[] arguments)
+      {
+#if UNITY_EDITOR
+        lock (mTaskQueue)
+        {
+          mTaskQueue.Enqueue(new Task(function, arguments));
+        }
+#else
+		throw new System.NotSupportedException("Dispatch is not supported in the Unity Player!");
+#endif
+      }
+
+      /// <summary>
+      /// Clears the queued tasks
+      /// </summary>
+      /// <exception cref='System.NotSupportedException'>
+      /// Is thrown when this method is called from the Unity Player
+      /// </exception>
+      public static void ClearTasks()
+      {
+#if UNITY_EDITOR
+        if (AreTasksAvailable)
+        {
+          lock (mTaskQueue)
+          {
+            mTaskQueue.Clear();
+          }
+        }
+#else
+		throw new System.NotSupportedException("ClearTasks is not supported in the Unity Player!");
+#endif
+      }
+
+      /// <summary>
+      /// Dispatches the tasks that has been requested since the last call to this function
+      /// </summary>
+      /// <exception cref='System.NotSupportedException'>
+      /// Is thrown when this method is called from the Unity Player
+      /// </exception>
+      private static void DispatchTasks()
+      {
+#if UNITY_EDITOR
+        if (AreTasksAvailable)
+        {
+          lock (mTaskQueue)
+          {
+            foreach (Task task in mTaskQueue)
+            {
+              task.Function.DynamicInvoke(task.Arguments);
+            }
+
+            mTaskQueue.Clear();
+          }
+        }
+#else
+		throw new System.NotSupportedException("DispatchTasks is not supported in the Unity Player!");
+#endif
       }
     }
   }
