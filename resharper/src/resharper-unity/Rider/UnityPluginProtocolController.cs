5bfda3df93c37ab6f0906de0268c00aed6251cd8
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2017-10-01 22:37:42 +0200

    backend should create protocol and set play if set on frontend

---
+++ resharper/src/resharper-unity/Rider/UnityPluginProtocolController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using JetBrains.DataFlow;
+using JetBrains.Platform.RdFramework;
+using JetBrains.Platform.RdFramework.Impl;
+using JetBrains.ProjectModel;
+using JetBrains.Rider.Model;
+using JetBrains.Util;
+
+namespace JetBrains.ReSharper.Plugins.Unity.Rider
+{
+
+    [SolutionComponent]
+    public class UnityPluginProtocolController
+    {
+        public Protocol Protocol { get; private set; }
+
+        public UnityPluginProtocolController(Lifetime lifetime, ILogger logger, Solution solution)
+        {
+            solution.CustomData
+                .Data.Advise(lifetime, e =>
+                {
+                    if (e.Key == "UNITY_ProcessId" && e.NewValue != e.OldValue && !string.IsNullOrEmpty(e.NewValue))
+                    {
+                        var pid = Convert.ToInt32(solution.CustomData.Data["UNITY_ProcessId"]);
+                        logger.Verbose($"UNITY_ProcessId {e.NewValue} came from frontend. Pid: {pid}");
+
+                        try
+                        {
+                            logger.Info("Unity process with Id = " + Process.GetCurrentProcess().Id);
+                            logger.Info("Start ControllerTask...");
+
+                            int port = 46000 + pid % 1000;
+                            var dispatcher = new RdSimpleDispatcher(lifetime, logger);
+
+                            logger.Info("Create protocol...");
+                            Protocol = new Protocol(new Serializers(), new Identities(IdKind.DynamicClient), dispatcher,
+                                creatingProtocol =>
+                                {
+                                    logger.Info("Creating SocketWire with port = {0}", port);
+                                    return new SocketWire.Client(lifetime, creatingProtocol, port, "UnityClient");
+                                });
+
+                        }
+                        catch (Exception ex)
+                        {
+                            logger.Error(ex);
+                        }
+                    }
+                });
+        }
+    }
+}
---
+++ resharper/src/resharper-unity/Rider/UnityPluginTransmitter.cs
@@ -0,0 +1,29 @@
+﻿#if RIDER
+using JetBrains.DataFlow;
+using JetBrains.Platform.Unity.Model;
+using JetBrains.ProjectModel;
+using JetBrains.Rider.Model;
+using JetBrains.Util;
+
+namespace JetBrains.ReSharper.Plugins.Unity.Rider
+{
+    [SolutionComponent]
+    public class UnityPluginTransmitter
+    {
+        public UnityPluginTransmitter(Lifetime lifetime, ILogger logger, Solution solution, UnityPluginProtocolController protocolController)
+        {
+            solution.CustomData
+                    .Data.Advise(lifetime, e =>
+                {
+                    if (e.Key == "UNITY_AttachEditorAndRun" && e.NewValue.ToLower()=="true" && e.NewValue!=e.OldValue)
+                    {
+                        var pid = solution.CustomData.Data["UNITY_ProcessId"];
+                        logger.Verbose($"UNITY_AttachEditorAndRun {e.NewValue} came from frontend. Pid: {pid}");
+                        var model = new UnityModel(lifetime, protocolController.Protocol);
+                        model.Play.Value = true;
+                    }
+                });
+        }
+    }
+}
+#endif
