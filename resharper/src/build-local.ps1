35879866b6e7a0ce087e12e629826b8ae53389a4
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2017-11-15 12:20:14 +0100

    fix local build to be debug

--- resharper/src/build-local.ps1
+++ resharper/src/build-local.ps1
@@ -1,7 +1,7 @@
 Push-Location ((Split-Path $MyInvocation.InvocationName) + "\..\..\rider")
 
 $isUnix = [System.Environment]::OSVersion.Platform -eq "Unix"
-$gradleArgs = @("-PConfiguration=Debug", "-PRiderOnly=true", "-PSkipNuGetRestore=true")
+$gradleArgs = @("-PBuildConfiguration=Debug", "-PRiderOnly=true", "-PSkipNuGetRestore=true")
 
 if ($isUnix) {
   .\gradlew "buildBackend" $gradleArgs
