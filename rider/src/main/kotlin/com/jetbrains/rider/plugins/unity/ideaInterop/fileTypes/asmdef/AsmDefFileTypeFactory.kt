552f4575f075fe548e51a4acbd98a5970ed24eb5
Matt Ellis <m.t.ellis@gmail.com>
2017-11-20 21:50:00 +0000

    Update comment with YT issue

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/ideaInterop/fileTypes/asmdef/AsmDefFileTypeFactory.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/ideaInterop/fileTypes/asmdef/AsmDefFileTypeFactory.kt
@@ -3,7 +3,7 @@ package com.jetbrains.rider.plugins.unity.ideaInterop.fileTypes.asmdef
 import com.intellij.openapi.fileTypes.*
 
 // Ideally, this should come automatically from ReSharper, via catalog.json
-// See RSRP-467093
+// See RSRP-467093 and RIDER-11470
 class AsmDefFileTypeFactory : FileTypeFactory() {
 
     override fun createFileTypes(consumer: FileTypeConsumer) {
