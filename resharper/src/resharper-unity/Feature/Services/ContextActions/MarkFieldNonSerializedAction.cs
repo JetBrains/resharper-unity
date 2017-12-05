13797c2c72b6ebdf93f5b59154d2d7df79b6f7bb
Matt Ellis <m.t.ellis@gmail.com>
2017-12-05 22:27:46 +0000

    Add context action to convert auto-property
    Fixes #195

--- resharper/src/resharper-unity/Feature/Services/ContextActions/MarkFieldNonSerializedAction.cs
+++ resharper/src/resharper-unity/Feature/Services/ContextActions/MarkFieldNonSerializedAction.cs
@@ -3,7 +3,6 @@ using JetBrains.Application.Progress;
 using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Feature.Services.ContextActions;
 using JetBrains.ReSharper.Feature.Services.CSharp.Analyses.Bulbs;
-using JetBrains.ReSharper.Feature.Services.CSharp.ContextActions;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.CSharp.Tree;
 using JetBrains.TextControl;
@@ -11,7 +10,7 @@ using JetBrains.Util;
 
 namespace JetBrains.ReSharper.Plugins.Unity.Feature.Services.ContextActions
 {
-    [ContextAction(Group = CSharpContextActions.GroupID,
+    [ContextAction(Group = UnityContextActions.GroupID,
         Name = "Annotate field with 'NonSerialized' attribute",
         Description =
             "Adds 'NonSerializedAttribute' to a field in a known Unity type, marking the field as not serialized by Unity")]
@@ -22,21 +21,6 @@ namespace JetBrains.ReSharper.Plugins.Unity.Feature.Services.ContextActions
         public MarkFieldNonSerializedAction(ICSharpContextActionDataProvider dataProvider)
         {
             myDataProvider = dataProvider;
-        }
-
-        protected override Action<ITextControl> ExecutePsiTransaction(ISolution solution, IProgressIndicator progress)
-        {
-            var fieldDeclaration = myDataProvider.GetSelectedElement<IFieldDeclaration>();
-            if (fieldDeclaration != null)
-            {
-                var typeElement = TypeFactory.CreateTypeByCLRName(PredefinedType.NONSERIALIZED_ATTRIBUTE_CLASS, myDataProvider.PsiModule).GetTypeElement();
-                if (typeElement == null)
-                    return null;
-                var attribute = myDataProvider.ElementFactory.CreateAttribute(typeElement);
-                fieldDeclaration.AddAttributeAfter(attribute, null);
-            }
-
-            return null;
         }
 
         public override string Text => "Annotate field with 'NonSerialized' attribute";
@@ -54,6 +38,15 @@ namespace JetBrains.ReSharper.Plugins.Unity.Feature.Services.ContextActions
 
             var field = fieldDeclaration.DeclaredElement;
             return field != null && unityApi.IsUnityField(field);
+        }
+
+        protected override Action<ITextControl> ExecutePsiTransaction(ISolution solution, IProgressIndicator progress)
+        {
+            var fieldDeclaration = myDataProvider.GetSelectedElement<IFieldDeclaration>();
+            AttributeUtil.AddAttribute(fieldDeclaration, PredefinedType.NONSERIALIZED_ATTRIBUTE_CLASS,
+                myDataProvider.PsiModule, myDataProvider.ElementFactory);
+
+            return null;
         }
     }
 }
---
+++ resharper/src/resharper-unity/Feature/Services/ContextActions/UnityContextActions.cs
@@ -0,0 +1,9 @@
+﻿using JetBrains.ReSharper.Feature.Services.CSharp.ContextActions;
+
+namespace JetBrains.ReSharper.Plugins.Unity.Feature.Services.ContextActions
+{
+    public static class UnityContextActions
+    {
+        public const string GroupID = CSharpContextActions.GroupID + " (Unity)";
+    }
+}
