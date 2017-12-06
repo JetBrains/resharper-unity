c3cea1425e5b66c0fd223e5550a2a6dbbed0912c
Ivan Shakhov <van800@gmail.com>
2017-12-06 16:16:04 +0100

    Template for Unity class library (#303)
    * add template for Unity project
    * improved Unity path detection for windows

---
+++ resharper/src/resharper-unity/Rider/UnityPathTemplateParameter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using JetBrains.Application;
+using JetBrains.ReSharper.Host.Features.ProjectModel.ProjectTemplates.DotNetExtensions;
+using JetBrains.ReSharper.Host.Features.ProjectModel.ProjectTemplates.DotNetTemplates;
+using JetBrains.Rider.Model;
+using JetBrains.Util;
+using JetBrains.Util.Interop;
+
+namespace JetBrains.ReSharper.Plugins.Unity.Rider
+{
+    public class UnityPathTemplateParameter : DotNetTemplateParameter
+    {
+        public UnityPathTemplateParameter() : base("PathToUnityEngine", "Path to UnityEngine.dll")
+        {
+        }
+
+        public override RdProjectTemplateContent CreateContent(DotNetProjectTemplateExpander expander, IDotNetTemplateContentFactory factory,
+            int index, IDictionary<string, string> context)
+        {
+            var content = factory.CreateNextParameters(new[] {expander}, index + 1, context);
+            var parameter = expander.TemplateInfo.GetParameter(Name);
+            if (parameter == null)
+            {
+                return content;
+            }
+
+            var defaultPath = "";
+            switch (Environment.OSVersion.Platform)
+            {
+                case PlatformID.MacOSX:
+                    defaultPath = @"/Applications/Unity.app/Contents/Frameworks/Managed/UnityEngine.dll";
+                    break;
+                case PlatformID.Unix:
+                    defaultPath = @"/opt/Unity/Editor/Data/Managed/UnityEngine.dll";
+                    break;
+                default:
+                    defaultPath = @"C:\Program Files\Unity\Editor\Data\Managed\UnityEngine.dll";
+                    
+                    var lnks = FileSystemPath.Parse(@"C:\ProgramData\Microsoft\Windows\Start Menu\Programs").GetChildDirectories("Unity*").SelectMany(a=>a.GetChildFiles("Unity.lnk")).ToArray();
+                    var dllPath = lnks.Select(a => ShellLinkHelper.ResolveLinkTarget(a).Directory.Combine(@"Data\Managed\UnityEngine.dll")).Where(b=>b.ExistsFile).OrderBy(c=>new FileInfo(c.FullPath).CreationTime).LastOrDefault();
+                    if (dllPath != null)
+                        defaultPath = dllPath.FullPath;
+                    
+                    break;
+            }
+
+            return new RdProjectTemplateTextParameter(Name, defaultPath, Tooltip, RdTextParameterStyle.FileChooser, content);
+        }
+    }
+
+    [ShellComponent]
+    public class UnityPathParameterProvider : IDotNetTemplateParameterProvider
+    {
+        public int Priority
+        {
+            get { return 50; }
+        }
+    
+        public IReadOnlyCollection<DotNetTemplateParameter> Get()
+        {
+            return new[] {new UnityPathTemplateParameter()};
+        }
+    }
+}
