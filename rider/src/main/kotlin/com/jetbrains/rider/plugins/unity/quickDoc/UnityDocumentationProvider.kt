008958c2c639b5ffc10d36676ef599ce18d2a98f
Matt Ellis <m.t.ellis@gmail.com>
2017-12-14 21:57:46 +0000

    Remove usage of VirtualFileSystem

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/quickDoc/UnityDocumentationProvider.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/quickDoc/UnityDocumentationProvider.kt
@@ -2,17 +2,18 @@ package com.jetbrains.rider.plugins.unity.quickDoc
 
 import com.intellij.lang.documentation.DocumentationProvider
 import com.intellij.openapi.util.SystemInfo
-import com.intellij.openapi.vfs.LocalFileSystem
-import com.intellij.openapi.vfs.VfsUtil
-import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiManager
+import com.intellij.util.SystemProperties
+import com.intellij.util.io.exists
 import com.jetbrains.rider.projectView.solution
+import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
 
 class UnityDocumentationProvider : DocumentationProvider {
 
-    private val documentationRoot by lazy(this::findDefaultDocumentationRoot)
+    private val documentationRoot by lazy(this::findFallbackDocumentationRoot)
 
     override fun getUrlFor(p0: PsiElement?, p1: PsiElement?): MutableList<String>? {
         val data = p0?.project?.solution?.customData?.data
@@ -31,8 +32,8 @@ class UnityDocumentationProvider : DocumentationProvider {
         // We know context will be a fully qualified type or method name, starting
         // with either `UnityEngine.` or `UnityEditor.`
         val keyword = stripPrefix(context)
-        return getFileUri(documentationRoot, "/ScriptReference/$keyword.html")
-            ?: getFileUri(documentationRoot, "/ScriptReference/${keyword.replace('.', '-')}.html")
+        return getFileUri(documentationRoot, "ScriptReference/$keyword.html")
+            ?: getFileUri(documentationRoot, "ScriptReference/${keyword.replace('.', '-')}.html")
             ?: "https://docs.unity3d.com/ScriptReference/30_search.html?q=$keyword"
     }
 
@@ -41,15 +42,17 @@ class UnityDocumentationProvider : DocumentationProvider {
         return context.drop(12)
     }
 
-    private fun getFileUri(documentationRoot: String, htmlPath: String): String? {
-        val path = documentationRoot + htmlPath
-        val file = LocalFileSystem.getInstance().findFileByPath(path)
-        if (file != null && file.exists())
-            return VfsUtil.pathToUrl(path)
+    private fun getFileUri(documentationRoot: Path?, htmlPath: String): String? {
+        if (documentationRoot == null)
+            return null
+        val path = documentationRoot.resolve(htmlPath)
+        if (path.exists()) {
+            return path.toUri().toASCIIString()
+        }
         return null
     }
 
-    private fun findDefaultDocumentationRoot(): String {
+    private fun findFallbackDocumentationRoot(): Path? {
         return when {
             SystemInfo.isWindows -> {
                 // Unity is installed to `C:\Program Files\Unity`, on both 32 bit and 64 bit
@@ -57,32 +60,22 @@ class UnityDocumentationProvider : DocumentationProvider {
                 // %PROGRAMFILES(X86)% only exists if we're 64 bit, and is always in the form `C:\Program Files (x86)`
                 // %PROGRAMFILESW6432% is always in the form `C:\Program Files`
                 val envvar = System.getenv("ProgramW6432") ?: System.getenv("ProgramFiles")
-                Paths.get(envvar).resolve("/Unity/Editor/Data/Documentation/en").toString()
+                Paths.get(envvar).resolve("/Unity/Editor/Data/Documentation/en")
             }
-            SystemInfo.isMac -> "/Applications/Unity/Documentation/en"
+            SystemInfo.isMac -> Paths.get("/Applications/Unity/Unity.app/Contents/Documentation/en")
             SystemInfo.isLinux -> {
                 // Unity 2017.3 is the first version to add support for local documentation.
                 // It will write to /opt if it has write permissions, otherwise, it writes to
                 // the user's home directory, but adds the version, e.g. ~/Unity-2017.3.0b1
-                val fileSystem = LocalFileSystem.getInstance()
-                fileSystem.findFileByPath("/opt")?.let {
-                    for (dir in getUnityChildDirs(it)) {
-                        fileSystem.findFileByPath(dir)?.let { return it.path }
-                    }
-                }
-                VfsUtil.getUserHomeDir()?.let {
-                    for (dir in getUnityChildDirs(it)) {
-                        fileSystem.findFileByPath(dir)?.let { return it.path }
-                    }
-                }
-                ""
+                val globalDocRoots = getUnityChildDirs(Paths.get("/opt"))
+                val homeDocRoots = getUnityChildDirs(Paths.get(SystemProperties.getUserHome()))
+                (globalDocRoots + homeDocRoots)
+                        .map { it.resolve("Editor/Data/Documentation/en") }
+                        .firstOrNull { it.exists() }
             }
-            else -> ""
+            else -> null
         }
     }
 
-    private fun getUnityChildDirs(root: VirtualFile): List<String> {
-        return VfsUtil.getChildren(root, { virtualFile -> virtualFile.isDirectory && virtualFile.name.startsWith("Unity") })
-                .map { it.path + "/Editor/Data/Documentation/en" }
-    }
+    private fun getUnityChildDirs(root: Path): Sequence<Path> = Files.newDirectoryStream(root, "Unity*").asSequence()
 }
