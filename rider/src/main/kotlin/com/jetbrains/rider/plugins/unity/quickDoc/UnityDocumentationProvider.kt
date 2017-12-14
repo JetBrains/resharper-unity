290caa3308761c48ebb26d2709eb1719b86ba8a3
Matt Ellis <m.t.ellis@gmail.com>
2017-12-14 11:19:38 +0000

    Improve method for finding fallback doc root

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/quickDoc/UnityDocumentationProvider.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/quickDoc/UnityDocumentationProvider.kt
@@ -4,12 +4,15 @@ import com.intellij.lang.documentation.DocumentationProvider
 import com.intellij.openapi.util.SystemInfo
 import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiManager
 import com.intellij.util.PathUtil
 import com.jetbrains.rider.projectView.solution
 
 class UnityDocumentationProvider : DocumentationProvider {
+
+    private val documentationRoot by lazy(this::findDefaultDocumentationRoot)
 
     override fun getUrlFor(p0: PsiElement?, p1: PsiElement?): MutableList<String>? {
         val data = p0?.project?.solution?.customData?.data
@@ -28,30 +31,14 @@ class UnityDocumentationProvider : DocumentationProvider {
         // We know context will be a fully qualified type or method name, starting
         // with either `UnityEngine.` or `UnityEditor.`
         val keyword = stripPrefix(context)
-        val documentationRoot = getDocumentationRoot()
         return getFileUri(documentationRoot, "/ScriptReference/$keyword.html")
             ?: getFileUri(documentationRoot, "/ScriptReference/${keyword.replace('.', '-')}.html")
             ?: "https://docs.unity3d.com/ScriptReference/30_search.html?q=$keyword"
     }
 
     private fun stripPrefix(context: String): String {
+        // 12 for `UnityEngine.` or `UnityEditor.`
         return context.drop(12)
-    }
-
-    private fun getDocumentationRoot(): String {
-        return when {
-            SystemInfo.isWindows -> {
-                // Unity is installed to `C:\Program Files\Unity`, on both 32 bit and 64 bit
-                // %PROGRAMFILES% differs if we're a 32 or 64 bit process
-                // %PROGRAMFILES(X86)% only exists if we're 64 bit, and is always in the form `C:\Program Files (x86)`
-                // %PROGRAMFILESW6432% is always in the form `C:\Program Files`
-                val envvar = System.getenv("ProgramW6432") ?: System.getenv("ProgramFiles")
-                PathUtil.toSystemDependentName(envvar).plus("/Unity/Editor/Data/Documentation/en")
-            }
-            SystemInfo.isMac -> "/Applications/Unity/Documentation/en"
-            SystemInfo.isUnix -> "/opt/Unity/Editor/Data/Documentation/en"
-            else -> ""
-        }
     }
 
     private fun getFileUri(documentationRoot: String, htmlPath: String): String? {
@@ -60,5 +47,42 @@ class UnityDocumentationProvider : DocumentationProvider {
         if (file != null && file.exists())
             return VfsUtil.pathToUrl(path)
         return null
+    }
+
+    private fun findDefaultDocumentationRoot(): String {
+        return when {
+            SystemInfo.isWindows -> {
+                // Unity is installed to `C:\Program Files\Unity`, on both 32 bit and 64 bit
+                // %PROGRAMFILES% differs if we're a 32 or 64 bit process
+                // %PROGRAMFILES(X86)% only exists if we're 64 bit, and is always in the form `C:\Program Files (x86)`
+                // %PROGRAMFILESW6432% is always in the form `C:\Program Files`
+                val envvar = System.getenv("ProgramW6432") ?: System.getenv("ProgramFiles")
+                PathUtil.toSystemDependentName(envvar) + "/Unity/Editor/Data/Documentation/en"
+            }
+            SystemInfo.isMac -> "/Applications/Unity/Documentation/en"
+            SystemInfo.isLinux -> {
+                // Unity 2017.3 is the first version to add support for local documentation.
+                // It will write to /opt if it has write permissions, otherwise, it writes to
+                // the user's home directory, but adds the version, e.g. ~/Unity-2017.3.0b1
+                val fileSystem = LocalFileSystem.getInstance()
+                fileSystem.findFileByPath("/opt")?.let {
+                    for (dir in getUnityChildDirs(it)) {
+                        fileSystem.findFileByPath(dir)?.let { return it.path }
+                    }
+                }
+                VfsUtil.getUserHomeDir()?.let {
+                    for (dir in getUnityChildDirs(it)) {
+                        fileSystem.findFileByPath(dir)?.let { return it.path }
+                    }
+                }
+                ""
+            }
+            else -> ""
+        }
+    }
+
+    private fun getUnityChildDirs(root: VirtualFile): List<String> {
+        return VfsUtil.getChildren(root, { virtualFile -> virtualFile.isDirectory && virtualFile.name.startsWith("Unity") })
+                .map { it.path + "/Editor/Data/Documentation/en" }
     }
 }
