af95afffde302346e74a68e8f301dd2e65d9d033
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2017-12-18 09:39:16 +0100

    add commented code, which should help wait for attach fully finished

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/run/configurations/UnityAttachToEditorProfileState.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/run/configurations/UnityAttachToEditorProfileState.kt
@@ -4,11 +4,18 @@ import com.intellij.execution.ExecutionResult
 import com.intellij.execution.Executor
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.execution.runners.ProgramRunner
+import com.intellij.execution.ui.ConsoleViewContentType
+import com.intellij.execution.ui.ObservableConsoleView
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.xdebugger.XDebuggerManager
 import com.jetbrains.rider.debugger.DebuggerWorkerProcessHandler
+import com.jetbrains.rider.debugger.DotNetDebugProcess
 import com.jetbrains.rider.projectView.solution
 import com.jetbrains.rider.run.configurations.remote.MonoConnectRemoteProfileState
+import com.jetbrains.rider.util.idea.pumpMessages
 import com.jetbrains.rider.util.lifetime.Lifetime
-import com.intellij.openapi.diagnostic.Logger
+import java.time.LocalDateTime
 
 class UnityAttachToEditorProfileState(val remoteConfiguration: UnityAttachToEditorConfiguration, executionEnvironment: ExecutionEnvironment)
     : MonoConnectRemoteProfileState(remoteConfiguration, executionEnvironment) {
@@ -16,6 +23,19 @@ class UnityAttachToEditorProfileState(val remoteConfiguration: UnityAttachToEdit
 
     override fun execute(executor: Executor, runner: ProgramRunner<*>, workerProcessHandler: DebuggerWorkerProcessHandler, lifetime: Lifetime): ExecutionResult {
         val result = super.execute(executor, runner, workerProcessHandler)
+
+//        var debuggerManager = executionEnvironment.project.getComponent(XDebuggerManager::class.java)
+//
+//        val debugProcess = debuggerManager.currentSession!!.debugProcess as DotNetDebugProcess
+//        val disposable:com.intellij.openapi.Disposable = Disposable {  }
+//        var time = LocalDateTime.now();
+//        debugProcess.debuggerOutputConsole.addChangeListener(ObservableConsoleView.ChangeListener {
+//            time = LocalDateTime.now();
+//        }, disposable)
+//
+//        pumpMessages(1000) {
+//            LocalDateTime.now().isBefore(time.plusSeconds(1))
+//        }
 
         if (remoteConfiguration.play) {
             logger.info("Pass value to backend, which will push Unity to enter play mode.")
