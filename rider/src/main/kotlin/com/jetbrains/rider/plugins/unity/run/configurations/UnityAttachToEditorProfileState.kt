c103b5d192570d12e0cd0b21eb2ec51b5654a8fc
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2017-12-20 15:11:53 +0100

    working on better couldn't connect debugger message

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/run/configurations/UnityAttachToEditorProfileState.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/run/configurations/UnityAttachToEditorProfileState.kt
@@ -6,9 +6,12 @@ import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.execution.runners.ProgramRunner
 import com.intellij.execution.ui.ConsoleViewContentType
 import com.intellij.execution.ui.ObservableConsoleView
+import com.intellij.notification.NotificationListener
+import com.intellij.notification.NotificationType
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.xdebugger.XDebuggerManager
+import com.intellij.xdebugger.impl.XDebuggerManagerImpl
 import com.jetbrains.rider.debugger.DebuggerWorkerProcessHandler
 import com.jetbrains.rider.debugger.DotNetDebugProcess
 import com.jetbrains.rider.projectView.solution
@@ -22,22 +25,32 @@ class UnityAttachToEditorProfileState(val remoteConfiguration: UnityAttachToEdit
     val logger = Logger.getInstance(UnityAttachToEditorProfileState::class.java)
 
     override fun execute(executor: Executor, runner: ProgramRunner<*>, workerProcessHandler: DebuggerWorkerProcessHandler, lifetime: Lifetime): ExecutionResult {
+        var debuggerManager = executionEnvironment.project.getComponent(XDebuggerManager::class.java)
+        val debugProcess = debuggerManager.currentSession!!.debugProcess as DotNetDebugProcess
+        debugProcess.sessionProxy.showNotification.advise(debugProcess.sessionLifetime) {
+            var message =it.output
+            if (message.contains("no connection could be made because the target machine actively refused it"))
+                message=message+"\n"+"Check if debug is allowed by \"Editor Attaching\" setting in Unity."
+            val notification = XDebuggerManagerImpl.NOTIFICATION_GROUP.createNotification(message, if (it.isError) NotificationType.ERROR else NotificationType.WARNING)
+            notification.setListener(NotificationListener.URL_OPENING_LISTENER)
+            notification.notify(debugProcess.session.project)
+        }
+
         val result = super.execute(executor, runner, workerProcessHandler)
 
-//        var debuggerManager = executionEnvironment.project.getComponent(XDebuggerManager::class.java)
-//
-//        val debugProcess = debuggerManager.currentSession!!.debugProcess as DotNetDebugProcess
-//        val disposable:com.intellij.openapi.Disposable = Disposable {  }
-//        var time = LocalDateTime.now();
-//        debugProcess.debuggerOutputConsole.addChangeListener(ObservableConsoleView.ChangeListener {
-//            time = LocalDateTime.now();
-//        }, disposable)
-//
-//        pumpMessages(1000) {
-//            LocalDateTime.now().isBefore(time.plusSeconds(1))
-//        }
-
         if (remoteConfiguration.play) {
+
+            // wait till debugger fully attached, because entering play mode in Unity will lead to reload appdomain. it may hang, if debugger is doing smth in background
+            val disposable:com.intellij.openapi.Disposable = Disposable {  }
+            var time = LocalDateTime.now();
+            debugProcess.debuggerOutputConsole.addChangeListener(ObservableConsoleView.ChangeListener {
+                time = LocalDateTime.now();
+            }, disposable)
+
+            pumpMessages(1000) {
+                LocalDateTime.now().isBefore(time.plusSeconds(1))
+            }
+
             logger.info("Pass value to backend, which will push Unity to enter play mode.")
             lifetime.bracket(opening = {
                 // pass value to backend, which will push Unity to enter play mode.
