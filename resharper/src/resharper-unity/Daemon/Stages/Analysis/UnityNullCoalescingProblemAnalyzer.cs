58470bea996a36d63a059ec1c9f02f651007f0f3
Julian Hangstörfer <Julian.Hangstoerfer@generic.de>
2018-01-17 17:03:28 +0100

    Create unity null coalescing analyzer and warning

---
+++ resharper/src/resharper-unity/Daemon/Stages/Analysis/UnityNullCoalescingProblemAnalyzer.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using JetBrains.Annotations;
+using JetBrains.ReSharper.Feature.Services.Daemon;
+using JetBrains.ReSharper.Plugins.Unity.Daemon.Stages.Dispatcher;
+using JetBrains.ReSharper.Plugins.Unity.Daemon.Stages.Highlightings;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+using JetBrains.ReSharper.Psi.Resolve;
+using JetBrains.ReSharper.Psi.Util;
+
+namespace JetBrains.ReSharper.Plugins.Unity.Daemon.Stages.Analysis
+{
+    [ElementProblemAnalyzer(typeof(INullCoalescingExpression), HighlightingTypes = new[] { typeof(UnityNullCoalescingWarning) })]
+    public class UnityNullCoalescingProblemAnalyzer : UnityElementProblemAnalyzer<INullCoalescingExpression>
+    {
+        public UnityNullCoalescingProblemAnalyzer([NotNull] UnityApi unityApi)
+            : base(unityApi)
+        {
+        }
+
+        protected override void Analyze(INullCoalescingExpression expression, ElementProblemAnalyzerData data, IHighlightingConsumer consumer)
+        {
+            if (expression.LeftOperand == null || expression.RightOperand == null)
+                return;
+
+            if (expression.LeftOperand is IReferenceExpression leftOperand && IsDescendantOfUnityObject(leftOperand))
+            {
+                consumer.AddHighlighting(new UnityNullCoalescingWarning(expression));
+            }
+        }
+
+        private static bool IsDescendantOfUnityObject([CanBeNull]IReferenceExpression expression)
+        {
+            var resolve = expression?.Reference.Resolve();
+            if (resolve == null || resolve.ResolveErrorType != ResolveErrorType.OK)
+                return false;
+
+            var typeElement = expression.Type().GetTypeElement();
+            if (typeElement == null)
+                return false;
+
+            if (typeElement.GetClrName().Equals(KnownTypes.Object))
+                return true;
+
+            return typeElement.GetAllSuperClasses().Reverse().Any(superClass => superClass.GetClrName().Equals(KnownTypes.Object));
+        }
+    }
+}
