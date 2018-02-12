7067193817e7fa92da425824627ca70d3097f92d
Matt Ellis <m.t.ellis@gmail.com>
2018-02-12 17:46:43 +0000

    Add declarations and declared elements

---
+++ resharper/src/resharper-unity/ShaderLab/Psi/DeclaredElements/PropertyDeclaredElement.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.ReSharper.Psi;
+
+namespace JetBrains.ReSharper.Plugins.Unity.ShaderLab.Psi.DeclaredElements
+{
+    public class PropertyDeclaredElement : ShaderLabDeclaredElementBase
+    {
+        public PropertyDeclaredElement(string shortName, IPsiSourceFile sourceFile, int treeOffset)
+            : base(shortName, sourceFile, treeOffset)
+        {
+        }
+
+        public override DeclaredElementType GetElementType() => ShaderLabDeclaredElementType.Property;
+    }
+}
---
+++ resharper/src/resharper-unity/ShaderLab/Psi/DeclaredElements/ShaderLabDeclaredElementPresenter.cs
@@ -0,0 +1,25 @@
+﻿using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.Resolve;
+using JetBrains.Util;
+
+namespace JetBrains.ReSharper.Plugins.Unity.ShaderLab.Psi.DeclaredElements
+{
+    [PsiSharedComponent]
+    public class ShaderLabDeclaredElementPresenter : IDeclaredElementPresenter
+    {
+        public string Format(DeclaredElementPresenterStyle style, IDeclaredElement element, ISubstitution substitution,
+            out DeclaredElementPresenterMarking marking)
+        {
+            marking = new DeclaredElementPresenterMarking();
+            if (!(element is IShaderLabDeclaredElement))
+                return null;
+
+            // TODO: Type (Float, Color, etc.), display name, etc.
+            marking.NameRange = new TextRange(0, element.ShortName.Length);
+            return element.ShortName;
+        }
+
+        public string Format(ParameterKind parameterKind) => string.Empty;
+        public string Format(AccessRights accessRights) => string.Empty;
+    }
+}
