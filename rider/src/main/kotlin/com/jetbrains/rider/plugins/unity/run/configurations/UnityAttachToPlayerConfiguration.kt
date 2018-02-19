0b1469724d89c547e6510e7cc9d604d0b05f4079
Arseny Chernyaev <arseny.chernyaev@jetbrains.com>
2018-02-19 13:14:05 +0300

    mark UnityAttachToPlayerRunProfile with IDotNetDebuggable

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/run/configurations/UnityAttachToPlayerConfiguration.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/run/configurations/UnityAttachToPlayerConfiguration.kt
@@ -6,7 +6,7 @@ import com.jetbrains.rider.plugins.unity.util.UnityIcons
 import com.jetbrains.rider.run.configurations.remote.RemoteConfiguration
 import javax.swing.Icon
 
-class UnityAttachToPlayerConfiguration(debuggerPort : Int, host: String = "127.0.0.1") : RemoteConfiguration, IDotNetDebuggable {
+class UnityAttachToPlayerConfiguration(debuggerPort : Int, host: String = "127.0.0.1") : RemoteConfiguration {
     override var port: Int = debuggerPort
     override var address: String = host
     override var listenPortForConnections: Boolean = false
--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/run/configurations/UnityAttachToPlayerRunProfile.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/run/configurations/UnityAttachToPlayerRunProfile.kt
@@ -4,10 +4,11 @@ import com.intellij.execution.Executor
 import com.intellij.execution.configurations.RemoteRunProfile
 import com.intellij.execution.configurations.RunProfileState
 import com.intellij.execution.runners.ExecutionEnvironment
+import com.jetbrains.rider.debugger.IDotNetDebuggable
 import com.jetbrains.rider.plugins.unity.util.UnityIcons
 import javax.swing.Icon
 
-class UnityAttachToPlayerRunProfile(private val configurationName: String, private val configuration: UnityAttachToPlayerConfiguration) : RemoteRunProfile {
+class UnityAttachToPlayerRunProfile(private val configurationName: String, private val configuration: UnityAttachToPlayerConfiguration) : RemoteRunProfile, IDotNetDebuggable {
 
     override fun getState(p0: Executor, p1: ExecutionEnvironment): RunProfileState? {
         return UnityAttachToPlayerProfileState(configuration, p1)
