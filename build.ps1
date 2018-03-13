47548759ba33fa2e69a04340d647f3f6fe96918e
Matt Ellis <m.t.ellis@gmail.com>
2018-03-13 00:12:28 +0000

    Remove plugin.xml patching
    The IntelliJ plugin already does this, and we don't specify since or until in any build scripts

--- build.ps1
+++ build.ps1
@@ -1,8 +1,6 @@
 param (
   [string]$Source, # Rider SDK Packages folder, optional
   [string]$BuildCounter = 9999, # Sets Rider plugin version to version from Packaging.Props with the last zero replaced by $BuildCounter
-  [string]$SinceBuild, # Set since-build in Rider plugin descriptor
-  [string]$UntilBuild, # Set until-build in Rider plugin descriptor
   [string]$Configuration = "Release", # Release / Debug
   [switch]$RiderOnly # Build only Rider csprojects
 )
