ebe4ca154be43d98556ebddb235cb2b488f123f3
Arseny Chernyaev <arseny.chernyaev@jetbrains.com>
2018-03-22 14:07:19 +0300

    updated unity editor process discovering restrictions

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/util/attach/UnityProcessUtil.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/util/attach/UnityProcessUtil.kt
@@ -8,12 +8,13 @@ class UnityProcessUtil {
         fun isUnityEditorProcess(processInfo : ProcessInfo) : Boolean {
             val name = processInfo.executableDisplayName
             return (name.startsWith("Unity", true) ||
-                name.startsWith("unity-", true) ||
                 name.contains("Unity.app")) &&
                 !name.contains("UnityDebug") &&
                 !name.contains("UnityShader") &&
                 !name.contains("UnityHelper") &&
-                !name.contains("Unity Helper")
+                !name.contains("Unity Helper") &&
+                !name.contains("Unity Hub") &&
+                !name.contains("UnityCrashHandler")
         }
     }
 
