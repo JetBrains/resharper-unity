e899d0296a280cc53d6ee97043d5aab955c72095
Matt Ellis <m.t.ellis@gmail.com>
2018-03-28 15:25:45 +0100

    Write VSTU flavour guid to project files

---
+++ unity/JetBrains.Rider.Unity.Editor/EditorPlugin.Tests/SlnAssetPostprocessorTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using JetBrains.Rider.Unity.Editor.AssetPostprocessors;
+using NUnit.Framework;
+
+namespace JetBrains.Rider.Unity.Editor.Tests
+{
+  [TestFixture]
+  public class SlnAssetPostprocessorTest
+  {
+    [Test]
+    public void TestFixRandomGuid()
+    {
+      var input =
+        "Project(\"{2150E333-8FDC-42A3-9474-1A3956D46DE8}\") = \"Shared\", \"Shared\", \"{8FCD7D66-2D91-456C-AE4E-E2F6F5B8BCAD}\"" +
+        Environment.NewLine;
+      var expected =
+        "Project(\"{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}\") = \"Shared\", \"Shared\", \"{8FCD7D66-2D91-456C-AE4E-E2F6F5B8BCAD}\"" +
+        Environment.NewLine;
+
+      Assert.AreEqual(expected, SlnAssetPostprocessor.ProcessSlnText(input));
+    }
+
+    [Test]
+    public void TestFixesInvalidCSharpGuid()
+    {
+      // See https://youtrack.jetbrains.com/issue/RIDER-1261 (demo project shows type in C# guid: FA*A*04EC0-301F-11D3-BF4B-00C04F79EFBC)
+      var input =
+        "Project(\"{FAA04EC0-301F-11D3-BF4B-00C04F79EFBC}\") = \"Shared\", \"Shared\", \"{8FCD7D66-2D91-456C-AE4E-E2F6F5B8BCAD}\"" +
+        Environment.NewLine;
+      var expected =
+        "Project(\"{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}\") = \"Shared\", \"Shared\", \"{8FCD7D66-2D91-456C-AE4E-E2F6F5B8BCAD}\"" +
+        Environment.NewLine;
+
+      Assert.AreEqual(expected, SlnAssetPostprocessor.ProcessSlnText(input));
+    }
+
+    [Test]
+    public void TestCorectGuidNotModified()
+    {
+      var input = "Project(\"{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}\") = \"BestHTTP\", \"BestHTTP.csproj\", \"{A311886C-D085-4914-A8E5-6DF7C92112D8}\""+Environment.NewLine;
+      var expected = "Project(\"{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}\") = \"BestHTTP\", \"BestHTTP.csproj\", \"{A311886C-D085-4914-A8E5-6DF7C92112D8}\""+Environment.NewLine;
+      Assert.AreEqual(expected, SlnAssetPostprocessor.ProcessSlnText(input));
+    }
+  }
+}
