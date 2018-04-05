dfdf70b59a8a14cd36b3a63829c97fadbe2533e5
Ivan Shakhov <van800@gmail.com>
2018-04-05 15:11:23 +0200

    #478 Setting to disable AutoRefresh (#497)
    * https://github.com/JetBrains/resharper-unity/issues/478 Disable refreshing when ctrl-s is pressed without disabling auto-refresh in Unity
    
    * wording
    
    * wording
    
    (cherry picked from commit 9752761e9f666792c967d15aadd4eab230e00691)

--- resharper/src/resharper-unity/Settings/UnitySettings.cs
+++ resharper/src/resharper-unity/Settings/UnitySettings.cs
@@ -7,8 +7,11 @@ namespace JetBrains.ReSharper.Plugins.Unity.Settings
     [SettingsKey(typeof(CodeEditingSettings), "Unity plugin settings")]
     public class UnitySettings
     {
-        [SettingsEntry(true, "If this option is enabled, Rider plugin will be automatically installed for Unity projects.")]
+        [SettingsEntry(true, "If this option is enabled, the Rider Unity editor plugin will be automatically installed and updated.")]
         public bool InstallUnity3DRiderPlugin;
+        
+        [SettingsEntry(true, "If this option is enabled, Rider will automatically notify the Unity editor to refresh assets.")]
+        public bool AllowAutomaticRefreshInUnity;
 
         [SettingsEntry(true, "Enables syntax error highlighting, brace matching and more of ShaderLab files.")]
         public bool EnableShaderLabParsing;
