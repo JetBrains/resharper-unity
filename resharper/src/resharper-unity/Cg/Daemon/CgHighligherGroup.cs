37d3082caffcf5050ec61a2096efd6c34072ad86
Ivan Pashchenko <ivan.pashchenko@jetbrains.com>
2018-04-07 11:36:10 +0300

    fixed backend compilation

--- resharper/src/resharper-unity/Cg/Daemon/CgHighligherGroup.cs
+++ resharper/src/resharper-unity/Cg/Daemon/CgHighligherGroup.cs
@@ -3,7 +3,6 @@ using JetBrains.TextControl.DocumentMarkup;
 
 [assembly: RegisterHighlighterGroup(
     CgHighligherGroup.ID, "Cg/HLSL", HighlighterGroupPriority.LANGUAGE_SETTINGS,
-    DemoText = CgHighligherGroup.DEMO_TEXT,
     RiderNamesProviderType = typeof(CgHighlighterNamesProvider))]
 
 namespace JetBrains.ReSharper.Plugins.Unity.Cg.Daemon
@@ -11,7 +10,6 @@ namespace JetBrains.ReSharper.Plugins.Unity.Cg.Daemon
     public static class CgHighligherGroup
     {
         public const string ID = "ReSharper Cg Highlighters";
-        public const string DEMO_TEXT = null;
     }
 
     public class CgHighlighterNamesProvider : PrefixBasedSettingsNamesProvider
