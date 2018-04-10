fdf91aa1fb5994c02d530e99775ffac387088aa9
kirillskrygan <kirill.skrygan@jetbrains.com>
2018-04-10 22:14:48 +0200

    new icons for status bar icon

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/util/UnityIcons.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/util/UnityIcons.kt
@@ -1,6 +1,8 @@
 package com.jetbrains.rider.plugins.unity.util
 
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.util.IconLoader
+import com.intellij.ui.AnimatedIcon
 
 class UnityIcons {
     class Icons {
@@ -22,6 +24,51 @@ class UnityIcons {
 
             @JvmField
             val EditorConnectionStatus = UnityLogo
+        }
+    }
+
+    class Status{
+        companion object {
+            @JvmField
+            val UnityStatus = IconLoader.getIcon("/Icons/UnityStatus/unityStatus.svg")
+            @JvmField
+            val UnityStatusPlay = IconLoader.getIcon("/Icons/UnityStatus/unityStatusPlay.svg")
+
+            @JvmField
+            val UnityStatusProgress1 = IconLoader.getIcon("/Icons/UnityStatus/unityStatusProgress1.svg")
+            @JvmField
+            val UnityStatusProgress2 = IconLoader.getIcon("/Icons/UnityStatus/unityStatusProgress2.svg")
+            @JvmField
+            val UnityStatusProgress3 = IconLoader.getIcon("/Icons/UnityStatus/unityStatusProgress3.svg")
+            @JvmField
+            val UnityStatusProgress4 = IconLoader.getIcon("/Icons/UnityStatus/unityStatusProgress4.svg")
+            @JvmField
+            val UnityStatusProgress5 = IconLoader.getIcon("/Icons/UnityStatus/unityStatusProgress5.svg")
+
+            val UnityStatusProgress = AnimatedIcon(150,
+                UnityStatusProgress5,
+                UnityStatusProgress4,
+                UnityStatusProgress3,
+                UnityStatusProgress2,
+                UnityStatusProgress1)
+
+            @JvmField
+            val UnityStatusPlayProgress1 = IconLoader.getIcon("/Icons/UnityStatus/unityStatusPlayProgress1.svg")
+            @JvmField
+            val UnityStatusPlayProgress2 = IconLoader.getIcon("/Icons/UnityStatus/unityStatusPlayProgress2.svg")
+            @JvmField
+            val UnityStatusPlayProgress3 = IconLoader.getIcon("/Icons/UnityStatus/unityStatusPlayProgress3.svg")
+            @JvmField
+            val UnityStatusPlayProgress4 = IconLoader.getIcon("/Icons/UnityStatus/unityStatusPlayProgress4.svg")
+            @JvmField
+            val UnityStatusPlayProgress5 = IconLoader.getIcon("/Icons/UnityStatus/unityStatusPlayProgress5.svg")
+
+            val UnityStatusPlayProgress = AnimatedIcon(150,
+                UnityStatusPlayProgress5,
+                UnityStatusPlayProgress4,
+                UnityStatusPlayProgress3,
+                UnityStatusPlayProgress2,
+                UnityStatusPlayProgress1)
         }
     }
 
