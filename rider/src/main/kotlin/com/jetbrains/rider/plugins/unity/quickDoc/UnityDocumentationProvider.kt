3980f917d86312a2c89bd9dc15a28801898aad03
Matt Ellis <m.t.ellis@gmail.com>
2018-05-01 15:34:07 +0900

    Use local documentation from Unity Hub
    If there are multiple documentation sets installed, arbitrarily picks
    one - should be the latest version. Should use the project version.
    
    Fixes #548. See also #306 and #504

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/quickDoc/UnityDocumentationProvider.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/quickDoc/UnityDocumentationProvider.kt
@@ -6,6 +6,8 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiManager
 import com.intellij.util.SystemProperties
 import com.intellij.util.io.exists
+import com.intellij.util.io.isDirectory
+import com.intellij.util.text.VersionComparatorUtil
 import com.jetbrains.rider.model.rdUnityModel
 import com.jetbrains.rider.projectView.solution
 import java.nio.file.Files
@@ -14,7 +16,7 @@ import java.nio.file.Paths
 
 class UnityDocumentationProvider : DocumentationProvider {
 
-    private val documentationRoot by lazy(this::findFallbackDocumentationRoot)
+    private val documentationRoot by lazy(this::findLocalDocumentationRoot)
 
     override fun getUrlFor(p0: PsiElement?, p1: PsiElement?): MutableList<String>? {
         val data = p0?.project?.solution?.rdUnityModel?.data
@@ -32,6 +34,8 @@ class UnityDocumentationProvider : DocumentationProvider {
     private fun getUrlForContext(context: String): String {
         // We know context will be a fully qualified type or method name, starting
         // with either `UnityEngine.` or `UnityEditor.`
+        // TODO: Use the current version for the fallback online search
+        // E.g. https://docs.unity3d.com/2017.4/Documentation/ScriptReference/30_search.html?q=...
         val keyword = stripPrefix(context)
         return getFileUri(documentationRoot, "ScriptReference/$keyword.html")
             ?: getFileUri(documentationRoot, "ScriptReference/${keyword.replace('.', '-')}.html")
@@ -53,7 +57,15 @@ class UnityDocumentationProvider : DocumentationProvider {
         return null
     }
 
-    private fun findFallbackDocumentationRoot(): Path? {
+    private fun findLocalDocumentationRoot(): Path? {
+        val hubRoot = findUnityHubDocumentationRoot()
+        if (hubRoot?.exists() == true) {
+            return hubRoot
+        }
+        return findUnityLocalInstallDocumentationRoot()
+    }
+
+    private fun findUnityLocalInstallDocumentationRoot(): Path? {
         return when {
             SystemInfo.isWindows -> {
                 // Unity is installed to `C:\Program Files\Unity`, on both 32 bit and 64 bit
@@ -68,6 +80,7 @@ class UnityDocumentationProvider : DocumentationProvider {
                 // Unity 2017.3 is the first version to add support for local documentation.
                 // It will write to /opt if it has write permissions, otherwise, it writes to
                 // the user's home directory, but adds the version, e.g. ~/Unity-2017.3.0b1
+                // TODO: Use the proper version. See the comment for findUnityHubDocumentationRoot
                 val globalDocRoots = getUnityChildDirs(Paths.get("/opt"))
                 val homeDocRoots = getUnityChildDirs(Paths.get(SystemProperties.getUserHome()))
                 (globalDocRoots + homeDocRoots)
@@ -79,4 +92,30 @@ class UnityDocumentationProvider : DocumentationProvider {
     }
 
     private fun getUnityChildDirs(root: Path): Sequence<Path> = Files.newDirectoryStream(root, "Unity*").asSequence()
+
+    // TODO: Use the version for the currently running instance
+    // Need to check if we can rely on ProjectSettings/ProjectVersion.txt - I'm not sure it's written to when the
+    // project is loaded into a different version of Unity. We should also centralise version handling - a component
+    // If the correct version isn't installed, fall back to the online docs
+    private fun findUnityHubDocumentationRoot(): Path? {
+        return when {
+            SystemInfo.isWindows -> {
+                null
+            }
+            SystemInfo.isMac -> {
+                val hubRoot = Paths.get("/Applications/Unity/Hub/Editor/")
+                if (!hubRoot.exists() || !hubRoot.isDirectory())
+                    return null
+
+                var version = "0.0"
+                for (file in hubRoot.toFile().listFiles()) {
+                    if (file.isDirectory) {
+                        version = VersionComparatorUtil.max(version, file.name)
+                    }
+                }
+                hubRoot.resolve(version).resolve("Documentation/en")
+            }
+            else -> null    // Unity Hub is currently only for Windows and Mac
+        }
+    }
 }
