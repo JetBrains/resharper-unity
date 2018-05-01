777e6387aa6f7c91372fbcd4a4e72fcb3d3373ec
Ivan Shakhov <van800@gmail.com>
2018-05-01 21:56:17 +0200

    Speedup play #527 (#532)
    * add logging to UT initialization in EditorPlugin
    
    * fix bug
    
    * fix https://github.com/JetBrains/resharper-unity/issues/541 and some rear plugin installation problems
    
    * Rewrite post-processing csproj once
    
    * fix bug and remove Location attribute

---
+++ unity/JetBrains.Rider.Unity.Editor/EditorPlugin/Utils/RiderScriptableSingleton.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace JetBrains.Rider.Unity.Editor.Utils
+{
+  // no need to set cache, because otherwise new Unity process will restore the value from the file cache. 
+  //[Location("JetBrainsRiderPluginCache.txt", LocationAttribute.Location.LibraryFolder)] 
+  internal class RiderScriptableSingleton: ScriptObjectSingleton<RiderScriptableSingleton>
+  {
+    [SerializeField] 
+    private bool myCsprojProcessedOnce;
+    public bool CsprojProcessedOnce
+    {
+      get => myCsprojProcessedOnce;
+      set
+      {
+        myCsprojProcessedOnce = value;
+        Save(true);
+      }
+    }
+
+  }
+}
