eab2f318b7de36f2985d49110001c916849c66b3
Matt Ellis <m.t.ellis@gmail.com>
2018-05-10 23:29:04 +0800

    Add GradleLauncher project and shared run config
    This will add a shared run configuration called "runIde", which is
    just a .NET project run config that builds the GradleLauncher project,
    and any referenced projects (i.e. the editor plugin and rider projects).
    When executed, it simply calls `gradle runIde -P dotNetUpToDate=true`.
    This will run the gradle script that prepares everything in order to run
    the IDE (generate the model, build the frontend plugin, copy all of the
    backend plugin artifacts, etc.) but it will also skip the ridiculously
    slow dotnet restore and msbuild calls.

---
+++ resharper/src/GradleLauncher/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace GradleLauncher
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: GradleLauncher workingDirectory args");
+                return;
+            }
+
+            Environment.CurrentDirectory = args[0];
+            Console.WriteLine("Current directory: {0}", Environment.CurrentDirectory);
+
+            var args2 = args.ToList();
+            args2.RemoveAt(0);
+            var arguments = args2.ToArray();
+
+            if (Environment.OSVersion.Platform == PlatformID.Win32Windows)
+                ExecuteBatchFile("gradlew.bat", arguments);
+            else
+                ExecuteShellScript("./gradlew", arguments);
+        }
+
+        static void ExecuteBatchFile(string command, string[] args)
+        {
+            var commandline = $"/c {command} {args.FormatArgs()}";
+            ExecuteScriptProcessor("cmd.exe", commandline);
+
+        }
+
+        static void ExecuteShellScript(string command, string[] args)
+        {
+            var commandline = $"-c \"{command} {string.Join(" ", args)}\"";
+            ExecuteScriptProcessor("sh", commandline);
+        }
+
+        static void ExecuteScriptProcessor(string processor, string commandline)
+        {
+            Console.WriteLine($"Starting {processor} {commandline}");
+            var processInfo = new ProcessStartInfo(processor, commandline)
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false
+            };
+
+            using (var process = Process.Start(processInfo))
+                process.WaitForExit();
+        }
+
+        static string FormatArgs(this string[] args)
+        {
+            return string.Join(" ", args);
+        }
+    }
+}
