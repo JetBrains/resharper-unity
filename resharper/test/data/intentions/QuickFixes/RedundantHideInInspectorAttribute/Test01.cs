b4013eca24786580062dd1f0861e25022f06ff97
Matt Ellis <m.t.ellis@gmail.com>
2018-06-07 16:24:40 +0100

    Add inspection for redundant HideInInspector
    Fixes #585

---
+++ resharper/test/data/intentions/QuickFixes/RedundantHideInInspectorAttribute/Test01.cs
@@ -0,0 +1,7 @@
+using System;
+using UnityEngine;
+
+public class Test
+{
+    [HideIn{caret}Inspector] [NonSerialized] public int Value;
+}
---
+++ resharper/test/src/Daemon/Stages/Analysis/RedundantHideInInspectorAttributeAnalyzerTests.cs
@@ -0,0 +1,21 @@
+using JetBrains.ReSharper.Feature.Services.Daemon;
+using JetBrains.ReSharper.FeaturesTestFramework.Daemon;
+using JetBrains.ReSharper.Plugins.Unity.Daemon.Stages.Highlightings;
+using JetBrains.ReSharper.Psi;
+using NUnit.Framework;
+
+namespace JetBrains.ReSharper.Plugins.Unity.Tests.Daemon.Stages.Analysis
+{
+    [TestUnity]
+    public class RedundantHideInInspectorAttributeAnalyzerTests : CSharpHighlightingTestBase
+    {
+        protected override string RelativeTestDataPath => @"daemon\Stages\Analysis";
+
+        protected override bool HighlightingPredicate(IHighlighting highlighting, IPsiSourceFile sourceFile)
+        {
+            return highlighting is IUnityHighlighting;
+        }
+
+        [Test] public void TestRedundantHideInInspectorAttribute() { DoNamedTest2(); }
+    }
+}
