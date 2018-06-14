fce5a501d838dae43d5fdec3ae1871374a7e415a
Kirill.Skrygan <kirill.skrygan@jetbrains.com>
2018-06-14 11:24:26 +0200

    some fixes for ui toggle button

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/ui/UnityUIMinimizer.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/ui/UnityUIMinimizer.kt
@@ -5,11 +5,10 @@ import com.intellij.openapi.startup.StartupActivity
 import com.intellij.openapi.wm.IdeFocusManager
 import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.openapi.wm.impl.ToolWindowImpl
+import com.jetbrains.rider.UnityReferenceDiscoverer
 import com.jetbrains.rider.build.actions.ActiveConfigurationAndPlatformAction
 import com.jetbrains.rider.util.idea.application
-import com.jetbrains.rider.util.idea.lifetime
 import com.jetbrains.rider.util.idea.tryGetComponent
-import com.jetbrains.rider.util.reactive.whenTrue
 
 class UnityUIMinimizer : StartupActivity {
     companion object {
@@ -53,12 +52,13 @@ class UnityUIMinimizer : StartupActivity {
 
     override fun runActivity(project: Project) {
         val unityUiManager = project.tryGetComponent<UnityUIManager>() ?: return
+        val unityReferenceDiscoverer = project.tryGetComponent<UnityReferenceDiscoverer>() ?: return
 
-        unityUiManager.hasMinimizedUi.whenTrue(project.lifetime, {
+        if(unityUiManager.hasMinimizedUi.hasTrueValue() && unityReferenceDiscoverer.isUnityGeneratedProject) {
             application.invokeLater {
                 ensureMinimizedUI(project)
             }
-        })
+        }
     }
 }
 
