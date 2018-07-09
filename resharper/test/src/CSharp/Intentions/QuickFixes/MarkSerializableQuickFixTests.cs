35231643a8586e063c9663a033244e9917e6b2f2
Matt Ellis <m.t.ellis@gmail.com>
2018-07-09 17:14:16 +0100

    Add "mark class serializable" quick fix

---
+++ resharper/test/src/CSharp/Intentions/QuickFixes/MarkSerializableQuickFixTests.cs
@@ -0,0 +1,23 @@
+﻿using JetBrains.ReSharper.FeaturesTestFramework.Intentions;
+using JetBrains.ReSharper.Plugins.Unity.CSharp.Feature.Services.QuickFixes;
+using NUnit.Framework;
+
+namespace JetBrains.ReSharper.Plugins.Unity.Tests.CSharp.Intentions.QuickFixes
+{
+    [TestUnity]
+    public class MarkSerializableQuickFixAvailabilityTests : QuickFixAvailabilityTestBase
+    {
+        protected override string RelativeTestDataPath=> @"CSharp\Intentions\QuickFixes\MarkSerializable\Availability";
+
+        [Test] public void Test01() { DoNamedTest(); }
+        [Test] public void Test02() { DoNamedTest(); }
+    }
+
+    [TestUnity]
+    public class MarkSerializableQuickFixTests : CSharpQuickFixTestBase<MarkSerializableQuickFix>
+    {
+        protected override string RelativeTestDataPath=> @"CSharp\Intentions\QuickFixes\MarkSerializable";
+
+        [Test] public void Test01() { DoNamedTest(); }
+    }
+}
