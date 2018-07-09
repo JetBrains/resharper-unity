fd44258ec931e82e0a359cf98c904ee85ea58b29
Ivan Pashchenko <ivan.pashchenko@jetbrains.com>
2018-07-09 23:04:48 +0300

    Three parameter change signature tests

--- resharper/test/src/CSharp/Intentions/QuickFixes/InvalidSignatureFixTests.cs
+++ resharper/test/src/CSharp/Intentions/QuickFixes/InvalidSignatureFixTests.cs
@@ -12,9 +12,20 @@ namespace JetBrains.ReSharper.Plugins.Unity.Tests.CSharp.Intentions.QuickFixes
         [Test] public void AddAllParameters() { DoNamedTest(); }
         [Test] public void AddMissingParameter() { DoNamedTest(); }
         [Test] public void RemoveAllParameters() { DoNamedTest(); }
+        [Test] public void RemoveAllParametersThreeParameters() { DoNamedTest(); }
+        [Test] public void RenameParameters() { DoNamedTest(); }
         [Test] public void ReorderParameters() { DoNamedTest(); }
         [Test] public void InitializeOnLoadMethod() { DoNamedTest(); }
         [Test] public void RuntimeInitializeOnLoadMethod() { DoNamedTest(); }
+        
+        private readonly string[] myAddMissingParametersSource = {"000", "010", "011", "100", "101", "110" };
+        private readonly string[] myReorderParametersSource = {"132", "213", "231", "312", "321"};
+        
+        [TestCaseSource(nameof(myAddMissingParametersSource))]
+        [Test] public void AddMissingParameterThreeParameters(string id) { DoOneTest($"{TestMethodName}_{id}"); }
+        
+        [TestCaseSource(nameof(myReorderParametersSource))]        
+        [Test] public void ReorderParametersThreeParameters(string id) { DoOneTest($"{TestMethodName}_{id}"); }
     }
 
     [TestUnity]
@@ -25,9 +36,19 @@ namespace JetBrains.ReSharper.Plugins.Unity.Tests.CSharp.Intentions.QuickFixes
         [Test] public void AddAllParameters() { DoNamedTest(); }
         [Test] public void AddMissingParameter() { DoNamedTest(); }
         [Test] public void RemoveAllParameters() { DoNamedTest(); }
+        [Test] public void RemoveAllParametersThreeParameters() { DoNamedTest(); }
         [Test] public void RenameParameters() { DoNamedTest(); }
         [Test] public void ReorderParameters() { DoNamedTest(); }
         [Test] public void InitializeOnLoadMethod() { DoNamedTest(); }
         [Test] public void RuntimeInitializeOnLoadMethod() { DoNamedTest(); }
+        
+        private readonly string[] myAddMissingParametersSource = {"000", "010", "011", "100", "101", "110" };
+        private readonly string[] myReorderParametersSource = {"132", "213", "231", "312", "321"};
+        
+        [TestCaseSource(nameof(myAddMissingParametersSource))]
+        [Test] public void AddMissingParameterThreeParameters(string id) { DoOneTest($"{TestMethodName}_{id}"); }
+        
+        [TestCaseSource(nameof(myReorderParametersSource))]
+        [Test] public void ReorderParametersThreeParameters(string id) { DoOneTest($"{TestMethodName}_{id}"); }
     }
 }
