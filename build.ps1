85c9df63745c19567ffdba3ff32f3d235aa174a4
Ivan Pashchenko <ivan.pashchenko@jetbrains.com>
2018-07-17 00:14:39 +0300

    fix unit tests always launching

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@ param (
   [string]$Source, # Rider SDK Packages folder, optional
   [string]$BuildCounter = 9999, # Sets Rider plugin version to version from Packaging.Props with the last zero replaced by $BuildCounter
   [string]$Configuration = "Release", # Release / Debug
-  [string]$RunTests = "False"
+  [switch]$RunTests
 )
 
 Set-StrictMode -Version Latest; $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
