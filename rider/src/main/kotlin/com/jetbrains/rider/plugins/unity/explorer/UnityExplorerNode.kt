48b79b5de82ae6904a9dd6392659e424326414d2
Matt Ellis <m.t.ellis@gmail.com>
2018-08-07 09:19:16 +0100

    Show solution/project loading in Assets root node

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/explorer/UnityExplorerNode.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/explorer/UnityExplorerNode.kt
@@ -6,13 +6,16 @@ import com.intellij.ide.scratch.ScratchProjectViewPane
 import com.intellij.ide.util.treeView.AbstractTreeNode
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.IconLoader
+import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.ui.SimpleTextAttributes
+import com.jetbrains.rider.model.*
 import com.jetbrains.rider.plugins.unity.util.UnityIcons
 import com.jetbrains.rider.projectView.ProjectModelViewHost
 import com.jetbrains.rider.projectView.nodes.*
 import com.jetbrains.rider.projectView.views.FileSystemNodeBase
 import com.jetbrains.rider.projectView.views.SolutionViewRootNodeBase
+import com.jetbrains.rider.projectView.views.addAdditionalText
 import com.jetbrains.rider.util.getOrCreate
 import javax.swing.Icon
 
@@ -149,11 +152,70 @@ open class UnityExplorerNode(project: Project,
         : UnityExplorerNode(project, virtualFile, listOf()) {
 
         private val referenceRoot = ReferenceRoot(project)
+        private val solutionNode = ProjectModelViewHost.getInstance(project).solutionNode
 
         override fun update(presentation: PresentationData) {
             if (!virtualFile.isValid) return
-            presentation.presentableText = "Assets"
+            presentation.addText("Assets", SimpleTextAttributes.REGULAR_ATTRIBUTES)
             presentation.setIcon(UnityIcons.Explorer.AssetsRoot)
+
+            val descriptor = solutionNode.descriptor as? RdSolutionDescriptor ?: return
+            val state = getAggregateSolutionState(descriptor)
+            when (state) {
+                RdSolutionState.Loading -> presentation.addAdditionalText("loading...")
+                RdSolutionState.Sync -> presentation.addAdditionalText("synchronizing...")
+                RdSolutionState.Ready -> {
+                    if (descriptor.projectsCount.failed + descriptor.projectsCount.unloaded > 0) {
+                        presentProjectsCount(presentation, descriptor.projectsCount, true)
+                    }
+                }
+                RdSolutionState.ReadyWithErrors -> presentation.addAdditionalText("load failed")
+                RdSolutionState.ReadyWithWarnings -> presentProjectsCount(presentation, descriptor.projectsCount, true)
+                else -> {}
+            }
+        }
+
+        private fun getAggregateSolutionState(descriptor: RdSolutionDescriptor): RdSolutionState {
+            var state = descriptor.state
+
+            // Solution loading/synchronizing takes precedence
+            if (state == RdSolutionState.Loading || state == RdSolutionState.Sync) {
+                return state
+            }
+
+            state = RdSolutionState.Ready
+            val children = solutionNode.getChildren(false, false)
+            for (child in children) {
+                if (child.isProject()) {
+                    val projectDescriptor = child.descriptor as? RdProjectDescriptor ?: continue
+
+                    // Aggregate project loading and sync. Loading takes precedence over sync
+                    if (projectDescriptor.state == RdProjectState.Loading) {
+                        state = RdSolutionState.Loading
+                    }
+                    else if (projectDescriptor.state == RdProjectState.Sync && state != RdSolutionState.Loading) {
+                        state = RdSolutionState.Sync
+                    }
+                }
+            }
+
+            // Make sure we don't miss solution ReadWithErrors
+            if (state == RdSolutionState.Ready) {
+                state = descriptor.state
+            }
+
+            return state
+        }
+
+        private fun presentProjectsCount(presentation: PresentationData, count: RdProjectsCount, showZero: Boolean) {
+            if (count.total == 0 && !showZero) return
+
+            var text = "${count.total} ${StringUtil.pluralize("project", count.total)}"
+            val unloadedCount = count.failed + count.unloaded
+            if (unloadedCount > 0) {
+                text += ", $unloadedCount unloaded"
+            }
+            presentation.addAdditionalText(text)
         }
 
         override fun isAlwaysExpand() = true
