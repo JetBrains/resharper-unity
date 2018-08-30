cdde1c1aad9129a6d2528539f6a5d8dd3c52272f
Matt Ellis <m.t.ellis@gmail.com>
2018-08-30 10:57:48 +0100

    Workaround layout issue for R# refactoring page

--- resharper/src/resharper-unity/CSharp/Feature/Services/Refactorings/Rename/FormerlySerializedAsRefactoringPage.cs
+++ resharper/src/resharper-unity/CSharp/Feature/Services/Refactorings/Rename/FormerlySerializedAsRefactoringPage.cs
@@ -15,14 +15,21 @@ namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Feature.Services.Refactorings
             : base(lifetime)
         {
             myModel = model;
-            myContent = new BeGrid(GridOrientation.Vertical);
             myShouldAddFormerlySerializedAs = new Property<bool>(lifetime, "ShouldAddAttribute", myModel.ShouldAddFormerlySerializedAs);
-            var checkBox = myShouldAddFormerlySerializedAs.GetBeCheckBox(lifetime, "Add _FormerlySerializedAs attribute");
-            myContent.AddElement(checkBox);
+            myContent = myShouldAddFormerlySerializedAs.GetBeCheckBox(lifetime, "Add _FormerlySerializedAs attribute").InAutoGrid();
         }
 
+#if RESHARPER
+        // ReSharper has a layout issue where the page doesn't resize properly. Reducing the description to a single
+        // line is the best workaround for now
+        public override string Title => "Renaming a serialized field can break existing serialized data";
+        public override string Description => "Adding the FormerlySerializedAs attribute tells Unity to also try the old name.";
+#else
+        // And Rider doesn't show the title, only the description
         public override string Title => "Rename Unity serialized field";
-        public override string Description => "Renaming a serialized field can break existing serialized data. The FormerlySerializedAs attribute tells Unity how to deserialize data with the old name.";
+        public override string Description => "Renaming a serialized field can break existing serialized data. Adding the FormerlySerializedAs attribute tells Unity to also try the old name.";
+#endif
+
         public override BeControl GetPageContent() => myContent;
 
         public override void Commit()
