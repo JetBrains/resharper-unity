999db7ecd5341a64b194b276936677cd49241331
Vladimir Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2018-09-17 20:16:43 +0300

    Add analyzer and quick fix for caching value of component's properties.

--- resharper/src/resharper-unity/KnownTypes.cs
+++ resharper/src/resharper-unity/KnownTypes.cs
@@ -21,6 +21,7 @@ namespace JetBrains.ReSharper.Plugins.Unity
         public static readonly IClrTypeName RuntimeInitializeOnLoadMethodAttribute = new ClrTypeName("UnityEngine.RuntimeInitializeOnLoadMethodAttribute");
         public static readonly IClrTypeName ScriptableObject = new ClrTypeName("UnityEngine.ScriptableObject");
         public static readonly IClrTypeName SerializeField = new ClrTypeName("UnityEngine.SerializeField");
+        public static readonly IClrTypeName Transform = new ClrTypeName("UnityEngine.Transform");
 
         // UnityEngine.Networking
         public static readonly IClrTypeName NetworkBehaviour = new ClrTypeName("UnityEngine.Networking.NetworkBehaviour");
