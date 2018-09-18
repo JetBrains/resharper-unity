62bd9f0da5b5d468ee679fc2acb1928f1ff6b7c3
Matt Ellis <m.t.ellis@gmail.com>
2018-09-18 14:08:13 +0100

    Use Kotlin extension function again

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/explorer/PackagesManager.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/explorer/PackagesManager.kt
@@ -7,6 +7,7 @@ import com.intellij.openapi.util.Key
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.util.io.isDirectory
+import com.jetbrains.rdclient.util.idea.getOrCreateUserData
 import com.jetbrains.rider.plugins.unity.util.SemVer
 import com.jetbrains.rider.plugins.unity.util.UnityCachesFinder
 import com.jetbrains.rider.plugins.unity.util.UnityInstallationFinder
@@ -54,17 +55,7 @@ class PackagesManager(private val project: Project) {
         private val key: Key<PackagesManager> = Key("UnityExplorer::PackagesManager")
         private val logger = Logger.getInstance(PackagesManager::class.java)
 
-        fun getInstance(project: Project): PackagesManager {
-            // We can't use the UserDataHolderEx.getOrCreateUserData extension function, because the compiler tries to
-            // inline it, but the scrambler has inserted references to a private static field
-            // This should be fixed in IDEA/Rider. Please test again!
-            var packagesManager = project.getUserData(key)
-            if (packagesManager == null) {
-                packagesManager = PackagesManager(project)
-                project.putUserData(key, packagesManager)
-            }
-            return packagesManager
-        }
+        fun getInstance(project: Project) = project.getOrCreateUserData(key) { PackagesManager(project) }
     }
 
     private val gson = Gson()
