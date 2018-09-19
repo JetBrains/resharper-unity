270d0567a7487d26585e24f9559efdd90c76fd11
Vladimir Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2018-09-19 19:44:16 +0300

    Implement new idea for handling properties which can be cached. Remove a lot of restriction and false-positive cases.

--- resharper/src/resharper-unity/CSharp/Feature/Services/QuickFixes/CachePropertyValueQuickFix.cs
+++ resharper/src/resharper-unity/CSharp/Feature/Services/QuickFixes/CachePropertyValueQuickFix.cs
@@ -4,20 +4,15 @@ using JetBrains.Annotations;
 using JetBrains.Application.Progress;
 using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Feature.Services.QuickFixes;
-using JetBrains.ReSharper.Intentions.Util;
 using JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Errors;
-using JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Stages.Analysis;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.CSharp;
 using JetBrains.ReSharper.Psi.CSharp.Tree;
 using JetBrains.ReSharper.Psi.CSharp.Util;
-using JetBrains.ReSharper.Psi.ExtensionsAPI.Tree;
 using JetBrains.ReSharper.Psi.Naming.Extentions;
 using JetBrains.ReSharper.Psi.Naming.Impl;
 using JetBrains.ReSharper.Psi.Naming.Settings;
-using JetBrains.ReSharper.Psi.Resolve.Managed;
 using JetBrains.ReSharper.Psi.Tree;
-using JetBrains.ReSharper.Resources.Shell;
 using JetBrains.TextControl;
 using JetBrains.Util;
 
@@ -27,41 +22,84 @@ namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Feature.Services.QuickFixes
     public class CachePropertyValueQuickFix : QuickFixBase
     {
         private readonly IReferenceExpression[] myReferences;
-        private readonly bool myCanApplyFix;
         private readonly IReferenceExpression myHighlightedReference;
 
         public CachePropertyValueQuickFix(InefficientPropertyAccessWarning warning)
         {
             myReferences = warning.References;
-            myCanApplyFix = warning.CanApplyFix;
             myHighlightedReference = warning.HighlightedReference;
         }
 
         protected override Action<ITextControl> ExecutePsiTransaction(ISolution solution, IProgressIndicator progress)
         {
             var factory = CSharpElementFactory.GetInstance(myHighlightedReference);
-            var propertyName = (myHighlightedReference.Reference.Resolve().DeclaredElement as IProperty).NotNull()
-                .ShortName;
+            var property = (myHighlightedReference.Reference.Resolve().DeclaredElement as IProperty).NotNull();
 
-            var name = GetUniqueName(myHighlightedReference, propertyName);
-            
-            var loadDeclaration = factory.CreateStatement("var $0 = $1;", propertyName, myHighlightedReference.Copy());
-            
-            var firstAnchor = myReferences.First().GetContainingStatement();
-            StatementUtil.InsertStatement(loadDeclaration, ref firstAnchor, true);
-            
-            var lastAnchor = myReferences.Last().GetContainingStatement();
-            var saveDeclaration = factory.CreateStatement("$0 = $1;", myHighlightedReference.Copy(), propertyName);
-            StatementUtil.InsertStatement(saveDeclaration, ref lastAnchor, false);
-            
-            foreach (var reference in myReferences)
+            // save declaration (in case of expression lambda)
+            var declaration = myHighlightedReference.GetContainingFunctionLikeDeclarationOrClosure();
+            var type = property.Type;
+
+            var name = GetUniqueName(myHighlightedReference, property.ShortName);
+
+            ICSharpStatement cacheValue = null;
+            IReferenceExpression originValue = myHighlightedReference.Copy();
+            for (var i = 0; i < myReferences.Length; i++)
             {
-                reference.ReplaceBy(factory.CreateReferenceExpression("$0", propertyName));
+                var reference = myReferences[i];
+                if (i == 0)
+                {
+                    if (IsAssignDestination(reference))
+                    {
+                        cacheValue = factory.CreateStatement("var $0 = $1;", name, reference.Copy());
+                    }
+                    else
+                    {
+                        cacheValue = factory.CreateStatement("$0 $1;", type, name);
+                        var node = (IAssignmentExpression)reference
+                            .ReplaceBy(factory.CreateExpression("($0 = $1)", name, myHighlightedReference.Copy()))
+                            .GetOperandThroughParenthesis();
+                        myReferences[0] = (node.Source as IReferenceExpression).NotNull("node.Source as IReferenceExpression != null");
+                        continue;
+                    }
+                }
+                myReferences[i] = reference.ReplaceBy(factory.CreateReferenceExpression("$0", name));
+            }
+            
+            Assertion.Assert(cacheValue != null, "cacheValue != null");
+            var firstReference = myReferences[0];
+            
+            
+                       
+            // handle case when lambda does not have body
+            if (declaration is ICSharpClosure closure && closure is ILambdaExpression lambda && lambda.BodyExpression != null)
+            {
+                var expression = firstReference.GetContainingNode<ILambdaExpression>().NotNull("Expression should be under lambda").BodyExpression;
+                var block = factory.CreateBlock("{$0return $1;}", cacheValue, expression);
+                lambda.SetBodyBlock(block);
+            }
+            else
+            {
+                var loadAnchor = firstReference.GetContainingStatement();
+                StatementUtil.InsertStatement(cacheValue, ref loadAnchor, true);
+
+                var lastStatementAssignment = myReferences.LastOrDefault(IsAssignDestination);
+
+                if (lastStatementAssignment != null)
+                {
+                    var restoreValue = factory.CreateStatement("$0 = $1;", originValue, name);
+                    var saveAnchor = lastStatementAssignment.GetContainingStatement();
+                    StatementUtil.InsertStatement(restoreValue, ref saveAnchor, false);
+                }
             }
             
             return null;
         }
-
+        
+        private static bool IsAssignDestination(IReferenceExpression expr)
+        {
+            var assignment = AssignmentExpressionNavigator.GetByDest(expr.GetContainingParenthesizedExpression());
+            return ExpressionStatementNavigator.GetByExpression(assignment) != null;
+        }
         private static string GetUniqueName([NotNull]IReferenceExpression referenceExpression,[NotNull] string baseName)
         {
             var namingManager = referenceExpression.GetPsiServices().Naming;
@@ -84,7 +122,7 @@ namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Feature.Services.QuickFixes
 
         public override bool IsAvailable(IUserDataHolder cache)
         {
-            return myCanApplyFix && myReferences[0].IsValid();
+            return myReferences.All(t => t.IsValid());
         }
     }
 }
