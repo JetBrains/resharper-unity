aa0c2647af1e10f61267ef8cbaa439da622e7e53
Vladimir Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2018-09-21 17:13:42 +0300

    Properties is reused now. Support constants value and concatenation of constants and literals. Reused fields/properties is searching not only in top-level class. Fix bug with wrong reference. Add tests for all new cases.

--- resharper/src/resharper-unity/CSharp/Feature/Services/QuickFixes/PreferAddressByIdToGraphicsParamsQuickFix.cs
+++ resharper/src/resharper-unity/CSharp/Feature/Services/QuickFixes/PreferAddressByIdToGraphicsParamsQuickFix.cs
@@ -1,4 +1,5 @@
 using System;
+using JetBrains.Annotations;
 using JetBrains.Application.Progress;
 using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Feature.Services.QuickFixes;
@@ -7,9 +8,11 @@ using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.CSharp;
 using JetBrains.ReSharper.Psi.CSharp.Tree;
 using JetBrains.ReSharper.Psi.CSharp.Util;
+using JetBrains.ReSharper.Psi.ExtensionsAPI.Resolve.Managed;
 using JetBrains.ReSharper.Psi.Naming.Extentions;
 using JetBrains.ReSharper.Psi.Naming.Impl;
 using JetBrains.ReSharper.Psi.Naming.Settings;
+using JetBrains.ReSharper.Psi.Resolve.Managed;
 using JetBrains.ReSharper.Psi.Tree;
 using JetBrains.TextControl;
 using JetBrains.Util;
@@ -19,94 +22,215 @@ namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Feature.Services.QuickFixes
     [QuickFix]
     public class PreferAddressByIdToGraphicsParamsQuickFix : QuickFixBase
     {
-        private IInvocationExpression myInvocationExpression;
-        private ICSharpArgument myArgument;
-        private string myLiteral;
-        private string myMapFuntion;
-        private string myTypeName; 
+        private readonly IInvocationExpression myInvocationExpression;
+        private readonly ICSharpArgument myArgument;
+        
+        // using for name of new generated field
+        private readonly string myFieldName;
+        
+        // using for find already declared field or property
+        private readonly string myGraphicsPropertyName;
+
+        // using to handle concatenation operations with const values and save possibility to correct refactoring
+        private readonly IExpression myArgumentExpression;
+        private readonly string myMapFunction;
+        private readonly string myTypeName; 
         
         public PreferAddressByIdToGraphicsParamsQuickFix(PreferAddressByIdToGraphicsParamsWarning warning)
         {
             myInvocationExpression = warning.InvocationMethod;
             myArgument = warning.Argument;
-            myLiteral = warning.Literal;
-            if (myLiteral != null && !ValidityChecker.IsValidIdentifier(myLiteral))
+            myFieldName = myGraphicsPropertyName = warning.Literal;
+            if (!ValidityChecker.IsValidIdentifier(myFieldName))
             {
-                myLiteral = "Property";
+                myFieldName = "Property";
             }
-            
-            myMapFuntion = warning.MapFunction;
+
+            myArgumentExpression = warning.ArgumentExpression;
+            myMapFunction = warning.MapFunction;
             myTypeName = warning.TypeName;
         }
         
         protected override Action<ITextControl> ExecutePsiTransaction(ISolution solution, IProgressIndicator progress)
         {
-            var psiModule = myInvocationExpression.PsiModule;
-            var classDeclaration = GetTopLevelClass(myInvocationExpression);
-
             var factory = CSharpElementFactory.GetInstance(myInvocationExpression);
 
-            var fieldInitializerValue = factory.CreateExpression("$0.$1($2)", 
-                TypeFactory.CreateTypeByCLRName(myTypeName, myInvocationExpression.PsiModule),
-                myMapFuntion,
-                $"\"{myLiteral}\"");
-
-
-            // generate field if we need it. If we have field which addresses to same property reuse it.
-            var name = TryFindField(myLiteral, classDeclaration, myTypeName, myMapFuntion);
-
-            if (name == null)
+            //  try find declaration where string to id conversation is done. If we don't find it, create in top-level class
+            var idDeclaration = TryFindDeclaration(myInvocationExpression, myGraphicsPropertyName, myTypeName, myMapFunction, out var name); 
+            if (idDeclaration == null)
             {
-                name = GetUniqueName(myInvocationExpression, myLiteral);
-                var newMemberDeclaration = factory.CreateFieldDeclaration(psiModule.GetPredefinedType().Int, name);
+                var psiModule = myInvocationExpression.PsiModule;
+                var fieldInitializerValue = factory.CreateExpression("$0.$1($2)", 
+                    TypeFactory.CreateTypeByCLRName(myTypeName, myInvocationExpression.PsiModule),
+                    myMapFunction,
+                    myArgumentExpression.Copy());
+                
+                name = GetUniqueName(myInvocationExpression, myFieldName).NotNull();
+                var newDeclaration = factory.CreateFieldDeclaration(psiModule.GetPredefinedType().Int, name);
+                idDeclaration = newDeclaration.DeclaredElement;
+                if (idDeclaration == null)
+                    return null;
+                
+                newDeclaration.SetReadonly(true);
+                newDeclaration.SetStatic(true);
+                newDeclaration.SetAccessRights(AccessRights.PRIVATE);
+                newDeclaration.SetInitial(factory.CreateExpressionInitializer(fieldInitializerValue));
 
-                newMemberDeclaration.SetReadonly(true);
-                newMemberDeclaration.SetStatic(true);
-                newMemberDeclaration.SetAccessRights(AccessRights.PRIVATE);
-                newMemberDeclaration.SetInitial(factory.CreateExpressionInitializer(fieldInitializerValue));
-
-                classDeclaration.AddClassMemberDeclaration(newMemberDeclaration);
+                // TODO: [C#8] default interface implementations
+                // interface is not good place to add field declaration
+                var classDeclaration = GetTopLevelClassLikeDeclaration(myInvocationExpression);
+                classDeclaration.AddClassMemberDeclaration(newDeclaration);
             }
 
             // replace argument
-            var argument = factory.CreateArgument(ParameterKind.VALUE, factory.CreateReferenceExpression(name));
+            var referenceExpression = factory.CreateReferenceExpression("$0", idDeclaration);
+            
+            
+            var argument = factory.CreateArgument(ParameterKind.VALUE, myArgument.NameIdentifier?.Name, referenceExpression);
             myArgument.ReplaceBy(argument);
             return null;
-        }
+        } 
 
-        private static string TryFindField(string propertyName, IClassDeclaration declaration, string requiredTypeName, string requiredMethodName)
+        private static IDeclaredElement TryFindIdDeclarationInClassLikeDeclaration(IClassLikeDeclaration declaration, string propertyName,
+            string requiredTypeName, string requiredMethodName, out string name)
         {
-            foreach (var field in declaration.FieldDeclarations)
+            name = null;
+            foreach (var member in declaration.ClassMemberDeclarations)
             {
-                var initializer = field.Initial;
-                if (initializer is IExpressionInitializer exprInit && exprInit.Value is IInvocationExpression invocation)
+
+                switch (member)
                 {
-                    var method = invocation.Reference?.Resolve()?.DeclaredElement as IMethod;
-                    
-                    if (method == null) continue;
-                    if (!method.ShortName.Equals(requiredMethodName)) continue;
-
-                    var containingType = method.GetContainingType();
-                    if (containingType == null) continue;
-
-                    if (!containingType.GetClrName().FullName.Equals(requiredTypeName)) continue;
-
-                    var arguments = invocation.Arguments;
-
-                    if (arguments.Count == 1 &&
-                        arguments[0].Expression is ILiteralExpression literal &&
-                        literal.ConstantValue.Value is string str &&
-                        str.Equals(propertyName))
-                    {
-                        return field.DeclaredName;
-                    }
-                }
+                    case IFieldDeclaration field:
+                        if (HandleField(field, propertyName, requiredTypeName, requiredMethodName, out name))
+                            return field.DeclaredElement;
+                        break;
+                    case IPropertyDeclaration property:
+                        if (HandleProperty(property, propertyName, requiredTypeName, requiredMethodName, out name))
+                        {
+                            return property.DeclaredElement;
+                        }
+                        break;
+                } 
             }
 
             return null;
         }
+
+        private static bool HandleProperty(IPropertyDeclaration property, string propertyName, string requiredTypeName, string requiredMethodName, out string name)
+        {
+            name = null;
+            if (!property.IsStatic)
+                return false;
+
+            var accessors = property.AccessorDeclarations;
+            if (accessors.Count != 1)
+                return false;
+
+            if (accessors[0].Kind != AccessorKind.GETTER)
+                return false;
+                
+            if (property.DeclaredElement == null) 
+                return false;
+                
+            var initializer = property.Initial;
+            if (initializer is IExpressionInitializer exprInit && exprInit.Value is IInvocationExpression invocation)
+            {
+                return HandleInvocation(invocation, propertyName, requiredTypeName, requiredMethodName, out name);
+            }
+
+            return false;
+        }
+
+        private static bool HandleField(IFieldDeclaration field, string propertyName, string requiredTypeName, string requiredMethodName, out string name)
+        {
+            name = null;
+            if (!field.IsStatic || !field.IsReadonly)
+                return false;
+                
+            if (field.DeclaredElement == null) 
+                return false;
+                
+            var initializer = field.Initial;
+            if (initializer is IExpressionInitializer exprInit && exprInit.Value is IInvocationExpression invocation)
+            {
+                return HandleInvocation(invocation, propertyName, requiredTypeName, requiredMethodName, out name);
+            }
+
+            return false;
+        }
+
+        private static bool HandleInvocation(IInvocationExpression invocation, string propertyName, string requiredTypeName,
+            string requiredMethodName, out string name)
+        {
+            name = null;
+            
+            var method = invocation.Reference?.Resolve().DeclaredElement as IMethod;
+                    
+            if (method == null) return false;
+            if (!method.ShortName.Equals(requiredMethodName)) return false;;
+
+            var containingType = method.GetContainingType();
+            if (containingType == null) return false;;
+
+            if (!containingType.GetClrName().FullName.Equals(requiredTypeName)) return false;;
+
+            var arguments = invocation.Arguments;
+
+            if (arguments.Count == 1)
+            {
+                var constantValue = arguments[0].Value.ConstantValue(new UniversalContext(invocation));
+                if (constantValue.Value?.Equals(propertyName) == true)
+                    return true;
+            }
+
+            return false;
+        }
+
+        private static IDeclaredElement TryFindDeclaration([NotNull]IInvocationExpression expression, [NotNull]string propertyName,
+            [NotNull]string requiredTypeName, [NotNull]string requiredMethodName, out string name)
+        {
+            name = null;
+            var baseClass = expression.GetContainingNode<IClassLikeDeclaration>();
+            if (baseClass == null)
+            {
+                return null;
+            }
+            var result = TryFindIdDeclarationInClassLikeDeclaration(baseClass, propertyName, requiredTypeName, requiredMethodName, out name);
+            if (result != null)
+                return result;
+            
+            var next = ClassLikeDeclarationNavigator.GetByNestedTypeDeclaration(baseClass);
+            while (next != null)
+            {
+                baseClass = next;
+                result = TryFindIdDeclarationInClassLikeDeclaration(baseClass, propertyName, requiredTypeName, requiredMethodName, out name);
+                if (result != null)
+                    return result;
+                
+                next = ClassLikeDeclarationNavigator.GetByNestedTypeDeclaration(baseClass);
+            }
+
+            return null;
+        } 
         
-        private static string GetUniqueName(IInvocationExpression invocationExpression, string baseName)
+        private static IClassLikeDeclaration GetTopLevelClassLikeDeclaration([NotNull]IInvocationExpression expression)
+        {
+            var baseClass = expression.GetContainingNode<IClassLikeDeclaration>();
+            if (baseClass == null)
+                return null;
+            
+            var next = ClassLikeDeclarationNavigator.GetByNestedTypeDeclaration(baseClass);
+            while (next != null)
+            {
+                baseClass = next;
+                next = ClassLikeDeclarationNavigator.GetByNestedTypeDeclaration(baseClass);
+            }
+
+            return baseClass;
+        }
+        
+        [NotNull]
+        private static string GetUniqueName( [NotNull]IInvocationExpression invocationExpression,  [NotNull]string baseName)
         {
             var namingManager = invocationExpression.GetPsiServices().Naming;
             var policyProvider = namingManager.Policy.GetPolicyProvider(invocationExpression.Language, invocationExpression.GetSourceFile());
@@ -123,32 +247,12 @@ namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Feature.Services.QuickFixes
             var namesSuggestion = namesCollection.Prepare(NamedElementKinds.PrivateStaticReadonly, ScopeKind.Common, suggestionOptions);
             return namesSuggestion.FirstName();
         }
-
-        private static IClassDeclaration GetTopLevelClass(IInvocationExpression expression)
-        {
-
-            var baseClass = expression.GetContainingNode<IClassDeclaration>();
-            if (baseClass == null)
-            {
-                return null;
-            }
-
-            var next = ClassDeclarationNavigator.GetByNestedTypeDeclaration(baseClass);
-            while (next!= null)
-            {
-                baseClass = next;
-                next = ClassDeclarationNavigator.GetByNestedTypeDeclaration(baseClass);
-            }
-
-            return baseClass;
-        }
-        
-        
-        public override string Text => "Use int overload";
+ 
+        public override string Text => "Use 'int' overload";
         
         public override bool IsAvailable(IUserDataHolder cache)
         {
-            return myLiteral != null && myArgument.IsValid();
+            return myInvocationExpression.IsValid() && myArgument.IsValid();
         }
     }
 }
--- resharper/test/src/CSharp/Intentions/QuickFixes/PreferAddressByIdToGraphicsParamsQuickFixTests.cs
+++ resharper/test/src/CSharp/Intentions/QuickFixes/PreferAddressByIdToGraphicsParamsQuickFixTests.cs
@@ -15,10 +15,17 @@ namespace JetBrains.ReSharper.Plugins.Unity.Tests.CSharp.Intentions.QuickFixes
         [Test] public void NewNameTest() { DoNamedTest(); }
         [Test] public void ReuseTest() { DoNamedTest(); }
         [Test] public void ReuseFailedCreateNewTest() { DoNamedTest(); }
+        [Test] public void ReuseConflictNameTest() { DoNamedTest(); }
         [Test] public void NestedClassTest() { DoNamedTest(); }
         [Test] public void WithoutUnityNamespaceTest() { DoNamedTest(); }
         [Test] public void InvalidLiteralForPropertyNameTest() { DoNamedTest(); }
         [Test] public void ShaderPropertyTest() { DoNamedTest(); }
         [Test] public void AnimatorPropertyTest() { DoNamedTest(); }
+        [Test] public void ConstantValueReuseTest() { DoNamedTest(); }
+        [Test] public void ConstantValueConcatReuseTest() { DoNamedTest(); }
+        [Test] public void PropertyReuseTest() { DoNamedTest(); }
+        [Test] public void StructTest() { DoNamedTest(); }
+        [Test] public void NestedReuseTest() { DoNamedTest(); }
+        [Test] public void ConstConcatCreateFieldTest() { DoNamedTest(); }
     }
 }
