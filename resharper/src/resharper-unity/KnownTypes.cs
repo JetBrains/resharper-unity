f8a5c483a679f2b279f708f98f6008cecdcaa57a
Vladimir Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2018-09-26 20:32:40 +0300

    Move analyzer to separated stage. Add custom highlightings and checkbox to disable analyzer to plugin options.

--- resharper/src/resharper-unity/KnownTypes.cs
+++ resharper/src/resharper-unity/KnownTypes.cs
@@ -23,6 +23,8 @@ namespace JetBrains.ReSharper.Plugins.Unity
         public static readonly IClrTypeName SerializeField = new ClrTypeName("UnityEngine.SerializeField");
         public static readonly IClrTypeName Physics = new ClrTypeName("UnityEngine.Physics");
         public static readonly IClrTypeName Physics2D = new ClrTypeName("UnityEngine.Physics2D");
+        public static readonly IClrTypeName Transform = new ClrTypeName("UnityEngine.Transform");
+        public static readonly IClrTypeName Resources = new ClrTypeName("UnityEngine.Resources");
         
         // UnityEngine.Networking
         public static readonly IClrTypeName NetworkBehaviour = new ClrTypeName("UnityEngine.Networking.NetworkBehaviour");
