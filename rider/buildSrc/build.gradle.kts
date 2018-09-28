f09bb05956a256111c2ee3239991f29827cc198b
Matt Ellis <m.t.ellis@gmail.com>
2018-09-28 13:08:54 +0100

    Move backend path logic to buildSrc

--- rider/buildSrc/build.gradle.kts
+++ rider/buildSrc/build.gradle.kts
@@ -1,8 +1,17 @@
-import org.jetbrains.kotlin.ir.backend.js.compile
 import java.net.URI
 
+// Note: The `buildscript` block affects the dependencies ONLY for the build scripts of the buildSrc projects
+// (i.e. buildSrc/build.gradle.kts et al)
+// Use top level `repositories` and `dependencies` for the buildSrc project itself. Also note that the dependencies of
+// the buildSrc project become dependencies for the root project, too
+
 repositories {
-    maven { url = URI("https://cache-redirector.jetbrains.com/maven-central") }
+    maven { setUrl("https://cache-redirector.jetbrains.com/maven-central") }
+    maven { setUrl("https://cache-redirector.jetbrains.com/plugins.gradle.org") }
+}
+
+dependencies {
+    "compile"("gradle.plugin.org.jetbrains.intellij.plugins", "gradle-intellij-plugin", "0.3.11")
 }
 
 plugins {
