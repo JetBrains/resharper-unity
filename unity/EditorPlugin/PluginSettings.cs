2e77a9c58d2a9bb08cd2c97eeaac21899bb442c6
Jurjen Biewenga <j.f.biewenga@gmail.com>
2018-10-02 19:49:15 +0200

    Added version info to preferences

--- unity/EditorPlugin/PluginSettings.cs
+++ unity/EditorPlugin/PluginSettings.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using JetBrains.DataFlow;
 using JetBrains.Util;
 using JetBrains.Util.Logging;
@@ -318,7 +321,17 @@ namespace JetBrains.Rider.Unity.Editor
       var githubRepo = "https://github.com/JetBrains/resharper-unity";
       var caption = $"<color=#0000FF>{githubRepo}</color>";
       LinkButton(caption: caption, url: githubRepo);
-
+      
+      GUILayout.FlexibleSpace();
+      GUILayout.BeginHorizontal();
+      
+      GUILayout.FlexibleSpace();
+      Assembly assembly = Assembly.GetExecutingAssembly();
+      FileVersionInfo versionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+      GUILayout.Label("Plugin version: " + versionInfo.FileVersion, "PR DisabledLabel");
+      
+      GUILayout.EndHorizontal();
+      
       // left for testing purposes
 /*      if (GUILayout.Button("reset RiderInitializedOnce = false"))
       {
