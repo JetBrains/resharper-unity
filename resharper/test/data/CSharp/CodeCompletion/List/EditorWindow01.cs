9404660a2fef7cbdac14f5f90479eeb2af0b66f9
Jurjen Biewenga <j.f.biewenga@gmail.com>
2018-10-02 20:09:33 +0200

    Added comment for clarification of the test

--- resharper/test/data/CSharp/CodeCompletion/List/EditorWindow01.cs
+++ resharper/test/data/CSharp/CodeCompletion/List/EditorWindow01.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEditor;  // More types in scope
 
+// This class inherits both Awake from EditorWindow and ScriptableObject, only one should show up in the autocompletion
 public class A : EditorWindow
 {
     {caret}   
