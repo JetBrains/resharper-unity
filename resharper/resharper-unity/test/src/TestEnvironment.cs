eedf2c7b3da81c2d0c73d25ef1c1ccdc37d0061d
Matt Ellis <m.t.ellis@gmail.com>
2018-10-16 22:28:47 +0100

    Explicitly set test data location

--- resharper/resharper-unity/test/src/TestEnvironment.cs
+++ resharper/resharper-unity/test/src/TestEnvironment.cs
@@ -6,6 +6,15 @@ using NUnit.Framework;
 
 [assembly: RequiresSTA]
 
+// This attribute is marked obsolete but is still supported. Use is discouraged in preference to convention, but the
+// convention doesn't work for us. That convention is to walk up the tree from the executing assembly and look for a
+// relative path called "test/data". This doesn't work because our common "build" folder is one level above our
+// "test/data" folder, so it doesn't get found. We want to keep the common "build" folder, but allow multiple "modules"
+// with separate "test/data" folders. E.g. "resharper-unity" and "resharper-yaml"
+#pragma warning disable 618
+[assembly: TestDataPathBase("resharper-unity/test/data")]
+#pragma warning restore 618
+
 namespace JetBrains.ReSharper.Plugins.Unity.Tests
 {
     [ZoneDefinition]
