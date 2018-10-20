c3c881149049c58982e282269ecd6cbcc0314b33
Matt Ellis <m.t.ellis@gmail.com>
2018-10-20 14:15:59 +0100

    Fix test data path when building in Unity plugin

--- resharper/resharper-yaml/test/src/TestEnvironment.cs
+++ resharper/resharper-yaml/test/src/TestEnvironment.cs
@@ -6,6 +6,18 @@ using NUnit.Framework;
 
 [assembly: RequiresSTA]
 
+// This attribute is marked obsolete but is still supported. Use is discouraged in preference to convention, but the
+// convention doesn't work for us. That convention is to walk up the tree from the executing assembly and look for a
+// relative path called "test/data". This doesn't work because our common "build" folder is one level above our
+// "test/data" folder, so it doesn't get found. We want to keep the common "build" folder, but allow multiple "modules"
+// with separate "test/data" folders. E.g. "resharper-unity" and "resharper-yaml"
+
+// TODO: This makes things work when building as part of the Unity project, but breaks standalone
+// Maybe it should be using product/subplatform markers?
+#pragma warning disable 618
+[assembly: TestDataPathBase("resharper-yaml/test/data")]
+#pragma warning restore 618
+
 namespace JetBrains.ReSharper.Plugins.Yaml.Tests
 {
     [ZoneDefinition]
