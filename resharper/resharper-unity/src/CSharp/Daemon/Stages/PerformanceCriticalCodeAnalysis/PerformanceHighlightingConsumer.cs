20536aa30e15982f861a1cad9b1defbc4c35317e
Vladimir Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2018-11-02 21:23:23 +0300

    Support disable warnings in comment, move higlightings to LineMarkerAPI

---
+++ resharper/resharper-unity/src/CSharp/Daemon/Stages/PerformanceCriticalCodeAnalysis/PerformanceHighlightingConsumer.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using JetBrains.Application.Settings;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Feature.Services.Daemon;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.Tree;
+using JetBrains.Util;
+
+namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Stages.PerformanceCriticalCodeAnalysis
+{
+    public class PerformanceHighlightingConsumer : DefaultHighlightingConsumer
+    {
+        private IgnoreWarningsRegionsInfo myRegionsInfo;
+
+        public PerformanceHighlightingConsumer([NotNull] IPsiSourceFile sourceFile, [NotNull] IFile psiFile)
+            : base(sourceFile)
+        {
+            var solution = psiFile.GetSolution();
+            myRegionsInfo = solution.GetComponent<IRegionsInfoProvider>().GetOrCreateInfoForDocument(SourceFile.Document) as IgnoreWarningsRegionsInfo;
+        }
+
+        public override void ConsumeHighlighting(HighlightingInfo highlightingInfo)
+        {
+            var highlighting = highlightingInfo.Highlighting;
+            Assertion.Assert(highlighting is PerformanceCriticalCodeHighlightingBase, "highlightingInfo is PerformanceCriticalCodeHighlightingBase");
+
+            var performanceHighlighting = highlighting as PerformanceCriticalCodeHighlightingBase;
+            if (myRegionsInfo != null)
+            {
+                if (myRegionsInfo.ShouldIgnoreWarningHighlighting(performanceHighlighting.SeverityId, highlightingInfo, false, out var _))
+                    return;
+            }
+
+            ConsumeInfo(highlightingInfo);
+        }
+    }
+}
