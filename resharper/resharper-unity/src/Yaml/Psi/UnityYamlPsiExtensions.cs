ed299662f4fb190fac9f17ef045943041ac535e5
Matt Ellis <m.t.ellis@gmail.com>
2018-11-14 12:47:02 +0000

    Move PSI extension methods

--- resharper/resharper-unity/src/Yaml/Psi/UnityYamlPsiExtensions.cs
+++ resharper/resharper-unity/src/Yaml/Psi/UnityYamlPsiExtensions.cs
@@ -2,7 +2,7 @@ using JetBrains.Annotations;
 using JetBrains.ReSharper.Plugins.Yaml.Psi;
 using JetBrains.ReSharper.Plugins.Yaml.Psi.Tree;
 
-namespace JetBrains.ReSharper.Plugins.Unity.Yaml
+namespace JetBrains.ReSharper.Plugins.Unity.Yaml.Psi
 {
     // ReSharper disable InconsistentNaming
     public class FileID
