782823ab39f4d8be8795f0ce9ee8879419ccaa18
Matt Ellis <m.t.ellis@gmail.com>
2018-11-15 21:39:37 +0000

    Disable rename for Unity event handlers

---
+++ resharper/resharper-unity/src/CSharp/Feature/Services/Refactorings/Rename/UnityEventTargetAtomicRenameFactory.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using JetBrains.Application;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Feature.Services.Refactorings.Specific.Rename;
+using JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.Caches;
+using JetBrains.ReSharper.Psi;
+using JetBrains.Util;
+
+namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Feature.Services.Refactorings.Rename
+{
+    [ShellFeaturePart]
+    public class UnityEventTargetAtomicRenameFactory : IAtomicRenameFactory
+    {
+        public bool IsApplicable(IDeclaredElement declaredElement)
+        {
+            if (declaredElement is IMethod method)
+            {
+                var eventHandlerCache = declaredElement.GetSolution().GetComponent<UnityEventHandlerReferenceCache>();
+                return eventHandlerCache.IsEventHandler(method);
+            }
+
+            return false;
+        }
+
+        // Disable rename completely for Unity event handlers
+        public RenameAvailabilityCheckResult CheckRenameAvailability(IDeclaredElement element)
+        {
+            if (IsApplicable(element))
+                return RenameAvailabilityCheckResult.CanNotBeRenamed;
+
+            return RenameAvailabilityCheckResult.CanBeRenamed;
+        }
+
+        public IEnumerable<AtomicRenameBase> CreateAtomicRenames(IDeclaredElement declaredElement, string newName, bool doNotAddBindingConflicts)
+        {
+            return EmptyList<AtomicRenameBase>.Instance;
+        }
+    }
+}
