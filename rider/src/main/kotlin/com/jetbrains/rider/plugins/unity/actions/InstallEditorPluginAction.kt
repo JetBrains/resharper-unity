20f17ab61a3fd129b46ac23c5143410a00d79541
Ivan Shakhov <van800@gmail.com>
2018-11-29 11:53:19 +0100

    Icons and order of Actions in UnityShowImportantAction (#937)
    * reorder actions in UnityShowImportantAction
    
    * update svg icons, remove png icon

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/actions/InstallEditorPluginAction.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/actions/InstallEditorPluginAction.kt
@@ -1,13 +1,11 @@
 package com.jetbrains.rider.plugins.unity.actions
 
-import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareAction
 import com.jetbrains.rider.model.rdUnityModel
-import com.jetbrains.rider.plugins.unity.util.UnityIcons
 import com.jetbrains.rider.projectView.solution
 
-open class InstallEditorPluginAction : DumbAwareAction("Install UnityEditor plugin", "Install/Update UnityEditor plugin.", UnityIcons.Actions.ImportantActions) {
+open class InstallEditorPluginAction : DumbAwareAction("Install UnityEditor plugin") {
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
         project.solution.rdUnityModel.installEditorPlugin.fire(Unit)
