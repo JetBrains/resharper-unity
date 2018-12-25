5562b4bcb3dc3be5d8f3eb657d4b1f0d584fa0c8
Ivan.Shakhov <van800@gmail.com>
2018-12-25 19:15:02 +0100

    improve sample test

--- rider/src/test/kotlin/UnityClassLibTest.kt
+++ rider/src/test/kotlin/UnityClassLibTest.kt
@@ -1,21 +1,59 @@
-import com.jetbrains.rider.test.TestCaseRunner
+import com.intellij.openapi.project.Project
 import com.jetbrains.rider.test.base.BaseTestWithSolutionBase
-import com.jetbrains.rider.test.base.RiderTemplatesTestCoreBase
-import com.jetbrains.rider.test.scriptingApi.checkSelectedRunConfigurationExecutionNotAllowed
-import com.jetbrains.rider.test.scriptingApi.checkSwea
+import com.jetbrains.rider.test.framework.closeProjectsWaitForBackendWillBeClosed
+import com.jetbrains.rider.test.framework.executeWithGold
+import com.jetbrains.rider.test.framework.persistAllFilesOnDisk
+import com.jetbrains.rider.test.scriptingApi.*
 import org.testng.annotations.Test
+import java.io.File
 
 class UnityClassLibTest : BaseTestWithSolutionBase() {
 
     var templateId = "JetBrains.Common.Unity.Library.CSharp"
+    private val editorGoldFile: File
+        get() = File(testCaseGoldDirectory, "${testMethod.name}_opened")
 
     @Test
     fun testXamarinFormsClassLibraryTemplate() {
         val projectName = "ClassLibrary"
+        val params = OpenSolutionParams()
+        params.restoreNuGetPackages = true //it's always true in getAndOpenSolution
+        params.waitForCaches = true
 
-//        doCoreTest(templateId, projectName) { project ->
-//            checkSwea(project)
-//            checkSelectedRunConfigurationExecutionNotAllowed(project)
-//        }
+        val newProject = getAndOpenSolution(templateId, true, params)
+
+        try {
+            executeWithGold(editorGoldFile) {
+                dumpOpenedDocument(it, newProject)
+            }
+
+            testProjectModel(testGoldFile, newProject) {
+                dump("Opened", newProject, activeSolutionDirectory, false, false) {} //contains close editors
+            }
+
+            checkSwea(newProject)
+
+        } finally {
+            closeSolutionAndResetSettings(newProject)
+        }
+    }
+
+    private fun getAndOpenSolution(
+            templateId: String,
+            sameDirectory: Boolean,
+            params: OpenSolutionParams
+    ): Project {
+        closeProjectsWaitForBackendWillBeClosed(60, false)
+        val newProject = createSolutionFromTemplate(templateId, null, activeSolutionDirectory, sameDirectory) { solutionFile ->
+
+        }!!
+
+        newProject.enableBackendAsserts()
+        persistAllFilesOnDisk(newProject)
+
+        waitForSolution(newProject, params)
+        assertCurrentSolutionToolset()
+
+        return newProject
     }
 }
