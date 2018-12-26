e8913fde95d132b6bacc46c457c96b352bb6df4a
Vladimir Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2018-12-26 14:28:03 +0300

    Handle disabled YamlSupport

--- resharper/resharper-unity/src/Yaml/UnityYamlSupport.cs
+++ resharper/resharper-unity/src/Yaml/UnityYamlSupport.cs
@@ -3,6 +3,7 @@ using JetBrains.DataFlow;
 using JetBrains.ProjectModel;
 using JetBrains.ProjectModel.DataContext;
 using JetBrains.ReSharper.Plugins.Unity.Settings;
+using JetBrains.ReSharper.Plugins.Yaml.Settings;
 
 namespace JetBrains.ReSharper.Plugins.Unity.Yaml
 {
@@ -10,11 +11,23 @@ namespace JetBrains.ReSharper.Plugins.Unity.Yaml
     public class UnityYamlSupport
     {
         public readonly IProperty<bool> IsYamlParsingEnabled;
-        public UnityYamlSupport(Lifetime lifetime, ISolution solution, ISettingsStore settingsStore)
+        public UnityYamlSupport(Lifetime lifetime, YamlSupport support, ISolution solution, ISettingsStore settingsStore)
         {
             var settings = settingsStore.BindToContextLive(lifetime,
                 ContextRange.ManuallyRestrictWritesToOneContext(solution.ToDataContext()));
             IsYamlParsingEnabled = settings.GetValueProperty(lifetime, (UnitySettings key) => key.IsYamlParsingEnabled);
+            
+            
+            if (!support.IsParsingEnabled.Value)
+                IsYamlParsingEnabled.Value = false;
+
+            IsYamlParsingEnabled.Change.Advise(lifetime, v =>
+            {
+                if (v.HasNew && v.New)
+                {
+                    support.IsParsingEnabled.Value = true;
+                }
+            });
         }
     }
 }
