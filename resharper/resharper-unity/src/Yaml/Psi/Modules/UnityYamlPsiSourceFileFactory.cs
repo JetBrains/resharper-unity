c9702b9a943d42bfcd0b4f1dac076d855b567001
Matt Ellis <m.t.ellis@gmail.com>
2019-01-08 10:54:49 +0000

    Add cache to exclude binary files from PSI

--- resharper/resharper-unity/src/Yaml/Psi/Modules/UnityYamlPsiSourceFileFactory.cs
+++ resharper/resharper-unity/src/Yaml/Psi/Modules/UnityYamlPsiSourceFileFactory.cs
@@ -1,6 +1,8 @@
+using System;
 using JetBrains.DocumentManagers;
 using JetBrains.Metadata.Reader.API;
 using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.Caches;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.Modules;
 using JetBrains.ReSharper.Psi.Modules.ExternalFileModules;
@@ -13,8 +15,8 @@ namespace JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.Modules
     {
         private readonly IProjectFileExtensions myProjectFileExtensions;
         private readonly PsiProjectFileTypeCoordinator myProjectFileTypeCoordinator;
+        private readonly UnityYamlSupport myUnityYamlSupport;
         private readonly DocumentManager myDocumentManager;
-        private readonly IPsiSourceFileProperties myPsiSourceFileProperties;
 
         public UnityYamlPsiSourceFileFactory(IProjectFileExtensions projectFileExtensions,
                                              PsiProjectFileTypeCoordinator projectFileTypeCoordinator,
@@ -23,25 +25,36 @@ namespace JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.Modules
         {
             myProjectFileExtensions = projectFileExtensions;
             myProjectFileTypeCoordinator = projectFileTypeCoordinator;
+            myUnityYamlSupport = unityYamlSupport;
             myDocumentManager = documentManager;
-
-            myPsiSourceFileProperties = new UnityExternalFileProperties(unityYamlSupport.IsUnityYamlParsingEnabled);
         }
 
         public IPsiProjectFile CreatePsiProjectFile(IPsiModule psiModule, IProjectFile projectFile)
         {
             return new UnityYamlAssetPsiSourceFile(projectFile, myProjectFileExtensions, myProjectFileTypeCoordinator,
-                psiModule, projectFile.Location, PropertiesFactory, myDocumentManager,
-                UniversalModuleReferenceContext.Instance);
+                psiModule, projectFile.Location, Memoize(PropertiesFactory), myDocumentManager, UniversalModuleReferenceContext.Instance);
         }
 
         public IExternalPsiSourceFile CreateExternalPsiSourceFile(IPsiModule psiModule, FileSystemPath path)
         {
             return new UnityYamlExternalPsiSourceFile(myProjectFileExtensions, myProjectFileTypeCoordinator, psiModule,
-                path, PropertiesFactory, myDocumentManager, UniversalModuleReferenceContext.Instance);
+                path, Memoize(PropertiesFactory), myDocumentManager, UniversalModuleReferenceContext.Instance);
         }
 
-        private IPsiSourceFileProperties PropertiesFactory(PsiSourceFileFromPath psiSourceFile) =>
-            myPsiSourceFileProperties;
+        // The PropertiesFactory passed to PsiSourceFileFromPath is called on EVERY access to IPsiSourceFile.Properties.
+        // This function allows us to create a single instance for each file. The cache variable (as well as the func
+        // parameter) are captured into a closure class. When the closure's is invoked, we can populate and return the
+        // cached value
+        private static Func<IPsiSourceFile, IPsiSourceFileProperties> Memoize(Func<IPsiSourceFile, IPsiSourceFileProperties> func)
+        {
+            IPsiSourceFileProperties cache = null;
+            return sf => cache ?? (cache = func(sf));
+        }
+
+        private IPsiSourceFileProperties PropertiesFactory(IPsiSourceFile psiSourceFile)
+        {
+            var binaryUnityFileCache = psiSourceFile.GetSolution().GetComponent<BinaryUnityFileCache>();
+            return new UnityExternalFileProperties(psiSourceFile, myUnityYamlSupport, binaryUnityFileCache);
+        }
     }
 }
