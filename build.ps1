a438b70afc1cecaffce44c985cbc72d731fc81a6
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2019-01-29 14:50:52 +0100

    run tests

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@ param (
   [string]$Source, # Rider SDK Packages folder, optional
   [string]$BuildCounter = 9999, # Sets Rider plugin version to version from Packaging.Props with the last zero replaced by $BuildCounter
   [string]$Configuration = "Debug", # Release / Debug
-  [switch]$RunTests = $true,
+  [switch]$RunTests,
   [switch]$Verbose
 )
 
