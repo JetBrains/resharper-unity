a287da6bdcde952ead9d96e0e1f917594355f4db
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2019-02-06 13:18:28 +0100

    refactoring

---
+++ unity/EditorPlugin/AfterUnity56/EntryPoint.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using JetBrains.Util.Logging;
+using UnityEditor;
+
+namespace JetBrains.Rider.Unity.Editor.AfterUnity56
+{
+  [InitializeOnLoad]
+  public static class EntryPoint
+  {
+    private static readonly ILog ourLogger = Log.GetLog("AfterUnity56.EntryPoint");
+    
+    static EntryPoint()
+    {
+      if (!PluginEntryPoint.Enabled)
+        return;
+      ourLogger.Verbose("AfterUnity56.EntryPoint");
+
+      PluginEntryPoint.OnModelInitialization += UnitTesting.Initialization.OnModelInitializationHandler;
+      PluginEntryPoint.OnModelInitialization += Navigation.Initialization.OnModelInitializationHandler;
+      AppDomain.CurrentDomain.DomainUnload += (EventHandler) ((_, __) =>
+      {
+        PluginEntryPoint.OnModelInitialization -= UnitTesting.Initialization.OnModelInitializationHandler;
+        PluginEntryPoint.OnModelInitialization -= Navigation.Initialization.OnModelInitializationHandler;
+      });
+    }
+  }
+}
