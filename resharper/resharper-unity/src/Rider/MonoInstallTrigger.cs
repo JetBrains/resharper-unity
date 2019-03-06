21d00209613a72a011091db33c203c7e1f42fac5
Ivan.Shakhov <van800@gmail.com>
2019-03-06 08:45:51 +0100

    move MonoInstallTrigger to background task

--- resharper/resharper-unity/src/Rider/MonoInstallTrigger.cs
+++ resharper/resharper-unity/src/Rider/MonoInstallTrigger.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Linq;
 using JetBrains.Application.Threading;
-using JetBrains.Collections.Viewable;
 using JetBrains.Lifetimes;
 using JetBrains.ProjectModel;
+using JetBrains.ProjectModel.Tasks;
 using JetBrains.ReSharper.Host.Features.Runtime;
 using JetBrains.ReSharper.Plugins.Unity.ProjectModel;
 using JetBrains.Util;
@@ -13,58 +13,65 @@ namespace JetBrains.ReSharper.Plugins.Unity.Rider
     [SolutionComponent]
     public class MonoInstallTrigger
     {
-        public MonoInstallTrigger(Lifetime lifetime, ILogger logger, UnityHost host,
-            ISolution solution,
-            UnitySolutionTracker unitySolutionTracker)
+        public MonoInstallTrigger(Lifetime lifetime, ILogger logger, ISolutionLoadTasksScheduler scheduler,
+            ISolution solution, UnitySolutionTracker unitySolutionTracker, UnityHost host)
         {
-            unitySolutionTracker.IsUnityGeneratedProject.WhenTrue(lifetime,
-                (lt =>
+            if (PlatformUtil.RuntimePlatform != PlatformUtil.Platform.MacOsX)
+                return;
+
+            scheduler.EnqueueTask(new SolutionLoadTask("Check mono runtime", SolutionLoadTaskKinds.AfterDone, () =>
+            {
+                if (!unitySolutionTracker.IsUnityGeneratedProject.Value)
+                    return;
+
+                if (!HasModernUnityProjects(solution))
+                    return;
+
+                solution.Locks.Tasks.Queue(lifetime, () =>
                 {
-                    if (PlatformUtil.RuntimePlatform != PlatformUtil.Platform.MacOsX)
-                        return;
-
-                    bool shouldShow;
-
-                    using (solution.Locks.UsingReadLock())
-                    {
-                        shouldShow = solution.GetAllProjects()
-                            .Any(a => a.TargetFrameworkIds.Any(x => x.Version.Major >= 4 && x.Version.Minor >= 7)
-//                                        && a.TargetFrameworkIds.Any(t =>
-//                                            (a.ProjectProperties.ActiveConfigurations.TryGetConfiguration(t) as
-//                                                ICSharpProjectConfiguration)
-//                                            .IfNotNull(s => s.LanguageVersion < CSharpLanguageVersion.CSharp73))
-                            );
-
-                    }
-
-                    if (!shouldShow) return;
-
                     var wellKnownMonoRuntimes = MonoRuntimeDetector.DetectWellKnownMonoRuntimes();
-                    if (!wellKnownMonoRuntimes.Any())
-                    {
-                        host.PerformModelAction(model => model.ShowInstallMonoDialog());
-                        return;
-                    }
-
-                    var installed =
-                        wellKnownMonoRuntimes
-                            .Any(runtime =>
+                    var installedValidMono = wellKnownMonoRuntimes
+                        .Any(runtime =>
+                        {
+                            var version = new Version(0, 0); // if we fail to parse version - consider it is old
+                            try
                             {
-                                var version = new Version(0, 0); // if we fail to parse version - consider it is old
-                                try
-                                {
-                                    version = new Version(ProcessOutputUtil.ExtractMonoVersion(runtime.ExePath));
-                                }
-                                catch (Exception e)
-                                {
-                                    logger.Warn(e);
-                                }
-                                return version.Major>=5 && version.Minor>=16;
-                            });
-                            
-                    if (!installed)
-                        host.PerformModelAction(model => model.ShowInstallMonoDialog());
-                }));
+                                version = new Version(ProcessOutputUtil.ExtractMonoVersion(runtime.ExePath));
+                            }
+                            catch (Exception e)
+                            {
+                                logger.Warn(e);
+                            }
+
+                            return version.Major >= 5 && version.Minor >= 16;
+                        });
+
+                    if (!installedValidMono)
+                    {
+                        solution.Locks.ExecuteOrQueue(lifetime, "Show install mono dialog",
+                            () => { host.PerformModelAction(model => model.ShowInstallMonoDialog()); });
+                    }
+                });
+            }));
+        }
+    
+        private static bool HasModernUnityProjects(ISolution solution)
+        {
+            using (solution.Locks.UsingReadLock())
+            {
+                foreach (var project in solution.GetAllProjects())
+                {
+                    var isNetFramework47 = project.TargetFrameworkIds
+                        .Any(x => x.IsNetFramework && x.Version.Major >= 4 && x.Version.Minor >= 7);
+                
+                    if (isNetFramework47)
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
         }
     }
 }
