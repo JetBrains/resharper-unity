c792646429575b7ceeebff910868d6412a510e3e
Vladimir Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2019-03-14 18:32:21 +0300

    Show usages in Unity is cancelable

--- resharper/resharper-unity/src/Rider/UnityEditorFindUsageResultCreator.cs
+++ resharper/resharper-unity/src/Rider/UnityEditorFindUsageResultCreator.cs
@@ -1,18 +1,17 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using JetBrains.Annotations;
 using JetBrains.Application.Progress;
 using JetBrains.Application.Threading;
 using JetBrains.Application.Threading.Tasks;
 using JetBrains.Lifetimes;
 using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Host.Features.BackgroundTasks;
 using JetBrains.ReSharper.Plugins.Unity.Yaml.Psi;
 using JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.Modules;
 using JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.Resolve;
 using JetBrains.ReSharper.Plugins.Yaml.Psi.Tree;
 using JetBrains.ReSharper.Psi;
-using JetBrains.ReSharper.Psi.Files;
 using JetBrains.ReSharper.Psi.Impl.Search.Operations;
 using JetBrains.ReSharper.Psi.Search;
 using JetBrains.ReSharper.Resources.Shell;
@@ -27,23 +26,23 @@ namespace JetBrains.ReSharper.Plugins.Unity.Rider
     {
         private readonly Lifetime myLifetime;
         private readonly ISolution mySolution;
-        private readonly SearchDomainFactory mySearchDomainFactory;
+        private readonly ISearchDomain myYamlSearchDomain;
         private readonly IShellLocks myLocks;
+        private readonly RiderBackgroundTaskHost myBackgroundTaskHost;
         private readonly UnitySceneProcessor mySceneProcessor;
         private readonly UnityHost myUnityHost;
-        private readonly UnityExternalFilesPsiModule myModule;
         private readonly FileSystemPath mySolutionDirectoryPath;
 
         public UnityEditorFindUsageResultCreator(Lifetime lifetime, ISolution solution, SearchDomainFactory searchDomainFactory, IShellLocks locks, 
-            UnitySceneProcessor sceneProcessor, UnityHost unityHost, UnityExternalFilesModuleFactory externalFilesModuleFactory)
+            RiderBackgroundTaskHost backgroundTaskHost, UnitySceneProcessor sceneProcessor, UnityHost unityHost, UnityExternalFilesModuleFactory externalFilesModuleFactory)
         {
             myLifetime = lifetime;
             mySolution = solution;
-            mySearchDomainFactory = searchDomainFactory;
             myLocks = locks;
+            myBackgroundTaskHost = backgroundTaskHost;
             mySceneProcessor = sceneProcessor;
+            myYamlSearchDomain = searchDomainFactory.CreateSearchDomain(externalFilesModuleFactory.PsiModule);
             myUnityHost = unityHost;
-            myModule = externalFilesModuleFactory.PsiModule;
             mySolutionDirectoryPath = solution.SolutionDirectory;
         }
 
@@ -65,15 +64,28 @@ namespace JetBrains.ReSharper.Plugins.Unity.Rider
                 ? null
                 : CreateRequest(mySolutionDirectoryPath, mySceneProcessor, selectedReference, null);
 
+            var lifetimeDef = myLifetime.CreateNested();
+            var pi = new ProgressIndicator(myLifetime);
+            
+            var task = RiderBackgroundTaskBuilder.Create()
+                .WithTitle("Finding usages in Unity for: " + declaredElement.ShortName)
+                .AsIndeterminate()
+                .AsCancelable(() =>
+                {
+                    pi.Cancel();
+                })
+                .Build();
+
+            myBackgroundTaskHost.AddNewTask(lifetimeDef.Lifetime, task);
+            
             myLocks.Tasks.StartNew(myLifetime, Scheduling.MainGuard, () =>
             {
                 using (ReadLockCookie.Create())
                 {
-
-                    finder.FindAsync(new[] {declaredElement}, declaredElement.GetSearchDomain(),
-                        consumer, SearchPattern.FIND_USAGES ,new ProgressIndicator(myLifetime),
-                        FinderSearchRoot.Empty, new UnityUsagesAsyncFinderCallback(consumer, myUnityHost, declaredElement.ShortName,
-                            selectRequest, focusUnity));
+                    finder.FindAsync(new[] {declaredElement}, myYamlSearchDomain,
+                        consumer, SearchPattern.FIND_USAGES ,pi,
+                        FinderSearchRoot.Empty, new UnityUsagesAsyncFinderCallback(lifetimeDef, consumer, myUnityHost, myLocks,
+                            declaredElement.ShortName, selectRequest, focusUnity));
                 }
             });
         }
@@ -117,7 +129,7 @@ namespace JetBrains.ReSharper.Plugins.Unity.Rider
 
             extension = path.ExtensionWithDot;
             fileName = path.NameWithoutExtension;
-            filePath =  String.Join("/", pathComponents);
+            filePath =  String.Join("/", pathComponents.Select(t => t.ToString()));
 
             return true;
         }
@@ -127,6 +139,8 @@ namespace JetBrains.ReSharper.Plugins.Unity.Rider
             private readonly UnitySceneProcessor mySceneProcessor;
             private readonly FileSystemPath mySolutionDirectoryPath;
             private readonly IUnityYamlReference mySelectedReference;
+            private FindExecution myFindExecution = FindExecution.Continue;
+            
             public List<FindUsageResultElement> Result = new List<FindUsageResultElement>();
 
             public UnityUsagesFinderConsumer(UnitySceneProcessor sceneProcessor, FileSystemPath solutionDirectoryPath, IUnityYamlReference selectedReference)
@@ -138,7 +152,7 @@ namespace JetBrains.ReSharper.Plugins.Unity.Rider
             
             public IUnityYamlReference Build(FindResult result)
             {
-                return null;
+                return (result as FindResultReference)?.Reference as IUnityYamlReference;
             }
 
             public FindExecution Merge(IUnityYamlReference data)
@@ -147,48 +161,52 @@ namespace JetBrains.ReSharper.Plugins.Unity.Rider
                 if (request != null)
                     Result.Add(request);
                 
-                return FindExecution.Continue;
+                return myFindExecution;
             }
+
         }
         
+
         private class UnityChildPathSceneConsumer : IUnitySceneProcessorConsumer
         {
             public readonly List<int> RootIndices = new List<int>();
             
             public void ConsumeGameObject(IYamlDocument gameObject, IBlockMappingNode modifications)
             {
+                int rootOrder = -1;
+                var transform = UnityObjectPsiUtil.FindTransformComponentForGameObject(gameObject);
+                if (modifications != null)
                 {
-                    int rootOrder = -1;
-                    var transform = UnityObjectPsiUtil.FindTransformComponentForGameObject(gameObject);
-                    if (modifications != null)
-                    {
-                        if (!int.TryParse(UnityObjectPsiUtil.GetValueFromModifications(modifications, transform.GetFileId(), UnityYamlConstants.RootOrderProperty)
-                            , out rootOrder))
-                            rootOrder = -1;
-                    }
-                    if (rootOrder == -1)
-                    {
-                        var rootOrderAsString = transform.GetUnityObjectPropertyValue(UnityYamlConstants.RootOrderProperty).AsString();
-                        if (!int.TryParse(rootOrderAsString, out rootOrder))
-                            rootOrder = -1;
-                    }
-                    RootIndices.Add(rootOrder);
+                    if (!int.TryParse(UnityObjectPsiUtil.GetValueFromModifications(modifications, transform.GetFileId(), UnityYamlConstants.RootOrderProperty), out rootOrder))
+                        rootOrder = -1;
                 }
+                if (rootOrder == -1)
+                {
+                    var rootOrderAsString = transform.GetUnityObjectPropertyValue(UnityYamlConstants.RootOrderProperty).AsString();
+                    if (!int.TryParse(rootOrderAsString, out rootOrder))
+                        rootOrder = -1;
+                }
+                RootIndices.Add(rootOrder);
             }
         }
-
+        
         private class UnityUsagesAsyncFinderCallback : IFinderAsyncCallback
         {
+            private readonly LifetimeDefinition myLifetimeDef;
             private readonly UnityUsagesFinderConsumer myConsumer;
             private readonly UnityHost myUnityHost;
+            private readonly IShellLocks myShellLocks;
             private readonly string myDisplayName;
             private readonly FindUsageResultElement mySelected;
             private readonly bool myFocusUnity;
 
-            public UnityUsagesAsyncFinderCallback(UnityUsagesFinderConsumer consumer, UnityHost unityHost, string displayName, FindUsageResultElement selected, bool focusUnity)
+            public UnityUsagesAsyncFinderCallback(LifetimeDefinition lifetimeDef, UnityUsagesFinderConsumer consumer, UnityHost unityHost, IShellLocks shellLocks, 
+                string displayName, FindUsageResultElement selected, bool focusUnity)
             {
+                myLifetimeDef = lifetimeDef;
                 myConsumer = consumer;
                 myUnityHost = unityHost;
+                myShellLocks = shellLocks;
                 myDisplayName = displayName;
                 mySelected = selected;
                 myFocusUnity = focusUnity;
@@ -196,16 +214,27 @@ namespace JetBrains.ReSharper.Plugins.Unity.Rider
 
             public void Complete()
             {
-                if (myFocusUnity)
-                    UnityFocusUtil.FocusUnity(myUnityHost.GetValue(t => t.UnityProcessId.Value));
-            
-                if (mySelected != null)
-                    myUnityHost.PerformModelAction(t => t.ShowGameObjectOnScene.Fire(mySelected));
-                myUnityHost.PerformModelAction(t => t.FindUsageResults.Fire(new FindUsageResult(myDisplayName, myConsumer.Result.ToArray())));
+                if (myConsumer.Result.Count == 0)
+                    return;
+                
+                myShellLocks.Tasks.StartNew(myLifetimeDef.Lifetime, Scheduling.MainGuard, () =>
+                {
+                    if (myFocusUnity)
+                        UnityFocusUtil.FocusUnity(myUnityHost.GetValue(t => t.UnityProcessId.Value));
+
+                    if (mySelected != null)
+                        myUnityHost.PerformModelAction(t => t.ShowGameObjectOnScene.Fire(mySelected));
+                    myUnityHost.PerformModelAction(t =>
+                        t.FindUsageResults.Fire(new FindUsageResult(myDisplayName, myConsumer.Result.ToArray())));
+                    
+                    myLifetimeDef.Terminate();
+                });
+
             }
 
             public void Error(string message)
             {
+                myLifetimeDef.Terminate();
             }
         }
     }
