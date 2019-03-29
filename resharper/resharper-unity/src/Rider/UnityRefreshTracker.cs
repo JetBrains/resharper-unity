6bdce747233ecdfd81372900801cacad33959503
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2019-03-29 13:22:11 +0100

    Revert "fix RIDER-26176"
    This reverts commit 5a67674d14235159d87581780e0757d025f2c447.

--- resharper/resharper-unity/src/Rider/UnityRefreshTracker.cs
+++ resharper/resharper-unity/src/Rider/UnityRefreshTracker.cs
@@ -55,7 +55,7 @@ namespace JetBrains.ReSharper.Plugins.Unity.Rider
             if (CurrentTask != null)
                 return CurrentTask;
 
-            if (myEditorProtocol.UnityModel.Value == null || myEditorProtocol.UnityModel.Value.IsBound)
+            if (myEditorProtocol.UnityModel.Value == null)
                 return new Task(()=>{});
 
             if (!myBoundSettingsStore.GetValue((UnitySettings s) => s.AllowAutomaticRefreshInUnity) && force == RefreshType.Normal)
