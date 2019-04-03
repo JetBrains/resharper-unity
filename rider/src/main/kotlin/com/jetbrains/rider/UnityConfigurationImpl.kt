df35ff700f62d8dd72dc308062eea2fedec4110d
Matt Ellis <m.t.ellis@gmail.com>
2019-04-03 09:39:46 +0100

    Exclude non-standard project folders from index

--- rider/src/main/kotlin/com/jetbrains/rider/UnityConfigurationImpl.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/UnityConfigurationImpl.kt
@@ -1,33 +1,74 @@
 package com.jetbrains.rider
 
+import com.intellij.openapi.components.ProjectComponent
 import com.intellij.openapi.project.Project
-import com.jetbrains.rider.projectView.indexing.contentModel.ContentModelExcludes
+import com.intellij.openapi.vfs.*
 import com.jetbrains.rider.projectView.indexing.contentModel.ContentModelUserStore
+import com.jetbrains.rider.projectView.indexing.contentModel.tryExclude
 import com.jetbrains.rider.projectView.indexing.contentModel.tryInclude
 import java.io.File
 
-class UnityConfigurationImpl(private val project: Project, unityProjectDiscoverer: UnityProjectDiscoverer,
-                             excludes: ContentModelExcludes, userStore: ContentModelUserStore) {
+class UnityConfigurationImpl(private val project: Project,
+                             private val unityProjectDiscoverer: UnityProjectDiscoverer,
+                             private val contentModel: ContentModelUserStore)
+    : ProjectComponent {
 
-    companion object {
-        private val ignoredDirectories = arrayOf("Library", "Temp")
-    }
-
-    init {
+    override fun projectOpened() {
         if (unityProjectDiscoverer.isUnityProject) {
-            val excludePaths = ignoredDirectories
-                    .mapNotNull(::getChildAsFile)
-                    .toHashSet()
-            excludes.updateExcludes(this, excludePaths)
 
-            getChildAsFile("Packages")?.let { userStore.tryInclude(arrayOf(it)) }
+            // It's common practice to have extra folders in the root project folder, e.g. a backup copy of Library for
+            // a different version of Unity, or target, which can be renamed instead of having to do a time consuming
+            // reimport. Also, folders containing builds for targets such as iOS.
+            // Exclude all folders apart from Assets, ProjectSettings and Packages
+            val baseDir = project.projectDir
+            val excludes = mutableListOf<File>()
+            val includes = mutableListOf<File>()
+            for (child in baseDir.children) {
+                if (isRootFolder(child)) {
+                    val file = VfsUtil.virtualToIoFile(child)
+                    if (child.name.equals("Assets", true)
+                            || child.name.equals("ProjectSettings", true)
+                            || child.name.equals("Packages", true)) {
+                        includes.add(file)
+                    }
+                    else {
+                        excludes.add(file)
+                    }
+                }
+            }
+
+            // TODO: Include all referenced package folders from various cache folders
+
+            contentModel.tryInclude(includes.toTypedArray(), false)
+            contentModel.tryExclude(excludes.toTypedArray(), true)
+
+            val listener = FileListener(contentModel)
+            VirtualFileManager.getInstance().addVirtualFileListener(listener, project)
         }
     }
 
-    private fun getChildAsFile(directoryName: String) : File?
-    {
-        val libraryFolder = project.projectDir.findChild(directoryName)
-        val path = libraryFolder?.canonicalPath ?: return null
-        return File(path)
+    private fun isRootFolder(file: VirtualFile?): Boolean {
+        return file != null && file.isDirectory && file.parent == project.projectDir
+    }
+
+    private inner class FileListener(private val contentModel: ContentModelUserStore)
+        : VirtualFileListener {
+
+        override fun fileCreated(event: VirtualFileEvent) {
+            // TODO: How to keep folders that are part of a project?
+            if (isRootFolder(event.file)) {
+                val file = VfsUtil.virtualToIoFile(event.file)
+                contentModel.tryExclude(arrayOf(file), true)
+            }
+        }
+
+        override fun fileDeleted(event: VirtualFileEvent) {
+            // TODO: Is this right?
+            if (isRootFolder(event.file)) {
+                val file = VfsUtil.virtualToIoFile(event.file)
+                contentModel.explicitExcludes.remove(file)
+                contentModel.update()
+            }
+        }
     }
 }
