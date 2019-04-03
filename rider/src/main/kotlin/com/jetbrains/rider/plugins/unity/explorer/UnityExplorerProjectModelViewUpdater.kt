353b495a6dac2d76f3229514dca5e8dbcf2e7ef3
Matt Ellis <m.t.ellis@gmail.com>
2019-04-03 12:21:19 +0100

    Refactor PackageManager to be more self contained

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/explorer/UnityExplorerProjectModelViewUpdater.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/explorer/UnityExplorerProjectModelViewUpdater.kt
@@ -6,14 +6,30 @@ import com.intellij.openapi.vfs.*
 import com.intellij.ui.tree.TreeVisitor
 import com.jetbrains.rider.model.RdProjectDescriptor
 import com.jetbrains.rider.model.RdSolutionDescriptor
+import com.jetbrains.rider.plugins.unity.packageManager.PackageManager
+import com.jetbrains.rider.plugins.unity.packageManager.PackageManagerListener
 import com.jetbrains.rider.projectView.ProjectModelViewUpdater
 import com.jetbrains.rider.projectView.nodes.ProjectModelNode
 import com.jetbrains.rider.projectView.views.SolutionViewVisitor
 
 class UnityExplorerProjectModelViewUpdater(project: Project) : ProjectModelViewUpdater(project) {
-    private var cachePane : UnityExplorer? = null
-    private val pane get() = cachePane ?: UnityExplorer.tryGetInstance(project).apply {
-        cachePane = this
+
+    private val pane: UnityExplorer? by lazy { UnityExplorer.tryGetInstance(project) }
+
+    init {
+        val packageManager = PackageManager.getInstance(project)
+        packageManager.addListener(object : PackageManagerListener {
+            override fun onRefresh(all: Boolean) {
+                // If the pane has a PackagesRoot node, but there's no Packages folder, or vice versa, update the entire
+                // model, to make sure the Packages node is added/removed. Else, just update the PackagesRoot node
+                if (all) {
+                    updateAll()
+                }
+                else {
+                    updatePackagesRoot()
+                }
+            }
+        })
     }
 
     override fun update(node: ProjectModelNode?) {
@@ -57,5 +73,19 @@ class UnityExplorerProjectModelViewUpdater(project: Project) : ProjectModelViewU
                 }
             }, false, false)
         }
+    }
+
+    private fun updatePackagesRoot() {
+        // Refresh the Packages root node, but no further. This causes the tree model for Packages to refresh, which
+        // updates the entire branch. We skip all other children (Assets, Scratches)
+        // Note that refresh will never refresh the root node, only its children
+        pane?.refresh(object : SolutionViewVisitor() {
+            override fun visit(node: AbstractTreeNode<*>): TreeVisitor.Action {
+                if (node is PackagesRoot) {
+                    return TreeVisitor.Action.INTERRUPT
+                }
+                return TreeVisitor.Action.SKIP_CHILDREN
+            }
+        }, false, true)
     }
 }
