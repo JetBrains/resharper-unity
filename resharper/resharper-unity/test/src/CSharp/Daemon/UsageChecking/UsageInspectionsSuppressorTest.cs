c3d269756cbe0b1ba10bc3e6d7656fbf1ed4f967
Jurjen Biewenga <j.f.biewenga@gmail.com>
2019-05-04 20:53:59 +0200

    Improved formatting

--- resharper/resharper-unity/test/src/CSharp/Daemon/UsageChecking/UsageInspectionsSuppressorTest.cs
+++ resharper/resharper-unity/test/src/CSharp/Daemon/UsageChecking/UsageInspectionsSuppressorTest.cs
@@ -16,7 +16,6 @@ namespace JetBrains.ReSharper.Plugins.Unity.Tests.CSharp.Daemon.UsageChecking
         [Test] public void MonoBehaviourMethods01() { DoNamedTest(); }
         [Test] public void MonoBehaviourFields01() { DoNamedTest(); }
         [Test] public void SerializableClassFields01() { DoNamedTest(); }
-
         [Test] public void PreprocessBuildInterface01() { DoNamedTest(); }
 
         protected override void DoTest(IProject project)
