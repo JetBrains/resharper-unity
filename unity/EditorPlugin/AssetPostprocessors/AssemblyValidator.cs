5c08945fd7de08f6893619e047a25bdc652a2a56
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2019-05-14 21:03:29 +0200

    simplify AssemblyValidator

--- unity/EditorPlugin/AssetPostprocessors/AssemblyValidator.cs
+++ unity/EditorPlugin/AssetPostprocessors/AssemblyValidator.cs
@@ -21,31 +21,25 @@ namespace JetBrains.Rider.Unity.Editor.AssetPostprocessors
       return true;
     }
 
-    private static ushort ReadUInt16(this Stream stream)
-    {
-      return (ushort) (stream.ReadByte() | stream.ReadByte() << 8);
-    }
-
-    private static uint ReadUInt32(this Stream stream)
-    {
-      return (uint) (stream.ReadByte() | stream.ReadByte() << 8 | stream.ReadByte() << 16 | stream.ReadByte() << 24);
-    }
-
     public static bool IsAssembly(string file)
     {
-      using (var fs = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read))
+      using (var fileStream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read))
       {
-        if (!fs.CanRead)
+        if (!fileStream.CanRead)
           throw new ArgumentException("Cannot read from stream.");
-        if (!fs.CanSeek)
+
+        if (!fileStream.CanSeek)
           throw new ArgumentException("Cannot seek in stream.");
 
-        if (fs.Length < 318L || fs.ReadUInt16() != 23117 ||
-            (!fs.Advance(58) || !fs.MoveTo(fs.ReadUInt32())) ||
-            (fs.ReadUInt32() != 17744U || !fs.Advance(20)) ||
-            !fs.Advance(fs.ReadUInt16() == (ushort) 523 ? 222 : 206))
-          return false;
-        return fs.ReadUInt32() > 0U;
+        using (var binaryReader = new BinaryReader(fileStream))
+        {
+          if (fileStream.Length < 318L || binaryReader.ReadUInt16() != 23117 ||
+              (!fileStream.Advance(58) || !fileStream.MoveTo(binaryReader.ReadUInt32())) ||
+              (binaryReader.ReadUInt32() != 17744U || !fileStream.Advance(20)) ||
+              !fileStream.Advance(binaryReader.ReadUInt16() == (ushort) 523 ? 222 : 206))
+            return false;
+          return binaryReader.ReadUInt32() > 0U;
+        }
       }
     }
   }
