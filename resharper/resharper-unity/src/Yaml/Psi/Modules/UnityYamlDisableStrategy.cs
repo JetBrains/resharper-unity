749830f05f2bba6ebfa3b3f03604213368175637
Vladimir Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2019-05-16 16:25:29 +0300

    Do not disable heuristic after first run

--- resharper/resharper-unity/src/Yaml/Psi/Modules/UnityYamlDisableStrategy.cs
+++ resharper/resharper-unity/src/Yaml/Psi/Modules/UnityYamlDisableStrategy.cs
@@ -47,9 +47,9 @@ namespace JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.Modules
         {
             if (myShouldRunHeuristic && myUnityYamlSupport.IsUnityYamlParsingEnabled.Value)
             {
-                mySolutionCaches.PersistentProperties[SolutionCachesId] = false.ToString();
                 if (IsAnyFilePreventYamlParsing(directoryEntries) || myTotalSize > TotalFileSizeThreshold)
                 {
+                    mySolutionCaches.PersistentProperties[SolutionCachesId] = false.ToString();
                     myUnityYamlSupport.IsUnityYamlParsingEnabled.Value = false;
                     NotifyYamlParsingDisabled();
                 }
