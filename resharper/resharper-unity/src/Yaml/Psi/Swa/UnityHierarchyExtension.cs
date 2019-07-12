48d95d29208133d069b195bc5cd181490b34614d
Vladimir Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2019-07-12 11:15:45 +0300

    Move from swa to caches

--- resharper/resharper-unity/src/Yaml/Psi/Swa/UnityHierarchyExtension.cs
+++ resharper/resharper-unity/src/Yaml/Psi/Swa/UnityHierarchyExtension.cs
@@ -1,79 +1,55 @@
 using System.Linq;
 using JetBrains.Collections;
-using JetBrains.Collections.Viewable;
 using JetBrains.Diagnostics;
+using JetBrains.Lifetimes;
 using JetBrains.ProjectModel;
-using JetBrains.ReSharper.Daemon.UsageChecking;
-using JetBrains.ReSharper.Daemon.UsageChecking.SwaExtension;
 using JetBrains.ReSharper.Plugins.Unity.ProjectModel;
+using JetBrains.ReSharper.Plugins.Unity.Yaml.ProjectModel;
 using JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.Caches;
+using JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.Caches.UnityEditorPropertyValues;
+using JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.Modules;
 using JetBrains.ReSharper.Plugins.Yaml.Psi.Tree;
-using JetBrains.ReSharper.Psi.Tree;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.Caches;
+using JetBrains.Util;
 using JetBrains.Util.Collections;
+using JetBrains.Util.PersistentMap;
 
 namespace JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.Swa
 {
     [SolutionComponent]
-    public class UnityHierarchyExtension : SwaExtensionProviderBase
+    public class UnityHierarchyExtension : SimpleICache<HierarchyDataElement>
     {
-        private readonly object myLockObject = new object();
-        
         private readonly MetaFileGuidCache myCache;
         private readonly UnityVersion myUnityVersion;
 
         public readonly OneToCompactCountingSet<FileID, IUnityHierarchyElement> Elements =
             new OneToCompactCountingSet<FileID, IUnityHierarchyElement>();
-        
-        public UnityHierarchyExtension(UnitySolutionTracker solutionTracker, MetaFileGuidCache cache, UnityVersion unityVersion) 
-            : base("UnityHierarchyInfo", solutionTracker.IsUnityProject.HasTrueValue())
+
+
+        public UnityHierarchyExtension(Lifetime lifetime, UnitySolutionTracker solutionTracker, MetaFileGuidCache cache,
+            UnityVersion unityVersion, IPersistentIndexManager persistentIndexManager)
+            : base(lifetime, persistentIndexManager, new UniversalMarshaller<HierarchyDataElement>(HierarchyDataElement.ReadDelegate, HierarchyDataElement.WriteDelegate))
         {
             myCache = cache;
             myUnityVersion = unityVersion;
         }
-
-        public override ISwaExtensionData CreateUsageDataElement(UsageData owner)
+        
+        protected override bool IsApplicable(IPsiSourceFile sourceFile)
         {
-            return new HierarchyDataElement(myCache, myUnityVersion);
+            return base.IsApplicable(sourceFile) &&
+                   sourceFile.LanguageType.Is<UnityYamlProjectFileType>() &&
+                   sourceFile.PsiModule is UnityExternalFilesPsiModule &&
+                   sourceFile.IsAsset();
+        }
+        
+        public override object Build(IPsiSourceFile sourceFile, bool isStartup)
+        {
+            var data = new HierarchyDataElement(myCache, myUnityVersion);
+            data.Build(sourceFile);
+            return data;
         }
 
-        public override void Merge(ISwaExtensionInfo old, ISwaExtensionInfo @new)
-        {
-            var oldData = old as HierarchyDataElement;
-            var newData = @new as HierarchyDataElement;
-
-            lock (myLockObject)
-            {
-                if (oldData != null)
-                {
-                    foreach (var (fileId, elements) in oldData.Elements)
-                    {
-                        foreach (var (element, count) in elements)
-                        {
-                            Elements.Remove(fileId, element, count);
-                        }
-                    }
-                }
-
-                if (newData != null)
-                {
-                    foreach (var (fileId, elements) in newData.Elements)
-                    {
-                        foreach (var (element, count) in elements)
-                        {
-                            Elements.Add(fileId, element, count);
-                        }
-                    }
-                }
-            }
-        }
-
-        public override void Clear()
-        {
-            lock (myLockObject)
-            {
-                Elements.Clear();
-            }
-        }
 
         public void ProcessSceneHierarchyFromComponentToRoot(IYamlDocument startComponent, IUnityCachedSceneProcessorConsumer consumer)
         {
@@ -82,61 +58,53 @@ namespace JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.Swa
             if (guid == null || anchor == null)
                 return;
             
-            ProcessSceneHierarchyFromComponentToRootUnderLock(new FileID(guid, anchor),  consumer);
+            ProcessSceneHierarchyFromComponentToRoot(new FileID(guid, anchor),  consumer);
         }
 
 
-        private void ProcessSceneHierarchyFromComponentToRootUnderLock(FileID id, IUnityCachedSceneProcessorConsumer consumer)
+        private void ProcessSceneHierarchyFromComponentToRoot(FileID id, IUnityCachedSceneProcessorConsumer consumer)
         {
-            lock (myLockObject)
+            var start = Elements.GetValues(id).FirstOrDefault();
+            if (start == null)
+                return;
+            if (!start.IsStripped) // start component can be stripped, e.g : prefab's MonoBehavior's function is passed to button event handler
             {
-                var start = Elements.GetValues(id).FirstOrDefault();
-                if (start == null)
-                    return;
-                if (!start.IsStripped) // start component can be stripped, e.g : prefab's MonoBehavior's function is passed to button event handler
-                {
 
-                    FileID gameObjectId;
-                    if (start is TransformHierarchyElement transformHierarchyElement)
-                    {
-                        gameObjectId = transformHierarchyElement.GameObject;
-                    } else if (start is ComponentHierarchyElement componentHierarchyElement)
-                    {
-                        gameObjectId = componentHierarchyElement.GameObject;
-                    }
-                    else
-                    {
-                        Assertion.Fail("Unexpected hierarchy element");
-                        return;
-                    }
-                    
-                    // Component must be attached to game object
-                    var gameObject = Elements.GetValues(gameObjectId).FirstOrDefault() as GameObjectHierarchyElement;
-                    if (gameObject == null)
-                        return;
-                
-                    // GameObject could be stripped, if another prefab's gameobject is modified via adding MonoBehaviour
-                    if (!gameObject.IsStripped)
-                    {
-                        // Each GameObject must have Transform. We will use it to process scene hierarcy
-                        ProcessSceneHierarchyFromComponentToRootInner(GetTransformComponent(gameObject), consumer, null);  
-                    } else 
-                    {
-                        ProcessSceneHierarchyFromComponentToRootInner(gameObject, consumer, null);
-                    }
+                FileID gameObjectId;
+                if (start is TransformHierarchyElement transformHierarchyElement)
+                {
+                    gameObjectId = transformHierarchyElement.GameObject;
+                } else if (start is ComponentHierarchyElement componentHierarchyElement)
+                {
+                    gameObjectId = componentHierarchyElement.GameObject;
                 }
                 else
                 {
-                    ProcessSceneHierarchyFromComponentToRootInner(start, consumer, null);
+                    Assertion.Fail("Unexpected hierarchy element");
+                    return;
                 }
+                
+                // Component must be attached to game object
+                var gameObject = Elements.GetValues(gameObjectId).FirstOrDefault() as GameObjectHierarchyElement;
+                if (gameObject == null)
+                    return;
+            
+                // GameObject could be stripped, if another prefab's gameobject is modified via adding MonoBehaviour
+                if (!gameObject.IsStripped)
+                {
+                    // Each GameObject must have Transform. We will use it to process scene hierarcy
+                    ProcessSceneHierarchyFromComponentToRootInner(GetTransformComponent(gameObject), consumer, null);  
+                } else 
+                {
+                    ProcessSceneHierarchyFromComponentToRootInner(gameObject, consumer, null);
+                }
+            }
+            else
+            {
+                ProcessSceneHierarchyFromComponentToRootInner(start, consumer, null);
             }
         }
         
-        public void ProcessSceneHierarchyFromComponentToRoot(FileID id, IUnityCachedSceneProcessorConsumer consumer)
-        {
-            ProcessSceneHierarchyFromComponentToRootUnderLock(id, consumer);
-        }
-
         
         // Invariant : startGameObject is Transform Component if it is not stripped 
         // This method traverse scene hierarchy via visiting transform components and push corresponding to transform GameObject into consumer
@@ -224,6 +192,69 @@ namespace JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.Swa
 
             return null;
         }
+        
+        
+        public override void Merge(IPsiSourceFile sourceFile, object builtPart)
+        {
+            RemoveFromLocalCache(sourceFile);
+            base.Merge(sourceFile, builtPart);
+            if (builtPart is HierarchyDataElement cache)
+            {
+                AddToLocalCache(sourceFile, cache);
+            }
+        }
 
+        public override void MergeLoaded(object data)
+        {
+            base.MergeLoaded(data);
+            PopulateLocalCache();
+        }
+
+        public override void Drop(IPsiSourceFile sourceFile)
+        {
+            RemoveFromLocalCache(sourceFile);
+            base.Drop(sourceFile);
+        }
+
+        private void PopulateLocalCache()
+        {
+            foreach (var (file, dataElement) in Map)
+                AddToLocalCache(file, dataElement);
+        }
+
+        private void AddToLocalCache(IPsiSourceFile sourceFile, HierarchyDataElement dataElement)
+        {
+            AddData(dataElement);
+        }
+
+        private void RemoveFromLocalCache(IPsiSourceFile sourceFile)
+        {
+            if (Map.TryGetValue(sourceFile, out var dataElement))
+            {
+                RemoveData(dataElement);
+            }
+        }
+        
+        public void AddData(HierarchyDataElement data)
+        {
+            foreach (var (id, elements) in data.Elements)
+            {
+                foreach (var (element, count) in elements)
+                {
+                    Elements.Add(id, element, count);
+                }
+            }
+        }
+        
+        public void RemoveData(HierarchyDataElement data)
+        {
+            foreach (var (id, elements) in data.Elements)
+            {
+                foreach (var (element, count) in elements)
+                {
+                    Elements.Add(id, element, -count);
+                }
+            }
+        }
     }
 }
