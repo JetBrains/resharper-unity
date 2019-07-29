1a60ecdc4fe7575c563d547675a630750ae70d44
Vladimir Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2019-07-29 14:21:50 +0300

    Do not show notification after leaving playmode and start typing right in that moment

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/notifications/UnityAutoSaveConfigureNotification.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/notifications/UnityAutoSaveConfigureNotification.kt
@@ -2,24 +2,24 @@ package com.jetbrains.rider.plugins.unity.notifications
 
 import com.intellij.ide.GeneralSettings
 import com.intellij.ide.util.PropertiesComponent
-import com.intellij.notification.NotificationGroup
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.editor.event.DocumentEvent
 import com.intellij.openapi.editor.event.DocumentListener
+import com.intellij.openapi.editor.impl.EditorImpl
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileEditor.impl.text.TextEditorProvider
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Key
 import com.intellij.ui.EditorNotificationPanel
 import com.intellij.ui.LightColors
+import com.intellij.util.SingleAlarm
 import com.jetbrains.rd.util.lifetime.Lifetime
 import com.jetbrains.rd.util.lifetime.isAlive
 import com.jetbrains.rd.util.lifetime.onTermination
 import com.jetbrains.rd.util.reactive.*
 import com.jetbrains.rdclient.util.idea.LifetimedProjectComponent
 import com.jetbrains.rider.UnityProjectDiscoverer
-import com.jetbrains.rider.debugger.settings.DotNetDebuggerSettings
 import com.jetbrains.rider.document.getFirstEditor
 import com.jetbrains.rider.model.EditorState
 import com.jetbrains.rider.model.ScriptCompilationDuringPlay
@@ -32,7 +32,7 @@ class UnityAutoSaveConfigureNotification (project: Project, private val unityPro
 
     private val propertiesComponent: PropertiesComponent = PropertiesComponent.getInstance()
     private var lifetimeDefinition = componentLifetime.createNested()
-    private val KEY = Key.create<EditorNotificationPanel>("PromoteAutoSave")
+    private val KEY = Key.create<Any>("PromoteAutoSave")
 
     companion object {
         private const val settingName = "do_not_show_unity_auto_save_notification"
@@ -77,38 +77,52 @@ class UnityAutoSaveConfigureNotification (project: Project, private val unityPro
     fun showNotification(lifetime: Lifetime, editor: Editor) {
         application.assertIsDispatchThread()
 
+
         if (!lifetime.isAlive) return
-        if (!DotNetDebuggerSettings.instance.shouldPromoteEditAndContinue) return
         val project = editor.project ?: return
 
         val textEditor = TextEditorProvider.getInstance().getTextEditor(editor)
         if (textEditor.getUserData(KEY) != null) return
 
-        val panel = EditorNotificationPanel(LightColors.RED)
-        panel.setText("You are modifying a script while Unity Editor is being in Play Mode. This can lead to a loss of the state in your running game.")
+        textEditor.putUserData(KEY, Any())
 
-        panel.createActionLabel("Configure Unity Editor") {
-            unityHost.model.showPreferences.fire()
-            lifetimeDefinition.terminate()
-        }
+        // Do not show notification, when user leaves play mode and start typing in that moment
+        val alarm = SingleAlarm(Runnable {
 
-        panel.createActionLabel("Don't show again") {
-            propertiesComponent.setValue(settingName, true)
-            lifetimeDefinition.terminate()
-        }
+            if (unityHost.model.editorState.valueOrDefault(EditorState.Disconnected) != EditorState.ConnectedPlay)
+                return@Runnable
 
-        panel.createActionLabel("X") {
-            lifetimeDefinition.terminate()
-        }
+            val panel = EditorNotificationPanel(LightColors.RED)
+            panel.setText("You are modifying a script while Unity Editor is being in Play Mode. This can lead to a loss of the state in your running game.")
 
-        lifetimeDefinition.onTermination {
-            if (textEditor.getUserData(KEY) != null) {
-                FileEditorManager.getInstance(project).removeTopComponent(textEditor, panel)
-                textEditor.putUserData(KEY, null)
+            panel.createActionLabel("Configure Unity Editor") {
+                unityHost.model.showPreferences.fire()
+                lifetimeDefinition.terminate()
             }
+
+            panel.createActionLabel("Don't show again") {
+                propertiesComponent.setValue(settingName, true)
+                lifetimeDefinition.terminate()
+            }
+
+            panel.createActionLabel("X") {
+                lifetimeDefinition.terminate()
+            }
+
+            lifetimeDefinition.onTermination {
+                if (textEditor.getUserData(KEY) != null) {
+                    FileEditorManager.getInstance(project).removeTopComponent(textEditor, panel)
+                    textEditor.putUserData(KEY, null)
+                }
+            }
+
+            FileEditorManager.getInstance(project).addTopComponent(textEditor, panel)
+        },1_500, (editor as? EditorImpl)?.disposable ?: return)
+
+        alarm.request()
+        lifetimeDefinition.onTermination {
+            alarm.dispose()
         }
 
-        textEditor.putUserData(KEY, panel)
-        FileEditorManager.getInstance(project).addTopComponent(textEditor, panel)
     }
 }
