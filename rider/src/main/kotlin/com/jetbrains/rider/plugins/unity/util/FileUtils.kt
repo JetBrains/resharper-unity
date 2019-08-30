52a39afabfdb699e7010e26bb7a75ef142113b4f
Matt Ellis <m.t.ellis@gmail.com>
2019-08-30 11:16:07 +0100

    Migrate to async VFS file listeners
    Also improves the cached package list and Unity Explorer from being updated too frequently, unnecessarily.

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/util/FileUtils.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/util/FileUtils.kt
@@ -6,8 +6,11 @@ import com.intellij.openapi.vfs.VirtualFile
 import com.jetbrains.rider.projectDir
 import java.nio.file.Paths
 
-fun Project.refreshAndFindFile(relativeFile: String): VirtualFile?
-{
+fun Project.refreshAndFindFile(relativeFile: String): VirtualFile? {
     val path = Paths.get(this.projectDir.path, relativeFile)
     return VfsUtil.findFile(path, true)
+}
+
+fun Project.findFile(relativeFile: String): VirtualFile? {
+    return this.projectDir.findFileByRelativePath(relativeFile)
 }
