b53d6fc2752f4f99715fade249dcb63e12d41faa
Matt Ellis <m.t.ellis@gmail.com>
2019-10-02 22:33:52 +0100

    Fix compile

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/run/attach/UnityLocalAttachProcessPresentationGroup.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/run/attach/UnityLocalAttachProcessPresentationGroup.kt
@@ -4,8 +4,8 @@ import com.intellij.execution.process.ProcessInfo
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.UserDataHolder
 import com.intellij.xdebugger.attach.XAttachProcessPresentationGroup
-import com.jetbrains.rider.plugins.unity.run.UnityRunUtil
 import icons.UnityIcons
+import javax.swing.Icon
 
 @Suppress("UnstableApiUsage")
 object UnityLocalAttachProcessPresentationGroup : XAttachProcessPresentationGroup {
@@ -24,4 +24,13 @@ object UnityLocalAttachProcessPresentationGroup : XAttachProcessPresentationGrou
     }
 
     override fun compare(p1: ProcessInfo, p2: ProcessInfo) = p1.pid.compareTo(p2.pid)
+
+    // Scheduled for removal 2020.1
+    override fun getProcessDisplayText(project: Project, processInfo: ProcessInfo, userDataHolder: UserDataHolder): String {
+        return getItemDisplayText(project, processInfo, userDataHolder)
+    }
+
+    override fun getProcessIcon(project: Project, processInfo: ProcessInfo, userDataHolder: UserDataHolder): Icon {
+        return getItemIcon(project, processInfo, userDataHolder)
+    }
 }
