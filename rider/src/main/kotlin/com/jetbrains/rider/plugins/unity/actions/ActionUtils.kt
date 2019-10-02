316039ad41ae2ef4b5b8c6fecca3ebaf43eb1978
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2019-10-02 15:48:04 +0200

    Show Unity actions for UnityProjectFolder (#1325)

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/actions/ActionUtils.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/actions/ActionUtils.kt
@@ -4,6 +4,7 @@ package com.jetbrains.rider.plugins.unity.actions
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.jetbrains.rd.util.reactive.valueOrDefault
 import com.jetbrains.rider.isUnityProject
+import com.jetbrains.rider.isUnityProjectFolder
 import com.jetbrains.rider.plugins.unity.UnityHost
 
 fun AnActionEvent.getHost(): UnityHost? {
@@ -14,6 +15,11 @@ fun AnActionEvent.getHost(): UnityHost? {
 fun AnActionEvent.isUnityProject(): Boolean {
     val project = this.project ?: return false
     return project.isUnityProject()
+}
+
+fun AnActionEvent.isUnityProjectFolder(): Boolean {
+    val project = this.project ?: return false
+    return project.isUnityProjectFolder()
 }
 
 fun AnActionEvent.handleUpdateForUnityConnection(fn: ((UnityHost) -> Boolean)? = null) {
