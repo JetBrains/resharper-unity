3e682ac8777cff03f30ffaa2447fe360499291a6
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2019-10-03 09:39:15 +0200

    fix changed api

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/run/attach/UnityAttachPresentationGroup.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/run/attach/UnityAttachPresentationGroup.kt
@@ -7,9 +7,9 @@ import com.intellij.xdebugger.attach.XAttachProcessPresentationGroup
 import icons.UnityIcons
 
 object UnityAttachPresentationGroup : XAttachProcessPresentationGroup {
+    override fun getProcessIcon(project: Project, process: ProcessInfo, userData: UserDataHolder) = UnityIcons.Icons.UnityLogo
+    override fun getProcessDisplayText(project: Project, process: ProcessInfo, userData: UserDataHolder) = process.executableDisplayName
     override fun getOrder(): Int = 3
     override fun getGroupName(): String = "Local Unity processes"
-    override fun getItemIcon(project: Project, process: ProcessInfo, userData: UserDataHolder) = UnityIcons.Icons.UnityLogo
-    override fun getItemDisplayText(project: Project, process: ProcessInfo, userData: UserDataHolder) = process.executableDisplayName
     override fun compare(p1: ProcessInfo, p2: ProcessInfo) = p1.pid.compareTo(p2.pid)
 }
