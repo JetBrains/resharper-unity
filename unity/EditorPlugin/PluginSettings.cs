52f3a382da03cd08866b23b193b5847c29fb2319
Vladimir Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2019-10-29 20:37:17 +0300

    Fix compilation (part1, Unity 3.5 runtime is broken)

--- unity/EditorPlugin/PluginSettings.cs
+++ unity/EditorPlugin/PluginSettings.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
 using JetBrains.Diagnostics;
 using JetBrains.Diagnostics.Internal;
 using JetBrains.Lifetimes;
