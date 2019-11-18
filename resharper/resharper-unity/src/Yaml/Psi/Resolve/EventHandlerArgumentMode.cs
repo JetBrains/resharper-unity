e4a31236be91900bf0e286859afdc004fef1eb39
Matt Ellis <m.t.ellis@gmail.com>
2019-11-18 23:35:53 +0000

    Show asset usages for UnityEvent based handlers

--- resharper/resharper-unity/src/Yaml/Psi/Resolve/EventHandlerArgumentMode.cs
+++ resharper/resharper-unity/src/Yaml/Psi/Resolve/EventHandlerArgumentMode.cs
@@ -1,14 +1,23 @@
 namespace JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.Resolve
 {
+    // Corresponds to UnityEngine.Events.PersistentListenerMode
+    // EventDefined means the handler receives the arguments from the event itself, i.e. UnityEvent (void) or
+    // UnityEvent<T, ...> and the handler's parameters must match this signature.
+    // The other values are static values set in the Inspector. The Inspector will list all possible event handlers
+    // and the single method parameter defines the argument mode.
+    // The Inspector will only list public methods with one parameter and a void return type.
+    // Static methods are listed, but described in the Inspector as "missing" and are not invoked.
+    // Private methods are not listed, but are invoked.
+    // Methods with return types are not listed, and are not invoked (there is a type mismatch exception at runtime).
+    // Methods with too many parameters are marked as "missing" and not invoked.
     public enum EventHandlerArgumentMode
     {
-        Unknown = 0,
-        None = 1,
-        UnityType = 2,
+        EventDefined = 0,
+        Void = 1,
+        UnityObject = 2,
         Int = 3,
         Float = 4,
         String = 5,
         Bool = 6
-
     }
 }
--- resharper/resharper-unity/src/Yaml/Psi/Resolve/EventHandlerSymbolFilter.cs
+++ resharper/resharper-unity/src/Yaml/Psi/Resolve/EventHandlerSymbolFilter.cs
@@ -1,61 +1,94 @@
+using JetBrains.Annotations;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.Modules;
 using JetBrains.ReSharper.Psi.Resolve;
+using JetBrains.ReSharper.Psi.Util;
+using JetBrains.Util.dataStructures;
 
 namespace JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.Resolve
 {
     public class EventHandlerSymbolFilter : SimpleSymbolFilter
     {
         private readonly EventHandlerArgumentMode myMode;
-        private readonly IType myType;
+        private readonly FrugalLocalList<IType> myTypes;
+
         public EventHandlerSymbolFilter(EventHandlerArgumentMode mode, string type, IPsiModule psiModule)
         {
+            myTypes = new FrugalLocalList<IType>();
+
             myMode = mode;
-            if (mode == EventHandlerArgumentMode.UnityType && type != null)
+            if (mode == EventHandlerArgumentMode.UnityObject && type != null)
             {
-                myType = TypeFactory.CreateTypeByCLRName(type, psiModule);
+                myTypes.Add(TypeFactory.CreateTypeByCLRName(type, psiModule));
             }
-            else if (mode != EventHandlerArgumentMode.None)
+            else if (mode == EventHandlerArgumentMode.EventDefined && type != null)
+            {
+                var eventType = TypeFactory.CreateTypeByCLRName(type, psiModule);
+                var unityEventType = GetUnityEventType(eventType);
+                var unityEventTypeElement = unityEventType?.GetTypeElement();
+                if (unityEventType != null && unityEventTypeElement != null)
+                {
+                    var (_, substitution) = unityEventType.Resolve();
+                    var typeParameters = unityEventTypeElement.TypeParameters;
+                    foreach (var t in typeParameters)
+                    {
+                        myTypes.Add(substitution.Apply(t));
+                    }
+                }
+            }
+            else if (mode != EventHandlerArgumentMode.Void)
             {
                 var predefinedTypes = psiModule.GetPredefinedType();
                 switch (mode)
                 {
                     case EventHandlerArgumentMode.Int:
-                        myType = predefinedTypes.Int;
+                        myTypes.Add(predefinedTypes.Int);
                         break;
                     case EventHandlerArgumentMode.Float:
-                        myType = predefinedTypes.Float;
+                        myTypes.Add(predefinedTypes.Float);
                         break;
                     case EventHandlerArgumentMode.String:
-                        myType = predefinedTypes.String;
+                        myTypes.Add(predefinedTypes.String);
                         break;
                     case EventHandlerArgumentMode.Bool:
-                        myType = predefinedTypes.Bool;
+                        myTypes.Add(predefinedTypes.Bool);
                         break;
                 }
             }
         }
-        
+
+        [CanBeNull]
+        private static IDeclaredType GetUnityEventType(IDeclaredType eventType)
+        {
+            foreach (var superType in eventType.GetAllSuperTypes())
+            {
+                if (superType.GetClrName().ShortName == "UnityEvent")
+                    return superType;
+            }
+
+            return null;
+        }
+
         public override ResolveErrorType ErrorType => ResolveErrorType.ARGUMENTS_MISMATCH;
-        
+
         public override bool Accepts(IDeclaredElement declaredElement, ISubstitution substitution)
         {
-            if (myMode == EventHandlerArgumentMode.Unknown)
+            if (myMode != EventHandlerArgumentMode.Void && myTypes.Count == 0)
                 return false;
-            if (myMode != EventHandlerArgumentMode.None && myType == null)
-                return false;
-            
+
             if (!(declaredElement is IMethod method))
                 return false;
 
             var parameters = method.Parameters;
-            if (parameters.Count == 0 && myType == null)
-                return true;
-
-            if (parameters.Count == 1)
+            if (parameters.Count == myTypes.Count)
             {
-                var param = parameters[0];
-                return param.Type.Equals(myType);
+                for (var i = 0; i < myTypes.Count; i++)
+                {
+                    if (!parameters[i].Type.Equals(myTypes[i]))
+                        return false;
+                }
+
+                return true;
             }
 
             return false;
