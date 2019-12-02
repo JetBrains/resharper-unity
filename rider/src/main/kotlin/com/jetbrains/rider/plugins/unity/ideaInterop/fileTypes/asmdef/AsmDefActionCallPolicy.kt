8755ba3ad1e25413cfbecbc01e221ebdd06808dd
Vladimir Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2019-12-02 15:45:07 +0300

    Use defined constant for CtrlClick

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/ideaInterop/fileTypes/asmdef/AsmDefActionCallPolicy.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/ideaInterop/fileTypes/asmdef/AsmDefActionCallPolicy.kt
@@ -5,13 +5,14 @@ import com.intellij.psi.PsiElement
 import com.jetbrains.rider.actions.RiderActionCallStrategy
 import com.jetbrains.rider.actions.RiderActionSupportPolicy
 import com.jetbrains.rider.actions.RiderActions
+import com.jetbrains.rider.hyperlinks.RiderCtrlClickHost
 
 class AsmDefActionCallPolicy : RiderActionSupportPolicy() {
     override fun getCallStrategy(psiElement: PsiElement, backendActionId: String): RiderActionCallStrategy {
         if (backendActionId == IdeActions.ACTION_RENAME)
             return RiderActionCallStrategy.FRONTEND_FIRST
         if (RiderActions.GOTO_DECLARATION == backendActionId || IdeActions.ACTION_FIND_USAGES == backendActionId
-            || backendActionId == "CtrlClick")
+            || RiderCtrlClickHost.resharperActionId == backendActionId)
             return RiderActionCallStrategy.BACKEND_FIRST
 
         return RiderActionCallStrategy.FRONTEND_ONLY
