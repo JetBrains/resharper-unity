c32653e92e36ec809cd74f9ebb15296dfd26c103
Vladimir Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2019-12-12 19:27:34 +0300

    Deferred cache prototype

---
+++ resharper/resharper-unity/src/Feature/Caches/IDeferredCache.cs
@@ -0,0 +1,13 @@
+using JetBrains.Lifetimes;
+using JetBrains.ReSharper.Psi;
+
+namespace JetBrains.ReSharper.Plugins.Unity.Feature.Caches
+{
+    public interface IDeferredCache
+    {
+        bool UpToDate(IPsiSourceFile sourceFile);
+        void Merge(IPsiSourceFile psiSourceFile, object build);
+        object Build(in Lifetime lifetime, IPsiSourceFile psiSourceFile);
+        void Drop(IPsiSourceFile psiSourceFile);
+    }
+}
