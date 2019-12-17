d64c5d53ba3a318ddf8da81d80d70b8a2d8c06b8
Matt Ellis <m.t.ellis@gmail.com>
2019-12-17 09:57:48 +0000

    Bump version to 2020.1

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/ui/UssDisabledEditorNotification.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/ui/UssDisabledEditorNotification.kt
@@ -1,17 +1,16 @@
 package com.jetbrains.rider.plugins.unity.ui
 
-import com.intellij.ide.plugins.PluginManager
+import com.intellij.ide.plugins.PluginManagerCore
 import com.intellij.ide.plugins.PluginManagerMain
 import com.intellij.ide.util.PropertiesComponent
+import com.intellij.openapi.extensions.PluginId
 import com.intellij.openapi.fileEditor.FileEditor
-import com.intellij.openapi.fileTypes.FileTypeRegistry
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Key
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.ui.EditorNotificationPanel
 import com.intellij.ui.EditorNotifications
 import com.jetbrains.rider.isUnityProject
-import com.jetbrains.rider.plugins.unity.ideaInterop.fileTypes.uss.UssFileType
 import com.jetbrains.rider.plugins.unity.util.isUssFile
 
 class UssDisabledEditorNotification: EditorNotifications.Provider<EditorNotificationPanel>() {
@@ -25,7 +24,7 @@ class UssDisabledEditorNotification: EditorNotifications.Provider<EditorNotifica
     override fun getKey(): Key<EditorNotificationPanel> = KEY
 
     override fun createNotificationPanel(file: VirtualFile, fileEditor: FileEditor, project: Project): EditorNotificationPanel? {
-        if (project.isUnityProject() && isUssFile(file) && PluginManager.isDisabled(CSS_PLUGIN_ID)) {
+        if (project.isUnityProject() && isUssFile(file) && PluginManagerCore.isDisabled(PluginId.getId(CSS_PLUGIN_ID))) {
             if (PropertiesComponent.getInstance(project).getBoolean(DO_NOT_SHOW_AGAIN_KEY, false)) {
                 return null
             }
@@ -36,7 +35,7 @@ class UssDisabledEditorNotification: EditorNotifications.Provider<EditorNotifica
                 // TODO: Maybe in 2020.1 we can do this dynamically without restart?
                 // That would require enabling the CSS plugin dynamically, and then enabling our PluginCssPart.xml part
                 // dynamically, too
-                PluginManager.enablePlugin(CSS_PLUGIN_ID)
+                PluginManagerCore.enablePlugin(PluginId.getId(CSS_PLUGIN_ID))
                 PluginManagerMain.notifyPluginsUpdated(project)
                 EditorNotifications.getInstance(project).updateAllNotifications()
             }
