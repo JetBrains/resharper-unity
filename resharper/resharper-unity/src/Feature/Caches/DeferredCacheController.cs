bd7abb7a6ac177779db79d76d72a51cd1ce33974
Vladimir Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2020-01-29 18:16:14 +0300

    Implement controller logic for deferred caches

--- resharper/resharper-unity/src/Feature/Caches/DeferredCacheController.cs
+++ resharper/resharper-unity/src/Feature/Caches/DeferredCacheController.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections.Generic;
-using JetBrains.Application.changes;
-using JetBrains.Application.PersistentMap;
-using JetBrains.Collections.Synchronized;
+using JetBrains.Application.Progress;
+using JetBrains.Application.Threading;
+using JetBrains.Collections;
+using JetBrains.Diagnostics;
 using JetBrains.Lifetimes;
 using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Feature.Services.Daemon;
@@ -14,45 +16,118 @@ namespace JetBrains.ReSharper.Plugins.Unity.Feature.Caches
     [SolutionComponent]
     public class DeferredCacheController
     {
+        private const int BATCH_SIZE = 10;
+        
+        private readonly IShellLocks myShellLocks;
         private readonly DaemonThread myDaemonThread;
         private readonly DeferredHelperCache myDeferredHelperCache;
         private readonly IEnumerable<IDeferredCache> myDeferredCaches;
+
+        private readonly Dictionary<IPsiSourceFile, Dictionary<IDeferredCache, object>> myPartlyCalculatedData = new Dictionary<IPsiSourceFile, Dictionary<IDeferredCache, object>>();
+        private readonly Dictionary<IPsiSourceFile, Dictionary<IDeferredCache, object>> myCalculatedData = new Dictionary<IPsiSourceFile, Dictionary<IDeferredCache, object>>();
         
-        public DeferredCacheController(IPersistentIndexManager persistentIndexManager, DaemonThread daemonThread,
+        public DeferredCacheController(IPersistentIndexManager persistentIndexManager, IShellLocks shellLocks, DaemonThread daemonThread,
             DeferredHelperCache deferredHelperCache, IEnumerable<IDeferredCache> deferredCaches)
         {
+            myShellLocks = shellLocks;
             myDaemonThread = daemonThread;
             myDeferredHelperCache = deferredHelperCache;
             myDeferredCaches = deferredCaches;
         }
         public void GetTasks(Lifetime lifetime)
         {
-            // TODO: remove task
-            foreach (var psiSourceFile in myDeferredHelperCache.FilesToDrop)
+            using (TryReadLockCookie.Create(NullProgressIndicator.Instance, myShellLocks, () => !lifetime.IsAlive))
             {
-                foreach (var cache in myDeferredCaches)
+                foreach (var psiSourceFile in new LocalList<IPsiSourceFile>(myDeferredHelperCache.FilesToDrop))
                 {
-                    cache.Drop(psiSourceFile);
+                    myPartlyCalculatedData.Remove(psiSourceFile);
+                    myCalculatedData.Remove(psiSourceFile);
+                    // TODO: enter write lock
+                    foreach (var cache in myDeferredCaches)
+                    {
+                        cache.Drop(psiSourceFile);
+                    }
+                    
+                    // DeferredCacheController is unique reader for data in DeferredHelperCache
+                    // Clearing list is safe operation here due to ReadLock
+                    myDeferredHelperCache.FilesToDrop.Remove(psiSourceFile);
+                    CheckForInterrupt(lifetime);
                 }
+                myDeferredHelperCache.FilesToDrop.Clear();
+
+                FlushBuildDataIfNeed(lifetime);
                 
-                if (lifetime.IsAlive)
-                    return;
-
-            }
-            myDeferredHelperCache.FilesToDrop.Clear();
-
-
-            foreach (var psiSourceFile in myDeferredHelperCache.FilesToProcess.ToArray())
-            {
-                foreach (var cache in myDeferredCaches)
+                foreach (var psiSourceFile in GetFilesToProcess())
                 {
-                    cache.Merge(psiSourceFile, cache.Build(lifetime, psiSourceFile));
-                    if (!lifetime.IsAlive)
-                        return;
+                    if (!myPartlyCalculatedData.TryGetValue(psiSourceFile, out var cacheToData))
+                    {
+                        cacheToData = new Dictionary<IDeferredCache, object>();
+                        myPartlyCalculatedData[psiSourceFile] = cacheToData;
+                    }
+                    
+                    foreach (var cache in myDeferredCaches)
+                    {
+                        if (cacheToData.ContainsKey(cache))
+                            continue;
+
+                        cacheToData[cache] = cache.Build(lifetime, psiSourceFile);
+                        CheckForInterrupt(lifetime);
+                    }
+                    myDeferredHelperCache.FilesToProcess.Remove(psiSourceFile);
+                    
+                    Assertion.Assert(!myCalculatedData.ContainsKey(psiSourceFile), "!myCalculatedData.ContainsKey(psiSourceFile)");
+                    myCalculatedData[psiSourceFile] = myPartlyCalculatedData[psiSourceFile];
+                    myPartlyCalculatedData.Remove(psiSourceFile);
+
+                    FlushBuildDataIfNeed(lifetime);
                 }
-                myDeferredHelperCache.FilesToProcess.Remove(psiSourceFile);
+
+                FlushBuildData(lifetime);
             }
+        }
+
+        private void CheckForInterrupt(Lifetime lifetime)
+        {
+            if (!lifetime.IsAlive)
+                throw new OperationCanceledException();
+        }
+
+        private IEnumerable<IPsiSourceFile> GetFilesToProcess()
+        {
+            foreach (var sourceFile in new LocalList<IPsiSourceFile>(myPartlyCalculatedData.Keys))
+            {
+                yield return sourceFile;
+            }
+
+            foreach (var sourceFile in myDeferredHelperCache.FilesToProcess)
+            {
+                yield return sourceFile;
+            }
+        }
+
+        private void FlushBuildData(Lifetime lifetime)
+        {
+            foreach (var sourceFile in new LocalList<IPsiSourceFile>(myCalculatedData.Keys))
+            {
+                var cacheToData = myCalculatedData[sourceFile];
+                foreach (var (cache, data) in cacheToData)
+                {
+                    cache.Merge(sourceFile, data);
+                }
+
+                myCalculatedData.Remove(sourceFile);
+
+                CheckForInterrupt(lifetime);
+            }
+        }
+
+        private void FlushBuildDataIfNeed(Lifetime lifetime)
+        {
             
+            if (myCalculatedData.Count > BATCH_SIZE)
+            {
+                FlushBuildData(lifetime);
+            }
         }
     }
 }
