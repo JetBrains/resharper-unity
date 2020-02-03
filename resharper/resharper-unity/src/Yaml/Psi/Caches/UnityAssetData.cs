859dba61d204c27319aea8bc50141d55d635b7c6
Vladimir Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2020-02-03 11:33:26 +0300

    New cache for asset data

---
+++ resharper/resharper-unity/src/Yaml/Psi/Caches/UnityAssetData.cs
@@ -0,0 +1,17 @@
+using JetBrains.Serialization;
+
+namespace JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.Caches
+{
+    public class UnityAssetData
+    {
+        public static void WriteDelegate(UnsafeWriter writer, UnityAssetData value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public static UnityAssetData ReadDelegate(UnsafeReader reader)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
