ecf2b8fc7e21ae00dca76041c2d8c26acb53264d
Vladimir Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2020-02-22 15:24:15 +0300

    fix backend tests

---
+++ resharper/resharper-unity/test/src/Yaml/UnityYamlProjectFileLanguageServiceTest.cs
@@ -0,0 +1,18 @@
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Plugins.Unity.Yaml.ProjectModel;
+using JetBrains.ReSharper.Plugins.Unity.Yaml.Psi;
+using JetBrains.ReSharper.Psi;
+
+namespace JetBrains.ReSharper.Plugins.Unity.Tests.Yaml
+{
+    
+    [ProjectFileType(typeof(UnityYamlProjectFileType))]
+    public class UnityYamlProjectFileLanguageServiceTest : UnityYamlProjectFileLanguageService
+    {
+        public override PsiLanguageType GetPsiLanguageType(IPsiSourceFile sourceFile)
+        {
+            IProjectFile projectFile = sourceFile.ToProjectFile();
+            return projectFile == null ? GetPsiLanguageType(sourceFile.LanguageType) : GetPsiLanguageType(projectFile);
+        }
+    }
+}
