97c7c0e8bde38a382ff91ebac531e04bd83330fd
Matt Ellis <m.t.ellis@gmail.com>
2020-03-11 09:41:26 +0000

    Enable testing on mac for YAML project

--- resharper/resharper-yaml/test/src/TestEnvironment.cs
+++ resharper/resharper-yaml/test/src/TestEnvironment.cs
@@ -1,7 +1,12 @@
-﻿using JetBrains.Application.BuildScript.Application.Zones;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using JetBrains.Application.BuildScript.Application.Zones;
 using JetBrains.ReSharper.TestFramework;
 using JetBrains.TestFramework;
 using JetBrains.TestFramework.Application.Zones;
+using JetBrains.TestFramework.Utils;
+using JetBrains.Util;
 using NUnit.Framework;
 
 [assembly: RequiresThread(System.Threading.ApartmentState.STA)]
@@ -28,5 +33,74 @@ namespace JetBrains.ReSharper.Plugins.Yaml.Tests
     [SetUpFixture]
     public class TestEnvironment : ExtensionTestEnvironmentAssembly<IYamlTestZone>
     {
+        static TestEnvironment()
+        {
+            try
+            {
+                // SetupLogging();
+                SetJetTestPackagesDir();
+                HackTestDataInNugets.ApplyPatches();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        // Default logging is verbose, but this is useful for debugging startup, before the tests get a chance to
+        // initialise properly
+        private static void SetupLogging()
+        {
+            var executingAssembly = Assembly.GetExecutingAssembly();
+            var url = new Uri(executingAssembly.CodeBase);
+            var assemblyPath = FileSystemPath.Parse(url.AbsolutePath);
+            var assemblyDir = assemblyPath.Directory;
+            var configFile = assemblyDir.CombineWithShortName(assemblyPath.NameWithoutExtension + "_log.xml");
+
+            // /resharper/build/tests.rider/bin/Debug/net461/JetBrains.ReSharper.Plugins.Unity.Tests.Rider_log.log
+            // Note that the logger will delete all files with basename.*, e.g. Rider.dll !!!!??!?!
+            var logfile = assemblyDir.CombineWithShortName(assemblyPath.NameWithoutExtension + "_log.log");
+            logfile.DeleteFile();
+
+            // Set to TRACE to get logging on basically everything (including component containers)
+            // Set to VERBOSE for most useful logging, but beware perf impact
+            File.WriteAllText(configFile.FullPath,
+                $@"<?xml version=""1.0"" encoding=""UTF-8"" ?>
+         <configuration>
+           <appender name=""file"" class=""JetBrains.Util.Logging.FileLogEventListener"" pattern=""%d{{HH:mm:ss.fff}} |%l| %-30c{
+                        1
+                    }| %M%n"">
+             <arg>{logfile.FullPath}</arg>
+           </appender>
+           <root level=""VERBOSE"">
+             <appender-ref>file</appender-ref>
+           </root>
+         </configuration>
+         ");
+            Environment.SetEnvironmentVariable("RESHARPER_LOG_CONF", configFile.FullPath);
+        }
+
+        // The repositoryPath setting in nuget.config points to the location of the test nuget packages that are
+        // downloaded as part of running tests. We can't have a path that is both Windows and *NIX friendly, so we set
+        // an environment variable.
+        // If the ~/JetTestPackages folder exists, we'll use that. If not, put it next to the TestDataPath, e.g.
+        // test/data/../JetTestPackages
+        private static void SetJetTestPackagesDir()
+        {
+            if (Environment.GetEnvironmentVariable("JET_TEST_PACKAGES_DIR") == null)
+            {
+                // If path is relative, it's relative to the folder of the NuGet.config file
+                var packages = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                    "JetTestPackages");
+                if (!Directory.Exists(packages))
+                {
+                    var testData = TestUtil.GetTestDataPathBase(typeof(TestEnvironment).Assembly);
+                    packages = testData.Parent.Combine("JetTestPackages").FullPath;
+                }
+
+                Environment.SetEnvironmentVariable("JET_TEST_PACKAGES_DIR", packages,
+                    EnvironmentVariableTarget.Process);
+            }
+        }
     }
 }
