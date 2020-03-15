1f843fe4a6a84569db3d5b06e8f2c3635be09540
Matt Ellis <m.t.ellis@gmail.com>
2020-03-15 22:36:08 +0000

    Add Unity specific spell check dictionaries
    Fixes #1187

---
+++ resharper/resharper-unity/src/CSharp/Feature/Respeller/AbbreviationsSettingsProvider.cs
@@ -0,0 +1,46 @@
+using System.IO;
+using JetBrains.Application.Settings;
+using JetBrains.Application.Settings.Implementation;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Plugins.Unity.Settings;
+using JetBrains.ReSharper.Psi.CSharp.Naming2;
+using JetBrains.Util;
+
+namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Feature.Respeller
+{
+    [SolutionComponent]
+    public class AbbreviationsSettingsProvider : IUnitySolutionSettingsProvider
+    {
+        private readonly ISettingsSchema mySettingsSchema;
+        private readonly ILogger myLogger;
+
+        public AbbreviationsSettingsProvider(ISettingsSchema settingsSchema, ILogger logger)
+        {
+            mySettingsSchema = settingsSchema;
+            myLogger = logger;
+        }
+
+        public void InitialiseSolutionSettings(ISettingsStorageMountPoint mountPoint)
+        {
+            var streamName = GetType().Namespace + ".Abbreviations.txt";
+            var stream = GetType().Assembly.GetManifestResourceStream(streamName);
+            if (stream == null)
+            {
+                myLogger.Warn($"Cannot load resource stream: {streamName}");
+                return;
+            }
+
+            using (var streamReader = new StreamReader(stream))
+            {
+                var entry = mySettingsSchema.GetIndexedEntry((CSharpNamingSettings s) => s.Abbreviations);
+
+                string abbreviation;
+                while ((abbreviation = streamReader.ReadLine()) != null)
+                {
+                    ScalarSettingsStoreAccess.SetIndexedValue(mountPoint, entry, abbreviation, null, abbreviation,
+                        null, myLogger);
+                }
+            }
+        }
+    }
+}
---
+++ resharper/resharper-unity/src/CSharp/Feature/Respeller/ReSharper/UnityInstalledDictionarySettingsProvider.cs
@@ -0,0 +1,53 @@
+using System;
+using JetBrains.Annotations;
+using JetBrains.Application.Settings;
+using JetBrains.Application.Settings.Implementation;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Plugins.Unity.Settings;
+using JetBrains.Util;
+
+namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Feature.Respeller
+{
+    [SolutionComponent]
+    public class UnityInstalledDictionarySettingsProvider : IUnitySolutionSettingsProvider
+    {
+        private readonly ISettingsSchema mySettingsSchema;
+        private readonly ILogger myLogger;
+
+        public UnityInstalledDictionarySettingsProvider(ISettingsSchema settingsSchema, ILogger logger)
+        {
+            mySettingsSchema = settingsSchema;
+            myLogger = logger;
+        }
+
+        public void InitialiseSolutionSettings(ISettingsStorageMountPoint mountPoint)
+        {
+            var entry = GetIndexedEntry();
+            if (entry == null)
+                return;
+            var path = FileSystemPath.Parse(GetType().Assembly.Location).Parent / "Extensions/JetBrains.unity/dictionary/unity.dic";
+            ScalarSettingsStoreAccess.SetIndexedValue(mountPoint, entry, path, null, true, null, myLogger);
+        }
+
+        [CanBeNull]
+        private SettingsIndexedEntry GetIndexedEntry()
+        {
+            // TODO: Fix reference to Psi.Features.VisualStudio and use the InstalledDictionariesSettings type directly
+            var type = Type.GetType(
+                "JetBrains.PsiFeatures.VisualStudio.ReSpeller.InstalledDictionariesSettings, " +
+                "JetBrains.PsiFeatures.VisualStudio.ReSpeller, Version=777.0.0.0, Culture=neutral, PublicKeyToken=1010a0d8d6380325");
+            if (type == null)
+            {
+                myLogger.Warn("Unable to find InstalledDictionariesSettings type");
+                return null;
+            }
+            var entry = mySettingsSchema.GetEntry(type, "InstalledDictionaries") as SettingsIndexedEntry;
+            if (entry == null)
+            {
+                myLogger.Warn("Unable to find settings entry");
+                return null;
+            }
+            return entry;
+        }
+    }
+}
