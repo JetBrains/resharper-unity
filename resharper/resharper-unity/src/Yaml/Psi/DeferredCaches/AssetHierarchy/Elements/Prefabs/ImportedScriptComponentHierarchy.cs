c8ca2886c78dc7b2c443da1d76da6ecca0463677
Vladimir Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2020-03-17 01:03:52 +0300

    fix RIDER-41204

--- resharper/resharper-unity/src/Yaml/Psi/DeferredCaches/AssetHierarchy/Elements/Prefabs/ImportedScriptComponentHierarchy.cs
+++ resharper/resharper-unity/src/Yaml/Psi/DeferredCaches/AssetHierarchy/Elements/Prefabs/ImportedScriptComponentHierarchy.cs
@@ -16,7 +16,7 @@ namespace JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.DeferredCaches.AssetHierarc
 
         public LocalReference Location => myScriptComponentHierarchy.Location.GetImportedReference(myPrefabInstanceHierarchy);
 
-        public LocalReference GameObjectReference => myScriptComponentHierarchy.GameObjectReference.GetImportedReference(myPrefabInstanceHierarchy);
+        public LocalReference GameObjectReference => myScriptComponentHierarchy.GameObjectReference?.GetImportedReference(myPrefabInstanceHierarchy);
         public bool IsStripped => false;
         public LocalReference PrefabInstance => null;
         public ExternalReference CorrespondingSourceObject => null;
