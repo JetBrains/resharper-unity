7d40607466c8465e94e7dce20563735b044786e8
Matt Ellis <m.t.ellis@gmail.com>
2020-03-25 17:10:44 +0000

    Use Visual Studio Psi.Features subplatform

--- resharper/resharper-unity/src/CSharp/Feature/Respeller/ReSharper/UnityInstalledDictionarySettingsProvider.cs
+++ resharper/resharper-unity/src/CSharp/Feature/Respeller/ReSharper/UnityInstalledDictionarySettingsProvider.cs
@@ -1,8 +1,7 @@
-using System;
-using JetBrains.Annotations;
 using JetBrains.Application.Settings;
 using JetBrains.Application.Settings.Implementation;
 using JetBrains.ProjectModel;
+using JetBrains.PsiFeatures.VisualStudio.ReSpeller;
 using JetBrains.ReSharper.Plugins.Unity.Settings;
 using JetBrains.Util;
 
@@ -22,32 +21,9 @@ namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Feature.Respeller
 
         public void InitialiseSolutionSettings(ISettingsStorageMountPoint mountPoint)
         {
-            var entry = GetIndexedEntry();
-            if (entry == null)
-                return;
+            var entry = mySettingsSchema.GetIndexedEntry((InstalledDictionariesSettings s) => s.InstalledDictionaries);
             var path = (FileSystemPath.Parse(GetType().Assembly.Location).Parent / "Extensions/JetBrains.unity/dictionaries/unity.dic").FullPath;
             ScalarSettingsStoreAccess.SetIndexedValue(mountPoint, entry, path, null, true, null, myLogger);
-        }
-
-        [CanBeNull]
-        private SettingsIndexedEntry GetIndexedEntry()
-        {
-            // TODO: Fix reference to Psi.Features.VisualStudio and use the InstalledDictionariesSettings type directly
-            var type = Type.GetType(
-                "JetBrains.PsiFeatures.VisualStudio.ReSpeller.InstalledDictionariesSettings, " +
-                "JetBrains.PsiFeatures.VisualStudio.ReSpeller, Version=777.0.0.0, Culture=neutral, PublicKeyToken=1010a0d8d6380325");
-            if (type == null)
-            {
-                myLogger.Warn("Unable to find InstalledDictionariesSettings type");
-                return null;
-            }
-            var entry = mySettingsSchema.GetEntry(type, "InstalledDictionaries") as SettingsIndexedEntry;
-            if (entry == null)
-            {
-                myLogger.Warn("Unable to find settings entry");
-                return null;
-            }
-            return entry;
         }
     }
 }
