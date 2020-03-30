ed95391687a5d23e115d72c1ef96d1ec5097f7d7
Vladimir Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2020-03-30 19:12:13 +0300

    increment cache version

--- resharper/resharper-unity/src/CacheVersion.cs
+++ resharper/resharper-unity/src/CacheVersion.cs
@@ -5,7 +5,7 @@ using JetBrains.Serialization;
 namespace JetBrains.ReSharper.Plugins.Unity
 {
     // Cache version
-    [PolymorphicMarshaller(16)]
+    [PolymorphicMarshaller(17)]
     public class CacheVersion
     {
         [UsedImplicitly] public static UnsafeReader.ReadDelegate<object> ReadDelegate = r => new CacheVersion();
