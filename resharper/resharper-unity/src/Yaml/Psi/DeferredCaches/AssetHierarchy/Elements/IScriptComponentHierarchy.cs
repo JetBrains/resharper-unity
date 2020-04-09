88834a709bd10525a7a0cb5885157c6ab66c951d
Vladimir Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2020-04-09 20:42:45 +0300

    Fix asset usages for field, when script component is assigned to field in Inspector

--- resharper/resharper-unity/src/Yaml/Psi/DeferredCaches/AssetHierarchy/Elements/IScriptComponentHierarchy.cs
+++ resharper/resharper-unity/src/Yaml/Psi/DeferredCaches/AssetHierarchy/Elements/IScriptComponentHierarchy.cs
@@ -2,7 +2,7 @@ using JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.DeferredCaches.AssetHierarchy.R
 
 namespace JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.DeferredCaches.AssetHierarchy.Elements
 {
-    public interface IScriptComponentHierarchy : IHierarchyElement
+    public interface IScriptComponentHierarchy : IComponentHierarchy
     {
         ExternalReference ScriptReference { get; }
     }
