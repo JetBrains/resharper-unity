9f78d2bc4388a4ded2420fdc40af654a1a6d82b7
Matt Ellis <m.t.ellis@gmail.com>
2020-05-05 17:39:08 +0100

    Make prefer generic method quick fix scoped
    Resolves #1648

--- resharper/resharper-unity/src/CSharp/Feature/Services/QuickFixes/PreferGenericMethodOverloadQuickFix.cs
+++ resharper/resharper-unity/src/CSharp/Feature/Services/QuickFixes/PreferGenericMethodOverloadQuickFix.cs
@@ -1,87 +1,64 @@
 using System;
-using System.Collections.Generic;
 using JetBrains.Application.Progress;
 using JetBrains.ProjectModel;
-using JetBrains.ReSharper.Feature.Services.Bulbs;
-using JetBrains.ReSharper.Feature.Services.Intentions;
 using JetBrains.ReSharper.Feature.Services.QuickFixes;
 using JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Errors;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.CSharp;
 using JetBrains.ReSharper.Psi.CSharp.Tree;
+using JetBrains.ReSharper.Psi.Tree;
 using JetBrains.TextControl;
 using JetBrains.Util;
 
 namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Feature.Services.QuickFixes
 {
     [QuickFix]
-    public class PreferGenericMethodOverloadQuickFix : IQuickFix
+    public class PreferGenericMethodOverloadQuickFix : UnityScopedQuickFixBase
     {
         private readonly IInvocationExpression myInvocationExpression;
         private readonly string myMethodName;
-        private readonly ITypeElement myTypeElement;
+        private readonly ITypeElement myTargetType;
 
         public PreferGenericMethodOverloadQuickFix(PreferGenericMethodOverloadWarning warning)
         {
             myInvocationExpression = warning.InvocationMethod;
             myMethodName = warning.MethodName;
-            myTypeElement = warning.TypeElement;
+            myTargetType = warning.TypeElement;
         }
 
-        public bool IsAvailable(IUserDataHolder cache)
+        public override bool IsAvailable(IUserDataHolder cache) => base.IsAvailable(cache) && myTargetType.IsValid();
+        public override string Text => $"Convert to '{myMethodName}<{myTargetType.GetClrName()}>()'";
+
+        // Can't use method name or target type for scoped fixes. This text is weak, but it's a sub-menu under something
+        // like "Convert to 'GetComponent<UnityEngine.Grid>()'", so I think it gets the point across. I don't think
+        // people will be too surprised that this will change other methods, too.
+        public override string ScopedText => "Use strongly typed overloads";
+        protected override ITreeNode TryGetContextTreeNode() => myInvocationExpression;
+
+        protected override Action<ITextControl> ExecutePsiTransaction(ISolution solution, IProgressIndicator progress)
         {
-            return myTypeElement.IsValid() && myInvocationExpression.IsValid();
-        }
-
-        public IEnumerable<IntentionAction> CreateBulbItems()
-        {
-            yield return new IntentionAction(
-                new UseExplicitTypeInsteadOfStringAction(myTypeElement, myInvocationExpression, myMethodName), null,
-                IntentionsAnchors.QuickFixesAnchor);
-        }
-
-
-        private class UseExplicitTypeInsteadOfStringAction : BulbActionBase
-        {
-            private readonly ITypeElement myType;
-            private readonly IInvocationExpression myOldInvocation;
-            private readonly string myMethodName;
-
-            public UseExplicitTypeInsteadOfStringAction(ITypeElement type, IInvocationExpression oldInvocation, string methodName)
+            var factory = CSharpElementFactory.GetInstance(myInvocationExpression);
+            if (myInvocationExpression.InvokedExpression is IReferenceExpression referenceExpression)
             {
-                myType = type;
-                myOldInvocation = oldInvocation;
-                myMethodName = methodName;
-            }
+                var builder = FactoryArgumentsBuilder.Create();
 
-            protected override Action<ITextControl> ExecutePsiTransaction(ISolution solution,
-                IProgressIndicator progress)
-            {
-                var factory = CSharpElementFactory.GetInstance(myOldInvocation);
-                if (myOldInvocation.InvokedExpression is IReferenceExpression referenceExpression)
+                var qualifier = referenceExpression.QualifierExpression;
+                if (qualifier != null)
                 {
-                    var builder = FactoryArgumentsBuilder.Create();
-
-                    var qualifier = referenceExpression.QualifierExpression;
-                    if (qualifier != null)
-                    {
-                        builder.Argument(qualifier);
-                        builder.Append(".");
-                    }
-
-                    builder.Append(myMethodName);
-                    builder.Append("<");
-                    builder.Argument(TypeFactory.CreateType(myType));
-                    builder.Append(">()");
-
-                    var newInvocation = factory.CreateExpression(builder.ToString(), builder.ToArguments());
-                    myOldInvocation.ReplaceBy(newInvocation);
+                    builder.Argument(qualifier);
+                    builder.Append(".");
                 }
 
-                return null;
+                builder.Append(myMethodName);
+                builder.Append("<");
+                builder.Argument(TypeFactory.CreateType(myTargetType));
+                builder.Append(">()");
+
+                var newInvocation = factory.CreateExpression(builder.ToString(), builder.ToArguments());
+                myInvocationExpression.ReplaceBy(newInvocation);
             }
 
-            public override string Text => $"Convert to '{myMethodName}<{myType.GetClrName()}>()'";
+            return null;
         }
     }
 }
