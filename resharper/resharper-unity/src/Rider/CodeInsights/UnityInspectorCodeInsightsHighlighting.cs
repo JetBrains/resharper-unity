6108e0e84a36db8963ac5ae9dc6bb0d671a281aa
Vladimir Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2020-05-06 15:36:09 +0300

    fix registration for UnityCodeInsight Higlighting

--- resharper/resharper-unity/src/Rider/CodeInsights/UnityInspectorCodeInsightsHighlighting.cs
+++ resharper/resharper-unity/src/Rider/CodeInsights/UnityInspectorCodeInsightsHighlighting.cs
@@ -1,28 +1,34 @@
-using System.Collections.Generic;
-using System.Linq;
 using JetBrains.Annotations;
-using JetBrains.Application.UI.Controls.BulbMenu.Items;
 using JetBrains.DocumentModel;
 using JetBrains.ReSharper.Daemon.CodeInsights;
 using JetBrains.ReSharper.Feature.Services.Daemon;
 using JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Stages.Highlightings;
-using JetBrains.ReSharper.Plugins.Unity.Rider.CodeInsights;
 using JetBrains.ReSharper.Psi;
 using JetBrains.Rider.Model;
 using JetBrains.TextControl.DocumentMarkup;
 using Severity = JetBrains.ReSharper.Feature.Services.Daemon.Severity;
 
-[assembly: RegisterHighlighter(UnityInspectorCodeInsightsHighlighting.Id, EffectType = EffectType.NONE,
-    Layer = HighlighterLayer.SYNTAX + 1, NotRecyclable = true, GroupId = HighlighterGroupIds.HIDDEN, TransmitUpdates = true)]
-
-
 namespace JetBrains.ReSharper.Plugins.Unity.Rider.CodeInsights
 {
-    [StaticSeverityHighlighting(Severity.INFO, HighlightingGroupIds.CodeInsightsGroup, AttributeId = Id,
-        OverlapResolve = OverlapResolveKind.NONE)]
+    [ 
+        RegisterHighlighter(
+            Id, 
+            GroupId = HighlighterGroupIds.HIDDEN,
+            Layer = HighlighterLayer.SYNTAX + 1, 
+            EffectType = EffectType.NONE, 
+            NotRecyclable = false, 
+            TransmitUpdates = true
+        )
+    ]
+    [StaticSeverityHighlighting(
+        Severity.INFO,
+        typeof(HighlightingGroupIds.CodeInsights),
+        AttributeId = Id,
+        OverlapResolve = OverlapResolveKind.NONE
+    )]
     public class UnityInspectorCodeInsightsHighlighting : CodeInsightsHighlighting, IUnityHighlighting
     {
-        public const string Id = "UnityInspectorCodeInsights";
+        public new const string Id = "UnityInspectorCodeInsights";
 
         public readonly UnityCodeInsightFieldUsageProvider.UnityPresentationType UnityPresentationType;
 
