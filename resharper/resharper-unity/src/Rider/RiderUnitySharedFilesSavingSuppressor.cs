79e2b845622c1274176e6557b0f6d2d902a8845f
Nikita Lyubimov <lyubimov.nkt@gmail.com>
2020-05-11 15:18:58 +0300

    Add saving suppressor for shared files in unity

---
+++ resharper/resharper-unity/src/Rider/RiderUnitySharedFilesSavingSuppressor.cs
@@ -0,0 +1,46 @@
+using JetBrains.Annotations;
+using JetBrains.DocumentManagers;
+using JetBrains.DocumentModel;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Host.Features.Documents;
+using JetBrains.ReSharper.Plugins.Unity.ProjectModel;
+using JetBrains.Util;
+
+namespace JetBrains.ReSharper.Plugins.Unity.Rider
+{
+    /// <summary>
+    /// Removes auto sync through disk for shared files, to avoid unity refresh
+    /// </summary>
+    [SolutionComponent]
+    public class RiderUnitySharedFilesSavingSuppressor : IRiderDocumentSavingSuppressor
+    {
+        [NotNull] private readonly DocumentToProjectFileMappingStorage myDocumentToProjectFileMappingStorage;
+        [NotNull] private readonly ILogger myLogger;
+
+        public RiderUnitySharedFilesSavingSuppressor(
+            [NotNull] DocumentToProjectFileMappingStorage documentToProjectFileMappingStorage,
+            [NotNull] ILogger logger)
+        {
+            myDocumentToProjectFileMappingStorage = documentToProjectFileMappingStorage;
+            myLogger = logger;
+        }
+        
+        
+        public bool ShouldSuppress(IDocument document, bool forceSaveOpenDocuments)
+        {
+            if (!forceSaveOpenDocuments) return false;
+            
+            var projectFile = myDocumentToProjectFileMappingStorage.TryGetProjectFile(document);
+            var isUnitySharedProjectFile = projectFile != null 
+                                           && projectFile.IsShared() 
+                                           && projectFile.GetProject().IsUnityProject();
+            if (isUnitySharedProjectFile)
+            {
+                myLogger.Verbose("File is shared and contained in Unity project. Skip saving.");
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
