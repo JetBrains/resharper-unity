aa669226846bf8b5167c44a72917a4c979778d6d
Matt Ellis <m.t.ellis@gmail.com>
2020-05-15 00:16:43 +0100

    Add message to display text

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/debugger/breakpoints/UnityPausepointBreakpointType.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/debugger/breakpoints/UnityPausepointBreakpointType.kt
@@ -2,6 +2,9 @@ package com.jetbrains.rider.plugins.unity.debugger.breakpoints
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.xdebugger.breakpoints.SuspendPolicy
+import com.intellij.xdebugger.breakpoints.XLineBreakpoint
+import com.jetbrains.rider.debugger.breakpoint.DotNetLineBreakpointProperties
 import com.jetbrains.rider.debugger.breakpoint.DotNetLineBreakpointType
 import icons.UnityIcons
 import java.util.*
@@ -10,10 +13,12 @@ import javax.swing.Icon
 class UnityPausepointBreakpointType : DotNetLineBreakpointType(Id, Title) {
     companion object {
         const val Id = "UnityPausepointType"
-        const val Title = "Unity pausepoints"
+        const val Title = "Unity Pausepoints"
     }
 
-    override fun getTitle(): String = Title
+    override fun getDisplayText(breakpoint: XLineBreakpoint<DotNetLineBreakpointProperties>?): String {
+        return "Pause Unity editor when debugger reaches " + super.getDisplayText(breakpoint)
+    }
 
     override fun getDisabledIcon(): Icon = UnityIcons.Debugger.Db_disabled_pausepoint
     override fun getEnabledIcon(): Icon = UnityIcons.Debugger.Db_set_pausepoint
@@ -28,9 +33,11 @@ class UnityPausepointBreakpointType : DotNetLineBreakpointType(Id, Title) {
     override fun getVerifiedIconWithNoSuspend() = UnityIcons.Debugger.Db_verified_no_suspend_pausepoint
     // getPendingIcon is not overridden. Base will return null, which then uses set icon
 
+    // Don't allow adding the breakpoint manually
     override fun canPutAt(file: VirtualFile, line: Int, project: Project): Boolean = false
 
     override fun getPriority(): Int = super.getPriority() - 1
+    override fun getDefaultSuspendPolicy() = SuspendPolicy.NONE
 
     override fun getVisibleStandardPanels(): EnumSet<StandardPanels> = EnumSet.of(StandardPanels.DEPENDENCY)
 }
