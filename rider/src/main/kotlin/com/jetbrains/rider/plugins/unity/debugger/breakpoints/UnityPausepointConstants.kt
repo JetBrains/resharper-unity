780239da2b2a9152bed3c9debdea8fa0c756a558
Matt Ellis <m.t.ellis@gmail.com>
2020-05-19 16:25:49 +0100

    Disable pausepoints when not in play mode

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/debugger/breakpoints/UnityPausepointConstants.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/debugger/breakpoints/UnityPausepointConstants.kt
@@ -4,5 +4,6 @@ object UnityPausepointConstants {
     const val pauseEditorCommand = "(UnityEditor.EditorApplication.isPaused = true) ? \"Unity pausepoint hit. Pausing Unity editor at the end of frame\" : \"Unable to pause Unity editor\""
     const val convertToPausepointText = "Convert to Unity pausepoint"
     const val convertToLineBreakpointText = "Convert to line breakpoint"
-    const val unsupportedDebugTargetMessage = "Unity pausepoints are only supported inside a Unity editor process"
+    const val unsupportedDebugTargetMessage = "Unity pausepoints are only available when debugging a Unity editor process"
+    const val unavailableWhenNotPlayingMessage = "Unity pausepoints are only available in play mode"
 }
