efca2a43106d99535b03fd5da0b0d62d8a4bed82
Matt Ellis <m.t.ellis@gmail.com>
2020-05-20 17:22:09 +0100

    Add missing CG tokens as keywords

--- resharper/resharper-unity/src/ShaderLab/Host/Features/SyntaxHighlighting/ShaderLabSyntaxHighlightingProcessor.cs
+++ resharper/resharper-unity/src/ShaderLab/Host/Features/SyntaxHighlighting/ShaderLabSyntaxHighlightingProcessor.cs
@@ -36,19 +36,26 @@ namespace JetBrains.ReSharper.Plugins.Unity.ShaderLab.Host.Features.SyntaxHighli
                    tokenType == ShaderLabTokenType.PP_ERROR
                    || tokenType == ShaderLabTokenType.PP_WARNING
                    || tokenType == ShaderLabTokenType.PP_LINE
+                   // Technically, these are also a kind of preprocessor token
                    || tokenType == ShaderLabTokenType.CG_INCLUDE
+                   || tokenType == ShaderLabTokenType.CG_PROGRAM
+                   || tokenType == ShaderLabTokenType.CG_END
                    || tokenType == ShaderLabTokenType.GLSL_INCLUDE
-                   || tokenType == ShaderLabTokenType.HLSL_INCLUDE;
+                   || tokenType == ShaderLabTokenType.GLSL_PROGRAM
+                   || tokenType == ShaderLabTokenType.GLSL_END
+                   || tokenType == ShaderLabTokenType.HLSL_INCLUDE
+                   || tokenType == ShaderLabTokenType.HLSL_PROGRAM
+                   || tokenType == ShaderLabTokenType.HLSL_END;
         }
 
         protected override string NumberAttributeId => ShaderLabHighlightingAttributeIds.NUMBER;
-        
+
         protected override string KeywordAttributeId => ShaderLabHighlightingAttributeIds.KEYWORD;
-        
+
         protected override string StringAttributeId => ShaderLabHighlightingAttributeIds.STRING;
-        
+
         protected override string LineCommentAttributeId => ShaderLabHighlightingAttributeIds.LINE_COMMENT;
-        
+
         protected override string BlockCommentAttributeId => ShaderLabHighlightingAttributeIds.BLOCK_COMMENT;
     }
 }
