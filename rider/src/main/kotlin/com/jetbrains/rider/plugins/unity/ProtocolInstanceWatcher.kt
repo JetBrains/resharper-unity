497d142dc2ab88b4768b93aae89bf6312c0e23eb
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2020-05-21 13:23:51 +0200

    FileSystemTracker instead of FSW (#1660)
    * use FileSystemTracker instead of FileSystemWatcher
    
    * use FileSystemTracker instead of FileSystemWatcher
    
    * add watch on ProtocolInstance.json
    
    * lost change
    
    * revert
    
    * working solution
    
    * minor fix
    
    * fix on review
    
    * watchService.close(), try to fix tests
    
    * lifetime
    
    * fix
    
    * fix lifetime exception
    
    * simplify code

---
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/ProtocolInstanceWatcher.kt
@@ -0,0 +1,55 @@
+package com.jetbrains.rider.plugins.unity
+
+import com.intellij.openapi.project.Project
+import com.intellij.util.io.exists
+import com.intellij.util.io.isDirectory
+import com.jetbrains.rd.platform.util.application
+import com.jetbrains.rd.util.lifetime.isAlive
+import com.jetbrains.rd.util.reactive.whenTrue
+import com.jetbrains.rdclient.util.idea.LifetimedProjectComponent
+import com.jetbrains.rider.isUnityProject
+import com.jetbrains.rider.model.RdDelta
+import com.jetbrains.rider.model.RdDeltaBatch
+import com.jetbrains.rider.model.RdDeltaType
+import com.jetbrains.rider.model.fileSystemModel
+import com.jetbrains.rider.projectView.solution
+import java.nio.file.*
+import java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY
+import kotlin.concurrent.thread
+
+
+class ProtocolInstanceWatcher(project: Project) : LifetimedProjectComponent(project) {
+    init {
+        if (project.isUnityProject()) {
+            project.solution.isLoaded.whenTrue(componentLifetime) {
+                thread(name = "ProtocolInstanceWatcher") {
+                    val watchService: WatchService = FileSystems.getDefault().newWatchService()
+                    val libraryPath: Path = Paths.get(project.basePath!!, "Library")
+
+                    if (!(libraryPath.exists() && libraryPath.isDirectory())) // todo: rethink, see com.jetbrains.rider.UnityProjectDiscoverer.Companion.hasUnityFileStructure
+                        return@thread
+
+                    libraryPath.register(watchService, ENTRY_MODIFY)
+
+                    it.onTerminationIfAlive {
+                        watchService.close() // releases watchService.take()
+                    }
+
+                    val watchedFileName = "ProtocolInstance.json"
+                    val delta = RdDelta(libraryPath.resolve(watchedFileName).toString(), RdDeltaType.Changed)
+                    var key: WatchKey
+                    while (watchService.take().also { key = it } != null && it.isAlive) {
+                        for (event in key.pollEvents()) {
+                            if (event.context().toString() == watchedFileName) {
+                                application.invokeLater {
+                                    project.solution.fileSystemModel.change.fire(RdDeltaBatch(listOf(delta)))
+                                }
+                            }
+                        }
+                        key.reset()
+                    }
+                }
+            }
+        }
+    }
+}
