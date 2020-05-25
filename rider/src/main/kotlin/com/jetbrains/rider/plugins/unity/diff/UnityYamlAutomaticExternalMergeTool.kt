05485a7dcb0359306e618dda1847bca29ccb1d8a
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2020-05-25 16:01:13 +0200

    call normal merge, when there is conflict

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/diff/UnityYamlAutomaticExternalMergeTool.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/diff/UnityYamlAutomaticExternalMergeTool.kt
@@ -1,5 +1,7 @@
 package com.jetbrains.rider.plugins.unity.diff
 
+import com.intellij.diff.DiffManagerImpl
+import com.intellij.diff.DiffRequestFactory
 import com.intellij.diff.contents.DiffContent
 import com.intellij.diff.contents.FileContent
 import com.intellij.diff.merge.MergeRequest
@@ -9,6 +11,7 @@ import com.intellij.diff.tools.external.ExternalDiffSettings
 import com.intellij.diff.tools.external.ExternalDiffToolUtil
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.SystemInfo
+import com.intellij.openapi.vfs.VirtualFile
 import com.jetbrains.rider.plugins.unity.util.UnityInstallationFinder
 
 class UnityYamlAutomaticExternalMergeTool: AutomaticExternalMergeTool {
@@ -16,19 +19,26 @@ class UnityYamlAutomaticExternalMergeTool: AutomaticExternalMergeTool {
         project ?: return
 
         val settings = ExternalDiffSettings()
-        settings.isMergeTrustExitCode = false
+        settings.isMergeTrustExitCode = true
         val appDataPath = UnityInstallationFinder.getInstance(project).getApplicationContentsPath() ?: return
         val extension = when {
             SystemInfo.isWindows -> ".exe"
             else -> ""
         }
         settings.mergeExePath = appDataPath.resolve("Tools/UnityYAMLMerge" + extension).toString()
-        settings.mergeParameters = "merge %3 %2 %1 %4"
-        ExternalDiffToolUtil.executeMerge(
+        settings.mergeParameters = "merge -p -h --fallback none %3 %2 %1 %4"
+
+        if (!ExternalDiffToolUtil.tryExecuteMerge(
             project,
             settings,
-            request as ThreesideMergeRequest
-        )
+            request as ThreesideMergeRequest)){
+//            val output: VirtualFile = (request.outputContent as FileContent).file // todo: checked cast
+//
+//            DiffRequestFactory.getInstance().createMergeRequest(project, output, newBases, request.title, request.contentTitles,
+//                { result -> request.applyResult(result) })
+
+            DiffManagerImpl.getInstance().showMergeBuiltin(project, request)
+        }
     }
 
     override fun canShow(project: Project?, request: MergeRequest): Boolean {
