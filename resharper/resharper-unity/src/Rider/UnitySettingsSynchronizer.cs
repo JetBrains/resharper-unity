7ca5d70ae942cb3232d0a00efcc6e88164312592
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2020-06-02 12:55:41 +0200

    fix tests

--- resharper/resharper-unity/src/Rider/UnitySettingsSynchronizer.cs
+++ resharper/resharper-unity/src/Rider/UnitySettingsSynchronizer.cs
@@ -18,21 +18,21 @@ namespace JetBrains.ReSharper.Plugins.Unity.Rider
             var entry = boundStore.Schema.GetScalarEntry((UnitySettings s) => s.EnableShaderLabHippieCompletion);
             boundStore.GetValueProperty<bool>(lifetime, entry, null).Change.Advise_HasNew(lifetime, args =>
             {
-                solution.Locks.ExecuteOrQueue(lifetime, "EnableShaderLabHippieCompletion", () =>
+                solution.Locks.ExecuteOrQueueEx(lifetime, "EnableShaderLabHippieCompletion", () =>
                     host.PerformModelAction(rd => rd.EnableShaderLabHippieCompletion.Value = args.New));
             });
 
             var useYamlMergeSetting = boundStore.Schema.GetScalarEntry((UnitySettings s) => s.UseUnityYamlMerge);
             boundStore.GetValueProperty<bool>(lifetime, useYamlMergeSetting, null).Change.Advise_HasNew(lifetime, args =>
             {
-                solution.Locks.ExecuteOrQueue(lifetime, "UseUnityYamlMerge", () =>
+                solution.Locks.ExecuteOrQueueEx(lifetime, "UseUnityYamlMerge", () =>
                     host.PerformModelAction(rd => rd.UseUnityYamlMerge.Value = args.New));
             });
             
             var mergeParametersSetting = boundStore.Schema.GetScalarEntry((UnitySettings s) => s.MergeParameters);
             boundStore.GetValueProperty<string>(lifetime, mergeParametersSetting, null).Change.Advise_HasNew(lifetime, args =>
             {
-                solution.Locks.ExecuteOrQueue(lifetime, "MergeParameters", () =>
+                solution.Locks.ExecuteOrQueueEx(lifetime, "MergeParameters", () =>
                     host.PerformModelAction(rd => rd.MergeParameters.Value = args.New));
             });
         }
