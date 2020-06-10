54e159e25b482702c475ce54152c50ac44eb920f
Vladimir Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2020-06-10 22:43:17 +0300

    add built-in includes

--- resharper/resharper-unity/src/HlslSupport/ShaderLabCppFileLocationTracker.cs
+++ resharper/resharper-unity/src/HlslSupport/ShaderLabCppFileLocationTracker.cs
@@ -1,12 +1,16 @@
 using System.Collections.Generic;
 using System.Linq;
+using JetBrains.Diagnostics;
 using JetBrains.Lifetimes;
 using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Feature.Services.Cpp.Injections;
 using JetBrains.ReSharper.Plugins.Unity.ShaderLab.Psi;
+using JetBrains.ReSharper.Plugins.Unity.ShaderLab.Psi.Tree;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.Caches;
 using JetBrains.ReSharper.Psi.Cpp.Caches;
+using JetBrains.ReSharper.Psi.Files;
+using JetBrains.ReSharper.Psi.Tree;
 using JetBrains.Util;
 
 namespace JetBrains.ReSharper.Plugins.Unity.HlslSupport
@@ -14,11 +18,16 @@ namespace JetBrains.ReSharper.Plugins.Unity.HlslSupport
     [SolutionComponent]
     public class ShaderLabCppFileLocationTracker : CppFileLocationTrackerBase<CppInjectionInfo>
     {
-        public ShaderLabCppFileLocationTracker(Lifetime lifetime, ISolution solution,
+        private readonly ISolution mySolution;
+        private readonly UnityVersion myUnityVersion;
+
+        public ShaderLabCppFileLocationTracker(Lifetime lifetime, ISolution solution, UnityVersion unityVersion,
             IPersistentIndexManager persistentIndexManager)
             : base(
                 lifetime, solution, persistentIndexManager, CppInjectionInfo.Read, CppInjectionInfo.Write)
         {
+            mySolution = solution;
+            myUnityVersion = unityVersion;
         }
 
         protected override CppFileLocation GetCppFileLocation(CppInjectionInfo t)
@@ -47,12 +56,46 @@ namespace JetBrains.ReSharper.Plugins.Unity.HlslSupport
             return false;
         }
 
-        public CppFileLocation GetCgIncludeLocation(IPsiSourceFile sourceFile)
+        private CppFileLocation GetCgIncludeLocation(IPsiSourceFile sourceFile)
         {
             return Map[sourceFile]
                 .Where(d => d.IsInclude)
                 .Select(d => d.ToCppFileLocation())
                 .FirstOrDefault(CppFileLocation.EMPTY);
+        }
+
+        public IEnumerable<CppFileLocation> GetIncludes(CppFileLocation cppFileLocation)
+        {
+            mySolution.GetPsiServices().Files.AssertAllDocumentAreCommitted("All documents should be commited");
+            var sourceFile = cppFileLocation.GetRandomSourceFile(mySolution);
+            var cgInclude = GetCgIncludeLocation(sourceFile);
+            if (!cgInclude.Equals(CppFileLocation.EMPTY))
+                yield return cgInclude;
+    
+            var cgIncludeFolder = CgIncludeDirectoryTracker.GetCgIncludeFolderPath(myUnityVersion);        
+            if (!cgIncludeFolder.ExistsDirectory)
+                yield break;
+            
+            var range = cppFileLocation.RootRange;
+            Assertion.Assert(range.IsValid, "range.IsValid");
+            var program = sourceFile.GetDominantPsiFile<ShaderLabLanguage>()?.GetContainingNodeAt<ICgContent>(new TreeOffset(range.StartOffset));
+            Assertion.Assert(program != null, "program != null");
+            
+            if (program?.Parent?.FirstChild is ICgProgramBlock)
+            {
+                var hlslSupport = cgIncludeFolder.Combine("HLSLSupport.cginc");
+                if (hlslSupport.ExistsFile)
+                {
+                    yield return new CppFileLocation(hlslSupport);
+                }
+                
+                var variables = cgIncludeFolder.Combine("UnityShaderVariables.cginc");
+                if (variables.ExistsFile)
+                {
+                    yield return new CppFileLocation(variables);
+                }
+            }
+            
         }
     }
 }
--- resharper/resharper-unity/src/HlslSupport/ShaderLabInclusionContextProvider.cs
+++ resharper/resharper-unity/src/HlslSupport/ShaderLabInclusionContextProvider.cs
@@ -41,10 +41,10 @@ namespace JetBrains.ReSharper.Plugins.Unity.HlslSupport
 
             // TODO 1) is cache ready? what will happen under document transaction? check for bad moment?
             // TODO 2) what will happen under psi transaction? include in cache could be out-of date. Try use include quickfix when cginclude is after cgprogram where QF is used
-            var includeLocation = shaderCache.GetCgIncludeLocation(rootFile.GetRandomSourceFile(cache.Solution));
+            var includeLocation = shaderCache.GetIncludes(rootFile);
 
             return InjectInclusionContextProviderUtil.CreateInclusionContextResult(cache, rootFile,
-                new[] {includeLocation}, options, properties, null, cacheVersion, lifetime);
+                includeLocation, options, properties, null, cacheVersion, lifetime);
         }
     }
 }
