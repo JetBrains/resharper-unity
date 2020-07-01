83a049ac0e71710f53d398b8b90ffacfc12d09b9
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2020-07-01 19:24:32 +0200

    use MONO_ARGUMENTS

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/run/configurations/unityExe/UnityExeDebugProfileState.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/run/configurations/unityExe/UnityExeDebugProfileState.kt
@@ -1,48 +1,40 @@
 package com.jetbrains.rider.plugins.unity.run.configurations.unityExe
 
-import com.intellij.execution.DefaultExecutionResult
 import com.intellij.execution.ExecutionResult
 import com.intellij.execution.Executor
 import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.execution.process.KillableProcessHandler
 import com.intellij.execution.process.ProcessAdapter
 import com.intellij.execution.process.ProcessEvent
+import com.intellij.execution.process.ProcessListener
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.execution.runners.ProgramRunner
-import com.intellij.execution.ui.ConsoleView
 import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.util.SystemInfo
-import com.intellij.util.io.exists
-import com.intellij.util.ui.UIUtil
-import com.jetbrains.rd.platform.util.application
+import com.intellij.openapi.util.Key
+import com.jetbrains.rd.util.addUnique
 import com.jetbrains.rd.util.lifetime.Lifetime
-import com.jetbrains.rd.util.lifetime.isAlive
 import com.jetbrains.rd.util.lifetime.onTermination
 import com.jetbrains.rider.debugger.DebuggerHelperHost
 import com.jetbrains.rider.debugger.DebuggerWorkerPlatform
 import com.jetbrains.rider.debugger.DebuggerWorkerProcessHandler
-import com.jetbrains.rider.plugins.unity.run.UnityPlayerListener
+import com.jetbrains.rider.debugger.tryWriteMessageToConsoleView
+import com.jetbrains.rider.model.debuggerWorker.OutputMessageWithSubject
+import com.jetbrains.rider.model.debuggerWorker.OutputSubject
+import com.jetbrains.rider.model.debuggerWorker.OutputType
 import com.jetbrains.rider.run.ExternalConsoleMediator
 import com.jetbrains.rider.run.WorkerRunInfo
 import com.jetbrains.rider.run.configurations.remote.MonoConnectRemoteProfileState
 import com.jetbrains.rider.run.configurations.remote.RemoteConfiguration
-import com.jetbrains.rider.run.createConsole
 import com.jetbrains.rider.run.createEmptyConsoleCommandLine
+import com.jetbrains.rider.util.NetUtils
 import com.jetbrains.rider.util.idea.createNestedAsyncPromise
 import org.jetbrains.concurrency.Promise
-import java.io.IOException
-import java.net.ServerSocket
-import java.nio.file.Paths
-import kotlin.math.absoluteValue
 
 class UnityExeDebugProfileState(private val exeConfiguration : UnityExeConfiguration, private val remoteConfiguration: RemoteConfiguration,
                                 executionEnvironment: ExecutionEnvironment)
     : MonoConnectRemoteProfileState(remoteConfiguration, executionEnvironment) {
 
     private val logger = Logger.getInstance(UnityExeDebugProfileState::class.java)
-    private val project = executionEnvironment.project
-    private lateinit var console: ConsoleView
-    private lateinit var targetProcessHandler: KillableProcessHandler
 
     override fun execute(executor: Executor?, runner: ProgramRunner<*>): ExecutionResult? {
         throw UnsupportedOperationException("Should use overload with session")
@@ -53,20 +45,10 @@ class UnityExeDebugProfileState(private val exeConfiguration : UnityExeConfigura
     }
 
     override fun execute(executor: Executor, runner: ProgramRunner<*>, workerProcessHandler: DebuggerWorkerProcessHandler, lifetime: Lifetime): ExecutionResult {
-        lifetime.onTermination {
-            if (!targetProcessHandler.isProcessTerminated)
-                targetProcessHandler.destroyProcess()
-        }
-
-        return DefaultExecutionResult(console, workerProcessHandler)
-    }
-
-    override fun createWorkerRunCmd(lifetime: Lifetime, helper: DebuggerHelperHost, port: Int): Promise<WorkerRunInfo> {
-        val result = lifetime.createNestedAsyncPromise<WorkerRunInfo>()
-        val nestedLifetimeDef = lifetime.createNested()
-
+        val envs = exeConfiguration.parameters.envs.toMutableMap()
+        envs.addUnique(lifetime, "MONO_ARGUMENTS", "--debugger-agent=transport=dt_socket,address=127.0.0.1:${remoteConfiguration.port},server=n,suspend=y")
         val runCommandLine = createEmptyConsoleCommandLine(exeConfiguration.parameters.useExternalConsole)
-            .withEnvironment(exeConfiguration.parameters.envs)
+            .withEnvironment(envs)
             .withParentEnvironmentType(if (exeConfiguration.parameters.isPassParentEnvs) {
                 GeneralCommandLine.ParentEnvironmentType.CONSOLE
             } else {
@@ -76,73 +58,52 @@ class UnityExeDebugProfileState(private val exeConfiguration : UnityExeConfigura
             .withWorkDirectory(exeConfiguration.parameters.workingDirectory)
             .withParameters(exeConfiguration.parameters.programParameters)
 
-        targetProcessHandler = if (exeConfiguration.parameters.useExternalConsole)
-            ExternalConsoleMediator.createProcessHandler(runCommandLine) as KillableProcessHandler
-        else
-            KillableProcessHandler(runCommandLine)
         val commandLineString = runCommandLine.commandLineString
-        logger.info("Process started: $commandLineString")
-        targetProcessHandler.addProcessListener(object: ProcessAdapter() {
-            override fun processTerminated(event: ProcessEvent){
-                logger.info("Process terminated: $commandLineString")
-                if (lifetime.isAlive)
-                    result.setError("Process $commandLineString has terminated.")
+        val monoConnectResult = super.execute(executor, runner, workerProcessHandler)
+        workerProcessHandler.debuggerWorkerRealHandler.addProcessListener(object : ProcessAdapter() {
+            override fun startNotified(event: ProcessEvent) {
+                val targetProcessHandler = if (exeConfiguration.parameters.useExternalConsole)
+                    ExternalConsoleMediator.createProcessHandler(runCommandLine) as KillableProcessHandler
+                else
+                    KillableProcessHandler(runCommandLine)
+
+                lifetime.onTermination {
+                    if (!targetProcessHandler.isProcessTerminated) {
+                        targetProcessHandler.killProcess()
+                    }
+                }
+
+                targetProcessHandler.addProcessListener(object : ProcessListener {
+                    override fun onTextAvailable(processEvent: ProcessEvent, key: Key<*>) {
+                        monoConnectResult.executionConsole.tryWriteMessageToConsoleView(
+                            OutputMessageWithSubject(processEvent.text, OutputType.Info, OutputSubject.Default)
+                        )
+                    }
+
+                    override fun processTerminated(processEvent: ProcessEvent) {
+                        monoConnectResult.executionConsole.tryWriteMessageToConsoleView(OutputMessageWithSubject(output = "Process \"$commandLineString\" terminated with exit code ${processEvent.exitCode}.\r\n", type = OutputType.Warning, subject = OutputSubject.Default))
+                    }
+
+                    override fun startNotified(processEvent: ProcessEvent) {
+                        monoConnectResult.executionConsole.tryWriteMessageToConsoleView(OutputMessageWithSubject("Process \"$commandLineString\" started.\r\n", OutputType.Info, OutputSubject.Default))
+                    }
+                })
+
+                targetProcessHandler.startNotify()
+                super.startNotified(event)
             }
         })
 
-        console = createConsole(exeConfiguration.parameters.useExternalConsole, targetProcessHandler, commandLineString, executionEnvironment.project)
-        console.attachToProcess(targetProcessHandler)
-        targetProcessHandler.startNotify()
-
-        // Read player-connection-guid from boot.config near the exePath
-        val exePath = Paths.get(exeConfiguration.parameters.exePath)
-        val config = if (SystemInfo.isMac) {
-            exePath.parent.parent.resolve("Resources/Data/boot.config")
-        } else {
-            exePath.parent.resolve(exePath.toFile().nameWithoutExtension + "_Data").resolve("boot.config")
-        }
-        assert(config.exists()) { "Config file $config doesn't exist." }
-        val guidPrefix = "player-connection-guid="
-        val ipPrefix = "player-connection-ip="
-        val lines = config.toFile().readLines()
-        val guid = lines.first { line -> line.startsWith(guidPrefix) }.substring(guidPrefix.length).toLong().absoluteValue
-        val ips = lines.filter { line -> line.startsWith(ipPrefix) }.map { it.substring(ipPrefix.length) }.toList()
-
-        application.executeOnPooledThread {
-            UnityPlayerListener(project, {
-                if (!nestedLifetimeDef.lifetime.isAlive)
-                    return@UnityPlayerListener
-
-                if (!it.isEditor && guid == it.editorId && ips.contains(it.host)) {
-                    if (!it.allowDebugging)
-                        result.setError("Make sure the \"Script Debugging\" is enabled for this Standalone Player.") //https://docs.unity3d.com/Manual/BuildSettings.html
-
-                    if (!SystemInfo.isMac)
-                        while (lifetime.isAlive && isAvailable(it.debuggerPort)) // this hack doesn't work on mac
-                            Thread.sleep(10)
-                    else
-                        if (lifetime.isAlive)
-                            Thread.sleep(10000)
-
-                    UIUtil.invokeLaterIfNeeded {
-                        logger.trace("Connecting to Player with port: ${it.debuggerPort}")
-                        remoteConfiguration.port = it.debuggerPort
-                        result.setResult(createWorkerRunInfoFor(port, DebuggerWorkerPlatform.AnyCpu))
-                    }
-                    nestedLifetimeDef.terminate()
-                }
-            }, {}, nestedLifetimeDef.lifetime)
-        }
-        return result
+        return monoConnectResult
     }
 
-    private fun isAvailable(port: Int): Boolean {
-        var portFree = false
-        try {
-            ServerSocket(port).use { portFree = true }
-        } catch (e: IOException) {
-            portFree = false
-        }
-        return portFree
+    override fun createWorkerRunCmd(lifetime: Lifetime, helper: DebuggerHelperHost, port: Int): Promise<WorkerRunInfo> {
+        remoteConfiguration.listenPortForConnections = true
+        remoteConfiguration.port = NetUtils.findFreePort(500013, setOf(port))
+        remoteConfiguration.address="127.0.0.1"
+
+        val result = lifetime.createNestedAsyncPromise<WorkerRunInfo>()
+        result.setResult(createWorkerRunInfoFor(port, DebuggerWorkerPlatform.AnyCpu))
+        return result
     }
 }
