8ba733d764c548f2f60948a80f52e558ed6a7011
Konstantin Krukhmalev <Konstantin.Krukhmalev@jetbrains.com>
2020-08-07 23:32:56 +0300

    remove .Replace('/', '\\') from paths

--- rider/testData/solutions/scripts/DumpExternalEditor.cs
+++ rider/testData/solutions/scripts/DumpExternalEditor.cs
@@ -7,7 +7,7 @@ public class DumpExternalEditor
 {
     static DumpExternalEditor()
     {
-        var path = Path.Combine(Application.dataPath.Replace('/', '\\'), "ExternalEditor.txt");
-        File.WriteAllText(path, EditorPrefs.GetString("kScriptsDefaultApp") ?? "Unknown");
+        var path = Path.Combine(Application.dataPath, "ExternalEditor.txt");
+        File.WriteAllText(path, EditorPrefs.GetString("kScriptsDefaultApp", null) ?? "Unknown");
     }
 }
