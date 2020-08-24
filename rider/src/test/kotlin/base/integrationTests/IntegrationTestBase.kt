f534474ba4c031a9edca1d26c1cf3a40c00a6348
Konstantin Krukhmalev <Konstantin.Krukhmalev@jetbrains.com>
2020-08-25 01:05:29 +0300

    split all @After and @Before test methods between 3 base classes

---
+++ rider/src/test/kotlin/base/integrationTests/IntegrationTestBase.kt
@@ -0,0 +1,52 @@
+package base.integrationTests
+
+import com.intellij.openapi.rd.createNestedDisposable
+import com.intellij.openapi.util.SystemInfo
+import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess
+import com.intellij.util.io.exists
+import com.jetbrains.rd.util.lifetime.LifetimeDefinition
+import com.jetbrains.rider.model.RdUnityModel
+import com.jetbrains.rider.model.rdUnityModel
+import com.jetbrains.rider.projectView.solution
+import com.jetbrains.rider.test.base.BaseTestWithSolution
+import org.testng.annotations.AfterClass
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.time.Duration
+
+abstract class IntegrationTestBase : BaseTestWithSolution() {
+
+    companion object {
+        val defaultTimeout: Duration = Duration.ofSeconds(120)
+    }
+
+    private val lifetimeDefinition = LifetimeDefinition()
+    val lifetime = lifetimeDefinition.lifetime
+
+    val rdUnityModel: RdUnityModel
+        get() = project.solution.rdUnityModel
+
+    private val unityPath = when {
+        SystemInfo.isWindows -> "C:/Program Files/Unity"
+        SystemInfo.isMac -> "/Applications/Unity"
+        else -> throw Exception("Not implemented")
+    }
+
+    override val waitForCaches = true
+    override fun preprocessTempDirectory(tempDir: File) {
+        VfsRootAccess.allowRootAccess(lifetimeDefinition.createNestedDisposable(), unityPath)
+
+        // Needed, because com.jetbrains.rider.plugins.unity.ProtocolInstanceWatcher
+        //  isn't initialized without correct unity file structure
+        val libraryFolder = Paths.get(tempDir.toString(), "Library")
+        if (!libraryFolder.exists()) {
+            Files.createDirectory(libraryFolder)
+        }
+    }
+
+    @AfterClass
+    fun terminateLifetime() {
+        lifetimeDefinition.terminate()
+    }
+}
---
+++ rider/src/test/kotlin/base/integrationTests/IntegrationTestWithEditorBase.kt
@@ -0,0 +1,43 @@
+package base.integrationTests
+
+import com.jetbrains.rider.test.scriptingApi.buildSolutionWithReSharperBuild
+import org.testng.annotations.AfterMethod
+import org.testng.annotations.BeforeMethod
+
+abstract class IntegrationTestWithEditorBase : IntegrationTestBase() {
+    protected open val resetEditorPrefs: Boolean
+        get() = false
+
+    protected open val useRiderTestPath: Boolean
+        get() = false
+
+    protected open val batchMode: Boolean
+        get() = true
+
+    private lateinit var unityProcess: Process
+
+    @AfterMethod(alwaysRun = true)
+    fun killUnityAndCheckSwea() {
+        killUnity(unityProcess)
+        checkSweaInSolution()
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    fun startUnityProcessAndWait() {
+        installPlugin()
+        val unityTestEnvironment = testMethod.unityEnvironment
+        unityProcess =
+            if (unityTestEnvironment != null)
+                startUnity(unityTestEnvironment.resetEditorPrefs, unityTestEnvironment.useRiderTestPath, unityTestEnvironment.batchMode)
+            else
+                startUnity(resetEditorPrefs, useRiderTestPath, batchMode)
+
+        waitFirstScriptCompilation()
+        waitConnection()
+    }
+
+    @BeforeMethod(alwaysRun = true, dependsOnMethods = ["startUnityProcessAndWait"])
+    fun buildSolutionAndWaitRunConfigurations() {
+        buildSolutionWithReSharperBuild()
+    }
+}
