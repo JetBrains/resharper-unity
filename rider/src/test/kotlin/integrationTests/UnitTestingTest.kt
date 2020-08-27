f9ad581b3745a1be082a804253556f084eb6f7ba
Konstantin Krukhmalev <Konstantin.Krukhmalev@jetbrains.com>
2020-08-27 11:12:05 +0300

    add Unit Testing tests (case for RIDER-46658)

---
+++ rider/src/test/kotlin/integrationTests/UnitTestingTest.kt
@@ -0,0 +1,43 @@
+package integrationTests
+
+import base.integrationTests.IntegrationTestWithEditorBase
+import com.jetbrains.rider.test.scriptingApi.RiderUnitTestScriptingFacade
+import com.jetbrains.rider.test.scriptingApi.buildSolutionWithReSharperBuild
+import com.jetbrains.rider.test.scriptingApi.replaceFileContent
+import com.jetbrains.rider.test.scriptingApi.withUtFacade
+import org.testng.annotations.Test
+
+class UnitTestingTest : IntegrationTestWithEditorBase() {
+    override fun getSolutionDirectoryName() = "SimpleUnityUnitTestingProject"
+
+    @Test
+    fun checkRunAllTestsFromSolution() {
+        buildSolutionWithReSharperBuild()
+        withUtFacade(project) {
+            it.waitForDiscovering(5)
+            val session = it.runAllTestsInSolution(
+                5,
+                RiderUnitTestScriptingFacade.defaultTimeout,
+                5,
+                testGoldFile
+            )
+            it.compareSessionTreeWithGold(session, testGoldFile)
+        }
+    }
+
+    @Test(description = "RIDER-46658")
+    fun checkTestFixtureAndValueSourceTests() {
+        replaceFileContent(project, "NewTestScript.cs")
+        buildSolutionWithReSharperBuild()
+        withUtFacade(project) {
+            it.waitForDiscovering(14)
+            val session = it.runAllTestsInSolution(
+                16,
+                RiderUnitTestScriptingFacade.defaultTimeout,
+                16,
+                testGoldFile
+            )
+            it.compareSessionTreeWithGold(session, testGoldFile)
+        }
+    }
+}
