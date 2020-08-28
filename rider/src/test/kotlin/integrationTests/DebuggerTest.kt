8e30d99eae0323e9c296c86e32c23d289d9c37a4
Konstantin Krukhmalev <Konstantin.Krukhmalev@jetbrains.com>
2020-08-28 12:33:25 +0300

    refactoring + add replaceNewBehaviourScriptContentIfNeeded() before method

--- rider/src/test/kotlin/integrationTests/DebuggerTest.kt
+++ rider/src/test/kotlin/integrationTests/DebuggerTest.kt
@@ -3,27 +3,37 @@ package integrationTests
 import base.integrationTests.*
 import com.jetbrains.rider.test.framework.combine
 import com.jetbrains.rider.test.scriptingApi.*
+import org.testng.annotations.BeforeMethod
 import org.testng.annotations.Test
-import base.integrationTests.waitForUnityEditorPlaying as waitForUnityEditorPlaying1
 
 class DebuggerTest : IntegrationDebuggerTestBase() {
     override fun getSolutionDirectoryName() = "SimpleUnityProjectWithoutPlugin"
 
+    @BeforeMethod(alwaysRun = true)
+    fun replaceNewBehaviourScriptContentIfNeeded() {
+        val newBehaviourScript = "NewBehaviourScript.cs"
+
+        if (testCaseSourceDirectory.resolve(newBehaviourScript).exists()) {
+            replaceFileContent(project, newBehaviourScript)
+            rebuildSolutionWithReSharperBuild()
+        }
+
+        selectAttachDebuggerToUnityEditorAndPlayConfiguration()
+    }
+
     @Test
     fun checkUnityPausePoint() {
-        selectAttachDebuggerToUnityEditorAndPlayConfiguration()
-
         val pauseFile = activeSolutionDirectory.combine("Assets", "pause").absoluteFile
         debugUnityProgramWithoutGold(
             {
                 toggleUnityPausepoint("NewBehaviourScript.cs", 17, "System.IO.File.Exists(\"${pauseFile.path}\")")
             }
         ) {
-            waitForUnityEditorPlaying1()
+            waitForUnityEditorPlaying()
             pauseFile.createNewFile()
             waitForUnityEditorPaused()
             unpause()
-            waitForUnityEditorPlaying1()
+            waitForUnityEditorPlaying()
         }
     }
 
