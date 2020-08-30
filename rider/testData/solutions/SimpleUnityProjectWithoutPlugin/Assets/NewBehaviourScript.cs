1d352f43f21a32e61755db6e0d3997b9fdad0f0e
Konstantin Krukhmalev <Konstantin.Krukhmalev@jetbrains.com>
2020-08-30 16:21:26 +0300

    change default NewBehavviourScript in tests solutions

--- rider/testData/solutions/SimpleUnityProjectWithoutPlugin/Assets/NewBehaviourScript.cs
+++ rider/testData/solutions/SimpleUnityProjectWithoutPlugin/Assets/NewBehaviourScript.cs
@@ -1,20 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void Start() => Debug.Log("Play");
+
+    private void OnApplicationFocus(bool hasFocus) => Debug.Log(hasFocus ? "Play" : "Pause");
+
+    private void OnApplicationQuit() => Debug.Log("Idle");
 
     // Update is called once per frame
     void Update()
     {
          // check that C# LangLevel is 7.3, fails if it is 7.1
          int binaryNotation = 0b_0001_1110_1000_0100_1000_0000; // 2 million
-         Debug.Log(binaryNotation);
+         Debug.Log("Step");
     }
 }
