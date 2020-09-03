11f3720292e5803bb9888d5a254aeda4b9b9dbcb
Konstantin Krukhmalev <Konstantin.Krukhmalev@jetbrains.com>
2020-09-04 00:13:41 +0300

    add simple Rider+Unity+dotCover test

---
+++ rider/src/test/kotlin/integrationTests/DotCoverTest.kt
@@ -0,0 +1,34 @@
+package integrationTests
+
+import base.integrationTests.IntegrationTestWithEditorBase
+import com.jetbrains.rider.test.scriptingApi.buildSolutionWithReSharperBuild
+import com.jetbrains.rider.test.scriptingApi.withDcFacade
+import org.testng.annotations.Test
+import java.io.File
+
+class DotCoverTest : IntegrationTestWithEditorBase() {
+    override fun getSolutionDirectoryName() = "SimpleUnityUnitTestingProject"
+
+    override val withCoverage: Boolean
+        get() = true
+
+    override fun preprocessTempDirectory(tempDir: File) {
+        super.preprocessTempDirectory(tempDir)
+
+        val newTestScript = "NewTestScript.cs"
+        val sourceScript = testCaseSourceDirectory.resolve(newTestScript)
+        if (sourceScript.exists()) {
+            sourceScript.copyTo(tempDir.resolve("Assets").resolve("Tests").resolve(newTestScript), true)
+        }
+    }
+
+    @Test(enabled = false)
+    fun checkCoverAllTestsFromSolution() {
+        buildSolutionWithReSharperBuild()
+        withDcFacade(project) { ut, dc ->
+            ut.waitForDiscovering(5)
+            ut.coverAllTestsInSolution(5)
+            dc.compareCoverageTreeWithGold(testGoldFile)
+        }
+    }
+}
