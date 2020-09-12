4d758c3a01022db6973ad32ff0417997951005dd
Konstantin Krukhmalev <Konstantin.Krukhmalev@jetbrains.com>
2020-09-12 20:07:54 +0300

    fix test data

--- rider/testData/integrationTests/PlayModeTest/checkPlayModeLogs/source/NewBehaviourScript.cs
+++ rider/testData/integrationTests/PlayModeTest/checkPlayModeLogs/source/NewBehaviourScript.cs
@@ -1,18 +1,22 @@
 using UnityEngine;
+using System.Threading;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     void Start() {
         Debug.Log("Start");
+        new Thread(() => { Debug.Log("StartFromBackgroundThread"); }).Start();
     }
 
     void Update()
     {
         Debug.Log("Update");
+        new Thread(() => { Debug.Log("UpdateFromBackgroundThread"); }).Start();
     }
 
     void OnApplicationQuit()
     {
         Debug.Log("Quit");
+        new Thread(() => { Debug.Log("QuitFromBackgroundThread"); }).Start();
     }
 }
