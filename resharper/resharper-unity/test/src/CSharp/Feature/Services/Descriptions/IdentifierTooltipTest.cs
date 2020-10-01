542da282f63e82cc4119cb5a5b240894987e077c
Matt Ellis <m.t.ellis@gmail.com>
2020-10-01 13:46:42 +0100

    Hack the tests to fix the build

--- resharper/resharper-unity/test/src/CSharp/Feature/Services/Descriptions/IdentifierTooltipTest.cs
+++ resharper/resharper-unity/test/src/CSharp/Feature/Services/Descriptions/IdentifierTooltipTest.cs
@@ -5,12 +5,13 @@ using NUnit.Framework;
 
 namespace JetBrains.ReSharper.Plugins.Unity.Tests.CSharp.Feature.Services.Descriptions
 {
+    // TODO: WHY DOES TESTSETTING NOT WORK!??!
     [TestUnity]
     // The tests include tooltips from all highlighters at the caret position, which will include gutter icons for
     // ReSharper, but not Rider as we prefer Code Vision. Force gutter icons on to avoid splitting the test per product.
     // TODO: Include tests for this setting always on, and always off
     // ReSharper currently ignores this option, and I can't get [TestSetting] to work correctly...
-    [TestSetting(typeof(UnitySettings), nameof(UnitySettings.GutterIconMode), GutterIconMode.Always)]
+    // [TestSetting(typeof(UnitySettings), nameof(UnitySettings.GutterIconMode), GutterIconMode.Always)]
     public class IdentifierTooltipTest : IdentifierTooltipTestBase
     {
         protected override string RelativeTestDataPath => @"CSharp\Daemon\IdentifierTooltip";
@@ -20,8 +21,13 @@ namespace JetBrains.ReSharper.Plugins.Unity.Tests.CSharp.Feature.Services.Descri
         // public void EventFunction01() { DoNamedTest(); }
         // [Test, TestSetting(typeof(UnitySettings), nameof(UnitySettings.GutterIconMode), GutterIconMode.None)]
         // public void EventFunction02() { DoNamedTest(); }
+#if RIDER
+        [Test] public void EventFunction01() { DoNamedTest(); }
+        [Test] public void EventFunction02() { DoNamedTest(); }
+#else
         [Test] public void EventFunctionWithGutterIcon01() { DoNamedTest(); }
         [Test] public void EventFunctionWithGutterIcon02() { DoNamedTest(); }
+#endif
         [Test] public void EventFunctionParameter01() { DoNamedTest(); }
         [Test] public void EventFunctionParameter02() { DoNamedTest(); }
     }
