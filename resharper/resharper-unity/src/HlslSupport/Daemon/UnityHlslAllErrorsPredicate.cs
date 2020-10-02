540a84abf59e77c895e8d124cf76b64ac796ceed
Matt Ellis <m.t.ellis@gmail.com>
2020-10-02 12:26:31 +0100

    Cache settings for performance

--- resharper/resharper-unity/src/HlslSupport/Daemon/UnityHlslAllErrorsPredicate.cs
+++ resharper/resharper-unity/src/HlslSupport/Daemon/UnityHlslAllErrorsPredicate.cs
@@ -1,21 +1,32 @@
 using JetBrains.Application.Settings;
 using JetBrains.Collections.Viewable;
+using JetBrains.DataFlow;
 using JetBrains.Lifetimes;
 using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Plugins.Unity.ProjectModel;
 using JetBrains.ReSharper.Plugins.Unity.Settings;
 using JetBrains.ReSharper.Psi.Cpp.Tree;
+using JetBrains.ReSharper.Psi.Util;
 
 namespace JetBrains.ReSharper.Plugins.Unity.HlslSupport.Daemon
 {
     [SolutionComponent]
     public class UnityHlslAllErrorsPredicate : IHlslUnresolvedUnqualifiedNamesErrorsPredicate {
         private readonly UnitySolutionTracker myUnitySolutionTracker;
+        private readonly IProperty<bool> mySuppressShaderErrors;
+        private readonly IProperty<bool> mySuppressShaderErrorsInRenderPipeline;
 
         public UnityHlslAllErrorsPredicate(Lifetime lifetime, ISolution solution,
-                                           UnitySolutionTracker unitySolutionTracker)
+                                           UnitySolutionTracker unitySolutionTracker,
+                                           IApplicationWideContextBoundSettingStore settingsStore)
         {
             myUnitySolutionTracker = unitySolutionTracker;
+
+            // Cache the suppression settings for performance
+            mySuppressShaderErrors = settingsStore.BoundSettingsStore
+                .GetValueProperty(lifetime, (UnitySettings key) => key.SuppressShaderErrorHighlighting);
+            mySuppressShaderErrorsInRenderPipeline = settingsStore.BoundSettingsStore
+                .GetValueProperty(lifetime, (UnitySettings key) => key.SuppressShaderErrorHighlightingInRenderPipelinePackages);
         }
 
         public bool SuppressUnresolvedErrors(CppFile cppFile, IContextBoundSettingsStore settingsStore)
@@ -23,15 +34,14 @@ namespace JetBrains.ReSharper.Plugins.Unity.HlslSupport.Daemon
             if (!myUnitySolutionTracker.IsUnityProject.HasTrueValue())
                 return false;
 
-            if (settingsStore.GetValue((UnitySettings key) =>
-                key.SuppressShaderErrorHighlightingInRenderPipelinePackages))
+            if (mySuppressShaderErrorsInRenderPipeline.Value)
             {
                 var location = cppFile.File;
                 if (location.FullPath.Contains(".render-pipelines"))
                     return true;
             }
 
-            return settingsStore.GetValue((UnitySettings key) => key.SuppressShaderErrorHighlighting);
+            return mySuppressShaderErrors.Value;
         }
 
         public bool SuppressUnresolvedUnqualifiedErrors(CppFile cppFile, IContextBoundSettingsStore settingsStore)
