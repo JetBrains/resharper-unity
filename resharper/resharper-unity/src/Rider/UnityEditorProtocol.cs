a5e25ae0db7c49c883dd5a14df5ec5c05266731b
Matt Ellis <m.t.ellis@gmail.com>
2020-10-13 14:20:34 +0100

    Refactor UnityEditorProtocol. Extract AdviseModel

--- resharper/resharper-unity/src/Rider/UnityEditorProtocol.cs
+++ resharper/resharper-unity/src/Rider/UnityEditorProtocol.cs
@@ -39,8 +39,7 @@ namespace JetBrains.ReSharper.Plugins.Unity.Rider
     public class UnityEditorProtocol
     {
         private readonly JetHashSet<FileSystemPath> myPluginInstallations;
-
-        private readonly Lifetime myComponentLifetime;
+        private readonly Lifetime myLifetime;
         private readonly SequentialLifetimes mySessionLifetimes;
         private readonly ILogger myLogger;
         private readonly IScheduler myDispatcher;
@@ -53,6 +52,8 @@ namespace JetBrains.ReSharper.Plugins.Unity.Rider
         private readonly IHostProductInfo myHostProductInfo;
         private readonly FrontendBackendHost myHost;
         private readonly IContextBoundSettingsStoreLive myBoundSettingsStore;
+
+        private DateTime myLastChangeTime;
 
         [NotNull]
         public readonly ViewableProperty<BackendUnityModel> BackendUnityModel = new ViewableProperty<BackendUnityModel>(null);
@@ -67,7 +68,7 @@ namespace JetBrains.ReSharper.Plugins.Unity.Rider
         {
             myPluginInstallations = new JetHashSet<FileSystemPath>();
 
-            myComponentLifetime = lifetime;
+            myLifetime = lifetime;
             myLogger = logger;
             myDispatcher = dispatcher;
             myLocks = locks;
@@ -92,31 +93,111 @@ namespace JetBrains.ReSharper.Plugins.Unity.Rider
 
                 // todo: consider non-Unity Solution with Unity-generated projects
                 var protocolInstancePath = solFolder.Combine("Library/ProtocolInstance.json");
-                fileSystemTracker.AdviseFileChanges(lf, protocolInstancePath, OnChangeAction);
+                fileSystemTracker.AdviseFileChanges(lf, protocolInstancePath, OnProtocolInstanceJsonChange);
+
                 // connect on start of Rider
-                CreateProtocols(protocolInstancePath);
+                CreateProtocol(protocolInstancePath);
             });
         }
 
-        private DateTime myLastChangeTime;
+        private void SafeExecuteOrQueueEx(string name, Action action)
+        {
+            if (myLifetime.IsAlive) myLocks.ExecuteOrQueueEx(myLifetime, name, action);
+        }
 
-        private void OnChangeAction(FileSystemChangeDelta delta)
+        private void OnProtocolInstanceJsonChange(FileSystemChangeDelta delta)
         {
             // Connect when protocols.json is updated (AppDomain start/reload in Unity editor)
             if (delta.ChangeType != FileSystemChangeType.ADDED && delta.ChangeType != FileSystemChangeType.CHANGED) return;
             if (delta.NewPath.FileModificationTimeUtc == myLastChangeTime) return;
             myLastChangeTime = delta.NewPath.FileModificationTimeUtc;
-            if (!myComponentLifetime.IsTerminated)
+
+            SafeExecuteOrQueueEx("CreateProtocol", () => CreateProtocol(delta.NewPath));
+        }
+
+        private void CreateProtocol(FileSystemPath protocolInstancePath)
+        {
+            var protocolInstance = GetProtocolInstanceData(protocolInstancePath);
+            if (protocolInstance == null)
+                return;
+
+            myLogger.Info($"EditorPlugin protocol port {protocolInstance.Port} for Solution: {protocolInstance.SolutionName}.");
+
+            if (protocolInstance.ProtocolGuid != ProtocolCompatibility.ProtocolGuid)
             {
-                myLocks.ExecuteOrQueue(myComponentLifetime, "CreateProtocol",
-                    () => CreateProtocols(delta.NewPath));
+                OnOutOfSync(myLifetime);
+                myLogger.Info("Avoid attempt to create protocol, incompatible.");
+                return;
+            }
+
+            try
+            {
+                var thisSessionLifetime = mySessionLifetimes.Next();
+                myLogger.Info("Create protocol...");
+
+                myLogger.Info("Creating SocketWire with port = {0}", protocolInstance.Port);
+                var wire = new SocketWire.Client(thisSessionLifetime, myDispatcher, protocolInstance.Port, "UnityClient")
+                {
+                    BackwardsCompatibleWireFormat = true
+                };
+
+                var protocol = new Rd.Impl.Protocol("UnityEditorPlugin", new Serializers(thisSessionLifetime, null, null),
+                    new Identities(IdKind.Client), myDispatcher, wire, thisSessionLifetime)
+                {
+                    ThrowErrorOnOutOfSyncModels = false
+                };
+
+                protocol.OutOfSyncModels.AdviseOnce(thisSessionLifetime, _ => OnOutOfSync(thisSessionLifetime));
+
+                wire.Connected.WhenTrue(thisSessionLifetime, connectionLifetime =>
+                {
+                    myLogger.Info("WireConnected.");
+
+                    var backendUnityModel = new BackendUnityModel(connectionLifetime, protocol);
+
+                    // Set up result for polling. Called before the Unity editor tries to use the protocol to open a
+                    // file. It ensures that the protocol is connected and active.
+                    // TODO: A property would be simpler
+                    // This would also requires checking the model is still connected, as properties maintain state even
+                    // when the connection has been lost
+                    backendUnityModel.IsBackendConnected.Set(_ => true);
+
+                    // TODO: Rename and move this to FrontendBackendHost
+                    // This is telling the frontend that the BackendUnityModel is available
+                    myHost.Do(frontendBackendModel => frontendBackendModel.SessionInitialized.Value = true);
+
+                    // TODO: Can we add the model before it's been advised?
+                    AdviseModel(backendUnityModel, connectionLifetime, thisSessionLifetime);
+
+                    SafeExecuteOrQueueEx("setModel", () => BackendUnityModel.SetValue(backendUnityModel));
+
+                    connectionLifetime.OnTermination(() =>
+                    {
+                        SafeExecuteOrQueueEx("clearModel", () =>
+                        {
+                            myLogger.Info("Wire disconnected.");
+
+                            // Tell the frontend the session is finished, and clear the model
+                            // TODO: Move this to FrontendBackendHost
+                            myHost.Do(m => m.SessionInitialized.Value = false);
+
+                            // Clear model
+                            BackendUnityModel.SetValue(null);
+                        });
+                    });
+                });
+            }
+            catch (Exception ex)
+            {
+                myLogger.Error(ex);
             }
         }
 
-        private void CreateProtocols(FileSystemPath protocolInstancePath)
+        [CanBeNull]
+        private ProtocolInstance GetProtocolInstanceData(FileSystemPath protocolInstancePath)
         {
             if (!protocolInstancePath.ExistsFile)
-                return;
+                return null;
 
             List<ProtocolInstance> protocolInstanceList;
             try
@@ -126,139 +207,96 @@ namespace JetBrains.ReSharper.Plugins.Unity.Rider
             catch (Exception e)
             {
                 myLogger.Warn($"Unable to parse {protocolInstancePath}" + Environment.NewLine + e);
-                return;
+                return null;
             }
 
-            var protocolInstance = protocolInstanceList?.SingleOrDefault(a => a.SolutionName == mySolution.SolutionFilePath.NameWithoutExtension);
+            if (protocolInstanceList == null)
+            {
+                myLogger.Info("No protocols listed in ProtocolInstance.json");
+                return null;
+            }
+
+            var protocolInstance = protocolInstanceList.SingleOrDefault(a =>
+                a.SolutionName == mySolution.SolutionFilePath.NameWithoutExtension);
             if (protocolInstance == null)
-                return;
-
-            myLogger.Info($"EditorPlugin protocol port {protocolInstance.Port} for Solution: {protocolInstance.SolutionName}.");
-
-            if (protocolInstance.ProtocolGuid != ProtocolCompatibility.ProtocolGuid)
             {
-                OnOutOfSync(myComponentLifetime);
-                myLogger.Info("Avoid attempt to create protocol, incompatible.");
-                return;
+                myLogger.Info($"Cannot find a protocol for the current solution. {protocolInstanceList.Count} options");
             }
 
-            try
+            return protocolInstance;
+        }
+
+        // TODO: We should only have one lifetime
+        private void AdviseModel(BackendUnityModel backendUnityModel, Lifetime lf, Lifetime lifetime)
+        {
+            if (PlatformUtil.RuntimePlatform == PlatformUtil.Platform.Windows)
             {
-                var lifetime = mySessionLifetimes.Next();
-                myLogger.Info("Create protocol...");
-
-                myLogger.Info("Creating SocketWire with port = {0}", protocolInstance.Port);
-                var wire = new SocketWire.Client(lifetime, myDispatcher, protocolInstance.Port, "UnityClient")
+                var frontendProcess =
+                    Process.GetCurrentProcess()
+                        .GetParent(); // RiderProcessId is not used on non-Windows, but this line gives bad warning in the log
+                if (frontendProcess != null)
                 {
-                    BackwardsCompatibleWireFormat = true
-                };
+                    backendUnityModel.RiderProcessId.SetValue(frontendProcess.Id);
+                }
+            }
 
-                var protocol = new Rd.Impl.Protocol("UnityEditorPlugin", new Serializers(),
-                    new Identities(IdKind.Client), myDispatcher, wire, lifetime) {ThrowErrorOnOutOfSyncModels = false};
 
-                protocol.OutOfSyncModels.AdviseOnce(lifetime, _ => OnOutOfSync(lifetime));
+            SubscribeToLogs(lf, backendUnityModel);
+            SubscribeToOpenFile(backendUnityModel);
 
-                wire.Connected.WhenTrue(lifetime, lf =>
+            backendUnityModel.Play.Advise(lf, b => myHost.Do(rd => rd.Play.SetValue(b)));
+            backendUnityModel.Pause.Advise(lf, b => myHost.Do(rd => rd.Pause.SetValue(b)));
+            backendUnityModel.LastPlayTime.Advise(lf, time => myHost.Do(rd => rd.LastPlayTime.SetValue(time)));
+            backendUnityModel.LastInitTime.Advise(lf, time => myHost.Do(rd => rd.LastInitTime.SetValue(time)));
+
+            backendUnityModel.UnityProcessId.View(lf, (_, pid) => myHost.Do(t => t.UnityProcessId.Set(pid)));
+
+            // I have split this into groups, because want to use async api for finding reference and pass them via groups to Unity
+            myHost.Do(t => t.ShowFileInUnity.Advise(lf, v => backendUnityModel.ShowFileInUnity.Fire(v)));
+            myHost.Do(t => t.ShowPreferences.Advise(lf, v => { backendUnityModel.ShowPreferences.Fire(); }));
+
+            backendUnityModel.EditorLogPath.Advise(lifetime,
+                s => myHost.Do(a => a.EditorLogPath.SetValue(s)));
+            backendUnityModel.PlayerLogPath.Advise(lifetime,
+                s => myHost.Do(a => a.PlayerLogPath.SetValue(s)));
+
+            // Note that these are late-init properties. Once set, they are always set and do not allow nulls.
+            // This means that if/when the Unity <-> Backend protocol closes, they still retain the last value
+            // they had - so the front end will retain the log and application paths of the just-closed editor.
+            // Opening a new editor instance will reconnect and push a new value through to the front end
+            backendUnityModel.UnityApplicationData.Advise(lifetime,
+                s => myHost.Do(a =>
                 {
-                    myLogger.Info("WireConnected.");
+                    var version = UnityVersion.Parse(s.ApplicationVersion);
+                    a.UnityApplicationData.SetValue(new UnityApplicationData(s.ApplicationPath,
+                        s.ApplicationContentsPath, s.ApplicationVersion, UnityVersion.RequiresRiderPackage(version)));
+                }));
+            myHost.Do(m =>
+            {
+                backendUnityModel.ScriptCompilationDuringPlay.FlowChangesIntoRd(lifetime, m.ScriptCompilationDuringPlay);
+            });
 
-                    var backendUnityModel = new BackendUnityModel(lf, protocol);
-                    backendUnityModel.IsBackendConnected.Set(rdVoid => true);
+            myHost.Do(rd =>
+            {
+                rd.GenerateUIElementsSchema.Set((l, u) =>
+                    backendUnityModel.GenerateUIElementsSchema.Start(l, u).ToRdTask(l));
+            });
 
-                    if (PlatformUtil.RuntimePlatform == PlatformUtil.Platform.Windows)
-                    {
-                        var frontendProcess = Process.GetCurrentProcess().GetParent(); // RiderProcessId is not used on non-Windows, but this line gives bad warning in the log
-                        if (frontendProcess != null)
-                        {
-                            backendUnityModel.RiderProcessId.SetValue(frontendProcess.Id);
-                        }
-                    }
+            backendUnityModel.BuildLocation.Advise(lf, b => myHost.Do(rd => rd.BuildLocation.SetValue(b)));
 
-                    myHost.Do(m => m.SessionInitialized.Value = true);
+            myHost.Do(rd =>
+            {
+                rd.RunMethodInUnity.Set((l, data) =>
+                {
+                    var editorRdTask = backendUnityModel.RunMethodInUnity.Start(l, data).ToRdTask(l);
+                    var frontendRes = new RdTask<RunMethodResult>();
 
-                    SubscribeToLogs(lf, backendUnityModel);
-                    SubscribeToOpenFile(backendUnityModel);
-
-                    backendUnityModel.Play.Advise(lf, b => myHost.Do(rd => rd.Play.SetValue(b)));
-                    backendUnityModel.Pause.Advise(lf, b => myHost.Do(rd => rd.Pause.SetValue(b)));
-                    backendUnityModel.LastPlayTime.Advise(lf, time => myHost.Do(rd => rd.LastPlayTime.SetValue(time)));
-                    backendUnityModel.LastInitTime.Advise(lf, time => myHost.Do(rd => rd.LastInitTime.SetValue(time)));
-
-                    backendUnityModel.UnityProcessId.View(lf, (_, pid) => myHost.Do(t => t.UnityProcessId.Set(pid)));
-
-                    // I have split this into groups, because want to use async api for finding reference and pass them via groups to Unity
-                    myHost.Do(t => t.ShowFileInUnity.Advise(lf, v => backendUnityModel.ShowFileInUnity.Fire(v)));
-                    myHost.Do(t => t.ShowPreferences.Advise(lf, v =>
-                    {
-                        backendUnityModel.ShowPreferences.Fire();
-                    }));
-
-                    backendUnityModel.EditorLogPath.Advise(lifetime,
-                        s => myHost.Do(a => a.EditorLogPath.SetValue(s)));
-                    backendUnityModel.PlayerLogPath.Advise(lifetime,
-                        s => myHost.Do(a => a.PlayerLogPath.SetValue(s)));
-
-                    // Note that these are late-init properties. Once set, they are always set and do not allow nulls.
-                    // This means that if/when the Unity <-> Backend protocol closes, they still retain the last value
-                    // they had - so the front end will retain the log and application paths of the just-closed editor.
-                    // Opening a new editor instance will reconnect and push a new value through to the front end
-                    backendUnityModel.UnityApplicationData.Advise(lifetime,
-                        s => myHost.Do(a =>
-                        {
-                            var version = UnityVersion.Parse(s.ApplicationVersion);
-                            a.UnityApplicationData.SetValue(new UnityApplicationData(s.ApplicationPath,
-                                    s.ApplicationContentsPath, s.ApplicationVersion, UnityVersion.RequiresRiderPackage(version)));
-                        }));
-                    myHost.Do(m =>
-                    {
-                        backendUnityModel.ScriptCompilationDuringPlay.FlowChangesIntoRd(lifetime, m.ScriptCompilationDuringPlay);
-                    });
-
-                    myHost.Do(rd =>
-                    {
-                        rd.GenerateUIElementsSchema.Set((l, u) =>
-                            backendUnityModel.GenerateUIElementsSchema.Start(l, u).ToRdTask(l));
-                    });
-
-                    backendUnityModel.BuildLocation.Advise(lf, b => myHost.Do(rd => rd.BuildLocation.SetValue(b)));
-
-                    myHost.Do(rd =>
-                    {
-                        rd.RunMethodInUnity.Set((l, data) =>
-                        {
-                            var editorRdTask = backendUnityModel.RunMethodInUnity.Start(l, data).ToRdTask(l);
-                            var frontendRes = new RdTask<RunMethodResult>();
-
-                            editorRdTask.Result.Advise(l, r =>
-                            {
-                                frontendRes.Set(r.Result);
-                            });
-                            return frontendRes;
-                        });
-                    });
-
-                    TrackActivity(backendUnityModel, lf);
-
-                    if (!myComponentLifetime.IsTerminated)
-                        myLocks.ExecuteOrQueueEx(myComponentLifetime, "setModel",
-                            () => { BackendUnityModel.SetValue(backendUnityModel); });
-
-                    lf.AddAction(() =>
-                    {
-                        if (!myComponentLifetime.IsTerminated)
-                            myLocks.ExecuteOrQueueEx(myComponentLifetime, "clearModel", () =>
-                            {
-                                myLogger.Info("Wire disconnected.");
-                                myHost.Do(m => m.SessionInitialized.Value = false);
-                                BackendUnityModel.SetValue(null);
-                            });
-                    });
+                    editorRdTask.Result.Advise(l, r => { frontendRes.Set(r.Result); });
+                    return frontendRes;
                 });
-            }
-            catch (Exception ex)
-            {
-                myLogger.Error(ex);
-            }
+            });
+
+            TrackActivity(backendUnityModel, lf);
         }
 
         private void OnOutOfSync(Lifetime lifetime)
