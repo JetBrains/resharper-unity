1ae8dec319608d6a25a6afd14272e5471da6de92
Artemiy Kononov <artemiy.kononov@jetbrains.com>
2020-10-23 16:50:52 +0300

    burst context local stage comments root fix

--- resharper/resharper-unity/src/CSharp/Daemon/Stages/PerformanceCriticalCodeAnalysis/ContextSystem/PerformanceCriticalContextProvider.cs
+++ resharper/resharper-unity/src/CSharp/Daemon/Stages/PerformanceCriticalCodeAnalysis/ContextSystem/PerformanceCriticalContextProvider.cs
@@ -5,10 +5,12 @@ using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Daemon.CSharp.CallGraph;
 using JetBrains.ReSharper.Daemon.UsageChecking;
 using JetBrains.ReSharper.Feature.Services.Daemon;
+using JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.CallGraph;
 using JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Stages.ContextSystem;
 using JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Stages.PerformanceCriticalCodeAnalysis.CallGraph;
 using JetBrains.ReSharper.Plugins.Unity.Settings;
 using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.Tree;
 using JetBrains.ReSharper.Psi.Util;
 
 namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Stages.PerformanceCriticalCodeAnalysis.ContextSystem
@@ -19,6 +21,17 @@ namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Stages.PerformanceCrit
         public override CallGraphContextElement Context => CallGraphContextElement.PERFORMANCE_CRITICAL_CONTEXT;
 
         public override bool IsContextAvailable => myIsPerformanceAnalysisEnabledProperty.Value;
+
+        public override bool HasContext(IDeclaration declaration, DaemonProcessKind processKind)
+        {
+            var functionDeclaration = declaration as IFunctionDeclaration;
+            var hasComment = UnityCallGraphUtil.HasAnalysisComment(functionDeclaration, PerformanceCriticalCodeMarksProvider.MarkId, ReSharperControlConstruct.Kind.Restore);
+
+            if (hasComment)
+                return true;
+            
+            return base.HasContext(declaration, processKind);
+        }
 
         public override bool IsMarked(IDeclaredElement declaredElement, DaemonProcessKind processKind)
         {
