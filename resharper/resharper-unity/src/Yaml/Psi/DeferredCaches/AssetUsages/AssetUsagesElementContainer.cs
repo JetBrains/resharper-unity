d48592e012c3fb4c31b453d4d365384184428d75
Ivan Yarkov <ivan.yarkov@jetbrains.com>
2020-11-03 13:28:07 +0300

    clean up: drop unused using

--- resharper/resharper-unity/src/Yaml/Psi/DeferredCaches/AssetUsages/AssetUsagesElementContainer.cs
+++ resharper/resharper-unity/src/Yaml/Psi/DeferredCaches/AssetUsages/AssetUsagesElementContainer.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using JetBrains.Annotations;
 using JetBrains.Application.Threading;
 using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.Caches;
