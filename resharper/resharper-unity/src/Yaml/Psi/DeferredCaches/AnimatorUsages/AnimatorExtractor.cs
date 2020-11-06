332de8ec699df269896892aee1688e2a42a23d37
Ivan Yarkov <ivan.yarkov@jetbrains.com>
2020-11-06 15:12:39 +0300

    refactor: drop LINQ syntax

--- resharper/resharper-unity/src/Yaml/Psi/DeferredCaches/AnimatorUsages/AnimatorExtractor.cs
+++ resharper/resharper-unity/src/Yaml/Psi/DeferredCaches/AnimatorUsages/AnimatorExtractor.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
 using JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.DeferredCaches.AssetHierarchy.References;
@@ -7,7 +6,6 @@ using JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.DeferredCaches.Utils;
 using JetBrains.ReSharper.Plugins.Yaml.Psi;
 using JetBrains.ReSharper.Plugins.Yaml.Psi.Tree;
 using JetBrains.ReSharper.Psi;
-using JetBrains.ReSharper.Psi.Tree;
 using JetBrains.Util;
 using JetBrains.Util.Maths;
 
@@ -42,15 +40,25 @@ namespace JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.DeferredCaches.AnimatorUsag
         private AnimatorStateMachineScriptUsage ExtractStateMachine()
         {
             var referenceToAnimatorState = CreateReferenceToAnimatorState();
-            var records = ExtractRecords();
-            var stateMachineName = ExtractAnimatorStateNameFrom(records);
-            var stateMachineBehavioursAnchors = TryExtractStateMachineBehavioursAnchorsFrom(records);
-            var childStateMachinesAnchors = TryExtractChildStateMachinesAnchorsFrom(records);
-            var childStatesAnchors = TryExtractChildStatesAnchorsFrom(records);
+            var root = ExtractRoot();
+            var stateMachineName = ExtractAnimatorStateNameFrom(root);
+            var stateMachineBehavioursAnchors = ExtractStateMachineBehavioursAnchorsFrom(root);
+            var childStateMachinesAnchors = ExtractChildStateMachinesAnchors(root);
+            var childStatesAnchors = ExtractChildStatesAnchors(root);
             AssertHaveUsefulRecords(stateMachineBehavioursAnchors.Count, childStateMachinesAnchors.Count,
                 childStatesAnchors.Count);
             return new AnimatorStateMachineScriptUsage(referenceToAnimatorState, stateMachineName,
                 stateMachineBehavioursAnchors, childStateMachinesAnchors, childStatesAnchors);
+        }
+
+        private static LocalList<long> ExtractChildStatesAnchors([NotNull] IBlockMappingNode root)
+        {
+            return ExtractChildAnchors(root, "m_ChildStates", "m_State");
+        }
+
+        private static LocalList<long> ExtractChildStateMachinesAnchors([NotNull] IBlockMappingNode root)
+        {
+            return ExtractChildAnchors(root, "m_ChildStateMachines", "m_StateMachine");
         }
 
         [AssertionMethod]
@@ -62,130 +70,19 @@ namespace JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.DeferredCaches.AnimatorUsag
                 throw new AnimatorExtractorException();
         }
 
-        private static LocalList<long> TryExtractChildStatesAnchorsFrom(
-            [ItemNotNull] in TreeNodeCollection<IBlockMappingEntry> records)
+        private static LocalList<long> ExtractChildAnchors(
+            [NotNull] IBlockMappingNode root, string recordKey, string innerRecordKey)
         {
-            try
-            {
-                return ExtractChildStatesAnchorsFrom(records);
-            }
-            catch (AnimatorExtractorException)
-            {
-            }
-
-            return new LocalList<long>();
-        }
-
-        private static LocalList<long> ExtractAnchorsFrom(
-            [NotNull] [ItemNotNull] in IEnumerable<IFlowMapEntry> childEntries)
-        {
-            var childStatesAnchors = new LocalList<long>();
-            foreach (var childEntry in childEntries)
-            {
-                if (!long.TryParse(childEntry.Value?.GetText(), out var anchor)) continue;
-                childStatesAnchors.Add(anchor);
-            }
-
-            return childStatesAnchors;
-        }
-
-        [CanBeNull]
-        [ItemNotNull]
-        private static IEnumerable<IFlowMapEntry> ExtractChildRecords(
-            [ItemNotNull] in TreeNodeCollection<ISequenceEntry> childStatesRecords,
-            [NotNull] Func<IBlockMappingEntry, bool> condition)
-        {
-            var flowMapEntries =
-                from childStateRecord in childStatesRecords
-                select childStateRecord?.Value as IBlockMappingNode
-                into blockMappingNode
-                where !(blockMappingNode is null)
-                select ExtractRecordFrom<IFlowMappingNode>(blockMappingNode.Entries, condition)
-                into stateRecord
-                select stateRecord.Entries
-                into stateRecordEntries
-                where stateRecordEntries.Count == 1
-                select stateRecordEntries[0]
-                into flowMapEntry
-                where !(flowMapEntry is null)
-                select flowMapEntry;
-            return flowMapEntries;
-        }
-
-        private static bool IsChildStatesRecord([NotNull] IBlockMappingEntry record)
-        {
-            return IsRecord(record, UnityYamlConstants.ChildStatesProperty);
-        }
-
-        private static bool IsRecord([NotNull] IBlockMappingEntry record, [NotNull] string propertyName)
-        {
-            return record.Key.MatchesPlainScalarText(propertyName);
-        }
-
-        private static bool IsStateRecord([NotNull] IBlockMappingEntry record)
-        {
-            return IsRecord(record, UnityYamlConstants.StateProperty);
-        }
-
-        private static bool IsStateMachineRecord([NotNull] IBlockMappingEntry record)
-        {
-            return IsRecord(record, UnityYamlConstants.StateMachineProperty);
-        }
-
-        private static LocalList<long> TryExtractStateMachineBehavioursAnchorsFrom(
-            [ItemNotNull] in TreeNodeCollection<IBlockMappingEntry> records)
-        {
-            try
-            {
-                return ExtractStateMachineBehavioursAnchorsFrom(records);
-            }
-            catch (AnimatorExtractorException)
-            {
-            }
-
-            return new LocalList<long>();
-        }
-
-        private static LocalList<long> TryExtractChildStateMachinesAnchorsFrom(
-            [ItemNotNull] in TreeNodeCollection<IBlockMappingEntry> records)
-        {
-            try
-            {
-                return ExtractChildStateMachinesAnchorsFrom(records);
-            }
-            catch (AnimatorExtractorException)
-            {
-            }
-
-            return new LocalList<long>();
-        }
-
-        private static LocalList<long> ExtractChildStatesAnchorsFrom(
-            [ItemNotNull] in TreeNodeCollection<IBlockMappingEntry> records)
-        {
-            return ExtractChildAnchors(records, IsChildStatesRecord, IsStateRecord);
-        }
-
-        private static LocalList<long> ExtractChildStateMachinesAnchorsFrom(
-            [ItemNotNull] in TreeNodeCollection<IBlockMappingEntry> records)
-        {
-            return ExtractChildAnchors(records, IsChildStateMachinesRecord, IsStateMachineRecord);
-        }
-
-        private static LocalList<long> ExtractChildAnchors(TreeNodeCollection<IBlockMappingEntry> records,
-                                                           [NotNull]
-                                                           Func<IBlockMappingEntry, bool> outerPropertyCondition,
-                                                           [NotNull]
-                                                           Func<IBlockMappingEntry, bool> nestedPropertyCondition)
-        {
-            var outerRecords = ExtractRecordFrom<IBlockSequenceNode>(records, outerPropertyCondition).Entries;
-            var innerRecords = ExtractChildRecords(outerRecords, nestedPropertyCondition);
-            return !(innerRecords is null) ? ExtractAnchorsFrom(innerRecords) : new LocalList<long>();
-        }
-
-        private static bool IsChildStateMachinesRecord([NotNull] IBlockMappingEntry record)
-        {
-            return IsRecord(record, UnityYamlConstants.ChildStateMachinesProperty);
+            var anchors = (root.FindMapEntryBySimpleKey(recordKey)?.Content?.Value as IBlockSequenceNode)?
+                .Entries
+                .SelectNotNull(t => t?.Value as IBlockMappingNode)
+                .SelectNotNull(t => t.FindMapEntryBySimpleKey(innerRecordKey)?.Content?.Value as IFlowMappingNode)
+                .SelectNotNull(t => t.FindMapEntryBySimpleKey("fileID")?.Value as IPlainScalarNode)
+                .SelectNotNull(t => long.TryParse(t?.GetText(), out var anchor) ? anchor : (long?) null);
+            var list = new LocalList<long>();
+            if (anchors is null) return list;
+            foreach (var anchor in anchors) list.Add(anchor);
+            return list;
         }
 
         [CanBeNull]
@@ -204,10 +101,10 @@ namespace JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.DeferredCaches.AnimatorUsag
         private AnimatorStateScriptUsage ExtractUsage()
         {
             var referenceToAnimatorState = CreateReferenceToAnimatorState();
-            var records = ExtractRecords();
-            var animatorStateName = ExtractAnimatorStateNameFrom(records);
-            var stateMachineBehavioursAnchors = ExtractStateMachineBehavioursAnchorsFrom(records);
-            return new AnimatorStateScriptUsage(referenceToAnimatorState, animatorStateName,
+            var root = ExtractRoot();
+            var animatorStateName = ExtractAnimatorStateNameFrom(root);
+            var stateMachineBehavioursAnchors = ExtractStateMachineBehavioursAnchorsFrom(root);
+            return new AnimatorStateScriptUsage(referenceToAnimatorState, animatorStateName, 
                 stateMachineBehavioursAnchors);
         }
 
@@ -221,94 +118,43 @@ namespace JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.DeferredCaches.AnimatorUsag
 
         private OWORD FindPersistentIndexInPsiStorageOfFile()
         {
-            var psiStoragePersistentIndex = myFile
-                .PsiStorage
-                .PersistentIndex;
+            var psiStoragePersistentIndex = myFile.PsiStorage.PersistentIndex;
             if (psiStoragePersistentIndex is null) throw new AnimatorExtractorException();
             return psiStoragePersistentIndex.Value;
         }
-
-        private TreeNodeCollection<IBlockMappingEntry> ExtractRecords()
-        {
-            var findRootBlockMapEntries = myDocument.Document.FindRootBlockMapEntries();
-            if (findRootBlockMapEntries == null) throw new AnimatorExtractorException();
-            return findRootBlockMapEntries.Entries;
-        }
-
-        [NotNull]
-        private static string ExtractAnimatorStateNameFrom(
-            [ItemNotNull] in TreeNodeCollection<IBlockMappingEntry> records)
-        {
-            var nameRecord =
-                (from record in records
-                    let assetDocumentRecordValue = record.Content?.Value
-                    where IsNameRecord(record)
-                    select assetDocumentRecordValue).FirstOrDefault();
-            if (nameRecord is null) throw new AnimatorExtractorException();
-            return nameRecord.GetText();
-        }
-
-        private static bool IsNameRecord([NotNull] IBlockMappingEntry record)
-        {
-            return IsRecord(record, UnityYamlConstants.NameProperty);
-        }
-
-        private static LocalList<long> ExtractStateMachineBehavioursAnchorsFrom(
-            [ItemNotNull] in TreeNodeCollection<IBlockMappingEntry> records)
-        {
-            var record = ExtractRecordFrom<IBlockSequenceNode>(records, IsStateMachinesBehavioursRecord);
-            return ExtractAnchorsFrom(record.Entries);
-        }
         
         [NotNull]
-        private static T ExtractRecordFrom<T>(
-            [ItemNotNull] in TreeNodeCollection<IBlockMappingEntry> records,
-            [NotNull] Func<IBlockMappingEntry, bool> condition) where T : class
+        private IBlockMappingNode ExtractRoot()
         {
-            var foundRecord =
-                (from record in records
-                    let recordValue = record.Content?.Value as T
-                    where recordValue != null && condition(record)
-                    select recordValue).FirstOrDefault();
-            if (foundRecord is null) throw new AnimatorExtractorException();
-            return foundRecord;
+            return myDocument.Document.FindRootBlockMapEntries() ?? throw new AnimatorExtractorException();
         }
 
-        private static bool IsStateMachinesBehavioursRecord([NotNull] IBlockMappingEntry record)
+        [NotNull]
+        private static string ExtractAnimatorStateNameFrom([NotNull] IBlockMappingNode root)
         {
-            return IsRecord(record, UnityYamlConstants.StateMachineBehavioursProperty);
+            var name = root.FindMapEntryBySimpleKey("m_Name")?.Content?.Value?.GetText();
+            if (name is null) throw new AnimatorExtractorException();
+            return name;
         }
 
-        private static LocalList<long> ExtractAnchorsFrom(
-            [NotNull] [ItemNotNull] in IEnumerable<ISequenceEntry> anchorsRecords)
+        private static LocalList<long> ExtractStateMachineBehavioursAnchorsFrom([NotNull] IBlockMappingNode root)
         {
-            return anchorsRecords.Aggregate(new LocalList<long>(), AddAnchor);
+            var node = root.FindMapEntryBySimpleKey("m_StateMachineBehaviours")?.Content?.Value;
+            if (!(node is IBlockSequenceNode record)) throw new AnimatorExtractorException();
+            var anchors = record.Entries.Aggregate(new LocalList<long>(), AddAnchor);
+            return !anchors.IsEmpty() ? anchors : throw new AnimatorExtractorException();
         }
 
         private static LocalList<long> AddAnchor(LocalList<long> anchors, [NotNull] ISequenceEntry record)
         {
-            var entry = ExtractEntryFrom(record);
-            if (!long.TryParse(entry.Value?.GetText(), out var anchor)) throw new AnimatorExtractorException();
+            if (!(record.Value is IFlowMappingNode node)) throw new AnimatorExtractorException();
+            var entries = node.Entries;
+            if (entries.Count != 1 ||
+                !long.TryParse(entries[0]?.Value?.GetText(), out var anchor)) throw new AnimatorExtractorException();
             anchors.Add(anchor);
             return anchors;
         }
-
-        [NotNull]
-        private static IFlowMapEntry ExtractEntryFrom([NotNull] ISequenceEntry entry)
-        {
-            if (!(entry.Value is IFlowMappingNode node)) throw new AnimatorExtractorException();
-            return ExtractStateMachineBehaviourEntryFrom(node);
-        }
-
-        [NotNull]
-        private static IFlowMapEntry ExtractStateMachineBehaviourEntryFrom([NotNull] IFlowMappingNode node)
-        {
-            var entries = node.Entries;
-            if (entries.Count != 1) throw new AnimatorExtractorException();
-            return entries[0] ?? throw new AnimatorExtractorException();
-        }
-
-
+        
         private class AnimatorExtractorException : Exception
         {
         }
