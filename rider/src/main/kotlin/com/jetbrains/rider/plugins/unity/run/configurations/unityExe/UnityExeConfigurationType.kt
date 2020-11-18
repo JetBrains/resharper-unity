ca62d4e964eccd1ba70245a99e1183b6a3959eed
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2020-11-18 19:38:01 +0100

    revert description of UnityExeConfigurationType

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/run/configurations/unityExe/UnityExeConfigurationType.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/run/configurations/unityExe/UnityExeConfigurationType.kt
@@ -3,8 +3,8 @@ package com.jetbrains.rider.plugins.unity.run.configurations.unityExe
 import com.intellij.execution.configurations.ConfigurationTypeBase
 import icons.UnityIcons
 
-class UnityExeConfigurationType : ConfigurationTypeBase("RunUnityExe", "Unity Executable", // "RunUnityExe" preserved for compatibility
-    "Either UnityEditor or Unity Standalone Player", UnityIcons.RunConfigurations.UnityExe) {
+class UnityExeConfigurationType : ConfigurationTypeBase("RunUnityExe", "Standalone Player", // "RunUnityExe" preserved for compatibility
+    "Unity Standalone Player configuration", UnityIcons.RunConfigurations.UnityExe) {
 
     val factory: UnityExeConfigurationFactory = UnityExeConfigurationFactory(this)
 
