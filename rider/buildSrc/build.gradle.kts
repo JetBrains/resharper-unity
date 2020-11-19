58e7ad5ed729ea06dc1432ba29c52f2c439dd29b
Mikhail Filippov <mikhail@filippov.me>
2020-11-19 15:54:57 +0300

    Bump IntelliJ Gradle plugin version

--- rider/buildSrc/build.gradle.kts
+++ rider/buildSrc/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.intellij.plugins", "gradle-intellij-plugin", "0.6.3")
+    implementation("org.jetbrains.intellij.plugins", "gradle-intellij-plugin", "0.6.4")
 }
 
 plugins {
