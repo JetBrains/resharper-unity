cc5f3fdb3759988733b1c34a591ee5344ffe4908
Matt Ellis <m.t.ellis@gmail.com>
2020-11-21 00:46:39 +0000

    Fix test

--- resharper/resharper-unity/test/data/CSharp/Daemon/Stages/PerformanceCriticalCodeAnalysis/rider/EditorClassesTest.cs
+++ resharper/resharper-unity/test/data/CSharp/Daemon/Stages/PerformanceCriticalCodeAnalysis/rider/EditorClassesTest.cs
@@ -1,20 +1,24 @@
+using UnityEngine;
+using UnityEditor;
+
+
 public class TestEditor : Editor
 {
-    public void DrawHeader() { }
-    public void OnInspectorGUI() { }
-    public void OnInteractivePreviewGUI() { }
-    public void OnPreviewGUI() { }
-    public void OnSceneGUI() { }
+    public void DrawHeader() { Debug.Log("test"); }
+    public void OnInspectorGUI() { Debug.Log("test"); }
+    public void OnInteractivePreviewGUI() { Debug.Log("test"); }
+    public void OnPreviewGUI() { Debug.Log("test"); }
+    public void OnSceneGUI() { Debug.Log("test"); }
 }
 
 public class TestEditorWindow : EditorWindow
 {
-    public void OnGUI() { }
-    public void OnInspectorUpdate() { }
+    public void OnGUI() { Debug.Log("test"); }
+    public void OnInspectorUpdate() { Debug.Log("test"); }
 }
 
 public class TestPropertyDrawer : PropertyDrawer
 {
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) { }
-    public override float GetPropertyHeight(SerializedProperty property, GUIContent label) { }
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) { Debug.Log("test"); }
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label) { Debug.Log("test"); }
 }
