89635e5841055471ebb92c0f767d7b0a8b853b28
Konstantin Krukhmalev <Konstantin.Krukhmalev@jetbrains.com>
2020-11-22 19:53:16 +0300

    try enable disabled tests

--- rider/src/test/kotlin/integrationTests/DebuggerTest.kt
+++ rider/src/test/kotlin/integrationTests/DebuggerTest.kt
@@ -29,7 +29,7 @@ class DebuggerTest : IntegrationTestWithEditorBase() {
         }
     }
 
-    @Test(enabled = false)
+    @Test(enabled = true)
     fun checkUnityPausePoint() {
         val pauseFile = activeSolutionDirectory.combine("Assets", ".pause").absoluteFile
         attachDebuggerToUnityEditorAndPlay(
@@ -71,7 +71,7 @@ class DebuggerTest : IntegrationTestWithEditorBase() {
             }, testGoldFile)
     }
 
-    @Test(description = "RIDER-23087", enabled = false)
+    @Test(description = "RIDER-23087", enabled = true)
     fun checkEvaluationAfterRestartGame() {
         var breakpoint: XLineBreakpoint<out XBreakpointProperties<*>>? = null
         attachDebuggerToUnityEditorAndPlay(
