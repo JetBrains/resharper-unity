644e41e7324ada679ead1ebe73cd3cd44ff8a09b
Vladimir Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2020-11-23 17:50:24 +0300

    fix RIDER-53968

--- resharper/resharper-unity/src/ShaderLab/Feature/Services/TypingAssist/ZoneMarker.cs
+++ resharper/resharper-unity/src/ShaderLab/Feature/Services/TypingAssist/ZoneMarker.cs
@@ -1,10 +1,11 @@
 ﻿using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.Feature.Services;
 using JetBrains.ReSharper.Psi;
 
 namespace JetBrains.ReSharper.Plugins.Unity.ShaderLab.Feature.Services.TypingAssist
 {
   [ZoneMarker]
-  public class ZoneMarker : IRequire<ILanguageCppZone>
+  public class ZoneMarker : IRequire<ILanguageCppZone>, IRequire<ICodeEditingZone>
   {
   }
 }
