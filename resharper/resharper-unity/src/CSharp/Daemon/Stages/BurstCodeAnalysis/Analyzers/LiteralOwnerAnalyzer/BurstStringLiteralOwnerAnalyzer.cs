6ac8da7017de2c47bc55c17fede89040c2ccdbfe
Artemiy Kononov <artemiy.kononov@jetbrains.com>
2020-11-27 17:49:04 +0300

    burst string literal refactoring, aggregated problem analyzers implementation

---
+++ resharper/resharper-unity/src/CSharp/Daemon/Stages/BurstCodeAnalysis/Analyzers/LiteralOwnerAnalyzer/BurstStringLiteralOwnerAnalyzer.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Feature.Services.Daemon;
+using JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Errors;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+using JetBrains.ReSharper.Psi.Tree;
+
+namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Stages.BurstCodeAnalysis.Analyzers.LiteralOwnerAnalyzer
+{
+    [SolutionComponent]
+    public sealed class BurstStringLiteralOwnerAnalyzer : BurstProblemAnalyzerBase<IStringLiteralOwner>
+    {
+        private readonly IEnumerable<IBurstStringSubAnalyzer> mySubAnalyzers;
+
+        public BurstStringLiteralOwnerAnalyzer(IEnumerable<IBurstStringSubAnalyzer> subAnalyzers)
+        {
+            mySubAnalyzers = subAnalyzers;
+        }
+
+        public bool CheckAndAnalyze(ICSharpExpression startNode, IHighlighting highlighting, IHighlightingConsumer consumer)
+        {
+            ITreeNode currentNode = startNode;
+            var result = false;
+            var analyzed = false;
+
+            while (!analyzed && currentNode is ICSharpExpression cSharpExpression)
+            {
+                foreach (var subAnalyzer in mySubAnalyzers)
+                {
+                    analyzed = subAnalyzer.TryAnalyze(cSharpExpression, out result);
+
+                    if (analyzed)
+                        break;
+                }
+
+                currentNode = currentNode.Parent;
+            }
+
+            if (!analyzed || result)
+            {
+                result = true;
+                consumer?.AddHighlighting(highlighting);
+            }
+
+            return result;
+        }
+
+        protected override bool CheckAndAnalyze(IStringLiteralOwner stringLiteralOwner, IHighlightingConsumer consumer)
+        {
+            bool isString;
+
+            if (stringLiteralOwner is ICSharpLiteralExpression cSharpLiteralExpression)
+                isString = cSharpLiteralExpression.Literal.GetTokenType().IsStringLiteral;
+            else
+                isString = true;
+
+            return isString && CheckAndAnalyze(stringLiteralOwner, new BurstManagedStringWarning(stringLiteralOwner), consumer);
+        }
+    }
+}
--- resharper/resharper-unity/src/CSharp/Daemon/Stages/BurstCodeAnalysis/Analyzers/ReferenceExpression/BurstReferenceExpressionAnalyzer.cs
+++ resharper/resharper-unity/src/CSharp/Daemon/Stages/BurstCodeAnalysis/Analyzers/ReferenceExpression/BurstReferenceExpressionAnalyzer.cs
@@ -1,38 +1,16 @@
 using System.Collections.Generic;
-using System.Linq;
 using JetBrains.ProjectModel;
-using JetBrains.ReSharper.Feature.Services.Daemon;
 using JetBrains.ReSharper.Psi.CSharp.Tree;
 
 namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Stages.BurstCodeAnalysis.Analyzers.ReferenceExpression
 {
     [SolutionComponent]
-    public sealed class BurstReferenceExpressionAnalyzer : BurstProblemAnalyzerBase<IReferenceExpression>
+    public sealed class BurstReferenceExpressionAnalyzer : BurstAggregatedProblemAnalyzer<IReferenceExpression>
     {
-        private readonly List<IBurstProblemSubAnalyzer<IReferenceExpression>> mySubAnalyzers;
-
         public BurstReferenceExpressionAnalyzer(
             IEnumerable<IBurstProblemSubAnalyzer<IReferenceExpression>> subAnalyzers)
+            : base(subAnalyzers)
         {
-            mySubAnalyzers = subAnalyzers.ToList();
-            mySubAnalyzers.Sort((analyzer1, analyzer2) => analyzer1.Priority - analyzer2.Priority);
-        }
-        
-        protected override bool CheckAndAnalyze(IReferenceExpression referenceExpression, IHighlightingConsumer consumer)
-        {
-            var warningPlaced = false;
-            
-            foreach (var subAnalyzer in mySubAnalyzers)
-            {
-                var res = subAnalyzer.CheckAndAnalyze(referenceExpression, consumer);
-
-                warningPlaced |= res.IsWarningPlaced();
-                
-                if (res.IsStop())
-                    break;
-            }
-
-            return warningPlaced;
         }
     }
 }
