cdd3d1d4bd6114388925c8826e1d08e5d3d67798
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2020-12-11 11:25:48 +0100

    add test

--- rider/src/test/kotlin/integrationTests/UnitTestingTest.kt
+++ rider/src/test/kotlin/integrationTests/UnitTestingTest.kt
@@ -5,6 +5,7 @@ import base.integrationTests.preferStandaloneNUnitLauncherInTests
 import com.jetbrains.rider.test.annotations.TestEnvironment
 import com.jetbrains.rider.test.enums.PlatformType
 import com.jetbrains.rider.test.scriptingApi.RiderUnitTestScriptingFacade
+import com.jetbrains.rider.test.scriptingApi.changeFileContent
 import com.jetbrains.rider.test.scriptingApi.withUtFacade
 import org.testng.annotations.Test
 import java.io.File
@@ -39,6 +40,24 @@ class UnitTestingTest : IntegrationTestWithEditorBase() {
     fun checkRunAllTestsFromProject() {
         withUtFacade(project) {
             it.waitForDiscovering(5)
+            val session = it.runAllTestsInProject(
+                "Tests",
+                5,
+                RiderUnitTestScriptingFacade.defaultTimeout,
+                5
+            )
+            it.compareSessionTreeWithGold(session, testGoldFile)
+        }
+    }
+
+    @Test(description = "RIDER-54359")
+    fun checkRefreshBeforeTest() {
+        withUtFacade(project) {
+            it.waitForDiscovering(5)
+            val file = activeSolutionDirectory.resolve("Assets").resolve("Tests").resolve("NewTestScript.cs")
+            changeFileContent(project, file) {
+                it.replace("NewTestScriptSimplePasses", "NewTestScriptSimplePasses2")
+            }
             val session = it.runAllTestsInProject(
                 "Tests",
                 5,
