64110507efe7d46d658edba2418c101de56a5081
Artemiy Kononov <artemiy.kononov@jetbrains.com>
2020-12-15 10:34:42 +0300

    burst calls

---
+++ resharper/resharper-unity/src/CSharp/Feature/Services/CallGraph/BurstCodeAnalysis/ShowBurstCalls/ShowBurstCallsBulbActionBase.cs
@@ -0,0 +1,40 @@
+using System;
+using JetBrains.Annotations;
+using JetBrains.Diagnostics;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Feature.Services.CallHierarchy.FindResults;
+using JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Stages.BurstCodeAnalysis.ContextSystem;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+
+namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Feature.Services.CallGraph.BurstCodeAnalysis.ShowBurstCalls
+{
+    public abstract class ShowBurstCallsBulbActionBase : ShowCallsBulbActionBase
+    {
+        [NotNull] private readonly DeclaredElementInstance<IClrDeclaredElement> myFunction;  
+        
+        protected ShowBurstCallsBulbActionBase([NotNull] IMethodDeclaration method)
+        {
+            var declaredElement = method.DeclaredElement;
+            
+            Assertion.AssertNotNull(declaredElement, "declared element is null, should be impossible");
+            
+            myFunction = new DeclaredElementInstance<IClrDeclaredElement>(declaredElement);
+        }
+
+        protected override DeclaredElementInstance<IClrDeclaredElement> GetStartElement() => myFunction;
+
+        protected override Func<CallHierarchyFindResult, bool> GetFilter(ISolution solution)
+        {
+            var burstContextProvider = solution.GetComponent<BurstContextProvider>();
+
+            return result =>
+            {
+                var referenceElement = result.ReferenceElement;
+                var containing = (referenceElement as ICSharpTreeNode)?.GetContainingFunctionLikeDeclarationOrClosure();
+
+                return burstContextProvider.IsMarkedSwea(containing);
+            };
+        }
+    }
+}
---
+++ resharper/resharper-unity/src/CSharp/Feature/Services/CallGraph/BurstCodeAnalysis/ShowBurstCalls/ShowBurstCallsUtil.cs
@@ -0,0 +1,8 @@
+namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Feature.Services.CallGraph.BurstCodeAnalysis.ShowBurstCalls
+{
+    internal static class ShowBurstCallsUtil
+    {
+        public const string OUTGOING_MESSAGE = "Show Burst outgoing calls";
+        public const string INCOMING_MESSAGE = "Show Burst incoming calls";
+    }
+}
