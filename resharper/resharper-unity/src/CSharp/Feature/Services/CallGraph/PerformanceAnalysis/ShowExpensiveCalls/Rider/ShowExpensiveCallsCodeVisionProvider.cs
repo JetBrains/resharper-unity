1f6cb5cd420132c207e01ce71c812d6a29cd9cca
Artemiy Kononov <artemiy.kononov@jetbrains.com>
2020-12-16 04:34:18 +0300

    show calls lense providers

---
+++ resharper/resharper-unity/src/CSharp/Feature/Services/CallGraph/PerformanceAnalysis/ShowExpensiveCalls/Rider/ShowExpensiveCallsCodeVisionProvider.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Feature.Services.Bulbs;
+using JetBrains.ReSharper.Feature.Services.Daemon;
+using JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Stages.PerformanceCriticalCodeAnalysis.ContextSystem;
+using JetBrains.ReSharper.Plugins.Unity.CSharp.Feature.Services.CallGraph.BurstCodeAnalysis.ShowBurstCalls;
+using JetBrains.ReSharper.Plugins.Unity.CSharp.Feature.Services.CallGraph.Rider;
+using JetBrains.ReSharper.Plugins.Unity.Rider.Highlightings.IconsProviders;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+
+namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Feature.Services.CallGraph.PerformanceAnalysis.ShowExpensiveCalls.Rider
+{
+    [SolutionComponent]
+    public class ShowExpensiveCallsCodeVisionProvider : SimpleCodeVisionMenuItemProviderBase, IPerformanceAnalysisCodeVisionMenuItemProvider
+    {
+        private readonly ExpensiveInvocationContextProvider myExpensiveContextProvider;
+
+        public ShowExpensiveCallsCodeVisionProvider(ISolution solution)
+            : base(solution)
+        {
+            myExpensiveContextProvider = solution.GetComponent<ExpensiveInvocationContextProvider>();
+        }
+
+        protected override bool CheckCallGraph(IMethodDeclaration methodDeclaration, DaemonProcessKind processKind)
+        {
+            return myExpensiveContextProvider.IsMarkedStage(methodDeclaration, processKind) == false;
+        }
+
+        protected override IEnumerable<IBulbAction> GetActions(IMethodDeclaration methodDeclaration)
+        {
+            var actions = ShowExpensiveCallsBulbAction.GetAllCalls(methodDeclaration);
+
+            return actions;
+        }
+    }
+}
