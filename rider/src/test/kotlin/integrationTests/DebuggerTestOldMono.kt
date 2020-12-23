e7311d9e4a7048911a247fd9433f923a6a853018
Ivan.Shakhov <van800@gmail.com>
2020-12-23 12:02:28 +0100

    add test for old mono runtime

---
+++ rider/src/test/kotlin/integrationTests/DebuggerTestOldMono.kt
@@ -0,0 +1,18 @@
+package integrationTests
+
+import java.io.File
+
+class DebuggerTestOldMono : DebuggerTestBase() {
+    override fun preprocessTempDirectory(tempDir: File) {
+        super.preprocessTempDirectory(tempDir)
+
+        // enable old mono
+        val projectSettings = tempDir.resolve("ProjectSettings/ProjectSettings.asset")
+        projectSettings.writeText(projectSettings.readText().replace("scriptingRuntimeVersion: 1","scriptingRuntimeVersion: 0"))
+
+        // make code compatible with old mono
+        val newBehaviourScript = "NewBehaviourScript.cs"
+        val script  = tempDir.resolve("Assets").resolve(newBehaviourScript)
+        script.writeText(script.readText().replace("b_0001_1110_1000_0100_1000_0000", "0"))
+    }
+}
