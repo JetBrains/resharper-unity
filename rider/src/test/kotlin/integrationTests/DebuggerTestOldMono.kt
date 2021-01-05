7686bff921f8cb7520829c31f87082e2fcad7b7d
Ivan.Shakhov <van800@gmail.com>
2021-01-05 08:52:19 +0100

    ++

--- rider/src/test/kotlin/integrationTests/DebuggerTestOldMono.kt
+++ rider/src/test/kotlin/integrationTests/DebuggerTestOldMono.kt
@@ -9,10 +9,5 @@ class DebuggerTestOldMono : DebuggerTestBase() {
         // enable old mono
         val projectSettings = tempDir.resolve("ProjectSettings/ProjectSettings.asset")
         projectSettings.writeText(projectSettings.readText().replace("scriptingRuntimeVersion: 1","scriptingRuntimeVersion: 0"))
-
-        // make code compatible with old mono
-        val newBehaviourScript = "NewBehaviourScript.cs"
-        val script  = tempDir.resolve("Assets").resolve(newBehaviourScript)
-        script.writeText(script.readText().replace("0b_0001_1110_1000_0100_1000_0000", "0"))
     }
 }
