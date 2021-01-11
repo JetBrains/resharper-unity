10d412a4ac0ce09f2a87ea95520aabcd4a55e0e9
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2021-01-11 17:19:19 +0100

    move code to Rider

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/explorer/UnityExplorerNode.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/explorer/UnityExplorerNode.kt
@@ -17,7 +17,6 @@ import com.jetbrains.rider.projectView.views.FileSystemNodeBase
 import com.jetbrains.rider.projectView.views.NestingNode
 import com.jetbrains.rider.projectView.views.SolutionViewRootNodeBase
 import com.jetbrains.rider.projectView.views.actions.ConfigureScratchesAction
-import com.jetbrains.rider.projectView.views.assemblyExplorer.AssemblyExplorerHost
 import com.jetbrains.rider.projectView.views.fileSystemExplorer.FileSystemExplorerCustomization
 import com.jetbrains.rider.projectView.views.solutionExplorer.SolutionExplorerViewPane
 import icons.UnityIcons
@@ -134,15 +133,6 @@ open class UnityExplorerNode(project: Project,
             }
             presentation.tooltip = tooltip + "This folder is not imported into the asset database"
         }
-    }
-
-    override fun processDoubleClick(): Boolean {
-        if (virtualFile.exists() && virtualFile.extension == "dll"){
-            AssemblyExplorerHost.getInstance(myProject).add(listOf(virtualFile))
-            return true;
-        }
-
-        return super.processDoubleClick()
     }
 
     override fun getName(): String {
