f9811b720fc30ba3ece988d2fc3f039d0cdc8898
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2021-01-28 11:56:28 +0100

    ++

--- unity/EditorPlugin/Logger/LogInitializer.cs
+++ unity/EditorPlugin/Logger/LogInitializer.cs
@@ -8,15 +8,18 @@ namespace JetBrains.Rider.Unity.Editor.Logger
 {
     public static class LogInitializer
     {
-        private static readonly string ourBaseLogPath = !UnityUtils.IsInRiderTests
-            ? Path.GetTempPath()
-            : new FileInfo(UnityUtils.UnityEditorLogPath).Directory.FullName;
-
-        internal static readonly string LogPath = Path.Combine(Path.Combine(ourBaseLogPath, "Unity3dRider"), $"EditorPlugin.{Process.GetCurrentProcess().Id}.log");
         public static void InitLog(LoggingLevel selectedLoggingLevel)
         {
             if (selectedLoggingLevel > LoggingLevel.OFF)
-                Log.DefaultFactory = Log.CreateFileLogFactory(Lifetime.Eternal, LogPath, true, selectedLoggingLevel);
+            {
+                var fileLogFactory = Log.CreateFileLogFactory(Lifetime.Eternal, PluginEntryPoint.LogPath, true, selectedLoggingLevel);
+                fileLogFactory.Handlers += message =>
+                {
+                    if (message.Level == LoggingLevel.ERROR || message.Level == LoggingLevel.FATAL)
+                        MainThreadDispatcher.Instance.Queue(() => UnityEngine.Debug.LogError(message.FormattedMessage));
+                };
+                Log.DefaultFactory = fileLogFactory;
+            }
             else
                 Log.DefaultFactory = new SingletonLogFactory(NullLog.Instance); // use profiler in Unity - this is faster than leaving TextWriterLogFactory with LoggingLevel OFF
         }
