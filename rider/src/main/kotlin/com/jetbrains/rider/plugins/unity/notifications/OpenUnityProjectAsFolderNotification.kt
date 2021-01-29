bd2149f4d23e8cb77f5e30f7288dc8f323efd248
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2021-01-29 15:41:09 +0100

    explicit param

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/notifications/OpenUnityProjectAsFolderNotification.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/notifications/OpenUnityProjectAsFolderNotification.kt
@@ -51,9 +51,9 @@ class OpenUnityProjectAsFolderNotification(project: Project) : ProtocolSubscribe
                         if (!UnityInstallationFinder.getInstance(project).requiresRiderPackage())
                             content = "Make sure Rider $marketingVersion is set as the External Editor in Unity preferences."
                         val notification = Notification(notificationGroupId.displayId, title, content, NotificationType.WARNING)
-                        startChildOnUi {
+                        startChildOnUi { lt ->
                             Notifications.Bus.notify(notification, project)
-                            project.solution.frontendBackendModel.unityEditorConnected.whenTrue(it) { notification.expire() }
+                            project.solution.frontendBackendModel.unityEditorConnected.whenTrue(lt) { notification.expire() }
                         }
                     }
                 }.noAwait()
