75cb3fe6f4791544b9241378dee18a17a1c1df87
Vladimir Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2021-02-11 13:27:57 +0300

    fix DEXP-573432

---
+++ resharper/resharper-unity/src/Feature/Internal/ZoneMarker.cs
@@ -0,0 +1,10 @@
+using JetBrains.Application.BuildScript.Application.Zones;
+
+namespace JetBrains.ReSharper.Plugins.Unity.Feature.Internal
+{
+    [ZoneMarker]
+    public class ZoneMarker : IRequire<IInternalVisibilityZone>
+    {
+        
+    }
+}
