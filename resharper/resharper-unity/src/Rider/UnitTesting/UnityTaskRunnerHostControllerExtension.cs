4f6699d7a2faec30163af1f61322b6d797c566e5
Evgeniy Shabanov <evgeniy.shabanov@jetbrains.com>
2021-02-16 10:58:27 +0300

    Fix minor

--- resharper/resharper-unity/src/Rider/UnitTesting/UnityTaskRunnerHostControllerExtension.cs
+++ resharper/resharper-unity/src/Rider/UnitTesting/UnityTaskRunnerHostControllerExtension.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using JetBrains.Application.Threading;
+using JetBrains.Application.Threading.Tasks;
 using JetBrains.Diagnostics;
 using JetBrains.Lifetimes;
 using JetBrains.ProjectModel;
@@ -64,7 +65,7 @@ namespace JetBrains.ReSharper.Plugins.Unity.Rider.UnitTesting
 
             lock (myStartUnitySync)
             {
-                WrapStartUnityTask(() => PrepareForRunInternal(lifetimeDef, run));
+                WrapStartUnityTask(() => PrepareForRunInternal(lifetimeDef.Lifetime, run));
                 WrapStartUnityTask(() => next.PrepareForRun(run));
                 
                 return myStartUnityTask;
@@ -75,25 +76,24 @@ namespace JetBrains.ReSharper.Plugins.Unity.Rider.UnitTesting
 
         public void Cancel(IUnitTestRun run) => run.GetData(ourLifetimeDefinitionKey)?.Terminate();
 
-        private Task PrepareForRunInternal(LifetimeDefinition lifetimeDefinition, IUnitTestRun run)
+        private async Task PrepareForRunInternal(Lifetime lifetime, IUnitTestRun run)
         {
             var unityEditorProcessId = myUnityController.TryGetUnityProcessId();
             if (unityEditorProcessId.HasValue)
-                return Task.CompletedTask;
-                    
-                    
+                return;
+
             var message = string.Format(StartUnityEditorQuestionMessage, 
-                myAvailableProviders[run.HostController.HostId],
-                myUnityController.GetPresentableUnityVersion());
-            
+                                             myAvailableProviders[run.HostController.HostId],
+                                             myUnityController.GetPresentableUnityVersion());
             if (!MessageBox.ShowYesNo(message, PluginName))
                 throw new Exception(string.Format(NotAvailableUnityEditorMessage, myAvailableProviders[run.HostController.HostId]));
-                    
-            myRiderBackgroundTaskHost.CreateIndicator(lifetimeDefinition.Lifetime, false, false, "Start Unity Editor");
-            var startUnityTask = StartUnity(lifetimeDefinition.Lifetime);
-            startUnityTask.ContinueWith(x =>   lifetimeDefinition.Terminate(), myLifetime, TaskContinuationOptions.None, myShellLocks.Tasks.Scheduler);
-                    
-            return startUnityTask;
+
+            var startUnityTask = StartUnity(lifetime);
+            
+            await myShellLocks.Tasks.YieldToIfNeeded(lifetime, Scheduling.MainGuard);
+            ShowProgress(lifetime, startUnityTask);
+            
+            await startUnityTask.ConfigureAwait(false);
         }
         
         private Task StartUnity(Lifetime lifetime)
@@ -111,6 +111,13 @@ namespace JetBrains.ReSharper.Plugins.Unity.Rider.UnitTesting
             return myUnityController.WaitConnectedUnityProcessId(lifetime);
         }
 
+        private void ShowProgress(Lifetime lifetime, Task task)
+        {
+            var innerLifeDef = lifetime.CreateNested();
+            myRiderBackgroundTaskHost.CreateIndicator(innerLifeDef.Lifetime, false, false, "Start Unity Editor");
+            task.ContinueWith(x =>   innerLifeDef.Terminate(), myLifetime, TaskContinuationOptions.None, myShellLocks.Tasks.Scheduler);
+        }
+        
         private void WrapStartUnityTask(Func<Task> run)
         {
             myStartUnityTask = myStartUnityTask.ContinueWith(_ => run(),
