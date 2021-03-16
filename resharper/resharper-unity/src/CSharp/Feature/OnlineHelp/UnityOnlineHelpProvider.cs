75edb7c0fed3732ad51649d0d25a6d921bfa8e0e
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2021-03-16 14:38:27 +0100

    ++

--- resharper/resharper-unity/src/CSharp/Feature/OnlineHelp/UnityOnlineHelpProvider.cs
+++ resharper/resharper-unity/src/CSharp/Feature/OnlineHelp/UnityOnlineHelpProvider.cs
@@ -1,42 +1,42 @@
-using System;
-using JetBrains.ProjectModel;
-using JetBrains.ReSharper.Psi;
-using JetBrains.Util;
-using JetBrains.Util.Dotnet.TargetFrameworkIds;
-
-namespace JetBrains.ReSharper.Host.Features.OnlineHelp
-{
-  [SolutionComponent]
-  public class UnityOnlineHelpProvider : IOnlineHelpProvider
-  {
-    public Uri GetUrl(ICompiledElement element,
-      TargetFrameworkId targetFrameworkId,
-      FileSystemPath assemblyLocation)
-    {
-      if (assemblyLocation == null || !assemblyLocation.ExistsFile)
-        return null;
-
-      if (!(assemblyLocation.Name.StartsWith("UnityEngine") || assemblyLocation.Name.StartsWith("UnityEditor")))
-        return null;
-      return GetUnityUri(element);
-    }
-
-    public int GetPriority()
-    {
-      return 20;
-    }
-
-    public bool IsAsync()
-    {
-      return false;
-    }
-    
-    public static Uri GetUnityUri(ICompiledElement element)
-    {
-      var searchableText = element.GetSearchableText();
-      return searchableText == null
-        ? null
-        : new Uri($"http://www.unity.com/search?q={Uri.EscapeDataString(searchableText)}");
-    }
-  }
-}
+// using System;
+// using JetBrains.ProjectModel;
+// using JetBrains.ReSharper.Psi;
+// using JetBrains.Util;
+// using JetBrains.Util.Dotnet.TargetFrameworkIds;
+//
+// namespace JetBrains.ReSharper.Host.Features.OnlineHelp
+// {
+//   [SolutionComponent]
+//   public class UnityOnlineHelpProvider : IOnlineHelpProvider
+//   {
+//     public Uri GetUrl(ICompiledElement element,
+//       TargetFrameworkId targetFrameworkId,
+//       FileSystemPath assemblyLocation)
+//     {
+//       if (assemblyLocation == null || !assemblyLocation.ExistsFile)
+//         return null;
+//
+//       if (!(assemblyLocation.Name.StartsWith("UnityEngine") || assemblyLocation.Name.StartsWith("UnityEditor")))
+//         return null;
+//       return GetUnityUri(element);
+//     }
+//
+//     public int GetPriority()
+//     {
+//       return 20;
+//     }
+//
+//     public bool IsAsync()
+//     {
+//       return false;
+//     }
+//     
+//     public static Uri GetUnityUri(ICompiledElement element)
+//     {
+//       var searchableText = element.GetSearchableText();
+//       return searchableText == null
+//         ? null
+//         : new Uri($"http://www.unity.com/search?q={Uri.EscapeDataString(searchableText)}");
+//     }
+//   }
+// }
