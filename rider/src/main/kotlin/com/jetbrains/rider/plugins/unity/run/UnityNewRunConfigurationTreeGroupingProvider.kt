74a81750a0e59c5a106c5ba0cbdd9d0c7a103dbc
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2021-04-30 15:40:03 +0200

    group Unity run-configuration templates

---
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/run/UnityNewRunConfigurationTreeGroupingProvider.kt
@@ -0,0 +1,19 @@
+package com.jetbrains.rider.plugins.unity.run
+
+import com.intellij.openapi.project.Project
+import com.jetbrains.rider.plugins.unity.run.configurations.UnityDebugConfigurationType
+import com.jetbrains.rider.plugins.unity.run.configurations.unityExe.UnityExeConfigurationType
+import com.jetbrains.rider.run.configurations.RiderNewRunConfigurationTreeGroupingProvider
+import icons.UnityIcons
+
+class UnityNewRunConfigurationTreeGroupingProvider: RiderNewRunConfigurationTreeGroupingProvider {
+    override fun getGroups(project: Project): List<RiderNewRunConfigurationTreeGroupingProvider.Group> {
+        return listOf(RiderNewRunConfigurationTreeGroupingProvider.Group(
+            UnityIcons.Icons.UnityLogo, "Unity",
+            listOf(
+                UnityDebugConfigurationType.id,
+                UnityExeConfigurationType.id
+            )
+        ))
+    }
+}
