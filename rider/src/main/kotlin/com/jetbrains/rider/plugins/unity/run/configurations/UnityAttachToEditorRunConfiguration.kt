d9608e7ffd672273b10486ecd6b5cdb2b8c4d96c
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2021-05-03 10:05:24 +0200

    Minor issue with attempt to Profile from UnityClassLib (#2080)
    * Only show "Unable to automatically discover correct Unity Editor to debug" for debug. avoid showing it for Profile.

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/run/configurations/UnityAttachToEditorRunConfiguration.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/run/configurations/UnityAttachToEditorRunConfiguration.kt
@@ -6,11 +6,13 @@ import com.intellij.execution.executors.DefaultDebugExecutor
 import com.intellij.execution.process.OSProcessUtil
 import com.intellij.execution.process.ProcessInfo
 import com.intellij.execution.runners.ExecutionEnvironment
+import com.intellij.execution.runners.ProgramRunner
 import com.intellij.execution.runners.RunConfigurationWithSuppressedDefaultRunAction
 import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.options.SettingsEditor
 import com.intellij.openapi.project.Project
 import com.intellij.util.xmlb.annotations.Transient
+import com.jetbrains.rider.debugger.DotNetDebugRunner
 import com.jetbrains.rider.isUnityClassLibraryProject
 import com.jetbrains.rider.isUnityProject
 import com.jetbrains.rider.isUnityProjectFolder
@@ -60,7 +62,6 @@ class UnityAttachToEditorRunConfiguration(project: Project, factory: Configurati
 
     override fun getState(executor: Executor, environment: ExecutionEnvironment): RunProfileState? {
         val executorId = executor.id
-
         for (ext in EP_NAME.getExtensions(project)) {
             if (ext.canExecute(executorId)) {
                 val finder = UnityInstallationFinder.getInstance(project)
@@ -92,27 +93,35 @@ class UnityAttachToEditorRunConfiguration(project: Project, factory: Configurati
         return null
     }
 
-    override fun checkSettingsBeforeRun() {
-        // This method lets us check settings before run. If we throw an instance of RuntimeConfigurationError, the Run
-        // Configuration editor is displayed. It's called on the EDT, so there's not a lot we can do - e.g. we can't get
-        // a process list.
+    override fun checkRunnerSettings(
+        runner: ProgramRunner<*>,
+        runnerSettings: RunnerSettings?,
+        configurationPerRunnerSettings: ConfigurationPerRunnerSettings?
+    ) {
+        if (runner is DotNetDebugRunner) {
+            // This method lets us check settings before run. If we throw an instance of RuntimeConfigurationError, the Run
+            // Configuration editor is displayed. It's called on the EDT, so there's not a lot we can do - e.g. we can't get
+            // a process list.
 
-        // If we already have a pid, that means this run configuration has been launched before, and we've successfully
-        // attached to a process. Use it again. If the pid is out of date (highly unlikely), we'll do our best to find
-        // the process again
-        if (pid != null) {
-            return
-        }
+            // If we already have a pid, that means this run configuration has been launched before, and we've successfully
+            // attached to a process. Use it again. If the pid is out of date (highly unlikely), we'll do our best to find
+            // the process again
+            if (pid != null) {
+                return
+            }
 
-        // If we're a class library project that isn't in a Unity project folder, we can't guess at the correct project
-        // to attach to, so throw an error and show the dialog. This value will be null until the backend has finished
-        // loading. However, because we're a Unity run configuration, we can safely assume we're a Unity project, and if
-        // we're not inside a Unity project folder, then we can't automatically attach, so throw an error and show the
-        // dialog
-        val isClassLibraryProject = project.isUnityClassLibraryProject()
-        if (!project.isUnityProjectFolder() && (isClassLibraryProject == null || isClassLibraryProject)) {
-            throw RuntimeConfigurationError("Unable to automatically discover correct Unity Editor to debug")
+            // If we're a class library project that isn't in a Unity project folder, we can't guess at the correct project
+            // to attach to, so throw an error and show the dialog. This value will be null until the backend has finished
+            // loading. However, because we're a Unity run configuration, we can safely assume we're a Unity project, and if
+            // we're not inside a Unity project folder, then we can't automatically attach, so throw an error and show the
+            // dialog
+
+            val isClassLibraryProject = project.isUnityClassLibraryProject()
+            if (!project.isUnityProjectFolder() && (isClassLibraryProject == null || isClassLibraryProject)) {
+                throw RuntimeConfigurationError("Unable to automatically discover correct Unity Editor to debug")
+            }
         }
+        super.checkRunnerSettings(runner, runnerSettings, configurationPerRunnerSettings)
     }
 
     fun updatePidAndPort() : Boolean {
