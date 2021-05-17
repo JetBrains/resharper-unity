6edbe9a5f0f62ee843f5718d142e65368d3f7810
Vladimir Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2021-05-17 20:23:24 +0300

    fix long-running GetCoroutineMustUseReturnValue in CSharpHighlightingTest.cs (part of RIDER-62709)

---
+++ resharper/resharper-unity/src/UnityTypeUtils.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.Modules;
+using JetBrains.Util;
+
+namespace JetBrains.ReSharper.Plugins.Unity
+{
+    public static class UnityTypeUtils
+    {
+        [NotNull]
+        public static IEnumerable<UnityType> GetBaseUnityTypes([CanBeNull] ITypeElement type,
+            UnityVersion unityVersion, UnityTypesProvider unityTypes, KnownTypesCache knownTypesCache)
+        {
+            if (type?.Module is IProjectPsiModule projectPsiModule)
+            {
+                var version = unityVersion.GetActualVersion(projectPsiModule.Project);
+                return GetBaseUnityTypes(type, version, unityTypes, knownTypesCache);
+            }
+
+            return EmptyArray<UnityType>.Instance;
+        }
+
+        [NotNull]
+        public static IEnumerable<UnityType> GetBaseUnityTypes([NotNull] ITypeElement type, Version unityVersion,
+            UnityTypesProvider unityTypes, KnownTypesCache knownTypesCache)
+        {
+            unityVersion = unityTypes.Types.NormaliseSupportedVersion(unityVersion);
+            return GetBaseUnityTypes(unityTypes, type, unityVersion, knownTypesCache);
+        }
+        
+        public static IEnumerable<UnityType> GetBaseUnityTypes(UnityTypesProvider typesProvider, ITypeElement type,
+            Version normalisedVersion, KnownTypesCache knownTypesCache)
+        {
+            return typesProvider.Types.Types.Where(t =>
+            {
+                using (CompilationContextCookie.GetExplicitUniversalContextIfNotSet())
+                    return t.SupportsVersion(normalisedVersion) && type.IsDescendantOf(t.GetTypeElement(knownTypesCache, type.Module));
+            });
+        }
+    }
+}
