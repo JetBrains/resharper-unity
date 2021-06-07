dc22c94f2415c229dfaaff1d271188a07c012676
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2021-06-07 11:01:37 +0200

    wip

--- rider/buildSrc/build.gradle.kts
+++ rider/buildSrc/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.intellij.plugins", "gradle-intellij-plugin", "0.7.2")
+    implementation("org.jetbrains.intellij.plugins", "gradle-intellij-plugin", "1.0")
 }
 
 plugins {
