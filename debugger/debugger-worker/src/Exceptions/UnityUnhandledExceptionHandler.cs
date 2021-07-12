df44b623e1158372d456267ccdd60769597bf8df
Matt Ellis <m.t.ellis@gmail.com>
2021-07-12 11:19:19 +0100

    Prevent break on unhandled ExitGUIException
    The behaviour for unhandled exceptions has changed in the Unity 2021.2 beta, thanks to a Mono upgrade. ExitGUIException is frequently thrown in the GUI, but handled in native code. Mono now reports this as an unhandled exception, which is very annoying and makes it hard to use the editor while debugging.

---
+++ debugger/debugger-worker/src/Exceptions/UnityUnhandledExceptionHandler.cs
@@ -0,0 +1,33 @@
+using Mono.Debugger.Soft;
+using Mono.Debugging.Autofac;
+using Mono.Debugging.Soft;
+using Mono.Debugging.Soft.Exceptions;
+
+namespace JetBrains.ReSharper.Plugins.Unity.Rider.Debugger.Exceptions
+{
+    [DebuggerSessionComponent(typeof(SoftDebuggerType))]
+    public class UnityUnhandledExceptionHandler : ISoftDebuggerUnhandledExceptionHandler
+    {
+        private readonly IUnityOptions myUnityOptions;
+
+        public UnityUnhandledExceptionHandler(IUnityOptions unityOptions)
+        {
+            myUnityOptions = unityOptions;
+        }
+
+        public bool ShouldContinueOnException(ObjectMirror exception)
+        {
+            if (!myUnityOptions.ExtensionsEnabled)
+                return false;
+
+            // Unity 2021.2 has an upgraded Mono (from 5.12ish to 6.something) and the behaviour around unhandled
+            // exceptions has changed. Previously, Unity's Mono would not break on unhandled exceptions - it would not
+            // notify the debugger. This appears to have been unintentional, and it's not currently clear if this
+            // behaviour will be rolled back. (See also Il2CppAwareSessionOptions)
+            // In the meantime, Unity uses ExitGUIException for control flow - thrown in managed code and caught in
+            // native code. Mono reports this as an unhandled exception, because it is unhandled in managed code. But it
+            // is used to break out of the immediate mode GUI loop, and is used frequently, thrown by the ExitGUI method
+            return exception.Type.FullName == "UnityEngine.ExitGUIException";
+        }
+    }
+}
