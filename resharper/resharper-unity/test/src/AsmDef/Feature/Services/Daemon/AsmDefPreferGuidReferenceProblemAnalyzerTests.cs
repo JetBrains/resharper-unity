635b22341ed3602c78dfe53cc209af90c4b0d8aa
Matt Ellis <m.t.ellis@gmail.com>
2021-09-30 12:16:50 +0100

    Add hint to prefer GUID references

---
+++ resharper/resharper-unity/test/src/AsmDef/Feature/Services/Daemon/AsmDefPreferGuidReferenceProblemAnalyzerTests.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.ReSharper.Plugins.Unity.AsmDef.Daemon.Errors;
+using JetBrains.ReSharper.Plugins.Unity.JsonNew.Psi;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.TestFramework;
+using NUnit.Framework;
+
+namespace JetBrains.ReSharper.Plugins.Unity.Tests.AsmDef.Feature.Services.Daemon
+{
+    [TestUnity]
+    [TestFileExtension(".asmdef")]
+    public class AsmDefPreferGuidReferenceProblemAnalyzerTests : JsonNewHighlightingTestBase<PreferGuidReferenceWarning>
+    {
+        protected override PsiLanguageType CompilerIdsLanguage => JsonNewLanguage.Instance;
+        protected override string RelativeTestDataPath => @"AsmDef\Daemon\Stages\Analysis\PreferGuidReference";
+
+        [Test] public void TestShowHint() { DoNamedTest2("Ref1.asmdef"); }
+        [Test] public void TestNoHintOnUnresolvedReference() { DoNamedTest2(); }
+    }
+}
