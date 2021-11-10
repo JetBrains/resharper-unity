62f04a7024472d60c4398bf645a9d135a7f1b2ab
Matt Ellis <m.t.ellis@gmail.com>
2021-11-10 12:07:24 +0000

    Add tests for completion in .asmref files

---
+++ resharper/resharper-unity/test/src/AsmDef/Feature/Services/CodeCompletion/AsmRefReferenceCompletionTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure.LookupItems;
+using JetBrains.ReSharper.FeaturesTestFramework.Completion;
+using JetBrains.ReSharper.TestFramework;
+using JetBrains.Util.Dotnet.TargetFrameworkIds;
+using NUnit.Framework;
+
+namespace JetBrains.ReSharper.Plugins.Unity.Tests.AsmDef.Feature.Services.CodeCompletion
+{
+    [TestUnity]
+    [TestFileExtension(".asmref")]
+    public class AsmRefReferenceCompletionListTests : CodeCompletionTestBase
+    {
+        protected override CodeCompletionTestType TestType => CodeCompletionTestType.List;
+        protected override string RelativeTestDataPath => @"AsmDef\CodeCompletion\AsmRefReference";
+
+        [Test] public void TestList01() { DoNamedTest("Ref01_SecondProject.asmdef"); }
+    }
+
+    [TestUnity]
+    [TestFileExtension(".asmref")]
+    public class AsmRefReferenceCompletionActionTests : CodeCompletionTestBase
+    {
+        protected override CodeCompletionTestType TestType => CodeCompletionTestType.Action;
+        protected override string RelativeTestDataPath => @"AsmDef\CodeCompletion\AsmRefReference";
+        protected override bool CheckAutomaticCompletionDefault() => true;
+        protected override LookupListSorting Sorting => LookupListSorting.ByRelevance;
+
+        [Test] public void TestAction01() { DoNamedTest("Ref01_SecondProject.asmdef"); }
+    }
+}
