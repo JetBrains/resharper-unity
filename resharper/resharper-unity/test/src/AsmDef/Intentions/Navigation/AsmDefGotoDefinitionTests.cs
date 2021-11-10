503c0297c38831b362c2041d1e50259c49492948
Matt Ellis <m.t.ellis@gmail.com>
2021-11-10 13:16:05 +0000

    Add tests for navigation in .asmref files

--- resharper/resharper-unity/test/src/AsmDef/Intentions/Navigation/AsmDefGotoDefinitionTests.cs
+++ resharper/resharper-unity/test/src/AsmDef/Intentions/Navigation/AsmDefGotoDefinitionTests.cs
@@ -18,11 +18,10 @@ namespace JetBrains.ReSharper.Plugins.Unity.Tests.AsmDef.Intentions.Navigation
         [Test] public void Test02() { DoNamedTest("Ref1.asmdef"); }
         [Test] public void Test03() { DoNamedTest("Ref1.asmdef", "Ref2.asmdef"); }
 
-        [Test]
-        public void TestGuidReference01()
-        {
-            DoNamedTest2("GuidReference_SecondProject.asmdef", "GuidReference_SecondProject.asmdef.meta");
-        }
+        [Test] public void TestGuidReference01() { DoNamedTest2("GuidReference_SecondProject.asmdef", "GuidReference_SecondProject.asmdef.meta"); }
+
+        [Test, TestFileExtension(".asmref")] public void TestAsmRefNamedReference() { DoNamedTest2("AsmRef_FirstProject.asmdef"); }
+        [Test, TestFileExtension(".asmref")] public void TestAsmRefGuidReference() { DoNamedTest2("AsmRef_FirstProject.asmdef", "AsmRef_FirstProject.asmdef.meta"); }
 
         protected override TestSolutionConfiguration CreateSolutionConfiguration(
             ICollection<KeyValuePair<TargetFrameworkId, IEnumerable<string>>> referencedLibraries,
