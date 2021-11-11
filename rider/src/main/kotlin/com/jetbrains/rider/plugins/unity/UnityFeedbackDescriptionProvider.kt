9b0ca21da06887c74cadb45d1e87c6f61455d1d5
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2021-11-11 17:00:14 +0100

    refactor

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/UnityFeedbackDescriptionProvider.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/UnityFeedbackDescriptionProvider.kt
@@ -6,14 +6,13 @@ import com.intellij.ide.FeedbackDescriptionProvider
 import com.intellij.openapi.project.Project
 import com.intellij.workspaceModel.ide.WorkspaceModel
 import com.jetbrains.rider.plugins.unity.util.UnityInstallationFinder
-import com.jetbrains.rider.plugins.unity.workspace.getPackages
+import com.jetbrains.rider.plugins.unity.workspace.tryGetPackage
 
 class UnityFeedbackDescriptionProvider: FeedbackDescriptionProvider {
     override fun getDescription(project: Project?): String? {
         if (project == null) return null
         val version = UnityInstallationFinder.getInstance(project).getApplicationVersion() ?: return null
-        val allPackages = WorkspaceModel.getInstance(project).getPackages()
-        val packageVersion = allPackages.firstOrNull { it.id == "com.unity.ide.rider"}?.version
+        val packageVersion = WorkspaceModel.getInstance(project).tryGetPackage("com.unity.ide.rider")?.version ?: "missing"
         return "Unity: $version, JetBrains Rider package: $packageVersion"
     }
 }
