ba9feaa5e1d484cc70e0d14ec800782e82f07c60
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2021-11-30 09:01:40 +0100

    fix RIDER-62209 Unity Engine integration doesn't allow opening projects where the path has spaces.

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/util/UnityUtils.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/util/UnityUtils.kt
@@ -1,8 +1,8 @@
 package com.jetbrains.rider.plugins.unity.util
 
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.text.StringUtil
 import com.intellij.util.Restarter
+import com.intellij.util.execution.ParametersListUtil
 import com.jetbrains.rider.projectView.solutionDirectory
 
 fun convertPidToDebuggerPort(port: Int) = convertPidToDebuggerPort(port.toLong())
@@ -44,5 +44,5 @@ fun MutableList<String>.withProjectPath(project: Project) : MutableList<String> 
 }
 
 fun MutableList<String>.toProgramParameters() : String {
-    return StringUtil.join(this, "\n")
+    return ParametersListUtil.join(this)
 }
