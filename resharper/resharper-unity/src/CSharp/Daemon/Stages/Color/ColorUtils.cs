6a9e982a8f89068c3e8972c429639771066c06d2
Matt Ellis <m.t.ellis@gmail.com>
2022-01-02 22:03:47 +0000

    Provide standalone implementation of GetHue

--- resharper/resharper-unity/src/CSharp/Daemon/Stages/Color/ColorUtils.cs
+++ resharper/resharper-unity/src/CSharp/Daemon/Stages/Color/ColorUtils.cs
@@ -14,18 +14,37 @@ namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Stages.Color
             int min = Math.Min(color.R, Math.Min(color.G, color.B));
 
             hue = GetHue(color) / 360.0f;
-            saturation = (float) (max == 0 ? 0 : 1d - 1d * min / max);
-            value = (float) (max / 255d);
+            saturation = max == 0 ? 0 : 1f - 1f * min / max;
+            value = max / 255f;
         }
 
-        public static float GetHue(JetRgbaColor color)
+        // Returned as value between 0 and 360
+        // See https://www.cs.rit.edu/~ncs/color/t_convert.html
+        private static float GetHue(JetRgbaColor color)
         {
-            // TODO add getHue impl for JetRgbaColor
-            var nativeColor = System.Drawing.Color.FromArgb(color.A, color.R, color.G, color.B);
-            var hue = nativeColor.GetHue();
+            var max = Math.Max(color.R, Math.Max(color.G, color.B));
+            var min = Math.Min(color.R, Math.Min(color.G, color.B));
+
+            var delta = (max - min) / 255f;
+
+            var r = color.R / 255f;
+            var g = color.G / 255f;
+            var b = color.B / 255f;
+
+            float hue;
+            if (color.R == max)
+                hue = g - b / delta;		// between yellow & magenta
+            else if (color.G == max)
+                hue = 2 + (b - r ) / delta;	// between cyan & yellow
+            else
+                hue = 4 + (r - g) / delta;	// between magenta & cyan
+
+            hue *= 60.0f;
+            if (hue < 0)
+                hue += 360.0f;
             return hue;
         }
-        
+
         // Expects h as 0..1, not 0..360
         public static JetRgbaColor ColorFromHSV(float hue, float saturation, float value)
         {
