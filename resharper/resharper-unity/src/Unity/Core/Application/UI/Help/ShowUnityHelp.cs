95c094a8829b65dde819db1bca34dfc56ffb8103
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2022-01-24 11:57:52 +0100

    fix for new possible location of local Documentation

--- resharper/resharper-unity/src/Unity/Core/Application/UI/Help/ShowUnityHelp.cs
+++ resharper/resharper-unity/src/Unity/Core/Application/UI/Help/ShowUnityHelp.cs
@@ -87,6 +87,11 @@ namespace JetBrains.ReSharper.Plugins.Unity.Core.Application.UI.Help
             var appPath = mySolutionsManager.Solution?.GetComponent<UnityVersion>().GetActualAppPathForSolution();
             var contentsPath = UnityInstallationFinder.GetApplicationContentsPath(appPath);
             var root = contentsPath.Combine("Documentation");
+
+            // I see /home/ivan-shakhov/Unity/Hub/Editor/2021.2.4f1/Editor/Data/Documentation/Documentation/en path on my machine
+            if (root.Combine("Documentation").ExistsDirectory)
+                root = root.Combine("Documentation");
+            
             var englishRoot = root.Combine("en");
             if (root.IsAbsolute && !englishRoot.ExistsDirectory && root.ExistsDirectory)
                 return root.GetChildDirectories().FirstOrDefault(englishRoot).ToNativeFileSystemPath();
