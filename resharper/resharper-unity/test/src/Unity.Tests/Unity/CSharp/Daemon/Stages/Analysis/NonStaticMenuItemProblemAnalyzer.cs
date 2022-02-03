7a91f6f8dc745a76b1d31ea487d846af5c120b68
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2022-02-03 14:27:12 +0100

    add test

---
+++ resharper/resharper-unity/test/src/Unity.Tests/Unity/CSharp/Daemon/Stages/Analysis/NonStaticMenuItemProblemAnalyzer.cs
@@ -0,0 +1,14 @@
+using JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Errors;
+using NUnit.Framework;
+
+namespace JetBrains.ReSharper.Plugins.Tests.Unity.CSharp.Daemon.Stages.Analysis
+{
+    [TestUnity]
+    public class NonStaticMenuItemProblemAnalyzerTest
+        : CSharpHighlightingTestBase<InvalidStaticModifierWarning>
+    {
+        protected override string RelativeTestDataPath => @"CSharp\Daemon\Stages\Analysis";
+
+        [Test] public void TestInvalidStaticModifierWarning() { DoNamedTest2(); }
+    }
+}
