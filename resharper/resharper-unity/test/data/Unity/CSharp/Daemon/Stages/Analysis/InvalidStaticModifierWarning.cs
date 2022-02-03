d54fd0a90609ab16b6adc23d2c89db2da15ba016
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2022-02-03 15:30:35 +0100

    ++

---
+++ resharper/resharper-unity/test/data/Unity/CSharp/Daemon/Stages/Analysis/InvalidStaticModifierWarning.cs
@@ -0,0 +1,19 @@
+using UnityEditor;
+
+public class Class1
+{
+    [MenuItem("")]
+    public void Test()
+    {
+        
+    }
+}
+
+public class Class2
+{
+    [MenuItem("")]
+    public static void Test()
+    {
+        
+    }
+}
