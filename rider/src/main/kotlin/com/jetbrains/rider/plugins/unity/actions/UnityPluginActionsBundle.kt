97506e63cc3414902f87a9d5bbf05374135e496d
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2022-02-16 15:27:16 +0100

    internalize strings

---
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/actions/UnityPluginActionsBundle.kt
@@ -0,0 +1,22 @@
+package com.jetbrains.rider.plugins.unity.actions
+
+import com.intellij.DynamicBundle
+import org.jetbrains.annotations.Nls
+import org.jetbrains.annotations.NonNls
+import org.jetbrains.annotations.PropertyKey
+
+class UnityPluginActionsBundle: DynamicBundle(BUNDLE) {
+    companion object {
+        @NonNls
+        private const val BUNDLE = "messages.UnityPluginActionsBundle"
+        private val INSTANCE: UnityPluginActionsBundle = UnityPluginActionsBundle()
+
+        @Nls
+        fun message(
+            @PropertyKey(resourceBundle = BUNDLE) key: String,
+            vararg params: Any
+        ): String {
+            return INSTANCE.getMessage(key, *params)
+        }
+    }
+}
