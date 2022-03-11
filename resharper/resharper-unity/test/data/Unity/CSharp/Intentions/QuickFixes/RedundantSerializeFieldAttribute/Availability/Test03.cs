5a8354d488212dfdcedbefabcd4fb8b488531a11
Matt Ellis <m.t.ellis@gmail.com>
2022-03-11 11:59:25 +0000

    Update gold

--- resharper/resharper-unity/test/data/Unity/CSharp/Intentions/QuickFixes/RedundantSerializeFieldAttribute/Availability/Test03.cs
+++ resharper/resharper-unity/test/data/Unity/CSharp/Intentions/QuickFixes/RedundantSerializeFieldAttribute/Availability/Test03.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class Test
 {
+    // ReSharper disable once ConvertToConstant.Local
     [Serialize{caret}Field] private readonly int myValue1 = 42;
 
     public void Thing()
