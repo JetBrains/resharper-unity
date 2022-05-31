57375df59add8f6f26d44572ad0edbd95fa5e23f
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2022-05-31 17:32:47 +0200

    wip

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/explorer/MetaTracker.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/explorer/MetaTracker.kt
@@ -33,6 +33,13 @@ import kotlin.io.path.name
 class MetaTracker : BulkFileListener, VfsBackendRequester, Disposable {
     companion object {
         private val logger = getLogger<MetaTracker>()
+        fun getMetaFileName(fileName: String) = "$fileName.meta"
+        fun getMetaFile(path: String?): Path? {
+            path ?: return null
+            val file = Paths.get(path)
+            val metaFileName = getMetaFileName(file.name)
+            return file.parent.resolve(metaFileName)
+        }
     }
 
     override fun after(events: MutableList<out VFileEvent>) {
@@ -59,29 +66,30 @@ class MetaTracker : BulkFileListener, VfsBackendRequester, Disposable {
                                     }
                                 }
                                 is VFileDeleteEvent -> {
-                                    val metaFile = getMetaFile(event.path) ?: continue
+                                    val metaFile = Companion.getMetaFile(event.path) ?: continue
                                     actions.add(metaFile, project) {
                                         VfsUtil.findFile(metaFile, true)?.delete(this)
                                     }
                                 }
                                 is VFileCopyEvent -> {
-                                    val metaFile = getMetaFile(event.file.path) ?: continue
+                                    val metaFile = Companion.getMetaFile(event.file.path) ?: continue
                                     val ls = event.file.detectedLineSeparator ?: "\n"
                                     actions.add(metaFile, project) {
-                                        createMetaFile(event.file, event.newParent, getMetaFileName(event.newChildName), ls)
+                                        createMetaFile(event.file, event.newParent,
+                                            Companion.getMetaFileName(event.newChildName), ls)
                                     }
                                 }
                                 is VFileMoveEvent -> {
-                                    val metaFile = getMetaFile(event.oldPath) ?: continue
+                                    val metaFile = Companion.getMetaFile(event.oldPath) ?: continue
                                     actions.add(metaFile, project) {
                                         VfsUtil.findFile(metaFile, true)?.move(this, event.newParent)
                                     }
                                 }
                                 is VFilePropertyChangeEvent -> {
                                     if (!event.isRename) continue
-                                    val metaFile = getMetaFile(event.oldPath) ?: continue
+                                    val metaFile = Companion.getMetaFile(event.oldPath) ?: continue
                                     actions.add(metaFile, project) {
-                                        val target = getMetaFileName(event.newValue as String)
+                                        val target = Companion.getMetaFileName(event.newValue as String)
                                         val origin = VfsUtil.findFile(metaFile, true)
                                         val conflictingMeta = origin?.parent?.findChild(target)
                                         if (conflictingMeta != null) {
@@ -134,15 +142,6 @@ class MetaTracker : BulkFileListener, VfsBackendRequester, Disposable {
 
         return false
     }
-
-    private fun getMetaFile(path: String?): Path? {
-        path ?: return null
-        val file = Paths.get(path)
-        val metaFileName = getMetaFileName(file.name)
-        return file.parent.resolve(metaFileName)
-    }
-
-    private fun getMetaFileName(fileName: String) = "$fileName.meta"
 
     private fun createMetaFile(assetFile: VirtualFile?, parent: VirtualFile, metaFileName: String, ls: String) {
         if (assetFile != null && isHiddenAsset(assetFile)) return // not that children of a hidden folder (like `Documentation~`), would still pass this check. I think it is fine.
--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/ui/vcs/MetaFilesCheckinHandlerFactory.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/ui/vcs/MetaFilesCheckinHandlerFactory.kt
@@ -1,14 +1,19 @@
 package com.jetbrains.rider.plugins.unity.ui.vcs
 
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.vcs.CheckinProjectPanel
+import com.intellij.openapi.vcs.FileStatus
+import com.intellij.openapi.vcs.changes.Change
 import com.intellij.openapi.vcs.changes.CommitContext
 import com.intellij.openapi.vcs.changes.CommitExecutor
 import com.intellij.openapi.vcs.changes.ui.BooleanCommitOption
 import com.intellij.openapi.vcs.checkin.CheckinHandler
 import com.intellij.openapi.vcs.checkin.CheckinHandlerFactory
 import com.intellij.openapi.vcs.ui.RefreshableOnComponent
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.util.PairConsumer
+import com.jetbrains.rider.plugins.unity.explorer.MetaTracker
 import com.jetbrains.rider.plugins.unity.isUnityProject
 import com.jetbrains.rider.plugins.unity.ui.UnityUIBundle
 
@@ -22,6 +27,7 @@ import com.jetbrains.rider.plugins.unity.ui.UnityUIBundle
 
  * Maybe
  * 2. When you commit file, check that meta is also committed.
+ * 3. When file is removed - check that its meta file is removed.
 */
 class MetaFilesCheckinHandlerFactory : CheckinHandlerFactory() {
     override fun createHandler(panel: CheckinProjectPanel, commitContext: CommitContext): CheckinHandler =
@@ -35,7 +41,6 @@ private class MetaFilesCheckHandler(
 ) : CheckinHandler() {
 
     private val project = panel.project
-    private val changes = panel.selectedChanges
     private val settings = MetaFilesCheckinState.getService(project)
     override fun getBeforeCheckinConfigurationPanel(): RefreshableOnComponent? {
         if (!project.isUnityProject())
@@ -52,13 +57,49 @@ private class MetaFilesCheckHandler(
         additionalDataConsumer: PairConsumer<Any, Any>
     ): ReturnResult {
         if (settings.checkMetaFiles && project.isUnityProject()) {
-            if (changes.any()){
-                logger.warn(UnityUIBundle.message("commitOption.check.meta.files"))
-                return ReturnResult.CANCEL
+            val changes = panel.selectedChanges
+            if (changes.any()) {
+                logger.info(UnityUIBundle.message("commitOption.check.meta.files"))
+                // attempt to add metafile without main file
+                val addedMetaFilesWithoutMainFile = changes.filter {
+                    it.virtualFile != null
+                    it.fileStatus == FileStatus.ADDED
+                        && isMetaFile(it.virtualFile)
+                        && !changes.any {change->
+                            change.fileStatus == FileStatus.ADDED &&
+                            MetaTracker.getMetaFile(change.virtualFile?.path) == it.virtualFile?.toNioPath()
+                        }
+                }
+                if (addedMetaFilesWithoutMainFile.any()) return askUser(addedMetaFilesWithoutMainFile)
             }
 
         }
 
         return ReturnResult.COMMIT
+    }
+
+    private fun askUser(changes: List<Change>): ReturnResult {
+        val dialogResult = Messages.showDialog(project,
+            "Attempt to commit meta without asset",
+            "Attempt to commit meta without asset",
+            arrayOf(UnityUIBundle.message("dialog.unsaved.button.commit.anyway"),
+                "Uncheck problematic meta files",
+                UnityUIBundle.message("dialog.unsaved.button.cancel")),
+            0,
+            Messages.getWarningIcon(),
+            null
+        )
+        if (dialogResult == Messages.OK) return ReturnResult.COMMIT
+        if (dialogResult == 1) {
+            changes.forEach{
+                // uncheck those changes in the panel
+            }
+        }
+        return ReturnResult.CANCEL
+    }
+
+    private fun isMetaFile(file: VirtualFile?): Boolean {
+        val extension = file?.extension ?: return false
+        return "meta".equals(extension, true)
     }
 }
