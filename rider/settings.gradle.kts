2069a4a51a86a55ee2165f774487c0cd09e777fd
denis.kuznetsov <denis.kuznetsov@jetbrains.com>
2022-07-11 11:26:07 +0200

    Allow use SNAPSHOT rd version with rd version synchronizer

--- rider/settings.gradle.kts
+++ rider/settings.gradle.kts
@@ -1,18 +1,23 @@
 rootProject.name = "rider-unity"
 
 pluginManagement {
+    val rdVersion: String by settings
     repositories {
+        if (rdVersion == "SNAPSHOT")
+            mavenLocal()
         maven { setUrl("https://cache-redirector.jetbrains.com/plugins.gradle.org") }
         // This is for snapshot version of 'org.jetbrains.intellij' plugin
         maven { setUrl("https://oss.sonatype.org/content/repositories/snapshots/") }
-        mavenLocal()
+    }
+    plugins {
+        id("com.jetbrains.rdgen") version rdVersion
     }
     resolutionStrategy {
         eachPlugin {
             when(requested.id.name) {
                 // This required to correctly rd-gen plugin resolution. May be we should switch our naming to match Gradle plugin naming convention.
                 "rdgen" -> {
-                    useModule("com.jetbrains.rd:rd-gen:${requested.version}")
+                    useModule("com.jetbrains.rd:rd-gen:${rdVersion}")
                 }
             }
         }
