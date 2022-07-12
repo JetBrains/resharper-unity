JetBrains/resharper-unity

commit a53840f16ad47ce28e1a514bd58afeec2a663c95

new Frontend meta-file-tracker (#2145)
* new Frontend meta-file-tracker

* update api

* wip

* fix

* fix SessionId

* Fix class not found for org/jdom/input/SAXBuilder

* Do not show misc project node for asset occurrences in find usages toolwindow

* fix gold

* LineSeparator for meta file, fix https://github.com/JetBrains/resharper-unity/issues/2001

* cleanup imports

Co-authored-by: Mikhail Filippov <mikhail@filippov.me>
Co-authored-by: Vladimir Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>

rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/workspace/UnityWorkspaceExtensions.kt
@@ -6,7 +6,7 @@ import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.workspaceModel.ide.WorkspaceModel
 import java.nio.file.Path
 
-val UNITY_EXCLUDED_PATTERNS = listOf("*.meta", "*.tmp")
+val UNITY_EXCLUDED_PATTERNS = listOf("*.tmp")
 val UNITY_PACKAGE_ID_MAPPING = "rider.unity.package.id"
 
 

resharper/resharper-unity/src/ProjectModel/MetaFileTracker.cs
@@ -1,321 +1,0 @@
-using System;
-using System.IO;
-using JetBrains.Application.changes;
-using JetBrains.Application.Progress;
-using JetBrains.Diagnostics;
-using JetBrains.DocumentManagers.Transactions;
-using JetBrains.Lifetimes;
-using JetBrains.ProjectModel;
-using JetBrains.ProjectModel.Tasks;
-using JetBrains.ReSharper.Resources.Shell;
-using JetBrains.Util;
-
-namespace JetBrains.ReSharper.Plugins.Unity.ProjectModel
-{
-    [SolutionComponent]
-    public class MetaFileTracker : IChangeProvider
-    {
-        private static readonly DateTime ourUnixTime = new DateTime(1970, 1, 1, 0, 0, 0);
-
-        private readonly ISolution mySolution;
-        private readonly ILogger myLogger;
-        private IProjectItem myLastAddedItem;
-
-        public MetaFileTracker(Lifetime lifetime, ChangeManager changeManager, ISolution solution, ILogger logger,
-                               ISolutionLoadTasksScheduler solutionLoadTasksScheduler,
-                               UnitySolutionTracker unitySolutionTracker)
-        {
-            mySolution = solution;
-            myLogger = logger;
-
-            solutionLoadTasksScheduler.EnqueueTask(new SolutionLoadTask("AdviseForChanges", SolutionLoadTaskKinds.AfterDone,
-                () =>
-                {
-                    if (!unitySolutionTracker.IsUnityProjectFolder.Value)
-                        return;
-
-                    changeManager.RegisterChangeProvider(lifetime, this);
-                    changeManager.AddDependency(lifetime, this, solution);
-                }));
-        }
-
-        public object Execute(IChangeMap changeMap)
-        {
-            var projectModelChange = changeMap.GetChange<ProjectModelChange>(mySolution);
-            if (projectModelChange == null) return null;
-            if (projectModelChange.IsOpeningSolution || projectModelChange.IsClosingSolution)
-                return null;
-
-            try
-            {
-                projectModelChange.Accept(new Visitor(this, myLogger));
-            }
-            catch (Exception e)
-            {
-                using (var sw = new StringWriter())
-                {
-                    projectModelChange.Dump(sw);
-                    myLogger.LogException(LoggingLevel.ERROR, e, ExceptionOrigin.Algorithmic,
-                        $"Unity::MetaFileTracker - Error processing project model change{Environment.NewLine}{sw}");
-                }
-            }
-            return null;
-        }
-
-        private class Visitor : RecursiveProjectModelChangeDeltaVisitor
-        {
-            private readonly MetaFileTracker myMetaFileTracker;
-            private readonly ILogger myLogger;
-
-            public Visitor(MetaFileTracker metaFileTracker, ILogger logger)
-            {
-                myMetaFileTracker = metaFileTracker;
-                myLogger = logger;
-            }
-
-            // Note that this method is called recursively, for projects, folders and files
-            public override void VisitItemDelta(ProjectItemChange change)
-            {
-                if (change.ProjectModelElement is IProject project)
-                    VisitProjectDelta(change, project);
-                else
-                    VisitFileOrFolderDelta(change);
-            }
-
-            private void VisitProjectDelta(ProjectItemChange projectChange, IProject project)
-            {
-                // When a project is reloaded, we get a removal notification for it and all of its
-                // files, followed by a load of addition notifications. If we don't handle this
-                // properly, we'll delete a load of .meta files and create new ones, causing big problems.
-                // So ignore any project removal messages. We can safely ignore them, as a) Unity will
-                // never do this, and b) you can't delete a project from Visual Studio/Rider, only remove it
-                if (projectChange.IsRemoved)
-                    return;
-
-                // Don't recurse if this project isn't a Unity project. Note that we don't do this
-                // for the IsRemoved case above, as the project doesn't have a solution at that point,
-                // and IsUnityProject will throw
-                if (!project.IsUnityProject())
-                    return;
-
-                base.VisitItemDelta(projectChange);
-            }
-
-            private void VisitFileOrFolderDelta(ProjectItemChange fileOrFolderChange)
-            {
-                var shouldVisitChildren = true;
-                if (IsRenamedAsset(fileOrFolderChange))
-                    shouldVisitChildren = OnItemRenamed(fileOrFolderChange);
-                else if (IsAddedAsset(fileOrFolderChange))
-                    OnItemAdded(fileOrFolderChange);
-                else if (IsRemovedAsset(fileOrFolderChange))
-                    OnItemRemoved(fileOrFolderChange);
-
-                if (shouldVisitChildren)
-                    base.VisitItemDelta(fileOrFolderChange);
-            }
-
-            private static bool IsRenamedAsset(ProjectItemChange change)
-            {
-                return change.IsMovedOut && ShouldHandleChange(change);
-            }
-
-            private bool IsAddedAsset(ProjectItemChange change)
-            {
-                if (change.IsAdded && ShouldHandleChange(change))
-                    return true;
-                return change.IsContentsExternallyChanged && myMetaFileTracker.myLastAddedItem != null &&
-                       myMetaFileTracker.myLastAddedItem.Location == change.ProjectItem.Location &&
-                       ShouldHandleChange(change);
-            }
-
-            private static bool IsRemovedAsset(ProjectItemChange change)
-            {
-                return change.IsRemoved && ShouldHandleChange(change);
-            }
-
-            private static bool ShouldHandleChange(ProjectItemChange change)
-            {
-                // String comparisons, treat as expensive if we're doing this very frequently
-                return IsAssetOrPackage(change) && !IsItemMetaFile(change) && !IsHiddenAsset(change);
-            }
-
-            private static bool IsAssetOrPackage(ProjectItemChange change)
-            {
-                var rootFolder = GetRootFolder(change.OldParentFolder);
-                var solution = change.GetSolution();
-                if (solution == null)
-                    return false;
-                if (rootFolder == null)
-                    return false;
-                if (rootFolder.Location.Equals(solution.SolutionDirectory.Combine(ProjectExtensions.AssetsFolder)))
-                    return true;
-                if (rootFolder.Location.Equals(solution.SolutionDirectory.Combine(ProjectExtensions.PackagesFolder))
-                       && !change.OldParentFolder.Location.Equals(solution.SolutionDirectory.Combine(ProjectExtensions.PackagesFolder))) // exclude direct children of PackagesFolder
-                    return true;
-                return change.OldParentFolder.IsLinked; // support local package linked by relative path
-            }
-
-            private static IProjectFolder GetRootFolder(IProjectItem item)
-            {
-                while (item?.ParentFolder != null && item.ParentFolder.Kind != ProjectItemKind.PROJECT)
-                    item = item.ParentFolder;
-                return item as IProjectFolder;
-            }
-
-            private static bool IsItemMetaFile(ProjectItemChange change)
-            {
-                return change.ProjectItem.Name.EndsWith(".meta", StringComparison.OrdinalIgnoreCase);
-            }
-
-            private static bool IsHiddenAsset(ProjectItemChange change)
-            {
-                // See "Hidden Assets" on this page: https://docs.unity3d.com/Manual/SpecialFolders.html
-                return change.ProjectItem.Name.EndsWith("~", StringComparison.Ordinal)
-                    || change.ProjectItem.Name.StartsWith(".", StringComparison.Ordinal)
-                    || (change.ProjectItem as IProjectFile)?.Name.EndsWith(".tmp", StringComparison.OrdinalIgnoreCase) == true
-                    || change.ProjectItem.Name.Equals("cvs");
-            }
-
-            private bool OnItemRenamed(ProjectItemChange change)
-            {
-                myLogger.Trace("*** resharper-unity: Item renamed {0} -> {1}", change.OldLocation, change.ProjectItem.Location);
-
-                var newMetaFile = GetMetaFile(change.ProjectItem.Location);
-                if (!newMetaFile.ExistsFile)
-                {
-                    var oldMetaFile = change.OldParentFolder.Location.Combine(change.OldLocation.Name + ".meta");
-                    if (newMetaFile != oldMetaFile)
-                    {
-                        if (oldMetaFile.ExistsFile)
-                            RenameMetaFile(oldMetaFile, newMetaFile, string.Empty);
-                        else
-                            CreateMetaFile(newMetaFile);
-                    }
-                }
-
-                // Don't recurse for folder renames - the child contents will be "renamed", but
-                // the old location will no longer be there, and the meta files don't need moving
-                return !(change.ProjectItem is IProjectFolder);
-            }
-
-            private void OnItemAdded(ProjectItemChange change)
-            {
-                myLogger.Trace("*** resharper-unity: Item added {0}", change.ProjectItem.Location);
-
-                CreateMetaFile(GetMetaFile(change.ProjectItem.Location));
-
-                // We sometimes get notified of an item being added when it's not actually on disk.
-                // This appears to be happen:
-                // 1) When loading a project, with a stale cache (it gets removed once loaded)
-                // 2) When invoking the Move To Folder refactoring on a type. ReSharper adds a new file
-                //    and then removes the existing file, but only if it's empty. We create a new meta
-                //    file, and overwrite it with the existing one if the empty file is removed.
-                myMetaFileTracker.myLastAddedItem = null;
-                if (change.ProjectItem.Location.Exists == FileSystemPath.Existence.Missing)
-                    myMetaFileTracker.myLastAddedItem = change.ProjectItem;
-            }
-
-            private void OnItemRemoved(ProjectItemChange change)
-            {
-                myLogger.Trace("*** resharper-unity: Item removed {0}", change.OldLocation);
-
-                var metaFile = GetMetaFile(change.OldLocation);
-                if (!metaFile.ExistsFile)
-                    return;
-
-                if (IsMoveToFolderRefactoring(change))
-                {
-                    var newMetaFile = GetMetaFile(myMetaFileTracker.myLastAddedItem.Location);
-                    RenameMetaFile(metaFile, newMetaFile, " via add/remove");
-                }
-                else
-                {
-                    DeleteMetaFile(metaFile);
-                }
-
-                myMetaFileTracker.myLastAddedItem = null;
-            }
-
-            private bool IsMoveToFolderRefactoring(ProjectItemChange change)
-            {
-                // Adding to one folder and removing from another is the Move To Folder refactoring. If
-                // we get a remove event, that means the original file is empty, and we should treat it
-                // like a rename
-                return myMetaFileTracker.myLastAddedItem != null &&
-                       myMetaFileTracker.myLastAddedItem.Name == change.OldLocation.Name &&
-                       myMetaFileTracker.myLastAddedItem.Location != change.OldLocation;
-            }
-
-            private static VirtualFileSystemPath GetMetaFile(VirtualFileSystemPath location)
-            {
-                return VirtualFileSystemPath.Parse(location + ".meta", InteractionContext.SolutionContext);
-            }
-
-            private void CreateMetaFile(VirtualFileSystemPath path)
-            {
-                if (path.ExistsFile)
-                    return;
-
-                try
-                {
-                    var guid = Guid.NewGuid();
-                    var timestamp = (long)(DateTime.UtcNow - ourUnixTime).TotalSeconds;
-                    DoUnderTransaction("Unity::CreateMetaFile", () => path.WriteAllText($"fileFormatVersion: 2{Environment.NewLine}guid: {guid:N}{Environment.NewLine}timeCreated: {timestamp}"));
-                    myLogger.Info("*** resharper-unity: Meta added {0}", path);
-                }
-                catch (Exception e)
-                {
-                    myLogger.LogException(LoggingLevel.ERROR, e, ExceptionOrigin.Assertion,
-                        $"Failed to create Unity meta file {path}");
-                }
-            }
-
-            private void RenameMetaFile(VirtualFileSystemPath oldPath, VirtualFileSystemPath newPath, string extraDetails)
-            {
-                try
-                {
-                    myLogger.Info("*** resharper-unity: Meta renamed{2} {0} -> {1}", oldPath, newPath, extraDetails);
-                    DoUnderTransaction("Unity::RenameMetaFile", () => oldPath.MoveFile(newPath, true));
-                }
-                catch (Exception e)
-                {
-                    myLogger.LogException(LoggingLevel.ERROR, e, ExceptionOrigin.Assertion,
-                        $"Failed to rename Unity meta file {oldPath} -> {newPath}");
-                }
-            }
-
-            private void DeleteMetaFile(VirtualFileSystemPath path)
-            {
-                try
-                {
-                    if (path.ExistsFile)
-                    {
-                        DoUnderTransaction("Unity::DeleteMetaFile", () =>
-                        {
-#if DEBUG
-                            path.MoveFile(VirtualFileSystemPath.Parse(path + ".deleted", InteractionContext.SolutionContext), true);
-#else
-                            path.DeleteFile();
-#endif
-                        });
-                        myLogger.Info("*** resharper-unity: Meta removed {0}", path);
-                    }
-                }
-                catch (Exception e)
-                {
-                    myLogger.LogException(LoggingLevel.ERROR, e, ExceptionOrigin.Assertion,
-                        $"Failed to delete Unity meta file {path}");
-                }
-            }
-
-            private void DoUnderTransaction(string command, Action action)
-            {
-                // Create a transaction - Rider will hook the file system and cause the VFS to refresh
-                using (WriteLockCookie.Create())
-                using (myMetaFileTracker.mySolution.CreateTransactionCookie(DefaultAction.Commit, command, NullProgressIndicator.Create()))
-                    action();
-            }
-        }
-    }
-}

resharper/resharper-unity/src/Rider/UnityRefreshTracker.cs
@@ -215,7 +215,7 @@ namespace JetBrains.ReSharper.Plugins.Unity.Rider
 
         private void AdviseFileAddedOrDeleted(FileSystemChangeDelta delta)
         {
-            if (delta.NewPath.ExtensionNoDot == "cs")
+            if (delta.NewPath.ExtensionNoDot == "cs" || delta.NewPath.ExtensionNoDot == "asmdef")
             {
                 myLogger.Verbose($"fileSystemTracker.AdviseDirectoryChanges {delta.ChangeType}, {delta.NewPath}, {delta.OldPath}");
                 myGroupingEvent.FireIncoming();

rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/explorer/MetaTracker.kt
@@ -1,0 +1,226 @@
+@file:Suppress("UnstableApiUsage")
+
+package com.jetbrains.rider.plugins.unity.explorer
+
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.command.CommandProcessor
+import com.intellij.openapi.command.undo.UndoManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.VirtualFileManager
+import com.intellij.openapi.vfs.newvfs.BulkFileListener
+import com.intellij.openapi.vfs.newvfs.events.*
+import com.intellij.util.PathUtil
+import com.jetbrains.rd.platform.util.application
+import com.jetbrains.rd.platform.util.getLogger
+import com.jetbrains.rdclient.util.idea.toIOFile
+import com.jetbrains.rider.plugins.unity.isUnityProjectFolder
+import com.jetbrains.rider.projectDir
+import com.jetbrains.rider.projectView.VfsBackendRequester
+import com.jetbrains.rider.projectView.workspace.impl.WorkspaceUserModelUpdater
+import org.jetbrains.annotations.Nls
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.util.*
+import kotlin.io.path.ExperimentalPathApi
+import kotlin.io.path.name
+
+@ExperimentalPathApi
+class MetaTracker(private val project: Project) : BulkFileListener, VfsBackendRequester, Disposable {
+
+    companion object {
+        private val logger = getLogger<MetaTracker>()
+    }
+
+    private var nextGroupIdIndex = 0
+
+    init {
+        project.messageBus.connect(this).subscribe(VirtualFileManager.VFS_CHANGES, this)
+    }
+
+    override fun after(events: MutableList<out VFileEvent>) {
+        if (!project.isUnityProjectFolder())
+            return
+
+        // Collect modified meta files at first (usually there is no such files, but still)
+        val metaFiles = hashSetOf<Path>()
+        for (event in events) {
+            if (!translateEvent(event)) continue
+            if (!isApplicable(event)) continue
+            if (isMetaFile(event)) {
+                metaFiles.add(Paths.get(event.path))
+            }
+        }
+
+        // ... and then construct meta actions
+        val actions = MetaActionList(metaFiles)
+        for (event in events) {
+            if (!translateEvent(event) || !isApplicable(event) || isMetaFile(event)) continue
+
+            try {
+                when (event) {
+                    is VFileCreateEvent -> {
+                        if (!isApplicableForCreatingMeta(event)) continue
+                        val metaFileName = getMetaFileName(event.childName)
+                        val metaFile = event.parent.toNioPath().resolve(metaFileName)
+                        val ls = event.file?.detectedLineSeparator ?: "\n" // from what I see, Unity 2020.3 always uses "\n", but lets use same as the main file.
+                        actions.add(metaFile) {
+                            createMetaFile(event.parent, metaFileName, ls)
+                        }
+                    }
+                    is VFileDeleteEvent -> {
+                        val metaFile = getMetaFile(event.path) ?: continue
+                        actions.add(metaFile) {
+                            VfsUtil.findFile(metaFile, true)?.delete(this)
+                        }
+                    }
+                    is VFileCopyEvent -> {
+                        if (!isApplicableForCreatingMeta(event)) continue
+                        val metaFile = getMetaFile(event.file.path) ?: continue
+                        val ls = event.file.detectedLineSeparator ?: "\n"
+                        actions.add(metaFile) {
+                            createMetaFile(event.newParent, getMetaFileName(event.newChildName), ls)
+                        }
+                    }
+                    is VFileMoveEvent -> {
+                        val metaFile = getMetaFile(event.oldPath) ?: continue
+                        actions.add(metaFile) {
+                            VfsUtil.findFile(metaFile, true)?.move(this, event.newParent)
+                        }
+                    }
+                    is VFilePropertyChangeEvent -> {
+                        if (!event.isRename) continue
+                        val metaFile = getMetaFile(event.oldPath) ?: continue
+                        actions.add(metaFile) {
+                            val target = getMetaFileName(event.newValue as String)
+                            val origin = VfsUtil.findFile(metaFile, true)
+                            val conflictingMeta = origin?.parent?.findChild(target)
+                            if (conflictingMeta != null) {
+                                logger.warn("Removing conflicting meta $conflictingMeta")
+                                conflictingMeta.delete(this)
+                            }
+                            origin?.rename(this, target)
+                        }
+                    }
+                }
+            }
+            catch (t: Throwable) {
+                logger.error(t)
+                continue
+            }
+        }
+
+        if (actions.isEmpty()) return
+
+        val commandProcessor = CommandProcessor.getInstance()
+        var groupId = commandProcessor.currentCommandGroupId
+        if (groupId == null) {
+            groupId = MetaGroupId(nextGroupIdIndex++)
+            commandProcessor.currentCommandGroupId = groupId
+        }
+
+        application.invokeLater {
+            commandProcessor.allowMergeGlobalCommands {
+                commandProcessor.executeCommand(project, {
+                    actions.executeUnderWriteLock()
+                }, actions.getCommandName(), groupId)
+            }
+        }
+    }
+
+    private fun translateEvent(event: VFileEvent): Boolean {
+        if (event.isFromRefresh) return false
+        if (UndoManager.getInstance(project).isUndoOrRedoInProgress) return false
+        if (event.fileSystem !is LocalFileSystem) return false
+        return CommandProcessor.getInstance().currentCommand != null
+    }
+
+    private fun isMetaFile(event: VFileEvent): Boolean {
+        val extension = event.file?.extension ?: PathUtil.getFileExtension(event.path)
+        return "meta".equals(extension, true)
+    }
+
+    private fun isApplicable(event: VFileEvent): Boolean {
+        val file = event.file ?: return false
+        val parentFolder = file.parent?.toIOFile() ?: return false
+        val parentPath = parentFolder.toPath()
+        // exclude direct children of Packages folder
+        if (parentPath == project.projectDir.toIOFile().toPath().resolve("Packages"))
+            return false
+
+        return true
+    }
+
+    private fun isApplicableForCreatingMeta(event: VFileEvent):Boolean {
+        // exclude files added manually with `Attach existing folder` action
+        val file = event.file?.toIOFile() ?: return false
+        for (attachedFolder in WorkspaceUserModelUpdater.getInstance(project).getAttachedFolders()) {
+            if (VfsUtil.isAncestor(attachedFolder, file, false)) return false
+        }
+        return true
+    }
+
+    private fun getMetaFile(path: String?): Path? {
+        path ?: return null
+        val file = Paths.get(path)
+        val metaFileName = getMetaFileName(file.name)
+        return file.parent.resolve(metaFileName)
+    }
+
+    private fun getMetaFileName(fileName: String) = "$fileName.meta"
+
+    private fun createMetaFile(parent: VirtualFile, metaFileName: String, ls:String) {
+        val file = parent.createChildData(this, metaFileName)
+        val guid = UUID.randomUUID().toString().replace("-", "").substring(0, 32)
+        val timestamp = LocalDateTime.now(ZoneOffset.UTC).atZone(ZoneOffset.UTC).toEpochSecond() // LocalDateTime to epoch seconds
+        val content = "fileFormatVersion: 2${ls}guid: ${guid}${ls}timeCreated: $timestamp"
+        VfsUtil.saveText(file, content)
+    }
+
+    override fun dispose() = Unit
+
+    private class MetaActionList(private val changedMetaFiles: HashSet<Path>) {
+
+        private val actions = mutableListOf<MetaAction>()
+
+        fun add(metaFile: Path, action: () -> Unit) {
+            if (changedMetaFiles.contains(metaFile)) return
+            actions.add(MetaAction(metaFile, action))
+        }
+
+        fun isEmpty() = actions.isEmpty()
+
+        fun executeUnderWriteLock() {
+            application.runWriteAction {
+                actions.forEach { it.execute() }
+            }
+        }
+
+        @Nls
+        fun getCommandName(): String {
+            return if (actions.count() == 1)
+                "Process '${actions.single().metaFile.name}'"
+            else
+                "Process ${actions.count()} .meta Files"
+        }
+    }
+
+    private class MetaAction(val metaFile: Path, private val action: () -> Unit) {
+        fun execute() {
+            try {
+                action()
+            }
+            catch (ex: Throwable) {
+                logger.error(ex)
+            }
+        }
+    }
+
+    private class MetaGroupId(val index: Int) {
+        override fun toString() = "MetaGroupId$index"
+    }
+}

rider/src/main/resources/META-INF/plugin.xml
@@ -102,6 +102,7 @@
     <projectViewPane implementation="com.jetbrains.rider.plugins.unity.explorer.UnityExplorer" id="UnityExplorer" order="first, before SolutionExplorer"/>
     <projectModelViewUpdater implementation="com.jetbrains.rider.plugins.unity.explorer.UnityExplorerProjectModelViewUpdater"/>
     <projectModelViewExtensions implementation="com.jetbrains.rider.plugins.unity.explorer.UnityProjectModelViewExtensions"/>
+    <projectService serviceImplementation="com.jetbrains.rider.plugins.unity.explorer.MetaTracker" preload="true" />
 
     <!-- Run configurations -->
     <configurationType implementation="com.jetbrains.rider.plugins.unity.run.configurations.UnityDebugConfigurationType" />

unity/EditorPlugin/PluginEntryPoint.cs
@@ -749,6 +749,7 @@ namespace JetBrains.Rider.Unity.Editor
         : new FileInfo(UnityUtils.UnityEditorLogPath).Directory.FullName;
 
     internal static readonly string LogPath = Path.Combine(Path.Combine(ourBaseLogPath, "Unity3dRider"), $"EditorPlugin.{Process.GetCurrentProcess().Id}.log");
+    [UsedImplicitly]
     internal static OnOpenAssetHandler OpenAssetHandler;
 
     // Creates and deletes Library/EditorInstance.json containing info about unity instance. Unity 2017.1+ writes this

commit 4429410701a16beb2737280a0e8150f9b4ac684e

fix RIDER-79712 Unity meta files from PackageCache folder are indexed this was occasionally broken in https://github.com/JetBrains/resharper-unity/pull/2145

rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/workspace/UnityWorkspaceExtensions.kt
@@ -4,9 +4,8 @@ package com.jetbrains.rider.plugins.unity.workspace
 
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.workspaceModel.ide.WorkspaceModel
-import java.nio.file.Path
 
-val UNITY_EXCLUDED_PATTERNS = listOf("*.tmp")
+val UNITY_EXCLUDED_PATTERNS = listOf("*.meta", "*.tmp")
 val UNITY_PACKAGE_ID_MAPPING = "rider.unity.package.id"
 
 