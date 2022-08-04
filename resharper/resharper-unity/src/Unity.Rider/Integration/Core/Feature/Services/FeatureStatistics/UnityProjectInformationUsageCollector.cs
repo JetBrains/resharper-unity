2e600e111ec7307c3cc24492e5ae73fdf886d8fb
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2022-08-04 10:27:59 +0200

    wip collect stats on EnterPlayModeOptions

--- resharper/resharper-unity/src/Unity.Rider/Integration/Core/Feature/Services/FeatureStatistics/UnityProjectInformationUsageCollector.cs
+++ resharper/resharper-unity/src/Unity.Rider/Integration/Core/Feature/Services/FeatureStatistics/UnityProjectInformationUsageCollector.cs
@@ -1,16 +1,24 @@
+using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using JetBrains.Collections.Viewable;
+using JetBrains.Diagnostics;
 using JetBrains.Lifetimes;
 using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Feature.Services.FeaturesStatistics;
 using JetBrains.ReSharper.Feature.Services.Project;
 using JetBrains.ReSharper.Plugins.Unity.Core.ProjectModel;
+using JetBrains.ReSharper.Plugins.Unity.Core.Psi.Modules;
 using JetBrains.ReSharper.Plugins.Unity.UnityEditorIntegration;
+using JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.Caches;
+using JetBrains.ReSharper.Plugins.Yaml.Psi;
+using JetBrains.ReSharper.Plugins.Yaml.Psi.Tree;
+using JetBrains.ReSharper.Psi.Files;
 using JetBrains.UsageStatistics.FUS.EventLog;
 using JetBrains.UsageStatistics.FUS.EventLog.Events;
 using JetBrains.UsageStatistics.FUS.EventLog.Fus;
+using JetBrains.Util;
 
 namespace JetBrains.ReSharper.Plugins.Unity.Rider.Integration.Core.Feature.Services.FeatureStatistics
 {
@@ -20,6 +28,7 @@ namespace JetBrains.ReSharper.Plugins.Unity.Rider.Integration.Core.Feature.Servi
         private readonly ISolution mySolution;
         private readonly UnitySolutionTracker myUnitySolutionTracker;
         private readonly FeaturesStartupMonitor myMonitor;
+        private readonly ILogger myLogger;
 
         private enum UnityProjectKind
         {
@@ -32,12 +41,16 @@ namespace JetBrains.ReSharper.Plugins.Unity.Rider.Integration.Core.Feature.Servi
         private EventLogGroup myGroup;
         private readonly EventId1<UnityProjectKind> myProjectKindEvent;
         private readonly EventId2<string, bool> myUnityVersionEvent;
+        private readonly EventId3<bool, bool, int> myEnterPlayModeOptionsEvent;
+        private readonly UnityExternalFilesPsiModule myUnityModule;
 
-        public UnityProjectInformationUsageCollector(ISolution solution, UnitySolutionTracker unitySolutionTracker, FeaturesStartupMonitor monitor, FeatureUsageLogger featureUsageLogger)
+        public UnityProjectInformationUsageCollector(ISolution solution, UnitySolutionTracker unitySolutionTracker, FeaturesStartupMonitor monitor, FeatureUsageLogger featureUsageLogger, ILogger logger)
         {
             mySolution = solution;
             myUnitySolutionTracker = unitySolutionTracker;
             myMonitor = monitor;
+            myLogger = logger;
+            myUnityModule = UnityProjectSettingsUtils.GetUnityModule(solution);
             myGroup = new EventLogGroup("dotnet.unity.projects", "Unity Project Information", 1, featureUsageLogger);
             myProjectKindEvent = myGroup.RegisterEvent("projectKind", "Project Kind", 
                 EventFields.Enum<UnityProjectKind>("type", "Type"));
@@ -45,7 +58,10 @@ namespace JetBrains.ReSharper.Plugins.Unity.Rider.Integration.Core.Feature.Servi
             myUnityVersionEvent = myGroup.RegisterEvent("version", "Project Unity Version", 
                 EventFields.StringValidatedByRegexp("version", "Unity Version", UnityVersion.VersionRegex),
                 EventFields.Boolean("isCustom", "Custom Unity Build")); 
-
+            myEnterPlayModeOptionsEvent = myGroup.RegisterEvent("EnterPlayModeOptions", "Enter Play Mode Options",
+                EventFields.Boolean("Exists", "EnterPlayModeOptionsEnabled exists in the project"),
+                EventFields.Boolean("EnterPlayModeOptionsEnabled", "Enter Play Mode Option"),
+                EventFields.Int("EnterPlayModeOptions", "EnterPlayModeOptions flags"));
         }
         
         public override EventLogGroup GetGroup()
@@ -82,6 +98,8 @@ namespace JetBrains.ReSharper.Plugins.Unity.Rider.Integration.Core.Feature.Servi
                     var hashSet = new HashSet<MetricEvent>();
                     hashSet.Add(myProjectKindEvent.Metric(GetProjectType()));
                     hashSet.Add(myUnityVersionEvent.Metric(verifiedVersion, isCustom));
+                    var (exists, isEnabled, options) = GetEnterPlayModeOptions();
+                    hashSet.Add(myEnterPlayModeOptionsEvent.Metric(exists, isEnabled, options));
 
                     tcs.TrySetResult(hashSet);
                     return true;
@@ -90,6 +108,27 @@ namespace JetBrains.ReSharper.Plugins.Unity.Rider.Integration.Core.Feature.Servi
                 return false;
             });
             return tcs.Task;
+        }
+
+        private (bool exists, bool isEnabled, int options) GetEnterPlayModeOptions()
+        {
+            var editorSettings = UnityProjectSettingsUtils.GetEditorSettings(myUnityModule);
+            Assertion.Assert(editorSettings != null);
+            // todo: fails with "This operation requires a reader lock."
+            var yamlFile = editorSettings.GetDominantPsiFile<YamlLanguage>() as IYamlFile;
+            Assertion.Assert(yamlFile != null);
+            var node = UnityProjectSettingsUtils.GetValue<INode>(yamlFile, "EditorSettings",
+                "m_EnterPlayModeOptionsEnabled");
+            var optionsNode = UnityProjectSettingsUtils.GetValue<INode>(yamlFile, "EditorSettings", "m_EnterPlayModeOptions");
+            if (node == null)
+                return (false, false, 0);
+            if (optionsNode == null)
+            {
+                myLogger.Error("m_EnterPlayModeOptions was not found, but m_EnterPlayModeOptionsEnabled was present.");
+                return (false, false, 0);
+            }
+            return (true, Convert.ToBoolean(Convert.ToInt32(node.GetText())), 
+               Convert.ToInt32(optionsNode.GetText()));
         }
 
         private UnityProjectKind GetProjectType()
