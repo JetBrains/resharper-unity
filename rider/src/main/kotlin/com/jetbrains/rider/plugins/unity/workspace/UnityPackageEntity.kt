a5e3ef1a874c1aaddec25a32359c9315a25eee93
Evgeniy.Stepanov <Evgeniy.Stepanov@JetBrains.com>
2022-08-11 15:45:58 +0200

    Update workspace model entities

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/workspace/UnityPackageEntity.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/workspace/UnityPackageEntity.kt
@@ -48,33 +48,33 @@ interface UnityPackageEntity : WorkspaceEntity {
 
     val packageFolder: VirtualFile? get() = contentRootEntity?.url?.virtualFile
 
-    //region generated code
-    @GeneratedCodeApiVersion(1)
-    interface Builder : UnityPackageEntity, ModifiableWorkspaceEntity<UnityPackageEntity>, ObjBuilder<UnityPackageEntity> {
-        override var descriptor: UnityPackage
-        override var entitySource: EntitySource
-        override var contentRootEntity: ContentRootEntity?
-    }
+  //region generated code
+  @GeneratedCodeApiVersion(1)
+  interface Builder : UnityPackageEntity, ModifiableWorkspaceEntity<UnityPackageEntity>, ObjBuilder<UnityPackageEntity> {
+    override var descriptor: UnityPackage
+    override var entitySource: EntitySource
+    override var contentRootEntity: ContentRootEntity?
+  }
 
-    companion object : Type<UnityPackageEntity, Builder>() {
-        operator fun invoke(descriptor: UnityPackage, entitySource: EntitySource, init: (Builder.() -> Unit)? = null): UnityPackageEntity {
-            val builder = builder()
-            builder.descriptor = descriptor
-            builder.entitySource = entitySource
-            init?.invoke(builder)
-            return builder
-        }
+  companion object : Type<UnityPackageEntity, Builder>() {
+    operator fun invoke(descriptor: UnityPackage, entitySource: EntitySource, init: (Builder.() -> Unit)? = null): UnityPackageEntity {
+      val builder = builder()
+      builder.descriptor = descriptor
+      builder.entitySource = entitySource
+      init?.invoke(builder)
+      return builder
     }
-    //endregion
+  }
+  //endregion
 
 }
 
 //region generated code
 fun MutableEntityStorage.modifyEntity(entity: UnityPackageEntity, modification: UnityPackageEntity.Builder.() -> Unit) = modifyEntity(
-    UnityPackageEntity.Builder::class.java, entity, modification)
+  UnityPackageEntity.Builder::class.java, entity, modification)
 
 var ContentRootEntity.Builder.unityPackageEntity: UnityPackageEntity?
-    by WorkspaceEntity.extension()
+  by WorkspaceEntity.extension()
 //endregion
 
 @Suppress("unused")
