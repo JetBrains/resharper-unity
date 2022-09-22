9f4664c24ee179135e3fbaec40b82dbfdeeed284
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2022-09-22 16:34:14 +0200

    add test

---
+++ rider/src/test/kotlin/markup/InputSystemTest.kt
@@ -0,0 +1,41 @@
+import base.integrationTests.prepareAssemblies
+import com.jetbrains.rd.platform.util.lifetime
+import com.jetbrains.rd.util.reactive.valueOrDefault
+import com.jetbrains.rdclient.daemon.util.annotateDocumentWithHighlighterTags
+import com.jetbrains.rdclient.daemon.util.backendAttributeIdOrThrow
+import com.jetbrains.rdclient.util.idea.waitAndPump
+import com.jetbrains.rider.plugins.unity.model.frontendBackend.frontendBackendModel
+import com.jetbrains.rider.projectView.solution
+import com.jetbrains.rider.test.base.BaseTestWithSolution
+import com.jetbrains.rider.test.framework.executeWithGold
+import com.jetbrains.rider.test.scriptingApi.markupContributor
+import com.jetbrains.rider.test.scriptingApi.runSwea
+import com.jetbrains.rider.test.scriptingApi.withOpenedEditor
+import org.testng.annotations.Test
+import java.io.File
+import java.time.Duration
+
+
+class InputSystemTest : BaseTestWithSolution() {
+    override fun getSolutionDirectoryName(): String {
+        return "InputSystemTestData"
+    }
+
+    override fun preprocessTempDirectory(tempDir: File) {
+        prepareAssemblies(activeSolutionDirectory)
+    }
+    @Test
+    fun usedCodeTest() {
+        val projectLifetime = project.lifetime
+        val model = project.solution.frontendBackendModel
+        runSwea(project) // otherwise public methods are never marked unused
+        waitAndPump(projectLifetime, { model.isDeferredCachesCompletedOnce.valueOrDefault(false) }, Duration.ofSeconds(10),
+                    { "Deferred caches are not completed" })
+        executeWithGold(testGoldFile) { ps ->
+            withOpenedEditor("Assets/NewBehaviourScript.cs") {
+                ps.println(annotateDocumentWithHighlighterTags(markupContributor.markupAdapter,
+                                                               valueFilter = { it.backendAttributeIdOrThrow == "ReSharper Dead Code" }))
+            }
+        }
+    }
+}
