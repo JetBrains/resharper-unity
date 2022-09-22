0d87316d0b4ce954baba8bf6558880827738451f
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2022-09-22 14:06:45 +0200

    local cache

--- resharper/resharper-unity/src/Unity/InputActions/Psi/Caches/InputActionCache.cs
+++ resharper/resharper-unity/src/Unity/InputActions/Psi/Caches/InputActionCache.cs
@@ -8,6 +8,7 @@ using JetBrains.ReSharper.Plugins.Unity.UnityEditorIntegration;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.Caches;
 using JetBrains.ReSharper.Psi.Files;
+using JetBrains.Util;
 
 #nullable enable
 
@@ -16,11 +17,15 @@ namespace JetBrains.ReSharper.Plugins.Unity.InputActions.Psi.Caches
     [PsiComponent]
     public class InputActionCache : SimpleICache<List<InputActionCacheItem>>
     {
+        private readonly IShellLocks myShellLocks;
+        private readonly OneToListMap<IPsiSourceFile, List<InputActionCacheItem>> myLocalCache = new();
+
         public InputActionCache(Lifetime lifetime,
             IShellLocks shellLocks,
             IPersistentIndexManager persistentIndexManager)
             : base(lifetime, shellLocks, persistentIndexManager, InputActionCacheItem.Marshaller)
         {
+            myShellLocks = shellLocks;
         }
 
         public override object? Build(IPsiSourceFile sourceFile, bool isStartup)
@@ -73,7 +78,33 @@ namespace JetBrains.ReSharper.Plugins.Unity.InputActions.Psi.Caches
             
             return results;
         }
-        
+
+        public override void Merge(IPsiSourceFile sourceFile, object builtPart)
+        {
+            myLocalCache.RemoveKey(sourceFile);
+            AddToLocalCache(sourceFile, builtPart as List<InputActionCacheItem>);
+            base.Merge(sourceFile, builtPart);
+        }
+
+        private void AddToLocalCache(IPsiSourceFile sourceFile, List<InputActionCacheItem>? item)
+        {
+            if (item == null) return;
+            myLocalCache.Add(sourceFile, item);
+        }
+
+        public override void MergeLoaded(object data)
+        {
+            foreach (var inputActionCacheItem  in Map)
+                AddToLocalCache(inputActionCacheItem.Key, inputActionCacheItem.Value);
+            base.MergeLoaded(data);
+        }
+
+        public override void Drop(IPsiSourceFile sourceFile)
+        {
+            myLocalCache.RemoveKey(sourceFile);
+            base.Drop(sourceFile);
+        }
+
         protected override bool IsApplicable(IPsiSourceFile sf)
         {
             return base.IsApplicable(sf) && sf.IsInputActions() && sf.IsLanguageSupported<JsonNewLanguage>();
@@ -81,7 +112,11 @@ namespace JetBrains.ReSharper.Plugins.Unity.InputActions.Psi.Caches
 
         public bool ContainsName(string name)
         {
-            return Map.SelectMany(item => item.Value)
+            // ConcurrentDictionary<>
+            // lock
+            
+            return myLocalCache.SelectMany(item => item.Value)
+                .SelectMany(a=>a)
                 .Any(inputActionCacheItem => inputActionCacheItem.Name == name);
         }
     }
