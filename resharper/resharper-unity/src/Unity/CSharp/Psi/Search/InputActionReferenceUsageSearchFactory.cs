b4d96dd5374fe4bbd6fcaa59a6de7a70c209fc0d
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2022-10-07 10:40:50 +0200

    wip on InputActionReferenceUsageSearchFactory.cs

--- resharper/resharper-unity/src/Unity/CSharp/Psi/Search/InputActionReferenceUsageSearchFactory.cs
+++ resharper/resharper-unity/src/Unity/CSharp/Psi/Search/InputActionReferenceUsageSearchFactory.cs
@@ -3,19 +3,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using JetBrains.Annotations;
 using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Feature.Services.Occurrences;
 using JetBrains.ReSharper.Plugins.Unity.Utils;
-using JetBrains.ReSharper.Plugins.Unity.Yaml.Feature.Services.Navigation;
-using JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.DeferredCaches.AnimatorUsages;
-using JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.DeferredCaches.AssetHierarchy.References;
-using JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.DeferredCaches.AssetScriptUsages;
 using JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.DeferredCaches.InputActions;
-using JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.Search;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.CSharp;
 using JetBrains.ReSharper.Psi.ExtensionsAPI;
+using JetBrains.ReSharper.Psi.Impl.Search.SearchDomain;
 using JetBrains.ReSharper.Psi.Search;
 using JetBrains.ReSharper.Psi.Tree;
 using JetBrains.Util;
@@ -24,10 +19,102 @@ namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Psi.Search
 {
     // from csharp to inputactions
     [PsiSharedComponent]
-    public class UnityInputActionReferenceUsageSearchFactory : DomainSpecificSearcherFactoryBase
+    public class UnityInputActionReferenceUsageSearchFactory : IDomainSpecificSearcherFactory
     {
-        public override bool IsCompatibleWithLanguage(PsiLanguageType languageType) =>
-            languageType.Is<CSharpLanguage>();
+        public bool IsCompatibleWithLanguage(PsiLanguageType languageType)
+        {
+            return languageType.Is<CSharpLanguage>();
+        }
+
+        public IDomainSpecificSearcher CreateConstructorSpecialReferenceSearcher(ICollection<IConstructor> constructors)
+        {
+            return null;
+        }
+
+        public IDomainSpecificSearcher CreateTargetTypedObjectCreationSearcher(IReadOnlyList<IConstructor> constructors, IReadOnlyList<ITypeElement> typeElements,
+            ReferenceSearcherParameters referenceSearcherParameters)
+        {
+            return null;
+        }
+
+        public IDomainSpecificSearcher CreateMethodsReferencedByDelegateSearcher(IDelegate @delegate)
+        {
+            return null;
+        }
+
+        public IDomainSpecificSearcher CreateReferenceSearcher(IDeclaredElementsSet elements,
+            ReferenceSearcherParameters referenceSearcherParameters)
+        {
+            if (elements.Any(e => e is not IMethod))
+                return null;
+        
+            var solution = elements.First().GetSolution();
+            var container = solution.GetComponent<InputActionsElementContainer>();
+        
+            return new CSharpInputActionsReferenceSearcher(container, elements.Where(e => e is IMethod && IsInterestingElement(e)));
+        }
+
+        public IDomainSpecificSearcher CreateLateBoundReferenceSearcher(IDeclaredElementsSet elements,
+            ReferenceSearcherParameters referenceSearcherParameters)
+        {
+            return null;
+        }
+
+        public IDomainSpecificSearcher CreateTextOccurrenceSearcher(IDeclaredElementsSet elements)
+        {
+            return null;
+        }
+
+        public IDomainSpecificSearcher CreateTextOccurrenceSearcher(string subject)
+        {
+            return null;
+        }
+
+        public IDomainSpecificSearcher CreateAnonymousTypeSearcher(IList<AnonymousTypeDescriptor> typeDescription, bool caseSensitive)
+        {
+            return null;
+        }
+
+        public IDomainSpecificSearcher CreateConstantExpressionSearcher(ConstantValue constantValue, bool onlyLiteralExpression)
+        {
+            return null;
+        }
+
+        public IEnumerable<string> GetAllPossibleWordsInFile(IDeclaredElement element)
+        {
+            if (IsInterestingElement(element))
+                yield return element.ShortName.Substring(2);
+        }
+
+        public IEnumerable<RelatedDeclaredElement> GetRelatedDeclaredElements(IDeclaredElement element)
+        {
+            return EmptyList<RelatedDeclaredElement>.ReadOnly;
+        }
+
+        public IEnumerable<FindResult> GetRelatedFindResults(IDeclaredElement element)
+        {
+            return EmptyList<FindResult>.ReadOnly;
+        }
+
+        public DerivedFindRequest GetDerivedFindRequest(IFindResultReference result)
+        {
+            return null;
+        }
+
+        public NavigateTargets GetNavigateToTargets(IDeclaredElement element)
+        {
+           return NavigateTargets.Empty;
+        }
+
+        public ICollection<FindResult>? TransformNavigationTargets(ICollection<FindResult> targets)
+        {
+            return null;
+        }
+
+        public ISearchDomain GetDeclaredElementSearchDomain(IDeclaredElement declaredElement)
+        {
+            return EmptySearchDomain.Instance;
+        }
         
         public static bool IsInterestingElement(IDeclaredElement? element)
         {
@@ -44,24 +131,19 @@ namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Psi.Search
             return false;
             // todo: is instance method
         }
-
-        public override IEnumerable<string> GetAllPossibleWordsInFile(IDeclaredElement? element)
-        {
-            if (IsInterestingElement(element))
-                yield return element.ShortName.Substring(2);
-        }
-
-        public override IDomainSpecificSearcher? CreateReferenceSearcher(
-            IDeclaredElementsSet elements, ReferenceSearcherParameters referenceSearcherParameters)
-        {
-            if (elements.Any(e => e is not IMethod))
-                return null;
-
-            var solution = elements.First().GetSolution();
-            var container = solution.GetComponent<InputActionsElementContainer>();
-
-            return new CSharpInputActionsReferenceSearcher(container, elements.Where(e => e is IMethod && IsInterestingElement(e)));
-        }
+        
+        // public IDomainSpecificSearcher? CreateReferenceSearcher(
+        //     IDeclaredElementsSet elements, ReferenceSearcherParameters referenceSearcherParameters)
+        // {
+        //     if (elements.Any(e => e is not IMethod))
+        //         return null;
+        //
+        //     var solution = elements.First().GetSolution();
+        //     var container = solution.GetComponent<InputActionsElementContainer>();
+        //
+        //     return new CSharpInputActionsReferenceSearcher(container, elements.Where(e => e is IMethod && IsInterestingElement(e)));
+        // }
+        
     }
 
     internal class CSharpInputActionsReferenceSearcher : IDomainSpecificSearcher
