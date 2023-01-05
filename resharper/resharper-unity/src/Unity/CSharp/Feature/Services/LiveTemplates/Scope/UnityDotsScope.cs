2f5eace4a1702337e3998d7f3092d9045e844594
george.lemeshko <heorhii.lemeshko@jetbrains.com>
2023-01-05 12:21:33 +0100

    UnityDotsScope improvements

--- resharper/resharper-unity/src/Unity/CSharp/Feature/Services/LiveTemplates/Scope/UnityDotsScope.cs
+++ resharper/resharper-unity/src/Unity/CSharp/Feature/Services/LiveTemplates/Scope/UnityDotsScope.cs
@@ -8,12 +8,8 @@ namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Feature.Services.LiveTemplate
     public class UnityDotsScope : InUnityCSharpProject
     {
         private static readonly Guid DefaultUID = new("3C1BBCD2-1CE0-4C44-8677-D01294BEE4B7");
-        private static readonly Guid QuickUID = new("A735778F-6C8C-4D4B-8638-A0815F2665EC");
 
         public override Guid GetDefaultUID() => DefaultUID;
         public override string PresentableShortName => Strings.UnityDots_projects;
-        public override PsiLanguageType RelatedLanguage => CSharpLanguage.Instance;
-        public override string QuickListTitle => Strings.UnityDots_projects;
-        public override Guid QuickListUID => QuickUID;
     }
 }
