f0af98e4c5c2b2e7785efb4c857dc93e39d0c922
george.lemeshko <heorhii.lemeshko@jetbrains.com>
2023-01-09 15:22:57 +0100

    [code-review] change record to struct

--- resharper/resharper-unity/test/src/Unity.Tests/Unity/CSharp/Daemon/Stages/Analysis/SuppressDotsPartialTypeWithSingleUsageWarningTest.cs
+++ resharper/resharper-unity/test/src/Unity.Tests/Unity/CSharp/Daemon/Stages/Analysis/SuppressDotsPartialTypeWithSingleUsageWarningTest.cs
@@ -3,30 +3,27 @@ using JetBrains.ReSharper.Daemon.CSharp.Errors;
 
 namespace JetBrains.ReSharper.Plugins.Tests.Unity.CSharp.Daemon.Stages.Analysis
 {
-    public partial class SuppressDotsSuperTypeDeclaredInOtherPartWarningTests
+    [TestUnity]
+    public class SuppressDotsPartialTypeWithSingleUsageWarningTest : CSharpHighlightingTestBase<PartialTypeWithSinglePartWarning>
     {
-        [TestUnity]
-        public class SuppressDotsPartialTypeWithSingleUsageWarningTest : CSharpHighlightingTestBase<PartialTypeWithSinglePartWarning>
+        protected override string RelativeTestDataPath => @"CSharp\Daemon\Stages\Analysis\Dots";
+
+        [Test]
+        public void TestISystemSinglePartialClass()
         {
-            protected override string RelativeTestDataPath => @"CSharp\Daemon\Stages\Analysis\Dots";
+            DoNamedTest2("DotsClasses.cs");
+        }
 
-            [Test]
-            public void TestISystemSinglePartialClass()
-            {
-                DoNamedTest2("DotsClasses.cs");
-            }
+        [Test]
+        public void TestSystemBaseSinglePartialClass()
+        {
+            DoNamedTest2("DotsClasses.cs");
+        }
 
-            [Test]
-            public void TestSystemBaseSinglePartialClass()
-            {
-                DoNamedTest2("DotsClasses.cs");
-            }
-
-            [Test]
-            public void TestIAspectSinglePartialClass()
-            {
-                DoNamedTest2("DotsClasses.cs");
-            }
+        [Test]
+        public void TestIAspectSinglePartialClass()
+        {
+            DoNamedTest2("DotsClasses.cs");
         }
     }
 }
--- resharper/resharper-unity/test/src/Unity.Tests/Unity/CSharp/Daemon/Stages/Analysis/SuppressDotsSuperTypeDeclaredInOtherPartWarningTests.cs
+++ resharper/resharper-unity/test/src/Unity.Tests/Unity/CSharp/Daemon/Stages/Analysis/SuppressDotsSuperTypeDeclaredInOtherPartWarningTests.cs
@@ -4,7 +4,7 @@ using NUnit.Framework;
 namespace JetBrains.ReSharper.Plugins.Tests.Unity.CSharp.Daemon.Stages.Analysis
 {
     [TestUnity]
-    public partial class SuppressDotsSuperTypeDeclaredInOtherPartWarningTests : CSharpHighlightingTestBase<SuperTypeDeclaredInOtherPartWarning>
+    public class SuppressDotsSuperTypeDeclaredInOtherPartWarningTests : CSharpHighlightingTestBase<SuperTypeDeclaredInOtherPartWarning>
     {
         protected override string RelativeTestDataPath => @"CSharp\Daemon\Stages\Analysis\Dots";
 
