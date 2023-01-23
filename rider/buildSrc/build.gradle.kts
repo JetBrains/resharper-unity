7d292eb18ecd896710b10531c633c95ac9395ddf
Matt Ellis <m.t.ellis@gmail.com>
2023-01-23 15:14:46 +0000

    Bump IntelliJ Gradle plugin to 1.12.0

--- rider/buildSrc/build.gradle.kts
+++ rider/buildSrc/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.intellij", "org.jetbrains.intellij.gradle.plugin", "1.11.0")
+    implementation("org.jetbrains.intellij", "org.jetbrains.intellij.gradle.plugin", "1.12.0")
 }
 
 plugins {
