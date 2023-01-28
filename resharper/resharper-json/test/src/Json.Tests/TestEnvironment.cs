bd7f2b17c87effce348253ab1fc8bbb29d1480f4
Matt Ellis <matt.ellis@jetbrains.com>
2023-01-28 01:41:47 +0000

    Fix test data path for Json.Tests

--- resharper/resharper-json/test/src/Json.Tests/TestEnvironment.cs
+++ resharper/resharper-json/test/src/Json.Tests/TestEnvironment.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 using JetBrains.Application.BuildScript.Application.Zones;
@@ -22,7 +23,11 @@ using NUnit.Framework;
 
 // Maybe it should be using product/subplatform markers?
 #pragma warning disable 618
+#if INDEPENDENT_BUILD
 [assembly: TestDataPathBase("resharper-json/test/data")]
+#else
+[assembly: TestDataPathBase("Plugins/ReSharperUnity/resharper/resharper-json/test/data")]
+#endif
 #pragma warning restore 618
 
 namespace JetBrains.ReSharper.Plugins.Tests
@@ -74,9 +79,11 @@ namespace JetBrains.ReSharper.Plugins.Tests
         // The default logger outputs to $TMPDIR/JetLogs/ReSharperTests/resharper.log, which is not very helpful when
         // we're testing more than one assembly. This sets up an environment variable to output the log to
         // /resharper/build/{project}/logs
+        [Conditional("INDEPENDENT_BUILD")]
         private static void ConfigureLoggingFolderPath()
         {
-            Environment.SetEnvironmentVariable(Logger.JETLOGS_DIRECTORY_ENV_VARIABLE, GetLogsFolder().FullPath);
+            if (Environment.GetEnvironmentVariable(Logger.JETLOGS_DIRECTORY_ENV_VARIABLE) == null)
+                Environment.SetEnvironmentVariable(Logger.JETLOGS_DIRECTORY_ENV_VARIABLE, GetLogsFolder().FullPath);
         }
 
         private static FileSystemPath GetLogsFolder()
