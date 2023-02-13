70f17aaf9dd4325dd3b6c05d914680ee54483b9c
Matt Ellis <matt.ellis@jetbrains.com>
2023-02-13 10:40:01 +0000

    Comment and attribute implicitly used public APIs

--- unity/EditorPlugin/AfterUnity56/EntryPoint.cs
+++ unity/EditorPlugin/AfterUnity56/EntryPoint.cs
@@ -1,25 +1,32 @@
 ﻿using System;
+using JetBrains.Annotations;
 using UnityEditor;
 
 namespace JetBrains.Rider.Unity.Editor.AfterUnity56
 {
-  [InitializeOnLoad]
+  // DO NOT CHANGE NAME OR NAMESPACE!
+  // Accessed from the package via reflection
+  [InitializeOnLoad, PublicAPI]
   public static class EntryPoint
   {
+    // DO NOT REMOVE OR REFACTOR!
+    // When loaded by Unity from the Assets folder, Unity will automatically run this static class constructor. When the
+    // package loads the type, it will explicitly invoke the class constructor
+    [PublicAPI]
     static EntryPoint()
     {
       if (UnityUtils.IsInBatchModeAndNotInRiderTests)
         return;
-      
+
       PluginEntryPoint.OnModelInitialization += Initialization.OnModelInitializationHandler;
       PluginEntryPoint.OnModelInitialization += Navigation.Initialization.OnModelInitializationHandler;
       PluginEntryPoint.OnModelInitialization += Packages.Initialization.OnModelInitializationHandler;
-      AppDomain.CurrentDomain.DomainUnload += (EventHandler) ((_, __) =>
+      AppDomain.CurrentDomain.DomainUnload += (_, __) =>
       {
         PluginEntryPoint.OnModelInitialization -= Initialization.OnModelInitializationHandler;
         PluginEntryPoint.OnModelInitialization -= Navigation.Initialization.OnModelInitializationHandler;
         PluginEntryPoint.OnModelInitialization -= Packages.Initialization.OnModelInitializationHandler;
-      });
+      };
     }
   }
 }
