34a6a3b0285c7624bb6292a9a080ad0005a451d5
Matt Ellis <matt.ellis@jetbrains.com>
2023-02-16 12:19:23 +0000

    Disable action if already attached to editor

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/actions/AttachToUnityEditorAction.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/actions/AttachToUnityEditorAction.kt
@@ -1,10 +1,12 @@
 package com.jetbrains.rider.plugins.unity.actions
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.DumbAwareAction
 import com.jetbrains.rider.plugins.unity.run.DefaultRunConfigurationGenerator
 import com.jetbrains.rider.plugins.unity.run.configurations.attachToUnityEditor
+import com.jetbrains.rider.plugins.unity.run.configurations.isAttachedToUnityEditor
 
 class AttachUnityEditorAction: DumbAwareAction() {
     private val logger = Logger.getInstance(AttachUnityEditorAction::class.java)
@@ -15,4 +17,11 @@ class AttachUnityEditorAction: DumbAwareAction() {
             logger.warn("Have not found run-configuration ${DefaultRunConfigurationGenerator.ATTACH_CONFIGURATION_NAME}.")
         }
     }
+
+    override fun update(e: AnActionEvent) {
+        val project = e.project ?: return
+        e.presentation.isEnabled = !isAttachedToUnityEditor(project)
+    }
+
+    override fun getActionUpdateThread() = ActionUpdateThread.BGT
 }
