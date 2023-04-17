27a8221ea8c9490a44e6a00a9043b30c633f60f7
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2023-04-17 11:35:46 +0200

    use getVarFunctionDescriptors implementation in the platform
    fix test
    
    add test
    
    fix exception about wide-keywords
    
    suppress url paths validation
    
    fix RIDER-88190 Unity USS properties not supported
    
    add root pseudo class
    
    support USS CustomProperties (var)
    
    fix usages of deprecated api

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/css/uss/codeInsight/css/UssCssElementDescriptorFactory.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/css/uss/codeInsight/css/UssCssElementDescriptorFactory.kt
@@ -1,15 +1,17 @@
 package com.jetbrains.rider.plugins.unity.css.uss.codeInsight.css
 
 import com.intellij.openapi.components.service
-import com.intellij.openapi.progress.ProgressManager
 import com.intellij.psi.css.impl.descriptor.value.CssGroupValue
 import com.intellij.psi.css.impl.descriptor.value.CssNameValue
 import com.intellij.psi.css.impl.descriptor.value.CssTextValue
 import com.intellij.psi.css.impl.descriptor.value.CssValueDescriptorVisitorImpl
 import com.intellij.psi.css.impl.util.scheme.CssDescriptorsHolder
 import com.intellij.psi.css.impl.util.scheme.CssDescriptorsLoader
-import com.intellij.reference.SoftReference
+import com.intellij.psi.css.impl.util.scheme.CssElementDescriptorFactory2
 import java.lang.ref.Reference
+import java.lang.ref.SoftReference
+import com.intellij.reference.SoftReference.dereference
+
 
 class UssCssElementDescriptorFactory {
     companion object {
@@ -20,11 +22,14 @@ class UssCssElementDescriptorFactory {
     private var valueIdentifiersRef: Reference<Set<String>>? = null
 
     fun getDescriptors(): CssDescriptorsHolder {
-        var descriptors = SoftReference.dereference(cssDescriptorsHolderRef)
+        var descriptors = dereference(cssDescriptorsHolderRef)
         if (descriptors == null) {
-            val progressManager = ProgressManager.getInstance()
-            val loader = CssDescriptorsLoader(progressManager.progressIndicator)
-            loader.loadDescriptors(this::class.java.getResource("/uss/element-descriptors.xml"))
+            val loader = CssDescriptorsLoader()
+            val schemesToLoad = listOf("css-cascade-4.xml", "css3-transitions.xml", "css-transforms-1.xml", "css-transforms-2.xml")
+            schemesToLoad.forEach {
+                CssElementDescriptorFactory2.getInstance().javaClass.getResource("xml/$it")!!.let { url -> loader.loadDescriptors(url) }
+            }
+            this::class.java.getResource("/uss/element-descriptors.xml")!!.let { loader.loadDescriptors(it) }
             descriptors = loader.descriptors
             cssDescriptorsHolderRef = SoftReference(descriptors)
         }
@@ -33,7 +38,7 @@ class UssCssElementDescriptorFactory {
 
     // CssDescriptorsHolder#validIdentifiers is package private, so we have to calculate it ourselves
     fun getValueIdentifiers(): Set<String> {
-        var identifiers = SoftReference.dereference(valueIdentifiersRef)
+        var identifiers = dereference(valueIdentifiersRef)
         if (identifiers == null) {
             identifiers = mutableSetOf()
             getDescriptors().properties.values().forEach {
