09e19448521c02b3ff72806442aae5a003d7b279
Mikhail Filippov <mikhail@filippov.me>
2023-04-22 21:36:46 +0400

    RIDER-92468 Bump gradle version

--- rider/buildSrc/settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-// Affects the repositories used to resolve the plugins { } block
-pluginManagement {
-    repositories {
-        maven { setUrl("https://cache-redirector.jetbrains.com/plugins.gradle.org") }
-        // This is for snapshot version of 'org.jetbrains.intellij' plugin
-        maven { setUrl("https://oss.sonatype.org/content/repositories/snapshots/") }
-    }
-}
