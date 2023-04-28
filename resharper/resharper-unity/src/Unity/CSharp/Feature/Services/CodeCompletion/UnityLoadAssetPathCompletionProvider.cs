c5a0f0af6b3c35ae8fb77f984ad6564beeee9196
george.lemeshko <heorhii.lemeshko@jetbrains.com>
2023-04-28 11:38:22 +0200

    [RIDER-84223] Fix RIDER-84223 AssetPath completion

---
+++ resharper/resharper-unity/src/Unity/CSharp/Feature/Services/CodeCompletion/UnityLoadAssetPathCompletionProvider.cs
@@ -0,0 +1,380 @@
+#nullable enable
+
+using System;
+using System.Linq;
+using JetBrains.Collections;
+using JetBrains.DocumentModel;
+using JetBrains.ProjectModel;
+using JetBrains.ProjectModel.Resources;
+using JetBrains.ReSharper.Feature.Services.CodeCompletion;
+using JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure;
+using JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure.LookupItems;
+using JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure.LookupItems.Impl;
+using JetBrains.ReSharper.Feature.Services.CSharp.CodeCompletion.Infrastructure;
+using JetBrains.ReSharper.Feature.Services.Lookup;
+using JetBrains.ReSharper.Features.Intellisense.CodeCompletion.CSharp.Rules;
+using JetBrains.ReSharper.Plugins.Unity.Core.ProjectModel;
+using JetBrains.ReSharper.Plugins.Unity.Core.Psi.Modules;
+using JetBrains.ReSharper.Plugins.Unity.Resources.Icons;
+using JetBrains.ReSharper.Plugins.Unity.UnityEditorIntegration;
+using JetBrains.ReSharper.Plugins.Unity.UnityEditorIntegration.Api;
+using JetBrains.ReSharper.Plugins.Unity.UnityEditorIntegration.Packages;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+using JetBrains.ReSharper.Psi.ExpectedTypes;
+using JetBrains.ReSharper.Psi.Modules;
+using JetBrains.ReSharper.Psi.Tree;
+using JetBrains.TextControl;
+using JetBrains.UI.Icons;
+using JetBrains.UI.RichText;
+using JetBrains.Util;
+using JetBrains.Util.Logging;
+using ProjectExtensions = JetBrains.ReSharper.Plugins.Unity.Core.ProjectModel.ProjectExtensions;
+
+namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Feature.Services.CodeCompletion
+{
+    ///     ex. AssetDatabase.LoadAssetAtPath(assetPath:"...")
+    ///     assetPath could be "Assets/.../file.ext"
+    ///     or "Packages/com.companyname.package-name/Folder/.../file.ext"
+    [Language(typeof(CSharpLanguage))]
+    public class UnityLoadAssetPathCompletionProvider : CSharpItemsProviderBase<CSharpCodeCompletionContext>
+    {
+        protected override bool IsAvailable(CSharpCodeCompletionContext context)
+        {
+            var stringLiteral = context.StringLiteral();
+            if (stringLiteral == null)
+                return false;
+
+            if (context.NodeInFile.GetContainingNode<IArgument>() == null)
+                return false;
+
+            var isUnityProject = context.BasicContext.CodeCompletionType == CodeCompletionType.BasicCompletion
+                                 && context.PsiModule is IProjectPsiModule projectPsiModule
+                                 && projectPsiModule.Project.IsUnityProject();
+
+            return isUnityProject;
+        }
+
+        protected override bool AddLookupItems(CSharpCodeCompletionContext context, IItemsCollector collector)
+        {
+            // System.Diagnostics.Debugger.Launch();
+            var stringLiteral = context.StringLiteral();
+            if (stringLiteral == null)
+                return false;
+
+            var nodeInFile = context.NodeInFile;
+
+            var argument = nodeInFile.GetContainingNode<IArgument>();
+            if (argument == null)
+                return false;
+
+            var matchingParameter = argument.MatchingParameter;
+            if (matchingParameter == null)
+                return false;
+
+            var (parameter, _) = matchingParameter;
+            if (parameter == null)
+                return false;
+
+            var attributes = parameter.GetAttributeInstances(KnownTypes.AssetPathAttribute, true);
+            if (attributes.Count == 0)
+                return false;
+
+            var completionSearchInfo = CollectSearchInfo(context, stringLiteral);
+
+            if (completionSearchInfo.AbsolutePathCompletionFolder == null)
+                return false;
+
+            switch (completionSearchInfo.SearchPathType)
+            {
+                case CompletionSearchInfo.PassType.ProjectRoot:
+                    AddPredefinedDirectories(collector, completionSearchInfo.Ranges);
+                    return true;
+                case CompletionSearchInfo.PassType.PackagesRootFolder:
+                case CompletionSearchInfo.PassType.InternalPackageFolder:
+                    AddResourcesFromPackage(context, collector, completionSearchInfo);
+                    return true;
+                case CompletionSearchInfo.PassType.Unknown:
+                case CompletionSearchInfo.PassType.AssetsFolder:
+                    AddResourcesFromAssetsFolder(context, collector, completionSearchInfo);
+                    return true;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private static CompletionSearchInfo CollectSearchInfo(CSharpCodeCompletionContext context, ICSharpLiteralExpression stringLiteral)
+        {
+            var nodeInFile = context.NodeInFile;
+            var solution = nodeInFile.GetSolution();
+            var unitySolutionPath = solution.SolutionDirectory;
+
+            var relativeSearchPath = CalculateSearchPath(context, stringLiteral, nodeInFile, out var textLookupRanges);
+
+            if (relativeSearchPath.IsEmpty)
+                return new CompletionSearchInfo(unitySolutionPath, CompletionSearchInfo.PassType.ProjectRoot,
+                    textLookupRanges);
+
+            var factory = solution.TryGetComponent<UnityExternalFilesModuleFactory>();
+            if (factory == null)
+                return CompletionSearchInfo.InvalidData;
+
+            var unityExternalFilesPsiModule = factory.PsiModule;
+
+            //Search path like: "Assets/Foo/.."
+            var firstDirectory = relativeSearchPath.Components.FirstOrEmpty;
+            if (firstDirectory.EqualTo(ProjectExtensions.AssetsFolder))
+            {
+                var searchPath = unitySolutionPath.Combine(relativeSearchPath);
+                //clear wrong not existing part of the path
+                while (!unityExternalFilesPsiModule.ContainsPath(searchPath) && !searchPath.Equals(unitySolutionPath))
+                    searchPath = searchPath.Parent;
+
+                return new CompletionSearchInfo(searchPath, CompletionSearchInfo.PassType.AssetsFolder,
+                    textLookupRanges);
+            }
+
+            if (firstDirectory.EqualTo(ProjectExtensions.PackagesFolder))
+            {
+                var packagesFolderPath = // F:/someFolders/MyProject/Packages
+                    unitySolutionPath.Combine(ProjectExtensions.PackagesFolder);
+
+                var packageData = TryGetPackageDataBySearchPath(relativeSearchPath, solution);
+                //not valid package - returns packages folder
+                if (packageData is null || packageData.PackageFolder == null)
+                    return new CompletionSearchInfo(packagesFolderPath,
+                        CompletionSearchInfo.PassType.PackagesRootFolder, textLookupRanges);
+
+                //extracting path relative to package folder -> FolderA
+                var innerPackagePath = relativeSearchPath.RemoveFirstComponent().RemoveFirstComponent();
+
+                // absolutePathCompletionFolder would be  F:/someFolders/MyProject/Library/PackagesCache/com.companyname.packName/FolderA/
+                // or another local directory, or even local Packages folder
+                var absolutePathCompletionFolder = packageData.PackageFolder.Combine(innerPackagePath);
+
+                return new CompletionSearchInfo(absolutePathCompletionFolder,
+                    CompletionSearchInfo.PassType.InternalPackageFolder, textLookupRanges);
+            }
+
+            return new CompletionSearchInfo(unitySolutionPath, CompletionSearchInfo.PassType.ProjectRoot,
+                textLookupRanges);
+        }
+
+        private static RelativePath CalculateSearchPath(CSharpCodeCompletionContext context,
+            ICSharpLiteralExpression stringLiteral, ITreeNode nodeInFile, out TextLookupRanges textLookupRanges)
+        {
+            var originalInput = stringLiteral.ConstantValue.AsString() ?? string.Empty;
+            originalInput = originalInput.RemoveQuotes() ?? string.Empty;
+
+
+            var treeEndOffset = nodeInFile.GetTreeEndOffset();
+
+            var firstLetterOffset = nodeInFile.GetTreeStartOffset() + 1; //first symbol is '"' - moving to the next one
+            var symbolAfterCaretOffset = context.BasicContext.CaretTreeOffset;
+            var symbolBeforeCaretOffset = symbolAfterCaretOffset - 1;
+
+            var lastSymbolIndexBeforeCaret = symbolBeforeCaretOffset - firstLetterOffset;
+            var lastSlashIndex = originalInput.LastIndexOf('/', lastSymbolIndexBeforeCaret);
+
+            var substringLength = lastSlashIndex >= 0 ? lastSlashIndex + 1 : 0;
+
+            var rootPath = originalInput[..substringLength];
+            var relativeSearchPath = RelativePath.Parse(rootPath);
+
+            var lastSlashOffset = firstLetterOffset + lastSlashIndex;
+
+            var contextCompletionRanges = context.CompletionRanges;
+            var originalInsertRange = contextCompletionRanges.InsertRange;
+
+            var insertTextRange = new TextRange(lastSlashOffset.Offset + 1, treeEndOffset.Offset);
+            var insertRange = new DocumentRange(originalInsertRange.Document, insertTextRange);
+
+            textLookupRanges = new TextLookupRanges(insertRange, insertRange, contextCompletionRanges.CustomPrefixRule);
+            return relativeSearchPath;
+        }
+
+        private static PackageData? TryGetPackageDataBySearchPath(RelativePath searchPath, ISolution solution)
+        {
+            var packageId = ExtractPackageIdFromSearchPath(searchPath);
+            if (string.IsNullOrEmpty(packageId))
+                return null;
+
+            var packageManager = solution.GetComponent<PackageManager>();
+            var packageData = packageManager.GetPackageById(packageId);
+            return packageData;
+        }
+
+
+        private static string ExtractPackageIdFromSearchPath(RelativePath searchPath)
+        {
+            //searchPath = "Packages/com.companyname.packName/FolderA/
+            return searchPath.RemoveFirstComponent().Components.FirstOrEmpty.ToString();
+        }
+
+
+        private static void AddPredefinedDirectories(IItemsCollector collector,
+            TextLookupRanges contextCompletionRanges)
+        {
+            collector.Add(new ResourcePathItem(ProjectExtensions.AssetsFolder, true,
+                contextCompletionRanges));
+            collector.Add(new ResourcePathItem(ProjectExtensions.PackagesFolder, true,
+                contextCompletionRanges));
+        }
+
+        private static void AddResourcesFromPackage(CSharpCodeCompletionContext context, IItemsCollector collector,
+            CompletionSearchInfo completionSearchInfo)
+        {
+            var solution = context.NodeInFile.GetSolution();
+            var packageManager = solution.GetComponent<PackageManager>();
+            // var packageFolderPath = solutionFolder.Combine(ProjectExtensions.PackagesRootFolder);
+            //
+            // var searchPath = completionSearchInfo.AbsolutePathCompletionFolder;
+            // if (searchPath == null)
+            //     return;
+
+            if ( /*searchPath.Equals(packageFolderPath)*/completionSearchInfo.SearchPathType ==
+                                                         CompletionSearchInfo.PassType.PackagesRootFolder)
+            {
+                AddPackagesLookupItems(packageManager, collector, completionSearchInfo.Ranges);
+                return;
+            }
+
+            AddResourcesFromAssetsFolder(context, collector, completionSearchInfo);
+        }
+
+        private static void AddPackagesLookupItems(PackageManager packageManager,
+            IItemsCollector collector, TextLookupRanges contextCompletionRanges)
+        {
+            foreach (var (key, _) in packageManager.Packages)
+            {
+                var resourcePathItem =
+                    new ResourcePathItem(key, true, contextCompletionRanges);
+                collector.Add(resourcePathItem);
+            }
+        }
+
+        private static void AddResourcesFromAssetsFolder(CSharpCodeCompletionContext context, IItemsCollector collector,
+            CompletionSearchInfo completionSearchInfo)
+        {
+            var factory = context.NodeInFile.GetSolution().TryGetComponent<UnityExternalFilesModuleFactory>();
+            if (factory == null)
+                return;
+
+            var absolutePathCompletionFolder = completionSearchInfo.AbsolutePathCompletionFolder;
+            if (absolutePathCompletionFolder == null)
+                return;
+
+            var unityExternalFilesPsiModule = factory.PsiModule;
+
+
+            var virtualFileSystemPaths = absolutePathCompletionFolder.GetDirectoryEntries();
+
+            var children = unityExternalFilesPsiModule.ContainsPath(absolutePathCompletionFolder)
+                ? unityExternalFilesPsiModule.GetChildFilesFolder(absolutePathCompletionFolder)
+                    .Select(sf => sf.GetLocation())
+                : virtualFileSystemPaths.Select(dirData => dirData.GetAbsolutePath());
+
+
+            foreach (var sourceFilePath in children)
+            {
+                if (sourceFilePath == null)
+                    continue;
+
+                if (!sourceFilePath.IsMeta())
+                    continue;
+
+                var nameWithoutExtension = sourceFilePath.NameWithoutExtension;
+
+                try
+                {
+                    var childAbsolutePath = sourceFilePath.Parent.Combine(nameWithoutExtension);
+                    var resourcePathItem = new ResourcePathItem(nameWithoutExtension, childAbsolutePath.ExistsDirectory,
+                        completionSearchInfo.Ranges);
+                    collector.Add(resourcePathItem);
+                }
+                catch (Exception e)
+                {
+                    Logger.LogException(e);
+                }
+            }
+        }
+
+        private readonly struct CompletionSearchInfo
+        {
+            public enum PassType
+            {
+                Unknown,
+                ProjectRoot,
+                AssetsFolder,
+                PackagesRootFolder,
+                InternalPackageFolder,
+            }
+
+            public static readonly CompletionSearchInfo InvalidData = new(null, PassType.Unknown,
+                new TextLookupRanges(DocumentRange.InvalidRange, DocumentRange.InvalidRange));
+
+            public readonly TextLookupRanges Ranges;
+            public readonly VirtualFileSystemPath? AbsolutePathCompletionFolder;
+            public readonly PassType SearchPathType;
+
+            public CompletionSearchInfo(VirtualFileSystemPath? absolutePathCompletionFolder, PassType passType,
+                TextLookupRanges ranges)
+            {
+                SearchPathType = passType;
+                AbsolutePathCompletionFolder = absolutePathCompletionFolder;
+                Ranges = ranges;
+            }
+
+            public override string ToString()
+            {
+                return
+                    $"{nameof(AbsolutePathCompletionFolder)}: {AbsolutePathCompletionFolder}, {nameof(Ranges)}: {Ranges}, {nameof(SearchPathType)}: {SearchPathType}";
+            }
+        }
+
+        private sealed class ResourcePathItem : TextLookupItemBase
+        {
+            private readonly string myCompletionItemName;
+
+            public ResourcePathItem(string completionItemName, bool isDirectory, TextLookupRanges ranges)
+            {
+                myCompletionItemName = completionItemName;
+                Ranges = ranges;
+                Text = $"{completionItemName}{(isDirectory ? '/' : "")}\"";
+                Image = isDirectory
+                    ? ProjectModelThemedIcons.Directory.Id
+                    : UnityFileTypeThemedIcons.FileUnity.Id;
+            }
+
+            protected override RichText GetDisplayName()
+            {
+                if (myCompletionItemName.Contains("/"))
+                    System.Diagnostics.Debugger.Launch();
+
+                return LookupUtil.FormatLookupString(myCompletionItemName, TextColor);
+            }
+
+            public override IconId Image { get; }
+
+            protected override void OnAfterComplete(ITextControl textControl, ref DocumentRange nameRange,
+                ref DocumentRange decorationRange,
+                TailType tailType, ref Suffix suffix, ref IRangeMarker caretPositionRangeMarker)
+            {
+                base.OnAfterComplete(textControl, ref nameRange, ref decorationRange, tailType, ref suffix,
+                    ref caretPositionRangeMarker);
+                // Consistently move caret to end of path; i.e., end of the string literal, before closing quote
+                textControl.Caret.MoveTo(Ranges!.ReplaceRange.StartOffset + Text.Length - 1,
+                    CaretVisualPlacement.DontScrollIfVisible);
+            }
+
+            public override void Accept(ITextControl textControl, DocumentRange nameRange,
+                LookupItemInsertType insertType,
+                Suffix suffix, ISolution solution, bool keepCaretStill)
+            {
+                // Force replace + keep caret still in order to place caret at consistent position (see override of OnAfterComplete)
+                base.Accept(textControl, nameRange, LookupItemInsertType.Replace, suffix, solution, true);
+            }
+        }
+    }
+}
