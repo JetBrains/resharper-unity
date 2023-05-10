a186a9a0a929d2be9cdb13e4ed6dbbb229580c14
Alexander Bondarev <alexander.bondarev@jetbrains.com>
2023-05-10 14:49:25 +0300

    Add frontend test for .shader breadcrumbs.

---
+++ rider/src/test/kotlin/documentModel/BreadcrumbsTest.kt
@@ -0,0 +1,19 @@
+import com.jetbrains.rider.test.base.BaseTestWithSolution
+import com.jetbrains.rider.test.scriptingApi.checkCrumbs
+import com.jetbrains.rider.test.scriptingApi.setCaretToPosition
+import com.jetbrains.rider.test.scriptingApi.withOpenedEditor
+import org.testng.annotations.Test
+
+@Test
+class BreadcrumbsTest : BaseTestWithSolution() {
+    override fun getSolutionDirectoryName() = "SimpleUnityProjectWithShaders"
+
+    @Test
+    fun simpleCheck() {
+        withOpenedEditor(project, "Assets/Shaders/MyShader.shader") {
+            setCaretToPosition(12, 20)
+
+            checkCrumbs("Shader", "SubShader", "Pass", "CGPROGRAM", "hsv2rgb")
+        }
+    }
+}
