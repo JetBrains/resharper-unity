92a94776ddae4a8768358301197db249cbcc3c86
Vladimir.Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2023-05-24 20:50:10 +0200

    [llm] add information about Unity project

---
+++ resharper/resharper-unity/src/Unity/Core/Feature/Services/Context/UnitySolutionInformation.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Collections.Viewable;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Feature.Services.AI.Unity;
+using JetBrains.ReSharper.Plugins.Unity.Core.ProjectModel;
+using JetBrains.ReSharper.Plugins.Unity.UnityEditorIntegration;
+using JetBrains.ReSharper.Plugins.Unity.UnityEditorIntegration.Packages;
+using JetBrains.ReSharper.Plugins.Unity.Utils;
+
+namespace JetBrains.ReSharper.Plugins.Unity.Core.Feature.Services.Context
+{
+    [SolutionComponent]
+    public class UnitySolutionInformation: IUnitySolutionInformation
+    {
+        private readonly UnitySolutionTracker mySolutionTracker;
+        private readonly ISolution mySolution;
+        private readonly PackageManager myPackageManager;
+
+        public UnitySolutionInformation(UnitySolutionTracker solutionTracker, ISolution solution, PackageManager packageManager)
+        {
+            mySolutionTracker = solutionTracker;
+            mySolution = solution;
+            myPackageManager = packageManager;
+        }
+        
+        public bool IsUnitySolution()
+        {
+            return mySolutionTracker.IsUnityProjectFolder.HasTrueValue();
+        }
+
+        public string GetUnityVersion()
+        {
+            var (verifiedVersion, isCustom) = UnityVersionUtils.GetUnityVersion(UnityVersion.GetProjectSettingsUnityVersion(mySolution.SolutionDirectory));
+            return verifiedVersion;
+        }
+
+        public IEnumerable<string> GetPackages()
+        {
+            return myPackageManager.Packages.Select(t => t.Value.Id);
+        }
+    }
+}
