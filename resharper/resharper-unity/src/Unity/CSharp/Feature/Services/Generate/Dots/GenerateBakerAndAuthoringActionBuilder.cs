e80ead8459fa1cd7b783c58e1a1d9ac1f9222c94
george.lemeshko <heorhii.lemeshko@jetbrains.com>
2023-06-15 16:46:41 +0200

    code cleanup

--- resharper/resharper-unity/src/Unity/CSharp/Feature/Services/Generate/Dots/GenerateBakerAndAuthoringActionBuilder.cs
+++ resharper/resharper-unity/src/Unity/CSharp/Feature/Services/Generate/Dots/GenerateBakerAndAuthoringActionBuilder.cs
@@ -72,17 +72,17 @@ namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Feature.Services.Generate.Dot
                 availableBakers.Add(name);
                 myExistedBakers[name] = typeElement;
             }
-
+         
+            var selector = new GeneratorOptionSelector(SelectedBaker, Strings.UnityDots_GenerateBakerAndAuthoring_Baker, availableBakers.ToIReadOnlyList())
+                { Value = Strings.UnityDots_GenerateBakerAndAuthoring_NewBaker_As_Nested };
+            
+            options.Add(selector);
             var componentTypeName = context.ClassDeclaration.DeclaredName;
             var overrideComponentInitialization = new GeneratorOptionBoolean(OverrideComponentInBaker,
                 string.Format(Strings.UnityDots_GenerateBakerAndAuthoring_Override_Component_In_Baker,
                     componentTypeName), false);
 
             options.Add(overrideComponentInitialization);
-            var selector = new GeneratorOptionSelector(SelectedBaker, Strings.UnityDots_GenerateBakerAndAuthoring_Baker, availableBakers.ToIReadOnlyList())
-                { Value = Strings.UnityDots_GenerateBakerAndAuthoring_NewBaker_As_Nested };
-            
-            options.Add(selector);
         }
 
         // Enables/disables the menu item
