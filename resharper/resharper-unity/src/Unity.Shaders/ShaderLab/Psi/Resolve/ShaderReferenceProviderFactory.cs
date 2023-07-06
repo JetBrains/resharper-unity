378beadd05eed6c51487ba4a56986f841583dc00
Alexander Bondarev <alexander.bondarev@jetbrains.com>
2023-07-06 14:48:46 +0300

    ShaderLab: Support shader references in Shader.Find.
    (cherry picked from commit b766aceb1936082412ee0a9dcc384a78541d14f8)

---
+++ resharper/resharper-unity/src/Unity.Shaders/ShaderLab/Psi/Resolve/ShaderReferenceProviderFactory.cs
@@ -0,0 +1,43 @@
+﻿#nullable enable
+
+using JetBrains.Collections.Viewable;
+using JetBrains.Lifetimes;
+using JetBrains.ReSharper.Plugins.Unity.Core.ProjectModel;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.Caches;
+using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.ReSharper.Psi.Resolve;
+using JetBrains.ReSharper.Psi.Tree;
+
+namespace JetBrains.ReSharper.Plugins.Unity.Shaders.ShaderLab.Psi.Resolve
+{
+    // Creates references from the "references" array to the "name" declaration
+    [ReferenceProviderFactory]
+    public class ShaderReferenceProviderFactory : IReferenceProviderFactory
+    {
+        private readonly UnitySolutionTracker myUnitySolutionTracker;
+
+        public ShaderReferenceProviderFactory(Lifetime lifetime, UnitySolutionTracker unitySolutionTracker)
+        {
+            myUnitySolutionTracker = unitySolutionTracker;
+
+            Changed = new DataFlow.Signal<IReferenceProviderFactory>(GetType().FullName!);
+        }
+
+        public IReferenceFactory? CreateFactory(IPsiSourceFile sourceFile, IFile file, IWordIndex? wordIndexForChecks)
+        {
+            // Make sure the source file is still valid. It's not clear why it would be invalid, but we get it
+            // See DEXP-672670
+            if (!sourceFile.IsValid())
+                return null;
+
+            // The source file might be in the external files module, so we can't look at what project it belongs to
+            if (!sourceFile.GetSolution().HasUnityReference() && !myUnitySolutionTracker.IsUnityProject.HasTrueValue())
+                return null;
+
+            return sourceFile.PrimaryPsiLanguage.Is<CSharpLanguage>() ? new ShaderReferenceFactory() : null;
+        }
+
+        public DataFlow.ISignal<IReferenceProviderFactory> Changed { get; }
+    }
+}
---
+++ resharper/resharper-unity/src/Unity/CSharp/Psi/Resolve/WellKnownUnityNamespaces.cs
@@ -0,0 +1,8 @@
+#nullable enable
+namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Psi.Resolve
+{
+    public static class WellKnownUnityNamespaces
+    {
+        public const string UnityEngine = "UnityEngine";
+    }
+}
