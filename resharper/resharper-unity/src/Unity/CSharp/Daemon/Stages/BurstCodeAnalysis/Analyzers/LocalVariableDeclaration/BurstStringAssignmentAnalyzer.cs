f44aa6b6139abbb42789f9e255a490b13681d497
george.lemeshko <heorhii.lemeshko@jetbrains.com>
2023-07-07 12:24:37 +0200

    Add quickfix for string local variables to specify explicitly FixedString type

---
+++ resharper/resharper-unity/src/Unity/CSharp/Daemon/Stages/BurstCodeAnalysis/Analyzers/LocalVariableDeclaration/BurstStringAssignmentAnalyzer.cs
@@ -0,0 +1,29 @@
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Feature.Services.Daemon;
+using JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Errors;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+
+namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Stages.BurstCodeAnalysis.Analyzers.LocalVariableDeclaration
+{
+    [SolutionComponent]
+    public class BurstStringAssignmentAnalyzer : IBurstProblemSubAnalyzer<IMultipleLocalVariableDeclaration>
+    {
+        public BurstProblemSubAnalyzerStatus CheckAndAnalyze(IMultipleLocalVariableDeclaration t, IHighlightingConsumer consumer)
+        {
+            var multipleDeclarationMembers = t.Declarators;
+            foreach (var multipleDeclarationMember in multipleDeclarationMembers)
+            {
+                if (multipleDeclarationMember is ILocalVariableDeclaration { IsVar: true,  } localVariableDeclaration
+                    && localVariableDeclaration.Type.IsString())
+                {
+                    consumer?.AddHighlighting(new BurstLocalStringVariableDeclarationWarning(t.VarKeyword, multipleDeclarationMember));
+                }
+            }
+
+            return BurstProblemSubAnalyzerStatus.NO_WARNING_CONTINUE;
+        }
+
+        public int Priority => 1000;
+    }
+}
