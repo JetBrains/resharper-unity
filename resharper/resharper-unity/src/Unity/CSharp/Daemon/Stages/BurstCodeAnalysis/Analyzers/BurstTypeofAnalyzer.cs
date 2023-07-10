b2563050e906dcd5a8c69064cddf9880ebefdebd
george.lemeshko <heorhii.lemeshko@jetbrains.com>
2023-07-10 15:05:55 +0200

    Fix RIDER-95554 - fix tests

--- resharper/resharper-unity/src/Unity/CSharp/Daemon/Stages/BurstCodeAnalysis/Analyzers/BurstTypeofAnalyzer.cs
+++ resharper/resharper-unity/src/Unity/CSharp/Daemon/Stages/BurstCodeAnalysis/Analyzers/BurstTypeofAnalyzer.cs
@@ -2,7 +2,6 @@ using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Feature.Services.Daemon;
 using JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Errors;
 using JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Stages.ContextSystem;
-using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.CSharp.Tree;
 
 namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Stages.BurstCodeAnalysis.Analyzers
@@ -14,26 +13,6 @@ namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Stages.BurstCodeAnalys
             IReadOnlyCallGraphContext context)
         {
             consumer?.AddHighlighting(new BurstTypeofExpressionWarning(typeofExpression));
-            return true;
-        }
-    }    
-    
-    [SolutionComponent]
-    public sealed class BurstObjectElementAccessAnalyzer : BurstProblemAnalyzerBase<IElementAccessExpression>, IBurstBannedAnalyzer
-    {
-        protected override bool CheckAndAnalyze(IElementAccessExpression elementAccessExpression, IHighlightingConsumer consumer,
-            IReadOnlyCallGraphContext context)
-        {
-            var elementAccessExpressionReference = elementAccessExpression.ElementAccessReference;
-
-            if (elementAccessExpressionReference.Resolve().DeclaredElement is not IProperty invokedAccessor)
-                return false;
-
-            if(invokedAccessor.ContainingType is not IClass accessorContainingType)
-                return false;
-            
-            consumer?.AddHighlighting(new BurstAccessingManagedIndexerWarning(elementAccessExpression,
-                 accessorContainingType.GetClrName().FullName));
             return true;
         }
     }
