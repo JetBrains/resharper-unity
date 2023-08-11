a95c0d035d604765c481b4cb012f2bd8c682919b
Eugene Strizhok <eugene.strizhok@gmail.com>
2023-08-11 19:27:11 +0200

    Revert "Fixed RSRP-492333 Issues with unit testing"
    This reverts commit dc19100bdadf7fb9551a6cc40096b7041e5c3f37.

--- resharper/resharper-unity/src/Unity.Rider/Integration/Core/Feature/UnitTesting/RunViaUnityEditorStrategy.cs
+++ resharper/resharper-unity/src/Unity.Rider/Integration/Core/Feature/UnitTesting/RunViaUnityEditorStrategy.cs
@@ -392,11 +392,11 @@ namespace JetBrains.ReSharper.Plugins.Unity.Rider.Integration.Core.Feature.UnitT
                         // part of parent id, which has the form {namespace}.{class}.{method}
                         var shortName = result.TestId.SubstringAfter(result.ParentId.SubstringBeforeLast(".") + ".");
                         
-                        run.CreateDynamicElement(() => new NUnitRowTestElement(shortName, elementParent));
+                        run.CreateDynamicElement(() => new NUnitRowTestElement(result.TestId, shortName, elementParent));
                     }
                     else if (parent is NUnitTestFixtureElement fixtureParent)
                     {
-                        run.CreateDynamicElement(() => new NUnitTestElement(fixtureParent,
+                        run.CreateDynamicElement(() => new NUnitTestElement(result.TestId, fixtureParent,
                             result.TestId.SubstringAfter($"{result.ParentId}."), null));
                     }
                 }
