d0aca8ecde6c89a79b37f4e7c773f476269a7912
Konstantin Krukhmalev <konstantin.krukhmalev@jetbrains.com>
2023-09-04 23:43:29 +0300

    Run Unity plugin tests in test episodes

---
+++ rider/src/test/kotlin/com/jetbrains/rider/unity/test/cases/FindUguiUsagesPrefabModificationsTest.kt
@@ -0,0 +1,66 @@
+package com.jetbrains.rider.unity.test.cases
+import com.jetbrains.rider.unity.test.framework.base.FindUsagesAssetTestBase
+import com.jetbrains.rider.test.annotations.TestEnvironment
+import com.jetbrains.rider.test.enums.PlatformType
+import com.jetbrains.rider.test.env.enums.SdkVersion
+import org.testng.annotations.Test
+
+@TestEnvironment(platform = [PlatformType.ALL], sdkVersion = SdkVersion.DOT_NET_6)
+class FindUguiUsagesPrefabModificationsTest : FindUsagesAssetTestBase() {
+
+    override val traceCategories: List<String>
+        get() = super.traceCategories + "JetBrains.ReSharper.Plugins.Unity.Yaml.Psi.DeferredCaches.UnityEvents"
+
+    override fun getSolutionDirectoryName(): String {
+        return "UI_PrefabModifications"
+    }
+
+    @Test
+    fun test01() {
+        disableAllGroups()
+        doTest(5, 20, "PlayerInput.cs")
+    }
+
+    @Test
+    fun test02() {
+        disableAllGroups()
+        doTest(10, 20, "PlayerInput.cs")
+    }
+
+    @Test
+    fun test03() {
+        disableAllGroups()
+        doTest(6, 20, "EventTrigger/even.cs")
+    }
+
+    @Test
+    fun test031() {
+        disableAllGroups()
+        doTest(11, 20, "EventTrigger/even.cs")
+    }
+
+    @Test
+    fun test032() {
+        disableAllGroups()
+        doTest(16, 20, "EventTrigger/even.cs")
+    }
+
+    @Test
+    fun test033() {
+        disableAllGroups()
+        doTest(21, 30, "EventTrigger/even.cs")
+    }
+
+    @Test
+    fun test04() {
+        disableAllGroups()
+        doTest(6, 20, "EventTrigger/NewBehaviourScript.cs")
+        //doTest(11, 20, "EventTrigger/NewBehaviourScript.cs")
+    }
+
+    @Test
+    fun test041() {
+        disableAllGroups()
+        doTest(11, 25, "EventTrigger/NewBehaviourScript.cs")
+    }
+}
---
+++ rider/src/test/kotlin/com/jetbrains/rider/unity/test/framework/base/DebuggerTestBase.kt
@@ -0,0 +1,112 @@
+package com.jetbrains.rider.unity.test.framework.base
+
+import base.integrationTests.*
+import com.intellij.xdebugger.XDebuggerManager
+import com.intellij.xdebugger.breakpoints.XBreakpointProperties
+import com.intellij.xdebugger.breakpoints.XLineBreakpoint
+import com.jetbrains.rider.debugger.breakpoint.DotNetLineBreakpointProperties
+import com.jetbrains.rider.plugins.unity.debugger.breakpoints.UnityPausepointBreakpointType
+import com.jetbrains.rider.plugins.unity.debugger.breakpoints.convertToLineBreakpoint
+import com.jetbrains.rider.test.annotations.TestEnvironment
+import com.jetbrains.rider.test.enums.PlatformType
+import com.jetbrains.rider.test.framework.combine
+import com.jetbrains.rider.test.scriptingApi.*
+import com.jetbrains.rider.unity.test.framework.api.*
+import com.jetbrains.rider.unity.test.framework.integrationTests.*
+import org.testng.annotations.AfterMethod
+import org.testng.annotations.Test
+import java.io.File
+
+@TestEnvironment(platform = [PlatformType.WINDOWS_ALL, PlatformType.MAC_OS_ALL])
+abstract class DebuggerTestBase : IntegrationTestWithGeneratedSolutionBase() {
+    override fun getSolutionDirectoryName() = "SimpleUnityProjectWithoutPlugin"
+
+    override fun preprocessTempDirectory(tempDir: File) {
+        super.preprocessTempDirectory(tempDir)
+
+        val newBehaviourScript = "NewBehaviourScript.cs"
+        val sourceScript = testCaseSourceDirectory.resolve(newBehaviourScript)
+        if (sourceScript.exists()) {
+            sourceScript.copyTo(tempDir.resolve("Assets").resolve(newBehaviourScript), true)
+        }
+    }
+
+    @Test(enabled = false)
+    fun checkUnityPausePoint() {
+        val pauseFile = activeSolutionDirectory.combine("Assets", ".pause").absoluteFile
+        attachDebuggerToUnityEditorAndPlay(
+            {
+                toggleUnityPausepoint(project, "NewBehaviourScript.cs", 9, "System.IO.File.Exists(\"${pauseFile.path}\")")
+            },
+            {
+                waitForUnityEditorPlayMode()
+                pauseFile.createNewFile()
+                waitForUnityEditorPauseMode()
+                removeAllUnityPausepoints()
+                unpause()
+            })
+    }
+
+    @Test
+    fun checkBreakpoint() {
+        attachDebuggerToUnityEditorAndPlay(
+            {
+                toggleBreakpoint("NewBehaviourScript.cs", 9)
+            },
+            {
+                waitForPause()
+                dumpFullCurrentData()
+                resumeSession()
+            }, testGoldFile)
+    }
+
+    @Test(description = "RIDER-24651")
+    fun checkExceptionBreakpointWithJustMyCode() {
+        attachDebuggerToUnityEditorAndPlay(
+            {
+                toggleExceptionBreakpoint("System.Exception").justMyCode = true
+            },
+            {
+                waitForPause()
+                dumpFullCurrentData()
+                resumeSession()
+            }, testGoldFile)
+    }
+
+    @Test(description = "RIDER-23087", enabled = false)
+    fun checkEvaluationAfterRestartGame() {
+        var breakpoint: XLineBreakpoint<out XBreakpointProperties<*>>? = null
+        attachDebuggerToUnityEditorAndPlay(
+            {
+                breakpoint = toggleBreakpoint(project, "NewBehaviourScript.cs", 9)
+            },
+            {
+                val toEvaluate = "binaryNotation / 25"
+
+                waitForPause()
+                printlnIndented("$toEvaluate = ${evaluateExpression(toEvaluate).result}")
+                dumpFullCurrentData()
+
+                breakpoint?.isEnabled = false
+                resumeSession()
+                waitForUnityEditorPlayMode()
+                restart()
+                breakpoint?.isEnabled = true
+
+                waitForPause()
+                printlnIndented("$toEvaluate = ${evaluateExpression(toEvaluate).result}")
+                dumpFullCurrentData()
+            }, testGoldFile)
+    }
+
+    @AfterMethod(alwaysRun = true)
+    fun removeAllUnityPausepoints() {
+        XDebuggerManager.getInstance(project).breakpointManager.allBreakpoints.filter {
+            it.type is UnityPausepointBreakpointType
+        }.forEach {
+            @Suppress("UNCHECKED_CAST")
+            convertToLineBreakpoint(project, it as XLineBreakpoint<DotNetLineBreakpointProperties>)
+        }
+        removeAllBreakpoints()
+    }
+}
