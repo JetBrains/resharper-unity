3a15cf145d96aa30be125c4d7d03d21d78cf434b
Konstantin Krukhmalev <konstantin.krukhmalev@jetbrains.com>
2023-09-07 18:28:31 +0300

    Restore deleted classes

--- rider/src/test/kotlin/com/jetbrains/rider/unity/test/cases/integrationTests/DebuggerTestNewMono.kt
+++ rider/src/test/kotlin/com/jetbrains/rider/unity/test/cases/integrationTests/DebuggerTestNewMono.kt
@@ -2,6 +2,4 @@ package com.jetbrains.rider.unity.test.cases.integrationTests
 
 import com.jetbrains.rider.unity.test.framework.base.DebuggerTestBase
 
-class DebuggerTestNewMono: DebuggerTestBase() {
-
-}
+class DebuggerTestNewMono: DebuggerTestBase()
