d56b241c1d098eae6bcc9a3fdb26978786189588
Vladimir.Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2023-09-21 21:05:07 +0200

    fix external annotations in local dev environment for Unity plugin

--- resharper/resharper-unity/src/Unity/BuildScript/CopyUnityAnnotations.cs
+++ resharper/resharper-unity/src/Unity/BuildScript/CopyUnityAnnotations.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Application.BuildScript;
 using JetBrains.Application.BuildScript.Compile;
 using JetBrains.Application.BuildScript.Solution;
 using JetBrains.Build;
+using JetBrains.Build.Helpers.TeamCity;
 using JetBrains.Util;
 using JetBrains.Util.Storage;
 
@@ -17,13 +19,29 @@ namespace JetBrains.ReSharper.Plugins.Unity.BuildScript
       if (allass.FindSubplatformByClass<CopyUnityAnnotations>() is SubplatformOnSources subplatform)
       {
         FileSystemPath dirAnnotations = homedir.ProductHomeDir / subplatform.Name.RelativePath / "annotations";
-        return dirAnnotations.GetChildFiles().Select(CopyFileToOutputRequest).ToArray();
+        return dirAnnotations.GetChildFiles().SelectMany(CopyFileToOutputRequest).ToArray();
 
-        SubplatformFileForPackaging CopyFileToOutputRequest(FileSystemPath path)
+        IEnumerable<SubplatformFileForPackaging> CopyFileToOutputRequest(FileSystemPath path)
         {
-          return new SubplatformFileForPackaging(
+          yield return new SubplatformFileForPackaging(
             subplatform.Name,
             ImmutableFileItem.CreateFromDisk(path).WithRelativePath((RelativePath)"Extensions" / "com.intellij.resharper.unity" / "annotations" / path.Name));
+
+          // see ExtensionsExternalAnnotationsFileProvider
+          // That HACK required only for local case. We should fix how we mount Plugin in local dev envrinment.
+          
+          // When we are running Rider from resharper-unity github repo (via SDK) or when we are running Rider from installer
+          // plugin is not installed as Subplatform, it is mounted as IntelliJ plugin (see JET_ADDITIONAL_DEPLOYED_PACKAGES_FILE usage) with com.intellij.resharper.unity
+          // In local dev environment we are extracting backend part of Unity plugin to Bin.Directory and it is mounted as JetBrains.Plugins.ReSharperUnity.resharper.resharper-unity.src.Unity
+          // in ApplicationPackagesFiles 
+          if (!TeamCityProperties.GetIsRunningInTeamCity())
+          {
+              yield return new SubplatformFileForPackaging(
+                  subplatform.Name,
+                  ImmutableFileItem.CreateFromDisk(path).WithRelativePath((RelativePath)"Extensions" /
+                                                                          "JetBrains.Plugins.ReSharperUnity.resharper.resharper-unity.src.Unity" /
+                                                                          "annotations" / path.Name));
+          }
         }
       }
 
