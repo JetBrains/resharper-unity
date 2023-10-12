3b1ef32667f52134824e39c6769430f5df186919
Alexander Bondarev <alexander.bondarev@jetbrains.com>
2023-10-12 11:53:46 +0300

    [unity] Refactor ShaderContextCache invalidation with ICppChangeProvider.

--- resharper/resharper-unity/src/Unity.Rider/Integration/Shaders/HlslSupport/ShaderContexts/ShaderContextCache.cs
+++ resharper/resharper-unity/src/Unity.Rider/Integration/Shaders/HlslSupport/ShaderContexts/ShaderContextCache.cs
@@ -1,34 +1,46 @@
 #nullable enable
 
-using JetBrains.Application.I18n;
+using System.Collections.Generic;
+using JetBrains.Application.changes;
+using JetBrains.Application.Progress;
+using JetBrains.Application.Threading;
 using JetBrains.DocumentManagers;
 using JetBrains.DocumentModel;
+using JetBrains.Lifetimes;
 using JetBrains.ProjectModel;
-using JetBrains.ReSharper.Feature.Services.Daemon;
+using JetBrains.ReSharper.Feature.Services.Cpp.Caches;
 using JetBrains.ReSharper.Plugins.Unity.Shaders.HlslSupport.Integration.Injections;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.Cpp.Caches;
 using JetBrains.ReSharper.Resources.Shell;
+using JetBrains.Threading;
 using JetBrains.Util;
 using JetBrains.Util.Caches;
+using JetBrains.Util.DataStructures.Collections;
 
 namespace JetBrains.ReSharper.Plugins.Unity.Rider.Integration.Shaders.HlslSupport.ShaderContexts
 {
     [SolutionComponent]
-    public class ShaderContextCache : IPreferredRootFileProvider
+    public class ShaderContextCache : IPreferredRootFileProvider, ICppChangeProvider
     {
+        private readonly Lifetime myLifetime;
         private readonly ISolution mySolution;
+        private readonly ChangeManager myChangeManager;
         private readonly InjectedHlslFileLocationTracker myLocationTracker;
         private readonly DocumentManager myManager;
         private readonly ILogger myLogger;
         private readonly DirectMappedCache<VirtualFileSystemPath, IRangeMarker> myShaderContext = new(100);
 
-        public ShaderContextCache(ISolution solution, InjectedHlslFileLocationTracker locationTracker, DocumentManager manager, ILogger logger)
+        public ShaderContextCache(Lifetime lifetime, ISolution solution, ChangeManager changeManager, InjectedHlslFileLocationTracker locationTracker, DocumentManager manager, ILogger logger)
         {
+            myLifetime = lifetime;
             mySolution = solution;
+            myChangeManager = changeManager;
             myLocationTracker = locationTracker;
             myManager = manager;
             myLogger = logger;
+            
+            myChangeManager.RegisterChangeProvider(lifetime, this);
         }
 
         public void SetContext(IPsiSourceFile psiSourceFile, IRangeMarker? range)
@@ -38,11 +50,11 @@ namespace JetBrains.ReSharper.Plugins.Unity.Rider.Integration.Shaders.HlslSuppor
             else
                 myShaderContext.RemoveFromCache(psiSourceFile.GetLocation());
 
-            var solution = psiSourceFile.GetSolution();
-            var psiFiles = solution.GetPsiServices().Files;
-
-            psiFiles.InvalidatePsiFilesCache(psiSourceFile);
-            solution.GetComponent<IDaemon>().Invalidate("ShaderContextCache.SetContext".NON_LOCALIZABLE());
+            mySolution.Locks.ExecuteOrQueueWithWriteLockWhenAvailableEx(myLifetime, $"Updating shader context for {psiSourceFile}", () =>
+            {
+                if (psiSourceFile.IsValid())
+                    myChangeManager.OnProviderChanged(this, new ShaderContextChange(psiSourceFile), SimpleTaskExecutor.Instance);
+            }).NoAwait();
         }
 
         public CppFileLocation GetPreferredRootFile(CppFileLocation currentFile)
@@ -75,6 +87,21 @@ namespace JetBrains.ReSharper.Plugins.Unity.Rider.Integration.Shaders.HlslSuppor
             }
 
             return CppFileLocation.EMPTY;
+        }
+
+        public object? Execute(IChangeMap changeMap) => null;
+        
+        private class ShaderContextChange : ICppChange
+        {
+            private readonly IPsiSourceFile mySourceFile;
+
+            public ShaderContextChange(IPsiSourceFile sourceFile) => mySourceFile = sourceFile;
+
+            public bool InvalidateProperties => false;
+            public IReadOnlyCollection<(IProject Project, CppProjectChangeFlags ChangeFlags)> GetProjectChanges(ISolution solution) => EmptyList<(IProject Project, CppProjectChangeFlags ChangeFlags)>.Instance;
+
+            public IReadOnlyCollection<VirtualFileSystemPath> ChangedFiles => EmptyList<VirtualFileSystemPath>.Instance;
+            public IReadOnlyCollection<(IPsiSourceFile SourceFile, CppFileLocation Location)> ChangedLocations => FixedList.Of((mySourceFile, new CppFileLocation(mySourceFile)));
         }
     }
 }
