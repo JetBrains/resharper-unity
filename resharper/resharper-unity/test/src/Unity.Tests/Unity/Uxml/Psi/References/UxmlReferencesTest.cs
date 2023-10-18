62dc6c007e11e3751ffcb1c49c6995111449cde2
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2023-10-18 18:12:52 +0200

    improve test

--- resharper/resharper-unity/test/src/Unity.Tests/Unity/Uxml/Psi/References/UxmlReferencesTest.cs
+++ resharper/resharper-unity/test/src/Unity.Tests/Unity/Uxml/Psi/References/UxmlReferencesTest.cs
@@ -40,25 +40,4 @@ namespace JetBrains.ReSharper.Plugins.Tests.Unity.Uxml.Psi.References
             DoTestSolution(ArrayUtil.Add(mainFile.FullPath, Files.Except(mainFile).Select(a=>a.FullPath).ToArray())); 
         }
     }
-    
-    [TestUnity(UnityVersion.Unity2022_3), ReuseSolutionScope("UnityUIElementsCompletionTest")]
-    public class UxmlReferencesTest2 : ReferenceTestBase
-    {
-        protected override string RelativeTestDataPath => @"UnityUIElementsCompletionTest";
-        
-        protected override string SolutionFileName => SolutionItemsBasePath.Combine("Solutions/UIElementsDemo/UIElementsDemo.sln").FullPath;
-
-        private VirtualFileSystemPath[] Files =>
-            VirtualTestDataPath.Combine("Solutions/UIElementsDemo/")
-                .GetChildFiles("*", PathSearchFlags.RecurseIntoSubdirectories).ToArray();
-
-        protected override bool AcceptReference(IReference reference) =>
-            reference is ITypeOrNamespaceReference or IXamlNamespaceAliasReference;
-
-        [Test] public void MainMenuTemplate2()
-        {
-            var mainFile = Files.Single(a => a.Name == "MainMenuTemplate2.uxml");
-            DoTestSolution(ArrayUtil.Add(mainFile.FullPath, Files.Except(mainFile).Select(a=>a.FullPath).ToArray())); 
-        }
-    }
 }
