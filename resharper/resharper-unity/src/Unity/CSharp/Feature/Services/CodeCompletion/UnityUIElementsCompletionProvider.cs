c17c64d39c4ca8e4aef2f468e81964af34470f32
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2023-10-20 11:45:05 +0200

    restore completion for Q extensions

--- resharper/resharper-unity/src/Unity/CSharp/Feature/Services/CodeCompletion/UnityUIElementsCompletionProvider.cs
+++ resharper/resharper-unity/src/Unity/CSharp/Feature/Services/CodeCompletion/UnityUIElementsCompletionProvider.cs
@@ -1,88 +1,87 @@
-// using System.Collections.Generic;
-// using JetBrains.Annotations;
-// using JetBrains.DocumentModel;
-// using JetBrains.ProjectModel;
-// using JetBrains.ReSharper.Feature.Services.CodeCompletion;
-// using JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure.LookupItems;
-// using JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure.LookupItems.Impl;
-// using JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure.Match;
-// using JetBrains.ReSharper.Feature.Services.CSharp.CodeCompletion.Infrastructure;
-// using JetBrains.ReSharper.Feature.Services.Lookup;
-// using JetBrains.ReSharper.Features.Intellisense.CodeCompletion.CSharp.Rules;
-// using JetBrains.ReSharper.Plugins.Unity.UIElements.Uxml.Psi.Caches;
-// using JetBrains.ReSharper.Psi;
-// using JetBrains.ReSharper.Psi.CSharp;
-// using JetBrains.ReSharper.Psi.Resources;
-// using JetBrains.ReSharper.Psi.Tree;
-// using JetBrains.TextControl;
-// using JetBrains.UI.Icons;
-//
-// namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Feature.Services.CodeCompletion
-// {
-//     [Language(typeof(CSharpLanguage))]
-//     public class UnityUIElementsCompletionProvider : CSharpItemsProviderBase<CSharpCodeCompletionContext>
-//     {
-//         protected override bool IsAvailable(CSharpCodeCompletionContext context)
-//         {
-//             return context.BasicContext.CodeCompletionType == CodeCompletionType.BasicCompletion;
-//         }
-//
-//         protected override bool AddLookupItems(CSharpCodeCompletionContext context, IItemsCollector collector)
-//         {
-//             if (!UnityCompletionUtils.IsSpecificArgumentInSpecificMethod(context, out var argumentLiteral, out var typeParamName,
-//                     ExpressionReferenceUtils.IsUQueryExtensionsQueueMethod,
-//                     UnityCompletionUtils.IsCorrespondingArgument("name", 0)))
-//                 return false;
-//
-//             var uxmlCache = context.NodeInFile.GetSolution().GetComponent<UxmlCache>();
-//
-//             var ranges = context.CompletionRanges;
-//             var completionItems = typeParamName == null ? uxmlCache.GetNames() : uxmlCache.GetNamesForTypeName(typeParamName);
-//             
-//             if (argumentLiteral != null)
-//             {
-//                 var offset = argumentLiteral.GetDocumentRange().EndOffset;
-//                 ranges = ranges.WithInsertRange(ranges.InsertRange.SetEndTo(offset)).WithReplaceRange(ranges.ReplaceRange.SetEndTo(offset));
-//             }
-//             
-//             var any = false;
-//             foreach (var name in completionItems)
-//             {
-//                 any = true;
-//                 var item = new StringLiteralItem($"\"{name}\"");
-//                 item.InitializeRanges(ranges, context.BasicContext);
-//                 collector.Add(item);
-//             }
-//
-//             return any;
-//         }
-//
-//         private sealed class StringLiteralItem : TextLookupItemBase, IMLSortingAwareItem
-//         {
-//             public StringLiteralItem([NotNull] string text)
-//             {
-//                 Text = text;
-//             }
-//
-//             public override IconId Image => PsiSymbolsThemedIcons.Const.Id;
-//
-//             public override MatchingResult Match(PrefixMatcher prefixMatcher)
-//             {
-//                 var matchingResult = prefixMatcher.Match(Text);
-//                 if (matchingResult == null)
-//                     return null;
-//                 return new MatchingResult(matchingResult.MatchedIndices, matchingResult.AdjustedScore - 100,
-//                     matchingResult.OriginalScore);
-//             }
-//
-//             public override void Accept(
-//                 ITextControl textControl, DocumentRange nameRange, LookupItemInsertType insertType,
-//                 Suffix suffix, ISolution solution, bool keepCaretStill)
-//             {
-//                 base.Accept(textControl, nameRange, LookupItemInsertType.Replace, suffix, solution, keepCaretStill);
-//             }
-//
-//             public bool UseMLSort() => false;
-//         }
-//     }
-// }
+using JetBrains.Annotations;
+using JetBrains.DocumentModel;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Feature.Services.CodeCompletion;
+using JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure.LookupItems;
+using JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure.LookupItems.Impl;
+using JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure.Match;
+using JetBrains.ReSharper.Feature.Services.CSharp.CodeCompletion.Infrastructure;
+using JetBrains.ReSharper.Feature.Services.Lookup;
+using JetBrains.ReSharper.Features.Intellisense.CodeCompletion.CSharp.Rules;
+using JetBrains.ReSharper.Plugins.Unity.UIElements.Uxml.Psi.Caches;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.ReSharper.Psi.Resources;
+using JetBrains.ReSharper.Psi.Tree;
+using JetBrains.TextControl;
+using JetBrains.UI.Icons;
+
+namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Feature.Services.CodeCompletion
+{
+    [Language(typeof(CSharpLanguage))]
+    public class UnityUIElementsCompletionProvider : CSharpItemsProviderBase<CSharpCodeCompletionContext>
+    {
+        protected override bool IsAvailable(CSharpCodeCompletionContext context)
+        {
+            return context.BasicContext.CodeCompletionType == CodeCompletionType.BasicCompletion;
+        }
+
+        protected override bool AddLookupItems(CSharpCodeCompletionContext context, IItemsCollector collector)
+        {
+            if (!UnityCompletionUtils.IsSpecificArgumentInSpecificMethod(context, out var argumentLiteral, out var typeParamName,
+                    ExpressionReferenceUtils.IsUQueryExtensionsQueueMethod,
+                    UnityCompletionUtils.IsCorrespondingArgument("name", 0)))
+                return false;
+
+            var uxmlCache = context.NodeInFile.GetSolution().GetComponent<UxmlCache>();
+
+            var ranges = context.CompletionRanges;
+            var completionItems = typeParamName == null ? uxmlCache.GetNames() : uxmlCache.GetNamesForTypeName(typeParamName);
+            
+            if (argumentLiteral != null)
+            {
+                var offset = argumentLiteral.GetDocumentRange().EndOffset;
+                ranges = ranges.WithInsertRange(ranges.InsertRange.SetEndTo(offset)).WithReplaceRange(ranges.ReplaceRange.SetEndTo(offset));
+            }
+            
+            var any = false;
+            foreach (var name in completionItems)
+            {
+                any = true;
+                var item = new StringLiteralItem($"\"{name}\"");
+                item.InitializeRanges(ranges, context.BasicContext);
+                collector.Add(item);
+            }
+
+            return any;
+        }
+
+        private sealed class StringLiteralItem : TextLookupItemBase, IMLSortingAwareItem
+        {
+            public StringLiteralItem([NotNull] string text)
+            {
+                Text = text;
+            }
+
+            public override IconId Image => PsiSymbolsThemedIcons.Const.Id;
+
+            public override MatchingResult Match(PrefixMatcher prefixMatcher)
+            {
+                var matchingResult = prefixMatcher.Match(Text);
+                if (matchingResult == null)
+                    return null;
+                return new MatchingResult(matchingResult.MatchedIndices, matchingResult.AdjustedScore - 100,
+                    matchingResult.OriginalScore);
+            }
+
+            public override void Accept(
+                ITextControl textControl, DocumentRange nameRange, LookupItemInsertType insertType,
+                Suffix suffix, ISolution solution, bool keepCaretStill)
+            {
+                base.Accept(textControl, nameRange, LookupItemInsertType.Replace, suffix, solution, keepCaretStill);
+            }
+
+            public bool UseMLSort() => false;
+        }
+    }
+}
--- resharper/resharper-unity/src/Unity/UIElements/Uxml/Psi/Caches/UxmlCacheItem.cs
+++ resharper/resharper-unity/src/Unity/UIElements/Uxml/Psi/Caches/UxmlCacheItem.cs
@@ -1,48 +1,48 @@
-// using System.Collections.Generic;
-// using JetBrains.Annotations;
-// using JetBrains.Serialization;
-// using JetBrains.Util.PersistentMap;
-//
-// namespace JetBrains.ReSharper.Plugins.Unity.UIElements.Uxml.Psi.Caches
-// {
-//     public class UxmlCacheItem
-//     {
-//          private static readonly IUnsafeMarshaller<UxmlCacheItem> ourUxmlCacheItemMarshaller =
-//             new UniversalMarshaller<UxmlCacheItem>(Read, Write);
-//          
-//          public static readonly UnsafeFilteredCollectionMarshaller<UxmlCacheItem, List<UxmlCacheItem>> Marshaller =
-//              new(ourUxmlCacheItemMarshaller, n => new List<UxmlCacheItem>(n), item => item != null);
-//
-//
-//         public UxmlCacheItem(string controlTypeName, [CanBeNull] UxmlElement nameElement, [CanBeNull] UxmlElement classNameElement)
-//         {
-//             ControlTypeName = controlTypeName;
-//             NameElement = nameElement;
-//             ClassNameElement = classNameElement;
-//         }
-//
-//         public string ControlTypeName { get; }
-//         public UxmlElement NameElement { get; }
-//         public UxmlElement ClassNameElement { get; }
-//
-//         private static UxmlCacheItem Read(UnsafeReader reader)
-//         {
-//             var controlTypeName = reader.ReadString();
-//             var nameElement = UxmlElement.Read(reader);
-//             var classNameElement = UxmlElement.Read(reader);
-//             return new UxmlCacheItem(controlTypeName, nameElement, classNameElement);
-//         }
-//
-//         private static void Write(UnsafeWriter writer, UxmlCacheItem value)
-//         {
-//             writer.Write(value.ControlTypeName);
-//             UxmlElement.Write(writer, value.NameElement);
-//             UxmlElement.Write(writer, value.ClassNameElement);
-//         }
-//         
-//         public override string ToString()
-//         {
-//             return $"{ControlTypeName}:{NameElement}:{ClassNameElement}";
-//         }
-//     }
-// }
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using JetBrains.Serialization;
+using JetBrains.Util.PersistentMap;
+
+namespace JetBrains.ReSharper.Plugins.Unity.UIElements.Uxml.Psi.Caches
+{
+    public class UxmlCacheItem
+    {
+         private static readonly IUnsafeMarshaller<UxmlCacheItem> ourUxmlCacheItemMarshaller =
+            new UniversalMarshaller<UxmlCacheItem>(Read, Write);
+         
+         public static readonly UnsafeFilteredCollectionMarshaller<UxmlCacheItem, List<UxmlCacheItem>> Marshaller =
+             new(ourUxmlCacheItemMarshaller, n => new List<UxmlCacheItem>(n), item => item != null);
+
+
+        public UxmlCacheItem(string controlTypeName, [CanBeNull] UxmlElement nameElement, [CanBeNull] UxmlElement classNameElement)
+        {
+            ControlTypeName = controlTypeName;
+            NameElement = nameElement;
+            ClassNameElement = classNameElement;
+        }
+
+        public string ControlTypeName { get; }
+        public UxmlElement NameElement { get; }
+        public UxmlElement ClassNameElement { get; }
+
+        private static UxmlCacheItem Read(UnsafeReader reader)
+        {
+            var controlTypeName = reader.ReadString();
+            var nameElement = UxmlElement.Read(reader);
+            var classNameElement = UxmlElement.Read(reader);
+            return new UxmlCacheItem(controlTypeName, nameElement, classNameElement);
+        }
+
+        private static void Write(UnsafeWriter writer, UxmlCacheItem value)
+        {
+            writer.Write(value.ControlTypeName);
+            UxmlElement.Write(writer, value.NameElement);
+            UxmlElement.Write(writer, value.ClassNameElement);
+        }
+        
+        public override string ToString()
+        {
+            return $"{ControlTypeName}:{NameElement}:{ClassNameElement}";
+        }
+    }
+}
