825ec6d3a6d2e4242992758ab9b8bf7e203aef79
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2023-10-20 08:56:33 +0200

    ++

--- resharper/resharper-unity/src/Unity/UIElements/Uxml/Psi/Searching/UxmlScriptSearcherFactory.cs
+++ resharper/resharper-unity/src/Unity/UIElements/Uxml/Psi/Searching/UxmlScriptSearcherFactory.cs
@@ -1,23 +1,12 @@
 using System.Collections.Generic;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.ExtensionsAPI;
-using JetBrains.ReSharper.Psi.Modules;
-using JetBrains.ReSharper.Psi.Search;
 
 namespace JetBrains.ReSharper.Plugins.Unity.UIElements.Uxml.Psi.Searching
 {
   [PsiComponent]
   public class UxmlScriptSearcherFactory : DomainSpecificSearcherFactoryBase
-  {
-    private readonly SearchDomainFactory mySearchDomainFactory;
-    private readonly IPsiModules myPSIModules;
-
-    public UxmlScriptSearcherFactory(SearchDomainFactory searchDomainFactory, IPsiModules psiModules)
-    {
-      mySearchDomainFactory = searchDomainFactory;
-      myPSIModules = psiModules;
-    }
-    
+  { 
     public override bool IsCompatibleWithLanguage(PsiLanguageType languageType)
     {
       return languageType.Is<UxmlLanguage>();
