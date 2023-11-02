677996c66b0c414b05593266f6f89918a134c351
Alexander Bondarev <alexander.bondarev@jetbrains.com>
2023-11-02 15:09:36 +0200

    [unity] Add macro icon for shader variants keywords.

--- resharper/resharper-unity/src/Unity.Shaders/HlslSupport/Feature/Services/CodeCompletion/ShaderVariantDefineSymbolsProvider.cs
+++ resharper/resharper-unity/src/Unity.Shaders/HlslSupport/Feature/Services/CodeCompletion/ShaderVariantDefineSymbolsProvider.cs
@@ -30,13 +30,13 @@ public class ShaderVariantDefineSymbolsProvider : ItemsProviderOfSpecificContext
         context.ReparseContext.TreeNode is CppIdentifierTokenNode && 
         context.Directive is Directive directive && 
         HlslUtils.IsConditionalDirective(directive);
-    
+
     protected override bool AddLookupItems(CppCodeCompletionInsideDirectiveContext context, IItemsCollector collector)
     {
         var basicContext = context.BasicContext;
         if (basicContext.File is not CppFile cppFile)
             return false;
-        
+
         var location = cppFile.InclusionContext.RootContext.BaseFile;
         var shaderProgramCache = context.BasicContext.Solution.GetComponent<ShaderProgramCache>();
         if (!shaderProgramCache.TryGetOrReadUpToDateProgramInfo(basicContext.SourceFile, location, out var shaderProgramInfo))
@@ -48,22 +48,23 @@ public class ShaderVariantDefineSymbolsProvider : ItemsProviderOfSpecificContext
             if ((item.Placement.Relevance & (ulong)CppCompletionRanks.Define) != 0)
                 existingDefineSymbols.Add(item.Placement.OrderString);
         }
-        
+
+        var icon = basicContext.PsiIconManager.GetImage(CppDeclaredElementTypes.MACRO);
         foreach (var keyword in shaderProgramInfo.Keywords) 
-            AddLookupItem(context, collector, keyword, existingDefineSymbols);
+            AddLookupItem(context, collector, icon, keyword, existingDefineSymbols);
 
         foreach (var symbol in ShaderDefineSymbolsRecognizer.KnownDefineSymbols) 
-            AddLookupItem(context, collector, symbol, existingDefineSymbols);
+            AddLookupItem(context, collector, icon, symbol, existingDefineSymbols);
 
         return true;
     }
 
-    private void AddLookupItem(CppCodeCompletionInsideDirectiveContext context, IItemsCollector collector, string defineSymbol, HashSet<string> existingDefineSymbols)
+    private void AddLookupItem(CppCodeCompletionInsideDirectiveContext context, IItemsCollector collector, IconId icon, string defineSymbol, HashSet<string> existingDefineSymbols)
     {
         if (!existingDefineSymbols.Add(defineSymbol))
             return;
-        
-        var info = new MyTextualInfo(defineSymbol) { Ranges = context.CompletionRanges };
+
+        var info = new MyTextualInfo(icon, defineSymbol) { Ranges = context.CompletionRanges };
         var item = LookupItemFactory.CreateLookupItem(info)
             .WithPresentation(static item => new TextPresentation<TextualInfo>(item.Info, item.Info.Icon, emphasize: false))
             .WithBehavior(static item => new TextualBehavior<TextualInfo>(item.Info))
@@ -73,10 +74,11 @@ public class ShaderVariantDefineSymbolsProvider : ItemsProviderOfSpecificContext
 
     public class MyTextualInfo : TextualInfo
     {
-        public IconId? Icon => null;
+        public IconId Icon { get; }
 
-        public MyTextualInfo(string defineSymbol) : base(defineSymbol, defineSymbol)
+        public MyTextualInfo(IconId icon, string defineSymbol) : base(defineSymbol, defineSymbol)
         {
+            Icon = icon;
             Placement.Relevance = (ulong)CppCompletionRanks.Define;
         }
     }
