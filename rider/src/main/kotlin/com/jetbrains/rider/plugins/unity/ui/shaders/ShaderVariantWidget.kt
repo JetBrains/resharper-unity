2e765c7459fbae95e01d5ab3ec7fd574d78bb5ae
Alexander Bondarev <alexander.bondarev@jetbrains.com>
2023-11-02 16:07:36 +0200

    [unity] Add got-it popup for shader variant widget.
    fixes RIDER-101115

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/ui/shaders/ShaderVariantWidget.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/ui/shaders/ShaderVariantWidget.kt
@@ -3,8 +3,10 @@ package com.jetbrains.rider.plugins.unity.ui.shaders
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
+import com.intellij.ui.GotItTooltip
 import com.intellij.ui.awt.RelativePoint
 import com.jetbrains.rider.editors.resolveContextWidget.RiderResolveContextWidget
+import com.jetbrains.rider.plugins.unity.UnityBundle
 import com.jetbrains.rider.plugins.unity.ideaInterop.fileTypes.shaderLab.ShaderLabFileType
 
 
@@ -14,4 +16,9 @@ class ShaderVariantWidget(project: Project, editor: Editor) : AbstractShaderWidg
     }
 
     override fun showPopup(showAt: RelativePoint) = ShaderVariantPopup.show(project, editor, showAt)
+
+    override fun addNotify() {
+        super.addNotify()
+        GotItTooltip("shader_variant.widget.got.it", UnityBundle.message("shaderVariant.widget.got.it.tooltip.text"), this).show(this, GotItTooltip.TOP_MIDDLE)
+    }
 }
