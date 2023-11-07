20a3f35a307c7570930671885cc4638f224c935d
Alexander Bondarev <alexander.bondarev@jetbrains.com>
2023-11-07 07:49:01 +0200

    [unity] Fix ShaderVariantsHost to finish on startup.

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/ui/shaders/ShaderVariantsHost.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/ui/shaders/ShaderVariantsHost.kt
@@ -3,27 +3,21 @@ package com.jetbrains.rider.plugins.unity.ui.shaders
 import com.intellij.openapi.application.EDT
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
-import com.jetbrains.rd.util.lifetime.LifetimeDefinition
 import com.jetbrains.rider.editors.resolveContextWidget.RiderResolveContextWidgetManager
 import com.jetbrains.rider.plugins.unity.FrontendBackendHost
+import com.jetbrains.rider.plugins.unity.UnityProjectLifetimeService
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.awaitCancellation
 import kotlinx.coroutines.withContext
 
 class ShaderVariantsHost : ProjectActivity {
     override suspend fun execute(project: Project) {
-        val lifetime = LifetimeDefinition()
-        try {
-            val frontendBackendHost = FrontendBackendHost.getInstance(project)
-            val model = frontendBackendHost.model
-            withContext(Dispatchers.EDT) {
-                model.backendSettings.previewShaderVariantsSupport.advise(lifetime) {
-                    RiderResolveContextWidgetManager.invalidateWidgets(project)
-                }
+        val lifetime = UnityProjectLifetimeService.getLifetime(project)
+        val frontendBackendHost = FrontendBackendHost.getInstance(project)
+        val model = frontendBackendHost.model
+        withContext(Dispatchers.EDT) {
+            model.backendSettings.previewShaderVariantsSupport.advise(lifetime) {
+                RiderResolveContextWidgetManager.invalidateWidgets(project)
             }
-            awaitCancellation()
-        } finally {
-          lifetime.terminate()
         }
     }
 }
