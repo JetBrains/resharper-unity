47cc992b06a610b448cc26333ff1fdd37c22c884
Alexander Bondarev <alexander.bondarev@jetbrains.com>
2023-12-07 12:02:12 +0200

    [unity] Fix duplicate items in shader variants keywords completion + optimization.
    fixes RIDER-102979

--- resharper/resharper-unity/test/src/Unity.Tests/Unity/ShaderLab/Feature/Services/CodeCompletion/ShaderLabShaderVariantCompletionTests.cs
+++ resharper/resharper-unity/test/src/Unity.Tests/Unity/ShaderLab/Feature/Services/CodeCompletion/ShaderLabShaderVariantCompletionTests.cs
@@ -2,7 +2,7 @@ using System;
 using JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure.LookupItems;
 using JetBrains.ReSharper.Feature.Services.CodeCompletion.LookupItems;
 using JetBrains.ReSharper.FeaturesTestFramework.Completion;
-using JetBrains.ReSharper.Plugins.Unity.Shaders.HlslSupport.Feature.Services.CodeCompletion;
+using JetBrains.ReSharper.Psi.Resources;
 using NUnit.Framework;
 
 namespace JetBrains.ReSharper.Plugins.Tests.Unity.ShaderLab.Feature.Services.CodeCompletion
@@ -18,8 +18,11 @@ namespace JetBrains.ReSharper.Plugins.Tests.Unity.ShaderLab.Feature.Services.Cod
         [TestCase("ShaderVariant02.shader")]
         [TestCase("ShaderVariant03.shader")]
         [TestCase("ShaderVariant04.shader")]
+        [TestCase("ShaderVariant05.shader")]
+        [TestCase("ShaderVariant06.shader")]
+        [TestCase("ShaderVariant07.shader")]
         public void Test(string name) => DoTestSolution(name);
 
-        protected override Func<ILookupItem, bool> ItemSelector { get; } = item => item is LookupItem { ItemInfo: ShaderVariantDefineSymbolsProvider.MyTextualInfo };
+        protected override Func<ILookupItem, bool> ItemSelector { get; } = item => item is LookupItem lookupItem && lookupItem.Presentation.Image == PsiSymbolsThemedIcons.Macro.Id && !lookupItem.ItemInfo.Text.StartsWith("__");
     }
 }
