5d8fa1e7648e4f2a1d3236133cbdefea45e5b24f
george.lemeshko <heorhii.lemeshko@jetbrains.com>
2023-12-27 12:16:06 +0100

    [rider][debugger][tests] Add tests for texture debugging
    GitOrigin-RevId: 41cf1bec7207cf521a2c0f1063fc431ab02834f7

--- rider/src/test/kotlin/com/jetbrains/rider/unity/test/cases/integrationTests/DebuggerTest2022.kt
+++ rider/src/test/kotlin/com/jetbrains/rider/unity/test/cases/integrationTests/DebuggerTest2022.kt
@@ -1,10 +1,17 @@
 package com.jetbrains.rider.unity.test.cases.integrationTests
 
+import com.intellij.openapi.rd.util.lifetime
 import com.intellij.xdebugger.XDebuggerManager
 import com.intellij.xdebugger.breakpoints.XLineBreakpoint
+import com.jetbrains.rd.util.string.printToString
+import com.jetbrains.rdclient.util.idea.pumpMessages
+import com.jetbrains.rider.debugger.DotNetStackFrame
 import com.jetbrains.rider.debugger.breakpoint.DotNetLineBreakpointProperties
 import com.jetbrains.rider.plugins.unity.debugger.breakpoints.UnityPausepointBreakpointType
 import com.jetbrains.rider.plugins.unity.debugger.breakpoints.convertToLineBreakpoint
+import com.jetbrains.rider.plugins.unity.debugger.valueEvaluators.UnityTextureCustomComponentEvaluator
+import com.jetbrains.rider.plugins.unity.debugger.valueEvaluators.UnityTextureCustomComponentEvaluator.Companion.getUnityTextureInfo
+import com.jetbrains.rider.plugins.unity.model.debuggerWorker.UnityTextureInfo
 import com.jetbrains.rider.test.allure.Subsystem
 import com.jetbrains.rider.test.annotations.TestEnvironment
 import com.jetbrains.rider.test.enums.PlatformType
@@ -13,9 +20,12 @@ import com.jetbrains.rider.unity.test.framework.UnityVersion
 import com.jetbrains.rider.unity.test.framework.api.attachDebuggerToUnityEditorAndPlay
 import com.jetbrains.rider.unity.test.framework.base.IntegrationTestWithUnityProjectBase
 import io.qameta.allure.*
+import kotlinx.coroutines.launch
 import org.testng.annotations.AfterMethod
 import org.testng.annotations.Test
 import java.io.File
+import kotlin.test.assertNotNull
+import kotlin.test.fail
 
 @Epic(Subsystem.UNITY_DEBUG)
 @Feature("Debug Unity2022")
@@ -50,6 +60,48 @@ class DebuggerTest2022 : IntegrationTestWithUnityProjectBase() {
                 waitForPause()
                 dumpFullCurrentData()
                 resumeSession()
+            }, testGoldFile)
+    }
+
+    @Test
+    @Description("Check Texture debugging with Unity2022")
+    fun checkTextureDebugging() {
+        attachDebuggerToUnityEditorAndPlay(
+            {
+                toggleBreakpoint("TextureDebuggingScript.cs", 13)
+            },
+            {
+                waitForPause()
+                dumpFullCurrentData()
+                try {
+                    val stackFrame = (session.currentStackFrame  as DotNetStackFrame)
+
+                    assertNotNull(stackFrame)
+                    val value = stackFrame.getNamedValue("texture2D")
+                    assertNotNull(value)
+                    val texture2DPresentation = value.getPresentation()
+                    val unityTextureCustomComponentEvaluator = texture2DPresentation.myFullValueEvaluator as UnityTextureCustomComponentEvaluator
+
+                    assertNotNull(unityTextureCustomComponentEvaluator)
+
+
+                    val lifetime = this.project.lifetime
+                    var textureInfo: UnityTextureInfo? = null
+                    val job = project.coroutineScope.launch {
+                        textureInfo = getUnityTextureInfo(stackFrame, value.objectProxy.id, lifetime, 10000, null) {
+                            fail(it)
+                        }
+
+                    }
+
+                    pumpMessages(DebugTestExecutionContext.waitForStopTimeout) {
+                        job.isCompleted
+                    }
+                    assertNotNull(textureInfo)
+                    printlnIndented(textureInfo.printToString())
+                }
+                finally {
+                }
             }, testGoldFile)
     }
 
