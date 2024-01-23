b5c7a75836a4ddef9e403957ee5ce721855c6705
Vladimir.Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2024-01-23 10:49:32 +0100

    [rider/unity] fix zones
    GitOrigin-RevId: 5ef9f5aafea6a421f87b2bcc07e8903d5cbc264b

---
+++ resharper/resharper-unity/src/Unity/Odin/Feature/Services/CodeCompletion/ZoneMarker.cs
@@ -0,0 +1,10 @@
+using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.Feature.Services;
+
+namespace JetBrains.ReSharper.Plugins.Unity.Odin.Feature.Services.CodeCompletion;
+
+[ZoneMarker]
+public class ZoneMarker : IRequire<ICodeEditingZone>
+{
+    
+}
