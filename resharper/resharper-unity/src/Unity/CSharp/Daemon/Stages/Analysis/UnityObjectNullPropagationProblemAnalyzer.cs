23fcb8705617d01f6de68326fc842cfd4596bf0d
Alexander Bondarev <alexander.bondarev@jetbrains.com>
2024-02-14 13:33:25 +0200

    [unity] Support assignment coalescing operator analysis.
    fixes RIDER-58175
    
    GitOrigin-RevId: eb8518dc192a5473276a7bfdbb772c803643c3d5

--- resharper/resharper-unity/src/Unity/CSharp/Daemon/Stages/Analysis/UnityObjectNullPropagationProblemAnalyzer.cs
+++ resharper/resharper-unity/src/Unity/CSharp/Daemon/Stages/Analysis/UnityObjectNullPropagationProblemAnalyzer.cs
@@ -1,35 +1,17 @@
-﻿using JetBrains.Annotations;
+﻿#nullable enable
 using JetBrains.ReSharper.Feature.Services.Daemon;
 using JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Errors;
 using JetBrains.ReSharper.Plugins.Unity.UnityEditorIntegration.Api;
-using JetBrains.ReSharper.Plugins.Unity.Utils;
 using JetBrains.ReSharper.Psi.CSharp.Tree;
-using JetBrains.ReSharper.Psi.Resolve;
-using JetBrains.ReSharper.Psi.Util;
 
-namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Stages.Analysis
+namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Stages.Analysis;
+
+[ElementProblemAnalyzer(typeof(IConditionalAccessExpression), HighlightingTypes = new[] { typeof(UnityObjectNullPropagationWarning) })]
+public class UnityObjectNullPropagationProblemAnalyzer(UnityApi unityApi) : UnityElementProblemAnalyzer<IConditionalAccessExpression>(unityApi)
 {
-    [ElementProblemAnalyzer(typeof(IConditionalAccessExpression), HighlightingTypes = new[] { typeof(UnityObjectNullPropagationWarning) })]
-    public class UnityObjectNullPropagationProblemAnalyzer : UnityElementProblemAnalyzer<IConditionalAccessExpression>
+    protected override void Analyze(IConditionalAccessExpression expression, ElementProblemAnalyzerData data, IHighlightingConsumer consumer)
     {
-        public UnityObjectNullPropagationProblemAnalyzer([NotNull] UnityApi unityApi)
-            : base(unityApi)
-        {
-        }
-
-        protected override void Analyze(IConditionalAccessExpression expression, ElementProblemAnalyzerData data, IHighlightingConsumer consumer)
-        {
-            if (!expression.HasConditionalAccessSign)
-                return;
-
-            if (expression.ConditionalQualifier is not { } qualifier)
-                return;
-
-            var type = qualifier.Type();
-            if (type.IsUnknown || !type.GetTypeElement().DerivesFrom(KnownTypes.Object))
-                return;
-
+        if (expression is { HasConditionalAccessSign: true, ConditionalQualifier: {} qualifier } && UnityTypeUtils.IsUnityObject(qualifier.Type()))
             consumer.AddHighlighting(new UnityObjectNullPropagationWarning(expression));
-        }
     }
 }
