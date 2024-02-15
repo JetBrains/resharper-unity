fe90fc1687f791909e753269477a301ef7de0bb2
Alexander Bondarev <alexander.bondarev@jetbrains.com>
2024-02-15 17:16:38 +0200

    [unity] Add inspection for Unity lifetime checks via null comparison.
    GitOrigin-RevId: 4e9f76f9fdb91280c5ccece8d1f540a7ad29b341

---
+++ resharper/resharper-unity/src/Unity/CSharp/Daemon/Stages/Analysis/UnityObjectLifetimeCheckViaNullEqualityAnalyzer.cs
@@ -0,0 +1,23 @@
+#nullable enable
+using JetBrains.ReSharper.Feature.Services.Daemon;
+using JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Errors;
+using JetBrains.ReSharper.Plugins.Unity.UnityEditorIntegration.Api;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+using JetBrains.ReSharper.Psi.CSharp.Util;
+
+namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Stages.Analysis;
+
+[ElementProblemAnalyzer(typeof(IEqualityExpression), HighlightingTypes = [typeof(UnityObjectLifetimeCheckViaNullEqualityWarning)])]
+public class UnityObjectLifetimeCheckViaNullEqualityAnalyzer(UnityApi unityApi) : UnityElementProblemAnalyzer<IEqualityExpression>(unityApi)
+{
+    protected override void Analyze(IEqualityExpression expression, ElementProblemAnalyzerData data, IHighlightingConsumer consumer)
+    {
+        var left = expression.LeftOperand.GetOperandThroughParenthesis();
+        var right = expression.RightOperand.GetOperandThroughParenthesis();
+        if (left == null || right == null)
+            return;
+        if (left.IsNullLiteral() && UnityTypeUtils.IsUnityObject(right.Type())
+            || right.IsNullLiteral() && UnityTypeUtils.IsUnityObject(left.Type()))
+            consumer.AddHighlighting(new UnityObjectLifetimeCheckViaNullEqualityWarning(expression));
+    }
+}
