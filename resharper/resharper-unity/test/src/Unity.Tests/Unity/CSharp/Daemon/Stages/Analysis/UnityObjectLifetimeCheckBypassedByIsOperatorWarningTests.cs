a1b89920a105ff07f67940ebb9736f8065e0c9cf
Alexander Bondarev <alexander.bondarev@jetbrains.com>
2024-02-15 17:16:38 +0200

    [unity] Add inspection for Unity lifetime checks via null comparison.
    (cherry picked from commit 4e9f76f9fdb91280c5ccece8d1f540a7ad29b341)
    
    GitOrigin-RevId: d8e055ba8491912f2410a453735d591730b236a5

--- resharper/resharper-unity/test/src/Unity.Tests/Unity/CSharp/Daemon/Stages/Analysis/UnityObjectLifetimeCheckBypassedByIsOperatorWarningTests.cs
+++ resharper/resharper-unity/test/src/Unity.Tests/Unity/CSharp/Daemon/Stages/Analysis/UnityObjectLifetimeCheckBypassedByIsOperatorWarningTests.cs
@@ -1,13 +1,12 @@
 ﻿using JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Errors;
 using NUnit.Framework;
 
-namespace JetBrains.ReSharper.Plugins.Tests.Unity.CSharp.Daemon.Stages.Analysis
-{
-    [TestUnity]
-    public class UnityObjectLifetimeCheckBypassedByIsOperatorWarningTests : CSharpHighlightingTestBase<UnityObjectLifetimeCheckBypassedByIsOperatorWarning>
-    {
-        protected override string RelativeTestDataPath => @"CSharp\Daemon\Stages\Analysis";
+namespace JetBrains.ReSharper.Plugins.Tests.Unity.CSharp.Daemon.Stages.Analysis;
 
-        [Test] public void TestUnityObjectLifetimeCheckBypassedByIsOperatorWarning() { DoNamedTest2(); }
-    }
+[TestUnity]
+public class UnityObjectLifetimeCheckBypassedByIsOperatorWarningTests : CSharpHighlightingTestBase<UnityObjectLifetimeCheckBypassedByIsOperatorWarning>
+{
+    protected override string RelativeTestDataPath => @"CSharp\Daemon\Stages\Analysis";
+
+    [Test] public void TestUnityObjectLifetimeCheckBypassedByIsOperatorWarning() { DoNamedTest2(); }
 }
