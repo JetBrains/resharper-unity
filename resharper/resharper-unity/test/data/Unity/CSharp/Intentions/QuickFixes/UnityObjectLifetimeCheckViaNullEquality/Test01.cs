df0b74058dc8c39a9f3dae305eda1064e9f93a70
Alexander Bondarev <alexander.bondarev@jetbrains.com>
2024-02-19 15:26:33 +0200

    [unity] Quick fixes for implicit Unity lifetime checks.
    (cherry picked from commit 78ac8e46802f8b4ab5d092700047e2e668e8091e)
    
    GitOrigin-RevId: d512aa2f08e1e21852f7eb98a73648755edc3c42

---
+++ resharper/resharper-unity/test/data/Unity/CSharp/Intentions/QuickFixes/UnityObjectLifetimeCheckViaNullEquality/Test01.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Test : MonoBehaviour
+{
+  public void Test(GameObject a, GameObject b)
+  {
+    if (a {caret}== null)
+      Debug.Log("Destroyed");
+  }
+}
+
---
+++ resharper/resharper-unity/test/data/Unity/CSharp/Intentions/QuickFixes/UnityObjectLifetimeCheckViaNullEquality/Test04.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Test : MonoBehaviour
+{
+  public void Test(GameObject a, GameObject b)
+  {
+    if (null !={caret} a)
+      Debug.Log("Destroyed");
+  }
+}
+
