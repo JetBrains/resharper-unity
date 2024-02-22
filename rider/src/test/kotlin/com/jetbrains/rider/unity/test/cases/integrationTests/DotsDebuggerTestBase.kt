54cb67ab9a463e9914b32aeef29ec17d832161d5
george.lemeshko <heorhii.lemeshko@jetbrains.com>
2024-02-22 17:15:49 +0100

    [rider][debugger][unity][dots] add tests for additional entity node
    (cherry picked from commit 3801341e79b42bfe83bde8557793232bab82a801)
    
    GitOrigin-RevId: 879ccf6aed7a2a99aafa5e717742995cdf5b69a8

--- rider/src/test/kotlin/com/jetbrains/rider/unity/test/cases/integrationTests/DotsDebuggerTestBase.kt
+++ rider/src/test/kotlin/com/jetbrains/rider/unity/test/cases/integrationTests/DotsDebuggerTestBase.kt
@@ -6,6 +6,7 @@ import com.jetbrains.rider.debugger.breakpoint.DotNetLineBreakpointProperties
 import com.jetbrains.rider.plugins.unity.debugger.breakpoints.UnityPausepointBreakpointType
 import com.jetbrains.rider.plugins.unity.debugger.breakpoints.convertToLineBreakpoint
 import com.jetbrains.rider.test.allure.Subsystem
+import com.jetbrains.rider.test.annotations.Mute
 import com.jetbrains.rider.test.annotations.TestEnvironment
 import com.jetbrains.rider.test.enums.PlatformType
 import com.jetbrains.rider.test.framework.combine
@@ -39,9 +40,7 @@ abstract class DotsDebuggerTestBase(private val unityVersion: UnityVersion) : In
                 toggleBreakpoint("ResetTransformSystem.cs", 24) //set new breakpoint
             },
             {
-                dumpProfile.customRegexToMask["<id>"] = Regex("\\((\\d+:\\d+)\\)")
-                dumpProfile.customRegexToMask["<float_value>"] = Regex("-?\\d+\\.*\\d*f")
-                dumpProfile.customRegexToMask["<ResetTransformSystemBase_LambdaJob_Job>"] = Regex("ResetTransformSystemBase_.*_Job")
+                setCustomRegextToMask()
 
                 waitForPause()
                 dumpFullCurrentData()
@@ -63,20 +62,18 @@ abstract class DotsDebuggerTestBase(private val unityVersion: UnityVersion) : In
                 toggleBreakpoint("ResetTransformSystem.cs", 24) //set new breakpoint
             },
             {
-                dumpProfile.customRegexToMask["<id>"] = Regex("\\((\\d+:\\d+)\\)") //Hides Entity's id
-                dumpProfile.customRegexToMask["<float_value>"] = Regex("-?\\d+\\.*\\d*f")
-                dumpProfile.customRegexToMask["<ResetTransformSystemBase_LambdaJob_Job>"] = Regex("ResetTransformSystemBase_.*_Job")
+                setCustomRegextToMask()
 
-                waitForPause()
-                dumpFullCurrentData(1)
-                toggleBreakpoint("ResetTransformSystem.cs", 24) //disable breakpoint
-                resumeSession()
-
-                toggleBreakpoint("ResetTransformSystem.cs", 34)//set new breakpoint
                 waitForPause()
                 dumpFullCurrentData(1)
                 resumeSession()
             }, testGoldFile)
+    }
+
+    private fun DebugTestExecutionContext.setCustomRegextToMask() {
+        dumpProfile.customRegexToMask["<id>"] = Regex("\\((\\d+:\\d+)\\)")
+        dumpProfile.customRegexToMask["<float_value>"] = Regex("-?\\d+\\.*\\d*f")
+        dumpProfile.customRegexToMask["<ResetTransformSystemBase_LambdaJob_Job>"] = Regex("ResetTransformSystemBase_.*_Job")
     }
 
     @Test
@@ -84,7 +81,7 @@ abstract class DotsDebuggerTestBase(private val unityVersion: UnityVersion) : In
         attachDebuggerToUnityEditorAndPlay(
             test = {
                 waitForUnityEditorPlayMode()
-                toggleUnityPausepoint(project, "ResetTransformSystem.cs", 24)
+                toggleUnityPausepoint(project, "ResetTransformSystem.cs", 26)
                 waitForUnityEditorPauseMode()
                 removeAllUnityPausepoints()
                 unpause()
@@ -119,7 +116,19 @@ abstract class DotsDebuggerTestBase(private val unityVersion: UnityVersion) : In
 @Severity(SeverityLevel.CRITICAL)
 @TestEnvironment(platform = [PlatformType.WINDOWS_ALL, PlatformType.MAC_OS_ALL])
 class DotsDebuggerTest {
-    class TestUnity2020 : DotsDebuggerTestBase(UnityVersion.V2020) {}
-    class TestUnity2022 : DotsDebuggerTestBase(UnityVersion.V2022) {}
-    class TestUnity2023 : DotsDebuggerTestBase(UnityVersion.V2023) {}
+    class TestUnity2020 : DotsDebuggerTestBase(UnityVersion.V2020) {
+        init {
+            addMute(Mute("RIDER-105466"), ::checkUnityPausePoint)
+        }
+    }
+    class TestUnity2022 : DotsDebuggerTestBase(UnityVersion.V2022) {
+        init {
+            addMute(Mute("RIDER-105466"), ::checkUnityPausePoint)
+        }
+    }
+    class TestUnity2023 : DotsDebuggerTestBase(UnityVersion.V2023) {
+        init {
+            addMute(Mute("RIDER-105466"), ::checkUnityPausePoint)
+        }
+    }
 }
