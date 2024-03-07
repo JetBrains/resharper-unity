a0774b3536bcba9526b6beca4915b2d7850d9004
Alexander Bondarev <alexander.bondarev@jetbrains.com>
2024-03-07 15:14:03 +0200

    [unity] Skip ShaderLab files for CLR declared elements search.
    fixes RIDER-81079
    
    GitOrigin-RevId: a993d866dab450cd71b0cf1b304491802e222b20

---
+++ resharper/resharper-unity/src/Unity.Shaders/ShaderLab/Feature/Services/Search/ShaderLabClrDeclaredElementSearchFilter.cs
@@ -0,0 +1,19 @@
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Plugins.Unity.Shaders.ShaderLab.ProjectModel;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.ExtensionsAPI.Finder;
+using JetBrains.ReSharper.Psi.Search;
+
+namespace JetBrains.ReSharper.Plugins.Unity.Shaders.ShaderLab.Feature.Services.Search;
+
+[PsiComponent]
+public class ShaderLabClrDeclaredElementSearchFilter : ISearchFilter
+{
+    public SearchFilterKind Kind => SearchFilterKind.Language;
+
+    public object TryGetKey(IDeclaredElement declaredElement) => declaredElement as IClrDeclaredElement;
+
+    public bool IsAvailable(SearchPattern pattern) => true;
+
+    public bool CanContainReferences(IPsiSourceFile sourceFile, object key) => !sourceFile.LanguageType.Is<ShaderLabProjectFileType>();
+}
