3689d99c358f0d1fb8ac4b65b00910756c756d87
Alexander Bondarev <alexander.bondarev@jetbrains.com>
2024-03-11 16:44:42 +0200

    [unity] Add symbols for built-in shaders.
    fixes RIDER-106803
    
    GitOrigin-RevId: 0f02485c8e7a962c92b717ca28419db1be64eded

--- resharper/resharper-unity/src/Unity.Shaders/ShaderLab/Psi/Caches/ShaderLabCache.cs
+++ resharper/resharper-unity/src/Unity.Shaders/ShaderLab/Psi/Caches/ShaderLabCache.cs
@@ -9,45 +9,25 @@ using JetBrains.ReSharper.Plugins.Unity.Shaders.ShaderLab.Psi.Tree.Impl;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.Caches;
 using JetBrains.ReSharper.Psi.ExtensionsAPI;
-using JetBrains.ReSharper.Psi.ExtensionsAPI.Resolve;
 using JetBrains.ReSharper.Psi.Files;
-using JetBrains.ReSharper.Psi.Impl.Resolve;
-using JetBrains.ReSharper.Psi.Resolve;
 
-namespace JetBrains.ReSharper.Plugins.Unity.Shaders.ShaderLab.Psi.Caches
+namespace JetBrains.ReSharper.Plugins.Unity.Shaders.ShaderLab.Psi.Caches;
+
+[PsiComponent]
+public class ShaderLabCache(Lifetime lifetime, IShellLocks locks, IPersistentIndexManager persistentIndexManager, ISolution solution)
+    : SimplePsiSourceFileCacheWithLocalCache<ShaderLabCacheItem, IDeclaredElement>(lifetime, locks, persistentIndexManager, ShaderLabCacheItem.Marshaller, "Unity::Shaders::ShaderLabCacheUpdated")
 {
-    [PsiComponent]
-    public class ShaderLabCache : SimplePsiSourceFileCacheWithLocalCache<ShaderLabCacheItem, IDeclaredElement>
+    protected override bool IsApplicable(IPsiSourceFile sourceFile) => base.IsApplicable(sourceFile) && sourceFile.LanguageType.Is<ShaderLabProjectFileType>();
+
+    public override object? Build(IPsiSourceFile sourceFile, bool isStartup)
     {
-        private readonly ISolution mySolution;
-        
-        public ShaderLabCache(Lifetime lifetime, IShellLocks locks, IPersistentIndexManager persistentIndexManager, ISolution solution) : base(lifetime, locks, persistentIndexManager, ShaderLabCacheItem.Marshaller, "Unity::Shaders::ShaderLabCacheUpdated")
-        {
-            mySolution = solution;
-        }
-
-        protected override bool IsApplicable(IPsiSourceFile sourceFile) => base.IsApplicable(sourceFile) && sourceFile.LanguageType.Is<ShaderLabProjectFileType>();
-
-        public override object? Build(IPsiSourceFile sourceFile, bool isStartup)
-        {
-            if (sourceFile.GetDominantPsiFile<ShaderLabLanguage>() is not ShaderLabFile file)
-                return null;
+        if (sourceFile.GetDominantPsiFile<ShaderLabLanguage>() is not ShaderLabFile file)
+            return null;
             
-            var name = file.DeclaredName;
-            return name != SharedImplUtil.MISSING_DECLARATION_NAME ? new ShaderLabCacheItem(name, file.GetTreeStartOffset().Offset) : null;
-        }
-
-        protected override IDeclaredElement BuildLocal(IPsiSourceFile sourceFile, ShaderLabCacheItem newPart) =>
-            new ShaderDeclaredElement(newPart.Name, sourceFile, newPart.DeclarationOffset);
-
-        /// <summary>Returns table of all shaders declared with ShaderLab.</summary>
-        public ISymbolTable GetShaderSymbolTable()
-        {
-            var values = LocalCacheValues;
-            if (values.Count == 0)
-                return EmptySymbolTable.INSTANCE;
-            var psiServices = mySolution.GetComponent<IPsiServices>();
-            return new DeclaredElementsSymbolTable<IDeclaredElement>(psiServices, values);
-        }
+        var name = file.DeclaredName;
+        return name != SharedImplUtil.MISSING_DECLARATION_NAME ? new ShaderLabCacheItem(name, file.GetTreeStartOffset().Offset) : null;
     }
+
+    protected override IDeclaredElement BuildLocal(IPsiSourceFile sourceFile, ShaderLabCacheItem newPart) =>
+        new ShaderDeclaredElement(newPart.Name, sourceFile, newPart.DeclarationOffset);
 }
