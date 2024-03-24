992b05ab06a024a68c3592e2d295134571c341ad
Alexander.Shvedov <alexander.shvedov@jetbrains.com>
2024-03-24 21:45:35 +0100

    Fix stage deps
    GitOrigin-RevId: ee381f44e40d620c0e8419130145d6aa186415f0

--- resharper/resharper-unity/src/Unity/CSharp/Daemon/Stages/SerializationAttributes/UnitySerializationAttributeAbstractStage.cs
+++ resharper/resharper-unity/src/Unity/CSharp/Daemon/Stages/SerializationAttributes/UnitySerializationAttributeAbstractStage.cs
@@ -13,7 +13,7 @@ using JetBrains.ReSharper.Psi.Tree;
 
 namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Stages.SerializationAttributes
 {
-    [DaemonStage(GlobalAnalysisStage = true)]
+    [DaemonStage(GlobalAnalysisStage = true, StagesBefore = [typeof(SolutionAnalysisFileStructureCollectorStage)])]
     public class UnitySerializationAttributeGlobalStage : CSharpDaemonStageBase
     {
         private readonly UnityApi myUnityApi;
