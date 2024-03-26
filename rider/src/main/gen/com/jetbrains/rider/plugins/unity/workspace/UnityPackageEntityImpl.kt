4d4db062f6aa428257be10024e7a86d31b46632e
Alex Plate <aleksei.plate@jetbrains.com>
2024-03-26 17:40:01 +0200

    [Workspace Model] [IJPL-857] Unbind WorkspaceEntityBase from ModifiableWorkspaceEntityBase
    GitOrigin-RevId: 52a73086474ca18d8e3d4e2e8030012d8544bff4

--- rider/src/main/gen/com/jetbrains/rider/plugins/unity/workspace/UnityPackageEntityImpl.kt
+++ rider/src/main/gen/com/jetbrains/rider/plugins/unity/workspace/UnityPackageEntityImpl.kt
@@ -79,7 +79,6 @@ open class UnityPackageEntityImpl(private val dataSource: UnityPackageEntityData
       }
 
       this.diff = builder
-      this.snapshot = builder
       addToBuilder()
       this.id = getEntityData().createEntityId()
       // After adding entity data to the builder, we need to unbind it and move the control over entity data to builder
@@ -181,7 +180,6 @@ class UnityPackageEntityData : WorkspaceEntityData<UnityPackageEntity>() {
   override fun wrapAsModifiable(diff: MutableEntityStorage): WorkspaceEntity.Builder<UnityPackageEntity> {
     val modifiable = UnityPackageEntityImpl.Builder(null)
     modifiable.diff = diff
-    modifiable.snapshot = diff
     modifiable.id = createEntityId()
     return modifiable
   }
