624b5209f4fdbbe40e2bd02248b38ab201543213
Rodion Iafarov <rodion.iafarov@jetbrains.com>
2024-04-12 20:43:29 +0200

    [Rider autotests]: Move mutes from TC to the code
    GitOrigin-RevId: 41b2706d13e65071ec0b89f17d98a1c18c74fa7f

--- rider/src/test/kotlin/com/jetbrains/rider/unity/test/cases/integrationTests/DebuggerTest.kt
+++ rider/src/test/kotlin/com/jetbrains/rider/unity/test/cases/integrationTests/DebuggerTest.kt
@@ -2,6 +2,7 @@ package com.jetbrains.rider.unity.test.cases.integrationTests
 
 import com.jetbrains.rider.test.allure.SubsystemConstants
 import com.jetbrains.rider.test.annotations.Feature
+import com.jetbrains.rider.test.annotations.Mute
 import com.jetbrains.rider.test.annotations.Subsystem
 import com.jetbrains.rider.test.annotations.TestEnvironment
 import com.jetbrains.rider.test.enums.PlatformType
@@ -15,7 +16,11 @@ import io.qameta.allure.SeverityLevel
 @TestEnvironment(platform = [PlatformType.WINDOWS_ALL, PlatformType.MAC_OS_ALL])
 @Suppress("unused")
 class DebuggerTest {
-    class TestUnity2020 : DebuggerTestBase(UnityVersion.V2020)
+    class TestUnity2020 : DebuggerTestBase(UnityVersion.V2020)  {
+        init {
+          addMute(Mute("RIDER-105466", platforms = arrayOf(PlatformType.WINDOWS_ALL)), ::checkUnityPausePoint)
+        }
+    }
     class TestUnity2022 : DebuggerTestBase(UnityVersion.V2022)
     class TestUnity2023 : DebuggerTestBase(UnityVersion.V2023)
 }
