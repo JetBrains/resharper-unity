d03700381dfd46bef10fe8b0d6dbd60a920963f0
Ivan Migalev <ivan.migalev@jetbrains.com>
2024-04-22 20:21:05 +0200

    Rider Unity plugin: move generated models to a separate directory
    GitOrigin-RevId: 36e62d153a91d0379a29ad51f61a69ce097b01d5

--- rider/build.gradle.kts
+++ rider/build.gradle.kts
@@ -118,7 +118,7 @@ sourceSets {
     main {
         java {
             srcDir("src/main/gen")
-            srcDir("src/main/rdgen/kotlin")
+            srcDir("src/generated/kotlin")
         }
         resources {
             srcDir("src/main/rdgen/resources")
