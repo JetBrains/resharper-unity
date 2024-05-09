dce68f622c7e965e3bc7e5f00ed184204d46f81c
Nikita Popov <nikita.popov@jetbrains.com>
2024-05-09 19:35:30 +0200

    [dotnet] mark DefaultSettingsAttribute as DemandAnyThreadSafe
    GitOrigin-RevId: d4a821ebf70191ab825aecd324de01c28748ff0a

--- resharper/resharper-unity/src/Unity/CSharp/Psi/CodeStyle/AdditionalFileLayoutDefaultSettings.cs
+++ resharper/resharper-unity/src/Unity/CSharp/Psi/CodeStyle/AdditionalFileLayoutDefaultSettings.cs
@@ -1,4 +1,3 @@
-using JetBrains.Application.Parts;
 using JetBrains.Application.Settings;
 using JetBrains.Application.Settings.Implementation;
 using JetBrains.Util;
@@ -7,7 +6,7 @@ namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Psi.CodeStyle
 {
     // This doesn't affect any non-Unity projects, so it's a global default, and can be overridden safely in a user's
     // global settings
-    [DefaultSettings(typeof(AdditionalFileLayoutSettings), Instantiation.DemandAnyThreadSafe)]
+    [DefaultSettings(typeof(AdditionalFileLayoutSettings))]
     public class AdditionalFileLayoutDefaultSettings : HaveDefaultSettings
     {
         public AdditionalFileLayoutDefaultSettings(ISettingsSchema settingsSchema, ILogger logger)
