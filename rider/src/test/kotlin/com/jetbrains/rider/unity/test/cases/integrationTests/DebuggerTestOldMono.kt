c93acbd50bc2b796201cb769d513bc7344b548de
Ekaterina.Kuznetsova <ekaterina.kuznetsova@jetbrains.com>
2024-05-12 19:12:01 +0300

    Remove old Debugger TestBase
    GitOrigin-RevId: ab04c78ae91cc6264ca4549dbbbf4a2361bceaeb

--- rider/src/test/kotlin/com/jetbrains/rider/unity/test/cases/integrationTests/DebuggerTestOldMono.kt
+++ rider/src/test/kotlin/com/jetbrains/rider/unity/test/cases/integrationTests/DebuggerTestOldMono.kt
@@ -1,19 +1,20 @@
 package com.jetbrains.rider.unity.test.cases.integrationTests
 
-import com.jetbrains.rider.test.reporting.SubsystemConstants
+import com.jetbrains.rider.test.allure.SubsystemConstants
 import com.jetbrains.rider.test.annotations.Feature
 import com.jetbrains.rider.test.annotations.Mute
 import com.jetbrains.rider.test.annotations.Subsystem
-import com.jetbrains.rider.test.annotations.Severity
-import com.jetbrains.rider.test.annotations.SeverityLevel
-import com.jetbrains.rider.unity.test.framework.base.DebuggerTestBase
+import com.jetbrains.rider.unity.test.framework.UnityVersion
+import io.qameta.allure.Severity
+import io.qameta.allure.SeverityLevel
 import java.io.File
 
 @Mute("RIDER-67296")
 @Subsystem(SubsystemConstants.UNITY_DEBUG)
 @Feature("Debug Unity OldMono")
 @Severity(SeverityLevel.NORMAL)
-class DebuggerTestOldMono : DebuggerTestBase() {
+class DebuggerTestOldMono : DebuggerTestBase(UnityVersion.V2022) {
+    override fun getSolutionDirectoryName() = "SimpleUnityProjectWithoutPlugin"
     override fun preprocessTempDirectory(tempDir: File) {
         super.preprocessTempDirectory(tempDir)
 
@@ -21,4 +22,5 @@ class DebuggerTestOldMono : DebuggerTestBase() {
         val projectSettings = tempDir.resolve("ProjectSettings/ProjectSettings.asset")
         projectSettings.writeText(projectSettings.readText().replace("scriptingRuntimeVersion: 1","scriptingRuntimeVersion: 0"))
     }
+
 }
