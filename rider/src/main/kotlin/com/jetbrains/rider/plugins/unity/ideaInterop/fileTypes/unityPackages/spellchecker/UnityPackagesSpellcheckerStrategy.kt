ff11143978f7bf5a8ee3900fd7bd4cec9e8a59ac
george.lemeshko <heorhii.lemeshko@jetbrains.com>
2024-06-05 17:37:17 +0200

    [rider][unity] fix RIDER-81913 Disable spellchecker for manifest.json and packages-lock.json files
    GitOrigin-RevId: 5c3ac1b7c154077f250042e7f2b206785b542183

---
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/ideaInterop/fileTypes/unityPackages/spellchecker/UnityPackagesSpellcheckerStrategy.kt
@@ -0,0 +1,31 @@
+package com.jetbrains.rider.plugins.unity.ideaInterop.fileTypes.unityPackages.spellchecker
+
+import com.intellij.json.JsonDialectUtil
+import com.intellij.json.JsonSpellcheckerStrategy
+import com.intellij.psi.PsiElement
+import com.intellij.spellchecker.tokenizer.Tokenizer
+
+class UnityPackagesSpellcheckerStrategy : JsonSpellcheckerStrategy() {
+    private val MANIFEST_DEFAULT_FILENAME: String = "manifest.json"
+    private val PACKAGE_LOCK_DEFAULT_FILENAME: String = "packages-lock.json"
+    private val PACKAGES_FOLDER_NAME = "Packages"
+
+    // There is nothing in a manifest or a package-lock file that we want to spell check
+    override fun getTokenizer(element: PsiElement): Tokenizer<PsiElement> = EMPTY_TOKENIZER
+
+    override fun isMyContext(element: PsiElement): Boolean {
+        val file = element.containingFile ?: return false
+        val fileName = file.name
+
+        //check if it's unity packages-related files
+        if(MANIFEST_DEFAULT_FILENAME != fileName && PACKAGE_LOCK_DEFAULT_FILENAME != fileName)
+            return false
+
+        //check if this file is in Packages folder
+        val parentFolderName = file.parent?.name
+        if(parentFolderName != PACKAGES_FOLDER_NAME)
+            return false
+
+        return JsonDialectUtil.isStandardJson(element)
+    }
+}
