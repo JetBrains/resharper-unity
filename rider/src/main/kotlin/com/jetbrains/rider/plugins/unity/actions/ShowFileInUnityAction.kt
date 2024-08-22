a865275e0d7490a3ff101a226999f1bcbe1b585d
Gregory.Shrago <gregory.shrago@jetbrains.com>
2024-08-22 18:57:44 +0400

    migrate `ActionPlaces.isPopupPlace` usages
    GitOrigin-RevId: 6caf1344423c8142a549d507c19e4ea40a721284

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/actions/ShowFileInUnityAction.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/actions/ShowFileInUnityAction.kt
@@ -1,7 +1,6 @@
 package com.jetbrains.rider.plugins.unity.actions
 
 import com.intellij.ide.actions.RevealFileAction
-import com.intellij.openapi.actionSystem.ActionPlaces
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
@@ -23,14 +22,15 @@ open class ShowFileInUnityAction : DumbAwareAction() {
     }
 
     override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
+
     override fun update(e: AnActionEvent) {
         val project = e.project ?: return
         // see `com.intellij.ide.actions.RevealFileAction.update`
         // Cleanup context menu on selection (IDEA-245559)
         val editor = e.getData(CommonDataKeys.EDITOR)
-        e.presentation.isEnabledAndVisible = project.isUnityProject.value && getFile(e) != null &&
-                                             (!ActionPlaces.isPopupPlace(
-                                                 e.place) || editor == null || !editor.selectionModel.hasSelection())
+        e.presentation.isEnabledAndVisible =
+            project.isUnityProject.value && getFile(e) != null &&
+            (!e.isFromContextMenu || editor == null || !editor.selectionModel.hasSelection())
 
         e.presentation.isEnabled = project.isConnectedToEditor()
         super.update(e)
