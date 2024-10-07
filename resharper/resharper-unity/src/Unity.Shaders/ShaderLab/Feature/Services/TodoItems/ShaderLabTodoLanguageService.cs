4b4a7c7341051adba00e7e80a6b21177893c4721
andrew.karpov <andrew.karpov@jetbrains.com>
2024-10-07 20:39:29 +0200

    [dotnet] Support multiline todo comments Part 3: Fix plugins
    GitOrigin-RevId: e396cf607865652125a2c85746cbc73909f9f6f8

--- resharper/resharper-unity/src/Unity.Shaders/ShaderLab/Feature/Services/TodoItems/ShaderLabTodoLanguageService.cs
+++ resharper/resharper-unity/src/Unity.Shaders/ShaderLab/Feature/Services/TodoItems/ShaderLabTodoLanguageService.cs
@@ -8,20 +8,26 @@ using JetBrains.Util;
 namespace JetBrains.ReSharper.Plugins.Unity.Shaders.ShaderLab.Feature.Services.TodoItems
 {
     [Language(typeof(ShaderLabLanguage))]
-    public class ShaderLabTodoContentsProvider : DefaultTodoContentsProvider
+    public class ShaderLabTodoLanguageService : DefaultTodoLanguageService
     {
+        public override bool IsMultiLineComment(string documentText, TextRange tokenRange, TokenNodeType tokenType)
+        {
+            return tokenType == ShaderLabTokenType.MULTI_LINE_COMMENT;
+        }
+
         public override TextRange? GetTokenContentsRange(string documentText, TextRange tokenRange, TokenNodeType tokenType)
         {
             if (tokenType == ShaderLabTokenType.END_OF_LINE_COMMENT)
             {
                 return new TextRange(tokenRange.StartOffset + 2, tokenRange.EndOffset);
             }
+
             if (tokenType == ShaderLabTokenType.MULTI_LINE_COMMENT)
             {
                 return new TextRange(tokenRange.StartOffset + 2, tokenRange.EndOffset - 2);
             }
 
-            return base.GetTokenContentsRange(documentText, tokenRange, tokenType);
+            return null;
         }
     }
 }
