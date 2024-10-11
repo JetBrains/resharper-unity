426c8e10d22d89538f635deccabd5d3eb9461a63
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2024-10-11 16:56:11 +0200

    fix RIDER-117479
    (cherry picked from commit 058fc15d6c53421aac230ddc48f3dbfa90dd2f4c)
    
    IJ-CR-146586
    
    GitOrigin-RevId: 41f3a283606bb997d32042523a00a289f380c162

---
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/gotoType/UnityGotoCustomizer.kt
@@ -0,0 +1,24 @@
+package com.jetbrains.rider.plugins.unity.gotoType
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.openapi.vfs.VirtualFile
+import com.jetbrains.rdclient.util.idea.toVirtualFile
+import com.jetbrains.rider.gotoType.GotoCustomizer
+import com.jetbrains.rider.plugins.unity.ideaInterop.fileTypes.yaml.UnityYamlFileType
+import com.jetbrains.rider.plugins.unity.isUnityProject
+import com.jetbrains.rider.projectView.solutionDirectory
+
+class UnityGotoCustomizer : GotoCustomizer {
+    // RIDER-117479
+    override fun isGotoTargetInProject(project: Project, file: VirtualFile): Boolean {
+        if (!project.isUnityProject.value) return false
+        if (file.fileType !is UnityYamlFileType) return false
+        val solutionDir = project.solutionDirectory.toVirtualFile(false)?:return false
+        val assetsFolder = solutionDir.findChild("Assets") ?: return false
+        if (!VfsUtil.isAncestor(assetsFolder, file, false)) return false
+
+        val localPackagesFolder = solutionDir.findChild("Packages") ?: return false
+        return VfsUtil.isAncestor(localPackagesFolder, file, false)
+    }
+}
