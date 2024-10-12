27c1a4533408d2407c15137063bbe8c17f7048f9
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2024-10-12 11:14:14 +0200

    avoid including Assets into indexing, due to possible perf degradation
    (cherry picked from commit 3469c5e7107355225f290317a8ffecb96c2c5ec7)
    
    IJ-MR-146616
    
    GitOrigin-RevId: df0d32279eff9b41430a45f417ba8f85336d79b9

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/workspace/UnityWorkspaceFileIndexContributor.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/workspace/UnityWorkspaceFileIndexContributor.kt
@@ -21,11 +21,13 @@ class UnityWorkspaceFileIndexContributor : WorkspaceFileIndexContributor<Project
     // Packages - almost works as expected
     // I have decided to manually add "Packages/manifest.json", "Packages/packages-lock.json", even though those are not in the generated projects
 
-    // Assets, ProjectSettings, UserSettings
+    // ProjectSettings, UserSettings
     // adding manually as EXTERNAL_SOURCE, search for it with Out-Of-Solution scope
     // files, which are in the project already, (like cs) should remain in the solution scope
 
     // Library - not sure, probably we don't need anything there, except packages, which are added differently
+
+    // Assets, would be great to include, but would cause a great perf penalty, time for indexing
 
     override fun registerFileSets(entity: ProjectModelEntity, registrar: WorkspaceFileSetRegistrar, storage: EntityStorage) {
         if (entity.descriptor is RdSolutionDescriptor) {
@@ -56,10 +58,12 @@ class UnityWorkspaceFileIndexContributor : WorkspaceFileIndexContributor<Project
                 registrar.registerFileSet(userSettingsDir.toVirtualFileUrl(virtualFileManager), WorkspaceFileKind.EXTERNAL_SOURCE, entity,
                                           UnityAssetsModulesFileSetData())
 
-            val assetsDir = solFolder.findChild("Assets")
-            if (assetsDir != null)
-                registrar.registerFileSet(assetsDir.toVirtualFileUrl(virtualFileManager), WorkspaceFileKind.EXTERNAL_SOURCE, entity,
-                                          UnityAssetsModulesFileSetData())
+            // we can't include this,
+            // I haven't checked myself, but on big projects this would cause a big perf penalty, delay in indexing
+            //val assetsDir = solFolder.findChild("Assets")
+            //if (assetsDir != null)
+            //    registrar.registerFileSet(assetsDir.toVirtualFileUrl(virtualFileManager), WorkspaceFileKind.EXTERNAL_SOURCE, entity,
+            //                              UnityAssetsModulesFileSetData())
         }
     }
 
