0c12f17d5b8880cc4af51db5d2139fd35adb7763
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2024-10-12 11:10:35 +0200

    fix RIDER-112393 Cannot add Open Unity Logs to toolbar
    (cherry picked from commit 69ec73bb9e79b703404e5ed6fce368775f7c5056)
    
    IJ-MR-146615
    
    GitOrigin-RevId: 68ac59395efd2d5fe6a387904d3039c5714683a5

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/actions/RiderUnityLogViewAction.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/actions/RiderUnityLogViewAction.kt
@@ -1,16 +1,14 @@
 package com.jetbrains.rider.plugins.unity.actions
 
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.PlatformDataKeys
 import com.intellij.openapi.project.DumbAwareAction
-import com.jetbrains.rider.plugins.unity.toolWindow.UnityToolWindowFactory
+import com.jetbrains.rider.plugins.unity.isUnityProject
 
 abstract class RiderUnityLogViewAction : DumbAwareAction() {
     override fun update(e: AnActionEvent) {
         e.presentation.apply {
-            val dataContext = e.dataContext
-            val tw = PlatformDataKeys.TOOL_WINDOW.getData(dataContext)
-            isVisible = tw != null && tw.stripeTitle == UnityToolWindowFactory.TOOL_WINDOW_ID
+            val project = e.project?: return@apply
+            isVisible = project.isUnityProject.valueOrDefault
         }
     }
 }
