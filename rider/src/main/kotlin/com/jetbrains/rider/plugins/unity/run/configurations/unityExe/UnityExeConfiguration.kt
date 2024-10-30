33711bf6ea40d35a37601d9f44c08ef76e9064f5
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2024-10-30 11:46:53 +0100

    fix RIDER-117700 Start Unity / UnitTest (batch mode) configurations for UnityCoreCLR
    (cherry picked from commit a991ca21506d902601db8146e2805ba60c221231)
    
    IJ-MR-147810
    
    GitOrigin-RevId: cfdcfe232d5368cc7cd3c49f273a586a73bb878d

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/run/configurations/unityExe/UnityExeConfiguration.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/run/configurations/unityExe/UnityExeConfiguration.kt
@@ -1,5 +1,6 @@
 package com.jetbrains.rider.plugins.unity.run.configurations.unityExe
 
+import com.intellij.execution.CantRunException
 import com.intellij.execution.Executor
 import com.intellij.execution.configurations.ConfigurationFactory
 import com.intellij.execution.configurations.ConfigurationTypeUtil
@@ -8,11 +9,18 @@ import com.intellij.execution.configurations.RunProfileState
 import com.intellij.execution.executors.DefaultDebugExecutor
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.openapi.project.Project
+import com.jetbrains.rider.multiPlatform.RiderMultiPlatformBundle
+import com.jetbrains.rider.plugins.unity.ui.hasTrueValue
+import com.jetbrains.rider.plugins.unity.util.UnityInstallationFinder
 import com.jetbrains.rider.run.RiderRunBundle
 import com.jetbrains.rider.run.configurations.exe.ExeConfiguration
 import com.jetbrains.rider.run.configurations.exe.ExeConfigurationParameters
 import com.jetbrains.rider.run.configurations.remote.DotNetRemoteConfiguration
 import com.jetbrains.rider.run.configurations.remote.MonoRemoteConfigType
+import com.jetbrains.rider.run.dotNetCore.DotNetCoreDebugProfile
+import com.jetbrains.rider.runtime.DotNetExecutable
+import com.jetbrains.rider.runtime.RiderDotNetActiveRuntimeHost
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.jetbrains.concurrency.Promise
 
 class UnityExeConfiguration(name: String,
@@ -32,14 +40,41 @@ class UnityExeConfiguration(name: String,
         return newConfiguration
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     override suspend fun getRunProfileStateAsync(executor: Executor, environment: ExecutionEnvironment): RunProfileState {
         val executorId = executor.id
 
-        if (executorId == DefaultDebugExecutor.EXECUTOR_ID)
-            return UnityExeDebugProfileState(this, DotNetRemoteConfiguration(project, ConfigurationTypeUtil.findConfigurationType(
-                MonoRemoteConfigType::class.java).factory, name), environment)
+        if (executorId == DefaultDebugExecutor.EXECUTOR_ID){
+            if (UnityInstallationFinder.getInstance(project).isCoreCLR.hasTrueValue())
+            {
+                val activeRuntimeHost = RiderDotNetActiveRuntimeHost.getInstance(environment.project)
+                val dotNetCoreRuntime = activeRuntimeHost.dotNetCoreRuntime.value ?: throw CantRunException(
+                    RiderMultiPlatformBundle.message("rider.mac.unable.to.get.runtime.information.message"))
+                return DotNetCoreDebugProfile(dotNetCoreRuntime, toDotNetExecutable(this.parameters), environment, dotNetCoreRuntime.cliExePath)
+            }
+            else{
+                return UnityExeDebugProfileState(this, DotNetRemoteConfiguration(project, ConfigurationTypeUtil.findConfigurationType(
+                    MonoRemoteConfigType::class.java).factory, name), environment)
+            }
+        }
 
         return super.getRunProfileStateAsync(executor, environment)
+    }
+
+    private fun toDotNetExecutable(parameters: ExeConfigurationParameters): DotNetExecutable {
+        return DotNetExecutable(parameters.exePath,
+                                null,
+                                parameters.workingDirectory,
+                                parameters.programParameters,
+                                false,
+                                parameters.useExternalConsole,
+                                parameters.envs,
+                                false,
+                                { _, _, _ -> },
+                                null,
+                                "",
+                                true
+        )
     }
 
     @Suppress("UsagesOfObsoleteApi")
