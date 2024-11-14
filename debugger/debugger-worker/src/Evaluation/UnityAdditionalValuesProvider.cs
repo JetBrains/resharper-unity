1c1f6e3af242dc37a1619b5abea6b77643cc5d33
george.lemeshko <heorhii.lemeshko@jetbrains.com>
2024-11-14 17:26:42 +0100

    [rider][debugger][unity] Add extra information about loaded scenes (fix RIDER-119882)
    GitOrigin-RevId: f0447426fe85f7885486cc18a0c1abb4e4555348

--- debugger/debugger-worker/src/Evaluation/UnityAdditionalValuesProvider.cs
+++ debugger/debugger-worker/src/Evaluation/UnityAdditionalValuesProvider.cs
@@ -12,7 +12,10 @@ using Mono.Debugging.Client.CallStacks;
 using Mono.Debugging.Client.Values;
 using Mono.Debugging.Client.Values.Render;
 using Mono.Debugging.Evaluation;
+using Mono.Debugging.MetadataLite.API;
+using Mono.Debugging.MetadataLite.Services;
 using Mono.Debugging.Soft;
+using Mono.Debugging.TypeSystem;
 
 namespace JetBrains.Debugger.Worker.Plugins.Unity.Evaluation
 {
@@ -20,8 +23,8 @@ namespace JetBrains.Debugger.Worker.Plugins.Unity.Evaluation
     public class UnityAdditionalValuesProvider : UnityAdditionalValuesProvider<Value>
     {
         public UnityAdditionalValuesProvider(IDebuggerSession session, IValueServicesFacade<Value> valueServices,
-                                             IUnityOptions unityOptions, ILogger logger)
-            : base(session, valueServices, unityOptions, logger)
+                                             IUnityOptions unityOptions, ILogger logger, IValueFactory<Value> factory)
+            : base(session, valueServices, unityOptions, logger, factory)
         {
         }
     }
@@ -33,14 +36,16 @@ namespace JetBrains.Debugger.Worker.Plugins.Unity.Evaluation
         private readonly IValueServicesFacade<TValue> myValueServices;
         private readonly IUnityOptions myUnityOptions;
         private readonly ILogger myLogger;
+        private readonly IValueFactory<TValue> myFactory;
 
         protected UnityAdditionalValuesProvider(IDebuggerSession session, IValueServicesFacade<TValue> valueServices,
-                                                IUnityOptions unityOptions, ILogger logger)
+                                                IUnityOptions unityOptions, ILogger logger, IValueFactory<TValue> factory)
         {
             mySession = session;
             myValueServices = valueServices;
             myUnityOptions = unityOptions;
             myLogger = logger;
+            myFactory = factory;
         }
 
         public IEnumerable<IValueEntity> GetAdditionalLocals(IStackFrame frame)
@@ -57,7 +62,11 @@ namespace JetBrains.Debugger.Worker.Plugins.Unity.Evaluation
             // Add "Active Scene" as a top level item to mimic the Hierarchy window in Unity
             var activeScene = GetActiveScene(frame);
             if (activeScene != null)
-                yield return activeScene.ToValue(myValueServices);
+                yield return activeScene;
+
+            var dontDestroyOnLoadScene = GetDontDestroyOnLoadScene(frame);
+            if(dontDestroyOnLoadScene != null)
+                yield return dontDestroyOnLoadScene;
 
             // If `this` is a MonoBehaviour, promote `this.gameObject` to top level to make it easier to find,
             // especially if inherited properties are hidden
@@ -66,45 +75,210 @@ namespace JetBrains.Debugger.Worker.Plugins.Unity.Evaluation
                 yield return thisGameObject.ToValue(myValueServices);
         }
 
-        private IValueReference<TValue>? GetActiveScene(IStackFrame frame)
+        private IValueEntity? GetDontDestroyOnLoadScene(IStackFrame frame)
         {
-            return myLogger.CatchEvaluatorException<TValue, IValueReference<TValue>?>(() =>
+            return myLogger.CatchEvaluatorException<TValue, IValueEntity?>(() =>
                 {
-                    var sceneManagerType = myValueServices.GetReifiedType(frame,
-                                               "UnityEngine.SceneManagement.SceneManager, UnityEngine.CoreModule")
-                                           ?? myValueServices.GetReifiedType(frame,
-                                               "UnityEngine.SceneManagement.SceneManager, UnityEngine");
+                    var editorSceneManagerType = GetEditorSceneManagerType(frame);
+                    if (editorSceneManagerType == null)
+                        return null;
+                    var getDontDestroyOnLoadMethod = GetDontDestroyOnLoadSceneMethod(editorSceneManagerType);
+                    if (getDontDestroyOnLoadMethod == null)
+                        return null;
+
+
+                    var dontDestroyOnLoadScene =
+                        editorSceneManagerType.CallStaticMethod(frame, mySession.EvaluationOptions,
+                            getDontDestroyOnLoadMethod);
+                    if (dontDestroyOnLoadScene == null)
+                    {
+                        myLogger.Warn("Unexpected response: EditorSceneManager.GetDontDestroyOnLoadScene() == null");
+                        return null;
+                    }
+
+                    return CreateValueEntity(dontDestroyOnLoadScene, "DontDestroyOnLoad Scene", editorSceneManagerType.MetadataType, frame);
+                },
+                exception =>
+                    myLogger.LogThrownUnityException(exception, frame, myValueServices, mySession.EvaluationOptions));
+        }
+        
+        private IValueEntity? GetActiveScene(IStackFrame frame)
+        {
+            return myLogger.CatchEvaluatorException<TValue, IValueEntity?>(() =>
+                {
+                    var sceneManagerType = GetSceneManagerType(frame);
                     if (sceneManagerType == null)
-                    {
-                        myLogger.Warn("Unable to get typeof(SceneManager). Not a Unity project?");
                         return null;
-                    }
 
-                    var getActiveSceneMethod = sceneManagerType.MetadataType.GetMethods()
-                        .FirstOrDefault(m => m.IsStatic && m.Parameters.Length == 0 && m.Name == "GetActiveScene");
+                    var getActiveSceneMethod = GetActiveSceneMethod(sceneManagerType);
                     if (getActiveSceneMethod == null)
-                    {
-                        myLogger.Warn("Unable to find SceneManager.GetActiveScene method");
                         return null;
-                    }
 
-                    // GetActiveScene can throw a UnityException if we call it from the wrong location, such as the
-                    // constructor of a MonoBehaviour
-                    var activeScene =
-                        sceneManagerType.CallStaticMethod(frame, mySession.EvaluationOptions, getActiveSceneMethod);
-                    if (activeScene == null)
-                    {
-                        myLogger.Warn("Unexpected response: SceneManager.GetActiveScene() == null");
-                        return null;
-                    }
+                    var getSceneCountProperty = GetSceneCountProperty(frame, sceneManagerType);
 
-                    // Don't show type presentation. We know it's a scene, the clue's in the name
-                    return new SimpleValueReference<TValue>(activeScene, sceneManagerType.MetadataType,
-                        "Active scene", ValueOriginKind.Property,
-                        ValueFlags.None | ValueFlags.IsReadOnly | ValueFlags.IsDefaultTypePresentation, frame,
-                        myValueServices.RoleFactory);
+                    var activeScene = GetActiveSceneValue(frame, sceneManagerType, getActiveSceneMethod);
+                    
+                    if (getSceneCountProperty == null)
+                        return CreateValueEntity(activeScene, "Active scene", sceneManagerType.MetadataType, frame);
+
+                    var scenesCount = getSceneCountProperty.AsPrimitive(mySession.EvaluationOptions).GetPrimitiveSafe<int>();
+
+                    var getSceneAtMethod = GetSceneAtMethod(sceneManagerType);
+
+                    if(getSceneAtMethod == null || scenesCount == null || scenesCount == 1)
+                        return CreateValueEntity(activeScene, "Active scene", sceneManagerType.MetadataType, frame);
+
+                    var simpleValueReferences = GetLoadedScenes(frame, sceneManagerType, getSceneAtMethod, scenesCount.Value, activeScene);
+                    return new SimpleEntityGroup("Loaded Scenes", simpleValueReferences, true);
+
                 },
                 exception => myLogger.LogThrownUnityException(exception, frame, myValueServices, mySession.EvaluationOptions));
+        }
+
+        private IValue<TValue> CreateValueEntity(TValue activeScene, string defaultName, IMetadataTypeLite metadataTypeLite, IStackFrame frame)
+        {
+            return new SimpleValueReference<TValue>(activeScene, metadataTypeLite,
+                defaultName, ValueOriginKind.Property,
+                ValueFlags.None | ValueFlags.IsReadOnly | ValueFlags.IsDefaultTypePresentation, frame,
+                myValueServices.RoleFactory).ToValue(myValueServices);
+        }
+
+        private List<IValueEntity> GetLoadedScenes(IStackFrame frame, IReifiedType<TValue> sceneManagerType,
+            IMetadataMethodLite getSceneAtMethod, int scenesCount, TValue activeScene)
+        {
+            var activeSceneHandle = TryGetActiveSceneHandle(activeScene);
+
+            var loadedScenes = new List<IValueEntity>();
+            for (int i = 0; i < scenesCount; i++)
+            {
+                var indexValue = myFactory.CreatePrimitive(frame, mySession.EvaluationOptions, i);
+                var sceneAtIndex =
+                    sceneManagerType.CallStaticMethod(frame,
+                        mySession.EvaluationOptions,
+                        getSceneAtMethod, indexValue);
+
+                if (sceneAtIndex == null)
+                {
+                    myLogger.Warn($"Can't get active scene by index: {i}");
+                    continue;
+                }
+
+                var defaultName = i.ToString();
+
+                var sceneAtIndexHandle = TryGetActiveSceneHandle(sceneAtIndex);
+
+                if (sceneAtIndexHandle != 0 && sceneAtIndexHandle == activeSceneHandle)
+                    defaultName += " (Active)";
+
+                loadedScenes.Add(CreateValueEntity(sceneAtIndex, defaultName, sceneManagerType.MetadataType, frame));
+            }
+
+            return loadedScenes;
+
+            int TryGetActiveSceneHandle(TValue sceneValue)
+            {
+                // Unity Scene struct has a handle field, which is a primitive int.
+                // We can use it to check if the scene is active.
+                var sceneHandle = ((sceneValue as StructMirror)?.Fields[0] as PrimitiveValue)?.Value;
+
+                if (sceneHandle is int intHandle)
+                    return intHandle;
+                
+                //handle comes from native code, 0 - means null
+                return 0;
+            }
+        }
+
+        private IMetadataMethodLite? GetDontDestroyOnLoadSceneMethod(IReifiedType<TValue>? editorSceneManagerType)
+        {
+            var getDontDestroyOnLoadSceneMethod = editorSceneManagerType?.MetadataType.GetMethods()
+                .FirstOrDefault(m => m.IsStatic && m.Parameters.Length == 0 && m.Name == "GetDontDestroyOnLoadScene");
+            
+            if(getDontDestroyOnLoadSceneMethod == null)
+                myLogger.Warn("Unable to find EditorSceneManager.GetDontDestroyOnLoadScene method");
+            
+            return getDontDestroyOnLoadSceneMethod;
+        }
+
+        private TValue GetActiveSceneValue(IStackFrame frame, IReifiedType<TValue> sceneManagerType,
+            IMetadataMethodLite getActiveSceneMethod)
+        {
+            // GetActiveScene can throw a UnityException if we call it from the wrong location, such as the
+            // constructor of a MonoBehaviour
+            var activeScene =
+                sceneManagerType.CallStaticMethod(frame, mySession.EvaluationOptions, getActiveSceneMethod);
+            if (activeScene == null)
+            {
+                myLogger.Warn("Unexpected response: SceneManager.GetActiveScene() == null");
+                return null;
+            }
+
+
+            return activeScene;
+        }
+
+        private IPropertyValueReference<TValue>? GetSceneCountProperty(IStackFrame frame,
+            IReifiedType<TValue> sceneManagerType)
+        {
+            var sceneCountProperty = sceneManagerType.GetStaticProperties(frame, p => p.Name == "sceneCount")
+                .FirstOrDefault();
+            if(sceneCountProperty is IPropertyValueReference<TValue> sceneCountPropertyReference)
+                return sceneCountPropertyReference;
+            
+            myLogger.Warn("Unablde to find SceneManager.sceneCount property");
+            return null;
+        }
+
+
+        private IMetadataMethodLite? GetSceneAtMethod(IReifiedType<TValue> sceneManagerType)
+        {
+            var getSceneAtMethod = sceneManagerType.MetadataType.GetMethods()
+                .FirstOrDefault(m => m.IsStatic && m.Parameters.Length == 1 && m.Name == "GetSceneAt");
+            if (getSceneAtMethod == null)
+            {
+                myLogger.Warn("Unable to find SceneManager.GetSceneAt method");
+            }
+
+            return getSceneAtMethod;
+        }
+
+        private IMetadataMethodLite? GetActiveSceneMethod(IReifiedType<TValue> sceneManagerType)
+        {
+            var getActiveSceneMethod = sceneManagerType.MetadataType.GetMethods()
+                .FirstOrDefault(m => m.IsStatic && m.Parameters.Length == 0 && m.Name == "GetActiveScene");
+            if (getActiveSceneMethod == null)
+            {
+                myLogger.Warn("Unable to find SceneManager.GetActiveScene method");
+            }
+
+            return getActiveSceneMethod;
+        }
+
+        private IReifiedType<TValue>? GetSceneManagerType(IStackFrame frame)
+        {
+            var sceneManagerType = myValueServices.GetReifiedType(frame,
+                                       "UnityEngine.SceneManagement.SceneManager, UnityEngine.CoreModule")
+                                   ?? myValueServices.GetReifiedType(frame,
+                                       "UnityEngine.SceneManagement.SceneManager, UnityEngine");
+            if (sceneManagerType == null)
+            {
+                myLogger.Warn("Unable to get typeof(SceneManager). Not a Unity project?");
+            }
+
+            return sceneManagerType;
+        }
+        private IReifiedType<TValue>? GetEditorSceneManagerType(IStackFrame frame)
+        {
+            var sceneManagerType = myValueServices.GetReifiedType(frame,
+                                       "UnityEditor.SceneManagement.EditorSceneManager, UnityEditor.CoreModule")
+                                   ?? myValueServices.GetReifiedType(frame,
+                                       "UnityEditor.SceneManagement.EditorSceneManager, UnityEditor");
+            if (sceneManagerType == null)
+            {
+                myLogger.Warn("Unable to get typeof(EditorSceneManager). Not a Unity project?");
+            }
+
+            return sceneManagerType;
         }
 
         private IValueReference<TValue>? GetThisGameObjectForMonoBehaviour(IStackFrame frame)
