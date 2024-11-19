bd67e3ef27055dd0c2886f3fda7a964b7f4c0e57
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2024-11-19 11:16:01 +0100

    fix RIDER-113037 Unity log view does not respect zoom setting
    (cherry picked from commit 09ae8f332a4e911413d1d91f0c317d6270103463)
    
    IJ-CR-149637
    
    GitOrigin-RevId: 589809def92341603ea0c86e7c7c1506a0c85f2f

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/toolWindow/log/UnityLogPanelEventList.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/toolWindow/log/UnityLogPanelEventList.kt
@@ -1,29 +1,17 @@
 package com.jetbrains.rider.plugins.unity.toolWindow.log
 
-import com.intellij.application.subscribe
 import com.intellij.ide.CopyProvider
-import com.intellij.ide.ui.UISettings
-import com.intellij.ide.ui.UISettingsListener
-import com.intellij.openapi.actionSystem.ActionUpdateThread
-import com.intellij.openapi.actionSystem.DataContext
-import com.intellij.openapi.actionSystem.DataSink
-import com.intellij.openapi.actionSystem.PlatformDataKeys
-import com.intellij.openapi.actionSystem.UiDataProvider
-import com.intellij.openapi.editor.colors.EditorColorsListener
-import com.intellij.openapi.editor.colors.EditorColorsManager
+import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.fileEditor.OpenFileDescriptor
 import com.intellij.openapi.ide.CopyPasteManager
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.rd.createNestedDisposable
 import com.intellij.pom.Navigatable
 import com.intellij.ui.TreeUIHelper
 import com.intellij.ui.components.JBList
-import com.intellij.util.ui.UIUtil
 import com.jetbrains.rd.util.lifetime.Lifetime
 import com.jetbrains.rdclient.util.idea.toVirtualFile
 import com.jetbrains.rider.plugins.unity.UnityBundle
 import com.jetbrains.rider.projectView.solutionDirectory
-import java.awt.Font
 import java.awt.datatransfer.StringSelection
 import javax.swing.DefaultListModel
 import javax.swing.ListSelectionModel
@@ -38,23 +26,6 @@ class UnityLogPanelEventList(lifetime: Lifetime) : JBList<LogPanelItem>(emptyLis
         selectionMode = ListSelectionModel.MULTIPLE_INTERVAL_SELECTION
         emptyText.text = UnityBundle.message("log.is.empty")
         TreeUIHelper.getInstance().installListSpeedSearch(this)
-
-        val disposable = lifetime.createNestedDisposable()
-        EditorColorsManager.TOPIC.subscribe(disposable, EditorColorsListener { updateFont() })
-        UISettingsListener.TOPIC.subscribe(disposable, UISettingsListener { updateFont() })
-
-        updateFont()
-    }
-
-    private fun updateFont() {
-        val sc = EditorColorsManager.getInstance().globalScheme
-        font = if (UISettings.getInstance().presentationMode) {
-            Font(sc.consoleFontName, Font.PLAIN, UISettings.getInstance().presentationModeFontSize)
-        }
-        else {
-            Font(sc.consoleFontName, Font.PLAIN, sc.consoleFontSize)
-        }
-        emptyText.setFont(UIUtil.getLabelFont())
     }
 
     fun getNavigatableForSelected(project: Project): Navigatable? {
