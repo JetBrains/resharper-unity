0b658778be3d3a9c93ff9fe7d9da74cd94f8051f
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2024-11-27 09:39:35 +0100

    renew obsolete override in SaveAllActionListener
    (cherry picked from commit 040ad19b8aeb4cc9481745e6651053a4301b71bf)
    
    IJ-CR-150305
    
    GitOrigin-RevId: 0d1bf2437ed435cfd3c14da877c2b2ee5f46317a

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/SaveAllActionListener.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/SaveAllActionListener.kt
@@ -4,6 +4,7 @@ import com.intellij.ide.actions.SaveAllAction
 import com.intellij.ide.actions.SaveDocumentAction
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.AnActionResult
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.actionSystem.ex.AnActionListener
 import com.jetbrains.rider.plugins.unity.model.frontendBackend.frontendBackendModel
@@ -11,10 +12,11 @@ import com.jetbrains.rider.projectView.solution
 import com.jetbrains.rider.util.idea.Project
 
 class SaveAllActionListener : AnActionListener {
-    override fun afterActionPerformed(action: AnAction, dataContext: DataContext, event: AnActionEvent) {
+    override fun afterActionPerformed(action: AnAction, event: AnActionEvent, result: AnActionResult) {
         if (action !is SaveAllAction && action !is SaveDocumentAction) return
-        val project = dataContext.Project ?: return
+        val project = event.project ?: return
 
         project.solution.frontendBackendModel.refresh.fire(false)
+        super.afterActionPerformed(action, event, result)
     }
 }
