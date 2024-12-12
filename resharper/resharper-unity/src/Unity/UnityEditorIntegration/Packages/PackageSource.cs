5a8f1fedee06073e87a4e9f22dff63e5022d04e1
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2024-12-12 22:45:14 +0100

    use new feature of UPM 9 to simplify resolve of Unity packages and fix built-in packages with UPM9
    (cherry picked from commit 140f7fb153423e082d8f9e57cadc9b5af8759634)
    
    IJ-CR-151509
    
    GitOrigin-RevId: f10aae01b8e0261d3e9727c5c8f4db774d0ba037

--- resharper/resharper-unity/src/Unity/UnityEditorIntegration/Packages/PackageSource.cs
+++ resharper/resharper-unity/src/Unity/UnityEditorIntegration/Packages/PackageSource.cs
@@ -10,4 +10,22 @@ namespace JetBrains.ReSharper.Plugins.Unity.UnityEditorIntegration.Packages
         LocalTarball,
         Git
     }
+
+    public static class PackageSourceExtensions
+    {
+        public static PackageSource ToPackageSource(string source)
+        {
+            if (string.IsNullOrEmpty(source)) return PackageSource.Unknown;
+            return source switch
+            {
+                "embedded" => PackageSource.Embedded,
+                "registry" => PackageSource.Registry,
+                "builtin" => PackageSource.BuiltIn,
+                "git" => PackageSource.Git,
+                "local" => PackageSource.Local,
+                "local-tarball" => PackageSource.LocalTarball,
+                _ => PackageSource.Unknown
+            };
+        }
+    }
 }
