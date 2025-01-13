9082b02396a9b72e40cf57f7cdb3ef9829886202
Nikita Popov <nikita.popov@jetbrains.com>
2025-01-13 14:45:44 +0100

    [dotnet] mark ShaderLabSupport as DemandAnyThreadSafe
    GitOrigin-RevId: a20750bd08264c14841ec94fdd83d6d363c5be4b

--- resharper/resharper-unity/src/Unity.Shaders/ShaderLab/Settings/ShaderLabSupport.cs
+++ resharper/resharper-unity/src/Unity.Shaders/ShaderLab/Settings/ShaderLabSupport.cs
@@ -1,24 +1,25 @@
 using JetBrains.Application;
 using JetBrains.Application.Parts;
 using JetBrains.Application.Settings;
+using JetBrains.Application.Threading;
 using JetBrains.DataFlow;
 using JetBrains.Lifetimes;
 using JetBrains.ReSharper.Plugins.Unity.Core.Application.Settings;
 
 namespace JetBrains.ReSharper.Plugins.Unity.Shaders.ShaderLab.Settings
 {
-    [ShellComponent(InstantiationEx.LegacyDefault)]
+    [ShellComponent(Instantiation.DemandAnyThreadSafe)]
     public class ShaderLabSupport
     {
         public IProperty<bool> IsParsingEnabled { get; }
 
-        public ShaderLabSupport(Lifetime lifetime, ISettingsStore settingsStore)
+        public ShaderLabSupport(Lifetime lifetime, ISettingsStore settingsStore, IThreading threading)
         {
             // We can't use IApplicationWideContextBoundSettingsStore here because this a ShellComponent, because it's used
             // in ShaderLabProjectFileLanguageService
             // Keep a live context so that we'll get new mount points, e.g. Solution
             IsParsingEnabled = settingsStore.BindToContextLive(lifetime, ContextRange.ApplicationWide)
-                .GetValueProperty(lifetime, (UnitySettings s) => s.EnableShaderLabParsing);
+                .GetValueProperty2(lifetime, (UnitySettings s) => s.EnableShaderLabParsing,ApartmentForNotifications.Primary(threading));
         }
     }
 }
