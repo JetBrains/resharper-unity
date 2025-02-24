b83cb3969db02f2a48d7cd654c3acac8c36f69af
Denis Korneev <denis.korneev@jetbrains.com>
2025-02-24 18:23:36 +0100

    [R#] Helper to check whether Visual Studio Tools for Unity is installed
    (cherry picked from commit 494c860f2777c84d4190f2b791fb1b7563475024)
    (cherry picked from commit 14c0c39d339c88de8d032446f172cc1c4eeca286)
    
    GitOrigin-RevId: dde228e9c8c2d12ea66bb0114d70b27d4cbd65ff

---
+++ resharper/resharper-unity-visual-studio-common/src/Utils/VsUnityWorkloadDetector.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using JetBrains.Util;
+using JetBrains.Util.DevEnv;
+
+namespace JetBrains.ReSharper.Plugins.Unity.VisualStudio.Utils
+{
+    public abstract class VsUnityWorkloadDetector
+    {
+        // https://learn.microsoft.com/en-us/visualstudio/gamedev/unity/get-started/getting-started-with-visual-studio-tools-for-unity?pivots=windows
+        // Workload marketing name: "Game development with Unity".
+        private const string VsUnityWorkloadName = "Microsoft.VisualStudio.Workload.ManagedGame";
+
+        public static bool IsUnityWorkloadInstalled()
+        {
+            var instance = DevenvHostDiscovery.TryGetCurrentInstanceSinceVs15(DevenvHostDiscovery.EnumInstalledVsFlags.AddInstalledPackages, OnError.Ignore);
+            if (instance == null || instance.PackagesIfKnown == null)
+                return false;
+
+            return instance.PackagesIfKnown.Any(pkg => pkg.Type == DevenvHostDiscovery.InstalledVsPackage.WellKnownTypes.Workload && pkg.Id == VsUnityWorkloadName);
+        }
+    }
+}
