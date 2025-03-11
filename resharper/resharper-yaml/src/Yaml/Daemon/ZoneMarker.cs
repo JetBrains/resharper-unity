07034c6624e05332be8536d7bdfac299651b53a8
Denis Korneev <denis.korneev@jetbrains.com>
2025-03-11 18:33:46 +0100

    [R#] When DaemonZone is not activated (e.g. there is no R# license) Yaml and Json daemon stages should be turned off, but Qodana daemon stage should be able to work
    (cherry picked from commit 58bca23cd2c61d636fffe7cb339c2a7333a42cf2)
    
    
    (cherry picked from commit e362ed38df91770eef57871a70be672fd311940c)
    
    IJ-MR-157795
    
    GitOrigin-RevId: 23590652d8c18f1b07cbf175925ab8f5ddfed21a

---
+++ resharper/resharper-yaml/src/Yaml/Daemon/ZoneMarker.cs
@@ -0,0 +1,7 @@
+﻿using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.Feature.Services.Daemon;
+
+namespace JetBrains.ReSharper.Plugins.Yaml.Daemon;
+
+[ZoneMarker]
+public class ZoneMarker : IRequire<DaemonZone>;
