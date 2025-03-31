b8e427b44e9b8639717655f2e78335d99feeb8df
george.lemeshko <heorhii.lemeshko@jetbrains.com>
2025-03-31 17:12:29 +0200

    [rider][unity] Add Unity settings opening on what's-new action (fix RIDER-123126)
    (cherry picked from commit f1a5b430e626625638069a45e8beec27e67a6a5a)
    
    NET-CR-16397
    
    GitOrigin-RevId: eee52d8254662fb59f6cb02ddbc553e9fa92a2cf

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/actions/EnableAutoProfilerSnapshotFetchingSettings.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/actions/EnableAutoProfilerSnapshotFetchingSettings.kt
@@ -1,13 +1,20 @@
 package com.jetbrains.rider.plugins.unity.actions
 
+import com.intellij.ide.actions.ShowSettingsUtilImpl
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareAction
 import com.jetbrains.rider.plugins.unity.model.frontendBackend.frontendBackendModel
+import com.jetbrains.rider.plugins.unity.ui.UnityUIBundle
+import com.jetbrains.rider.plugins.unity.ui.profilerIntegration.unitySettingsPageId
 import com.jetbrains.rider.projectView.solution
 
 class EnableAutoProfilerSnapshotFetchingSettings : DumbAwareAction() {
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
         project.solution.frontendBackendModel.unityApplicationSettings.profilerSnapshotFetchingSettings.fire(1) // 1 - stands for autoFetch
+
+        ShowSettingsUtilImpl.showSettingsDialog(project,
+                                                unitySettingsPageId,
+                                                UnityUIBundle.message("unity.profiler.integration.widget.setting.filter"))
     }
 }
