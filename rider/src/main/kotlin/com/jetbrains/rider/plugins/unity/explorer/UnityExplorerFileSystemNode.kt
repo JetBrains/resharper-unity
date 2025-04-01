53dd8b0bb9264fd15a787a35d5fced45df2f5d4c
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2025-04-01 11:34:28 +0200

    Revert "fix RIDER-123607 Text lag while typing"
    This reverts commit 96d434644df19f9646887a8c5693cbb78eca76c7.
    
    
    (cherry picked from commit 13845254de5a8dfd6928e379831a5b39dff163e8)
    
    IJ-CR-159013
    
    GitOrigin-RevId: 7de75b50c0f88bcf135e1871de374eec911a5b0d

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/explorer/UnityExplorerFileSystemNode.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/explorer/UnityExplorerFileSystemNode.kt
@@ -85,7 +85,7 @@ open class UnityExplorerFileSystemNode(project: Project,
             .toList()
 
     public override fun hasProblemFileBeneath(): Boolean {
-        return Registry.`is`("unityExplorer.showHierarchyErrors") && entities.any {
+        return Registry.`is`("projectView.showHierarchyErrors") && entities.any {
             WorkspaceEntityErrorsSupport.getInstance(myProject).hasErrors(it)
         }
     }
