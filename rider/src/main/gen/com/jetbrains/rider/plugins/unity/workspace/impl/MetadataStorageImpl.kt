8b89d0e98f651fd560022a70a9f32c5c170a2690
Kirill Bochkarev <kirill.bochkarev@jetbrains.com>
2025-05-08 04:51:59 +0000

    [workspace model] IJPL-895 Stabilization of entity generation in tests
    - Regenerate Workspace Model code for the whole IntelliJ
    - IJPL-177108 change properties order in ModuleSettingsFacetBridgeEntity (API breakage for BSP)
    - IJPL-14740 Run the test for the whole module, not for each package in the module
    - do not include entity source inheritors from other modules
    - generator tests: Refactor IntelliJ generation test, fix missing libraries for various modules, ignore workspace.storage.testEntities, add code generator tests for packages, open classes
    - treat open classes as abstract in K2, fix package for kotlin.ByteArray in K2 MetadataBuilder
    - sort enum entries in metadata and fix cache test
    - fix and move WorkspaceModelJsonDumpTest
    - fix IntelliJ Workspace Code Update action (use K1)
    - do not exclude node_modules dir in javascript plugin in tests
    
    
    Merge-request: IJ-MR-155397
    Merged-by: Kirill Bochkarev <kirill.bochkarev@jetbrains.com>
    
    GitOrigin-RevId: 861f59ad002a63d493824318dff73d5483c31e60

--- rider/src/main/gen/com/jetbrains/rider/plugins/unity/workspace/impl/MetadataStorageImpl.kt
+++ rider/src/main/gen/com/jetbrains/rider/plugins/unity/workspace/impl/MetadataStorageImpl.kt
@@ -12,49 +12,42 @@ import com.intellij.platform.workspace.storage.metadata.model.ValueTypeMetadata
 internal object MetadataStorageImpl: MetadataStorageBase() {
     override fun initializeMetadata() {
         val primitiveTypeStringNotNullable = ValueTypeMetadata.SimpleType.PrimitiveType(isNullable = false, type = "String")
-        val primitiveTypeStringNullable = ValueTypeMetadata.SimpleType.PrimitiveType(isNullable = true, type = "String")
         val primitiveTypeListNotNullable = ValueTypeMetadata.SimpleType.PrimitiveType(isNullable = false, type = "List")
-        
+        val primitiveTypeStringNullable = ValueTypeMetadata.SimpleType.PrimitiveType(isNullable = true, type = "String")
+
         var typeMetadata: StorageTypeMetadata
-        
-        typeMetadata = FinalClassMetadata.ObjectMetadata(fqName = "com.jetbrains.rider.plugins.unity.workspace.UnityWorkspacePackageUpdater\$RiderUnityPackageEntitySource", properties = listOf(OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "virtualFileUrl", valueType = ValueTypeMetadata.SimpleType.CustomType(isNullable = true, typeMetadata = FinalClassMetadata.KnownClass(fqName = "com.intellij.platform.workspace.storage.url.VirtualFileUrl")), withDefault = false)), supertypes = listOf("com.jetbrains.rider.projectView.workspace.RiderEntitySource",
-"com.intellij.platform.workspace.storage.EntitySource"))
-        
-        addMetadata(typeMetadata)
-        
+
         typeMetadata = EntityMetadata(fqName = "com.jetbrains.rider.plugins.unity.workspace.UnityPackageEntity", entityDataFqName = "com.jetbrains.rider.plugins.unity.workspace.impl.UnityPackageEntityData", supertypes = listOf("com.intellij.platform.workspace.storage.WorkspaceEntity"), properties = listOf(OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "entitySource", valueType = ValueTypeMetadata.SimpleType.CustomType(isNullable = false, typeMetadata = FinalClassMetadata.KnownClass(fqName = "com.intellij.platform.workspace.storage.EntitySource")), withDefault = false),
-OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "descriptor", valueType = ValueTypeMetadata.SimpleType.CustomType(isNullable = false, typeMetadata = FinalClassMetadata.ClassMetadata(fqName = "com.jetbrains.rider.plugins.unity.model.frontendBackend.UnityPackage", properties = listOf(OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "id", valueType = primitiveTypeStringNotNullable, withDefault = false),
-OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "version", valueType = primitiveTypeStringNotNullable, withDefault = false),
+OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "descriptor", valueType = ValueTypeMetadata.SimpleType.CustomType(isNullable = false, typeMetadata = FinalClassMetadata.ClassMetadata(fqName = "com.jetbrains.rider.plugins.unity.model.frontendBackend.UnityPackage", properties = listOf(OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "dependencies", valueType = ValueTypeMetadata.ParameterizedType(generics = listOf(ValueTypeMetadata.SimpleType.CustomType(isNullable = false, typeMetadata = FinalClassMetadata.ClassMetadata(fqName = "com.jetbrains.rider.plugins.unity.model.frontendBackend.UnityPackageDependency", properties = listOf(OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "id", valueType = primitiveTypeStringNotNullable, withDefault = false),
+OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "version", valueType = primitiveTypeStringNotNullable, withDefault = false)), supertypes = listOf("com.jetbrains.rd.util.string.IPrintable")))), primitive = primitiveTypeListNotNullable), withDefault = false),
+OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "description", valueType = primitiveTypeStringNullable, withDefault = false),
+OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "displayName", valueType = primitiveTypeStringNotNullable, withDefault = false),
+OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "gitDetails", valueType = ValueTypeMetadata.SimpleType.CustomType(isNullable = true, typeMetadata = FinalClassMetadata.ClassMetadata(fqName = "com.jetbrains.rider.plugins.unity.model.frontendBackend.UnityGitDetails", properties = listOf(OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "hash", valueType = primitiveTypeStringNullable, withDefault = false),
+OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "revision", valueType = primitiveTypeStringNullable, withDefault = false),
+OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "url", valueType = primitiveTypeStringNotNullable, withDefault = false)), supertypes = listOf("com.jetbrains.rd.util.string.IPrintable"))), withDefault = false),
+OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "id", valueType = primitiveTypeStringNotNullable, withDefault = false),
 OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "packageFolderPath", valueType = primitiveTypeStringNullable, withDefault = false),
-OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "source", valueType = ValueTypeMetadata.SimpleType.CustomType(isNullable = false, typeMetadata = FinalClassMetadata.EnumClassMetadata(fqName = "com.jetbrains.rider.plugins.unity.model.frontendBackend.UnityPackageSource", properties = listOf(), supertypes = listOf("kotlin.Enum",
+OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "source", valueType = ValueTypeMetadata.SimpleType.CustomType(isNullable = false, typeMetadata = FinalClassMetadata.EnumClassMetadata(fqName = "com.jetbrains.rider.plugins.unity.model.frontendBackend.UnityPackageSource", properties = listOf(), supertypes = listOf("java.io.Serializable",
 "kotlin.Comparable",
-"java.io.Serializable"), values = listOf("Unknown",
-"BuiltIn",
-"Registry",
+"kotlin.Enum"), values = listOf("BuiltIn",
 "Embedded",
+"Git",
 "Local",
 "LocalTarball",
-"Git"))), withDefault = false),
-OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "displayName", valueType = primitiveTypeStringNotNullable, withDefault = false),
-OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "description", valueType = primitiveTypeStringNullable, withDefault = false),
-OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "dependencies", valueType = ValueTypeMetadata.ParameterizedType(generics = listOf(ValueTypeMetadata.SimpleType.CustomType(isNullable = false, typeMetadata = FinalClassMetadata.ClassMetadata(fqName = "com.jetbrains.rider.plugins.unity.model.frontendBackend.UnityPackageDependency", properties = listOf(OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "id", valueType = primitiveTypeStringNotNullable, withDefault = false),
-OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "version", valueType = primitiveTypeStringNotNullable, withDefault = false)), supertypes = listOf("com.jetbrains.rd.util.string.IPrintable")))), primitive = primitiveTypeListNotNullable), withDefault = false),
+"Registry",
+"Unknown"))), withDefault = false),
 OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "tarballLocation", valueType = primitiveTypeStringNullable, withDefault = false),
-OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "gitDetails", valueType = ValueTypeMetadata.SimpleType.CustomType(isNullable = true, typeMetadata = FinalClassMetadata.ClassMetadata(fqName = "com.jetbrains.rider.plugins.unity.model.frontendBackend.UnityGitDetails", properties = listOf(OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "url", valueType = primitiveTypeStringNotNullable, withDefault = false),
-OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "hash", valueType = primitiveTypeStringNullable, withDefault = false),
-OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "revision", valueType = primitiveTypeStringNullable, withDefault = false)), supertypes = listOf("com.jetbrains.rd.util.string.IPrintable"))), withDefault = false)), supertypes = listOf("com.jetbrains.rd.util.string.IPrintable"))), withDefault = false)), extProperties = listOf(), isAbstract = false)
-        
+OwnPropertyMetadata(isComputable = false, isKey = false, isOpen = false, name = "version", valueType = primitiveTypeStringNotNullable, withDefault = false)), supertypes = listOf("com.jetbrains.rd.util.string.IPrintable"))), withDefault = false)), extProperties = listOf(), isAbstract = false)
+
         addMetadata(typeMetadata)
     }
 
     override fun initializeMetadataHash() {
-        addMetadataHash(typeFqn = "com.jetbrains.rider.plugins.unity.workspace.UnityPackageEntity", metadataHash = -165100314)
-        addMetadataHash(typeFqn = "com.jetbrains.rider.plugins.unity.model.frontendBackend.UnityPackage", metadataHash = 1875418550)
-        addMetadataHash(typeFqn = "com.jetbrains.rider.plugins.unity.model.frontendBackend.UnityPackageSource", metadataHash = -217023979)
+        addMetadataHash(typeFqn = "com.jetbrains.rider.plugins.unity.workspace.UnityPackageEntity", metadataHash = 277233226)
+        addMetadataHash(typeFqn = "com.jetbrains.rider.plugins.unity.model.frontendBackend.UnityPackage", metadataHash = 363072722)
         addMetadataHash(typeFqn = "com.jetbrains.rider.plugins.unity.model.frontendBackend.UnityPackageDependency", metadataHash = 333232422)
-        addMetadataHash(typeFqn = "com.jetbrains.rider.plugins.unity.model.frontendBackend.UnityGitDetails", metadataHash = -55238709)
-        addMetadataHash(typeFqn = "com.intellij.platform.workspace.storage.EntitySource", metadataHash = -1030321237)
-        addMetadataHash(typeFqn = "com.jetbrains.rider.plugins.unity.workspace.UnityWorkspacePackageUpdater\$RiderUnityPackageEntitySource", metadataHash = -1748851553)
+        addMetadataHash(typeFqn = "com.jetbrains.rider.plugins.unity.model.frontendBackend.UnityGitDetails", metadataHash = -1563565493)
+        addMetadataHash(typeFqn = "com.jetbrains.rider.plugins.unity.model.frontendBackend.UnityPackageSource", metadataHash = 429756747)
     }
 
 }
--- rider/src/main/gen/com/jetbrains/rider/plugins/unity/workspace/impl/UnityPackageEntityImpl.kt
+++ rider/src/main/gen/com/jetbrains/rider/plugins/unity/workspace/impl/UnityPackageEntityImpl.kt
@@ -18,178 +18,178 @@ import com.jetbrains.rider.plugins.unity.workspace.UnityPackageEntity
 @GeneratedCodeApiVersion(3)
 @GeneratedCodeImplVersion(6)
 @OptIn(WorkspaceEntityInternalApi::class)
-internal class UnityPackageEntityImpl(private val dataSource: UnityPackageEntityData) : UnityPackageEntity, WorkspaceEntityBase(
-    dataSource) {
+internal class UnityPackageEntityImpl(private val dataSource: UnityPackageEntityData) : UnityPackageEntity,
+                                                                                        WorkspaceEntityBase(dataSource) {
 
-    private companion object {
+  private companion object {
 
 
-        private val connections = listOf<ConnectionId>(
-        )
+    private val connections = listOf<ConnectionId>(
+    )
 
+  }
+
+  override val descriptor: UnityPackage
+    get() {
+      readField("descriptor")
+      return dataSource.descriptor
     }
 
-    override val descriptor: UnityPackage
-        get() {
-            readField("descriptor")
-            return dataSource.descriptor
-        }
+  override val entitySource: EntitySource
+    get() {
+      readField("entitySource")
+      return dataSource.entitySource
+    }
 
-    override val entitySource: EntitySource
-        get() {
-            readField("entitySource")
-            return dataSource.entitySource
+  override fun connectionIdList(): List<ConnectionId> {
+    return connections
+  }
+
+
+  internal class Builder(result: UnityPackageEntityData?) :
+    ModifiableWorkspaceEntityBase<UnityPackageEntity, UnityPackageEntityData>(result), UnityPackageEntity.Builder {
+    internal constructor() : this(UnityPackageEntityData())
+
+    override fun applyToBuilder(builder: MutableEntityStorage) {
+      if (this.diff != null) {
+        if (existsInBuilder(builder)) {
+          this.diff = builder
+          return
         }
+        else {
+          error("Entity UnityPackageEntity is already created in a different builder")
+        }
+      }
+
+      this.diff = builder
+      addToBuilder()
+      this.id = getEntityData().createEntityId()
+      // After adding entity data to the builder, we need to unbind it and move the control over entity data to builder
+      // Builder may switch to snapshot at any moment and lock entity data to modification
+      this.currentEntityData = null
+
+      // Process linked entities that are connected without a builder
+      processLinkedEntities(builder)
+      checkInitialization() // TODO uncomment and check failed tests
+    }
+
+    private fun checkInitialization() {
+      val _diff = diff
+      if (!getEntityData().isEntitySourceInitialized()) {
+        error("Field WorkspaceEntity#entitySource should be initialized")
+      }
+      if (!getEntityData().isDescriptorInitialized()) {
+        error("Field UnityPackageEntity#descriptor should be initialized")
+      }
+    }
 
     override fun connectionIdList(): List<ConnectionId> {
-        return connections
+      return connections
+    }
+
+    // Relabeling code, move information from dataSource to this builder
+    override fun relabel(dataSource: WorkspaceEntity, parents: Set<WorkspaceEntity>?) {
+      dataSource as UnityPackageEntity
+      if (this.entitySource != dataSource.entitySource) this.entitySource = dataSource.entitySource
+      if (this.descriptor != dataSource.descriptor) this.descriptor = dataSource.descriptor
+      updateChildToParentReferences(parents)
     }
 
 
-    internal class Builder(result: UnityPackageEntityData?) : ModifiableWorkspaceEntityBase<UnityPackageEntity, UnityPackageEntityData>(
-        result), UnityPackageEntity.Builder {
-        internal constructor() : this(UnityPackageEntityData())
+    override var entitySource: EntitySource
+      get() = getEntityData().entitySource
+      set(value) {
+        checkModificationAllowed()
+        getEntityData(true).entitySource = value
+        changedProperty.add("entitySource")
 
-        override fun applyToBuilder(builder: MutableEntityStorage) {
-            if (this.diff != null) {
-                if (existsInBuilder(builder)) {
-                    this.diff = builder
-                    return
-                }
-                else {
-                    error("Entity UnityPackageEntity is already created in a different builder")
-                }
-            }
+      }
 
-            this.diff = builder
-            addToBuilder()
-            this.id = getEntityData().createEntityId()
-            // After adding entity data to the builder, we need to unbind it and move the control over entity data to builder
-            // Builder may switch to snapshot at any moment and lock entity data to modification
-            this.currentEntityData = null
+    override var descriptor: UnityPackage
+      get() = getEntityData().descriptor
+      set(value) {
+        checkModificationAllowed()
+        getEntityData(true).descriptor = value
+        changedProperty.add("descriptor")
 
-            // Process linked entities that are connected without a builder
-            processLinkedEntities(builder)
-            checkInitialization() // TODO uncomment and check failed tests
-        }
+      }
 
-        private fun checkInitialization() {
-            val _diff = diff
-            if (!getEntityData().isEntitySourceInitialized()) {
-                error("Field WorkspaceEntity#entitySource should be initialized")
-            }
-            if (!getEntityData().isDescriptorInitialized()) {
-                error("Field UnityPackageEntity#descriptor should be initialized")
-            }
-        }
-
-        override fun connectionIdList(): List<ConnectionId> {
-            return connections
-        }
-
-        // Relabeling code, move information from dataSource to this builder
-        override fun relabel(dataSource: WorkspaceEntity, parents: Set<WorkspaceEntity>?) {
-            dataSource as UnityPackageEntity
-            if (this.entitySource != dataSource.entitySource) this.entitySource = dataSource.entitySource
-            if (this.descriptor != dataSource.descriptor) this.descriptor = dataSource.descriptor
-            updateChildToParentReferences(parents)
-        }
-
-
-        override var entitySource: EntitySource
-            get() = getEntityData().entitySource
-            set(value) {
-                checkModificationAllowed()
-                getEntityData(true).entitySource = value
-                changedProperty.add("entitySource")
-
-            }
-
-        override var descriptor: UnityPackage
-            get() = getEntityData().descriptor
-            set(value) {
-                checkModificationAllowed()
-                getEntityData(true).descriptor = value
-                changedProperty.add("descriptor")
-
-            }
-
-        override fun getEntityClass(): Class<UnityPackageEntity> = UnityPackageEntity::class.java
-    }
+    override fun getEntityClass(): Class<UnityPackageEntity> = UnityPackageEntity::class.java
+  }
 }
 
 @OptIn(WorkspaceEntityInternalApi::class)
 internal class UnityPackageEntityData : WorkspaceEntityData<UnityPackageEntity>() {
-    lateinit var descriptor: UnityPackage
+  lateinit var descriptor: UnityPackage
 
-    internal fun isDescriptorInitialized(): Boolean = ::descriptor.isInitialized
+  internal fun isDescriptorInitialized(): Boolean = ::descriptor.isInitialized
 
-    override fun wrapAsModifiable(diff: MutableEntityStorage): WorkspaceEntity.Builder<UnityPackageEntity> {
-        val modifiable = UnityPackageEntityImpl.Builder(null)
-        modifiable.diff = diff
-        modifiable.id = createEntityId()
-        return modifiable
+  override fun wrapAsModifiable(diff: MutableEntityStorage): WorkspaceEntity.Builder<UnityPackageEntity> {
+    val modifiable = UnityPackageEntityImpl.Builder(null)
+    modifiable.diff = diff
+    modifiable.id = createEntityId()
+    return modifiable
+  }
+
+  @OptIn(EntityStorageInstrumentationApi::class)
+  override fun createEntity(snapshot: EntityStorageInstrumentation): UnityPackageEntity {
+    val entityId = createEntityId()
+    return snapshot.initializeEntity(entityId) {
+      val entity = UnityPackageEntityImpl(this)
+      entity.snapshot = snapshot
+      entity.id = entityId
+      entity
     }
+  }
 
-    @OptIn(EntityStorageInstrumentationApi::class)
-    override fun createEntity(snapshot: EntityStorageInstrumentation): UnityPackageEntity {
-        val entityId = createEntityId()
-        return snapshot.initializeEntity(entityId) {
-            val entity = UnityPackageEntityImpl(this)
-            entity.snapshot = snapshot
-            entity.id = entityId
-            entity
-        }
+  override fun getMetadata(): EntityMetadata {
+    return MetadataStorageImpl.getMetadataByTypeFqn("com.jetbrains.rider.plugins.unity.workspace.UnityPackageEntity") as EntityMetadata
+  }
+
+  override fun getEntityInterface(): Class<out WorkspaceEntity> {
+    return UnityPackageEntity::class.java
+  }
+
+  override fun createDetachedEntity(parents: List<WorkspaceEntity.Builder<*>>): WorkspaceEntity.Builder<*> {
+    return UnityPackageEntity(descriptor, entitySource) {
     }
+  }
 
-    override fun getMetadata(): EntityMetadata {
-        return MetadataStorageImpl.getMetadataByTypeFqn("com.jetbrains.rider.plugins.unity.workspace.UnityPackageEntity") as EntityMetadata
-    }
+  override fun getRequiredParents(): List<Class<out WorkspaceEntity>> {
+    val res = mutableListOf<Class<out WorkspaceEntity>>()
+    return res
+  }
 
-    override fun getEntityInterface(): Class<out WorkspaceEntity> {
-        return UnityPackageEntity::class.java
-    }
+  override fun equals(other: Any?): Boolean {
+    if (other == null) return false
+    if (this.javaClass != other.javaClass) return false
 
-    override fun createDetachedEntity(parents: List<WorkspaceEntity.Builder<*>>): WorkspaceEntity.Builder<*> {
-        return UnityPackageEntity(descriptor, entitySource) {
-        }
-    }
+    other as UnityPackageEntityData
 
-    override fun getRequiredParents(): List<Class<out WorkspaceEntity>> {
-        val res = mutableListOf<Class<out WorkspaceEntity>>()
-        return res
-    }
+    if (this.entitySource != other.entitySource) return false
+    if (this.descriptor != other.descriptor) return false
+    return true
+  }
 
-    override fun equals(other: Any?): Boolean {
-        if (other == null) return false
-        if (this.javaClass != other.javaClass) return false
+  override fun equalsIgnoringEntitySource(other: Any?): Boolean {
+    if (other == null) return false
+    if (this.javaClass != other.javaClass) return false
 
-        other as UnityPackageEntityData
+    other as UnityPackageEntityData
 
-        if (this.entitySource != other.entitySource) return false
-        if (this.descriptor != other.descriptor) return false
-        return true
-    }
+    if (this.descriptor != other.descriptor) return false
+    return true
+  }
 
-    override fun equalsIgnoringEntitySource(other: Any?): Boolean {
-        if (other == null) return false
-        if (this.javaClass != other.javaClass) return false
+  override fun hashCode(): Int {
+    var result = entitySource.hashCode()
+    result = 31 * result + descriptor.hashCode()
+    return result
+  }
 
-        other as UnityPackageEntityData
-
-        if (this.descriptor != other.descriptor) return false
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = entitySource.hashCode()
-        result = 31 * result + descriptor.hashCode()
-        return result
-    }
-
-    override fun hashCodeIgnoringEntitySource(): Int {
-        var result = javaClass.hashCode()
-        result = 31 * result + descriptor.hashCode()
-        return result
-    }
+  override fun hashCodeIgnoringEntitySource(): Int {
+    var result = javaClass.hashCode()
+    result = 31 * result + descriptor.hashCode()
+    return result
+  }
 }
