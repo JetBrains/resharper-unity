42c297cb36bc1f2d7c9a0b187274bb61cca5d1f9
Ruslan Isakiev <ruslan.isakiev@jetbrains.com>
2025-06-18 09:05:59 +0200

    [dotCover] update UnityRiderUnitTestCoverageAvailabilityChecker in context of DCVR-13111
    GitOrigin-RevId: 2415a1cbfc387ef4286b17f5521ce1482d30556c

--- resharper/resharper-unity/src/Unity.Rider/Integration/Core/Feature/UnitTesting/UnityRiderUnitTestCoverageAvailabilityChecker.cs
+++ resharper/resharper-unity/src/Unity.Rider/Integration/Core/Feature/UnitTesting/UnityRiderUnitTestCoverageAvailabilityChecker.cs
@@ -1,48 +1,23 @@
-using System;
 using JetBrains.Application;
 using JetBrains.Application.Components;
 using JetBrains.Application.Parts;
-using JetBrains.ReSharper.Feature.Services.Protocol;
 using JetBrains.ReSharper.Plugins.Unity.Core.ProjectModel;
-using JetBrains.ReSharper.Plugins.Unity.UnityEditorIntegration;
 using JetBrains.ReSharper.UnitTestFramework.Elements;
 using JetBrains.ReSharper.UnitTestFramework.Execution.Hosting;
 using JetBrains.Rider.Backend.Features.UnitTesting;
-using JetBrains.Rider.Model.Unity.FrontendBackend;
 
-namespace JetBrains.ReSharper.Plugins.Unity.Rider.Integration.Core.Feature.UnitTesting
+namespace JetBrains.ReSharper.Plugins.Unity.Rider.Integration.Core.Feature.UnitTesting;
+
+[ShellComponent(Instantiation.DemandAnyThreadSafe)]
+public class UnityRiderUnitTestCoverageAvailabilityChecker :
+    IRiderUnitTestCoverageAvailabilityChecker,
+    IHideImplementation<DefaultRiderUnitTestCoverageAvailabilityChecker>
 {
-    [ShellComponent(Instantiation.DemandAnyThreadSafe)]
-    public class UnityRiderUnitTestCoverageAvailabilityChecker : IRiderUnitTestCoverageAvailabilityChecker, IHideImplementation<DefaultRiderUnitTestCoverageAvailabilityChecker>
+    public HostProviderAvailability GetAvailability(IUnitTestElement element)
     {
-        private static readonly Version ourMinSupportedUnityVersion = new(2019, 2);
+        if (!element.Project.IsUnityProject())
+            return HostProviderAvailability.Available;
 
-        // this method should be very fast as it gets called a lot
-        public HostProviderAvailability GetAvailability(IUnitTestElement element)
-        {
-            if (!element.Project.IsUnityProject())
-                return HostProviderAvailability.Available;
-
-            var solution = element.Project.GetSolution();
-            var frontendBackendModel = solution.GetProtocolSolution().GetFrontendBackendModel();
-            switch (frontendBackendModel.UnitTestPreference.Value)
-            {
-                case UnitTestLaunchPreference.NUnit:
-                    return HostProviderAvailability.Available;
-                case UnitTestLaunchPreference.Both:
-                case UnitTestLaunchPreference.PlayMode:
-                case UnitTestLaunchPreference.EditMode:
-                {
-                    var unityVersion = UnityVersion.Parse(frontendBackendModel.UnityApplicationData.Maybe.ValueOrDefault?.ApplicationVersion ?? string.Empty);
-
-                    return unityVersion == null || unityVersion < ourMinSupportedUnityVersion
-                        ? HostProviderAvailability.Nonexistent
-                        : HostProviderAvailability.Available;
-                }
-
-                default:
-                    return HostProviderAvailability.Nonexistent;
-            }
-        }
+        return HostProviderAvailability.Nonexistent;
     }
 }
