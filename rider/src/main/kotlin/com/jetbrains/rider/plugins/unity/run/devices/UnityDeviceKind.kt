8cee182a3b515ed1b699302218ee2419c4a0d741
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2025-06-30 13:51:41 +0200

    fix RIDER-126820 Collects stats on UnityDevice selector
    (cherry picked from commit 47f24788843a5fe758a2f9eeb26fd013a79ddd0f)
    
    IJ-CR-167525
    
    GitOrigin-RevId: c1c58932277593e57dd2f5ea31d2c8b8a004080e

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/run/devices/UnityDeviceKind.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/run/devices/UnityDeviceKind.kt
@@ -5,29 +5,30 @@ import com.jetbrains.rider.plugins.unity.UnityBundle
 import com.jetbrains.rider.run.devices.DeviceKind
 import org.jetbrains.annotations.Nls
 
-open class UnityDeviceKind(@Nls name: String) : DeviceKind(name, name) {
+open class UnityDeviceKind(id: String, @Nls name: String) : DeviceKind(id, name, name) {
+
   override fun getMissingDevicesAction(): AnAction? {
     return null
   }
 }
 
-object UnityUsbDeviceKind : UnityDeviceKind(UnityBundle.message("project.name.usb.devices")) {
+object UnityUsbDeviceKind : UnityDeviceKind("unity_usb_device", UnityBundle.message("project.name.usb.devices")) {
 }
 
-object UnityCustomPlayerDeviceKind : UnityDeviceKind(UnityBundle.message("unity.custom.devices.kind.name")) {
+object UnityCustomPlayerDeviceKind : UnityDeviceKind("unity_custom_player", UnityBundle.message("unity.custom.devices.kind.name")) {
 }
 
-object UnityRemotePlayerDeviceKind : UnityDeviceKind(UnityBundle.message("unity.remote.devices.kind.name")) {
+object UnityRemotePlayerDeviceKind : UnityDeviceKind("unity_remote_player", UnityBundle.message("unity.remote.devices.kind.name")) {
 }
 
-object UnityLocalPlayerDeviceKind : UnityDeviceKind(UnityBundle.message("unity.local.devices.kind.name")) {
+object UnityLocalPlayerDeviceKind : UnityDeviceKind("unity_local_player", UnityBundle.message("unity.local.devices.kind.name")) {
 }
 
-object UnityLocalUwpPlayerDeviceKind : UnityDeviceKind(UnityBundle.message("unity.local.uwp.devices.kind.name")) {
+object UnityLocalUwpPlayerDeviceKind : UnityDeviceKind("unity_local_uwp_player", UnityBundle.message("unity.local.uwp.devices.kind.name")) {
 }
 
-object UnityEditorDeviceKind : UnityDeviceKind(UnityBundle.message("unity.editor.devices.kind.name")) {
+object UnityEditorDeviceKind : UnityDeviceKind("unity_editor", UnityBundle.message("unity.editor.devices.kind.name")) {
 }
 
-object UnityVirtualPlayerDeviceKind : UnityDeviceKind(UnityBundle.message("unity.virtual.player.devices.kind.name")) {
+object UnityVirtualPlayerDeviceKind : UnityDeviceKind("unity_virtual_player", UnityBundle.message("unity.virtual.player.devices.kind.name")) {
 }
