cf99647e798164d4163a72abe5efba7fc73c413b
Slava Trenogin <slava.trenogin@jetbrains.com>
2025-08-23 16:42:15 +0200

    [rsat] include Unity plugins
    GitOrigin-RevId: 394702dcd3afda5d72c76f41af43700c55c40877

---
+++ resharper/resharper-unity/src/Unity.Shaders/BuildScript/SubplatformForAutomationTools.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using JetBrains.Application.BuildScript.Compile;
+using JetBrains.Application.BuildScript.Solution;
+using JetBrains.Build;
+
+namespace JetBrains.ReSharper.Plugins.Unity.Shaders.BuildScript
+{
+  public sealed class SubplatformForAutomationTools
+  {
+    [BuildStep]
+    public static IEnumerable<SubplatformComponentForPackaging> Include(AllAssembliesOnEverything aae) =>
+      AutomationToolsMarker.IncludeIn<SubplatformForAutomationTools>(aae);
+  }
+}
