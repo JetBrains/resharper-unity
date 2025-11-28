f32b2ce30da841406e4ea496c924fd5b5a8834bb
Alexander.Shvedov <alexander.shvedov@jetbrains.com>
2025-11-28 20:53:09 +0100

    RIDER-128433 Rider .NET Intentions Previews: fix Unity tests
    GitOrigin-RevId: fb65d538e73479c72467d638e70e3365212d15eb

--- resharper/resharper-unity/src/Unity/CSharp/Feature/Services/CallGraph/AddCommentBulbActionBase.cs
+++ resharper/resharper-unity/src/Unity/CSharp/Feature/Services/CallGraph/AddCommentBulbActionBase.cs
@@ -1,39 +1,37 @@
 using JetBrains.Annotations;
+using JetBrains.Application.Progress;
 using JetBrains.Application.Threading;
 using JetBrains.ProjectModel;
-using JetBrains.ReSharper.Feature.Services.Bulbs;
+using JetBrains.ReSharper.Feature.Services.BulbActions;
 using JetBrains.ReSharper.Feature.Services.Comment;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.CSharp.Tree;
 using JetBrains.ReSharper.Psi.Tree;
-using JetBrains.TextControl;
 
 namespace JetBrains.ReSharper.Plugins.Unity.CSharp.Feature.Services.CallGraph
 {
-    public abstract class AddCommentBulbActionBase : IBulbAction
+    public abstract class AddCommentBulbActionBase : ModernBulbActionBase
     {
         [NotNull] private readonly IMethodDeclaration myMethodDeclaration;
-        
+
         protected AddCommentBulbActionBase([NotNull] IMethodDeclaration methodDeclaration)
         {
             myMethodDeclaration = methodDeclaration;
         }
-        
-        public abstract string Text { get; }
+
         [NotNull] protected abstract string Comment { get; }
-        
-        public void Execute(ISolution solution, ITextControl textControl)
+
+        protected override IBulbActionCommand ExecutePsiTransaction(ISolution solution, IProgressIndicator progress)
         {
             solution.Locks.AssertReadAccessAllowed();
+
             var file = myMethodDeclaration.GetContainingFile();
-            
-            if (file == null) 
-                return;
-            
+            if (file == null) return null;
+
             var treeRange = myMethodDeclaration.GetTreeTextRange();
             var inserter = LanguageManager.Instance.GetService<ICommentOrDirectiveInserter>(file.Language);
 
-            inserter.Insert(treeRange, file, Text, Comment);
+            return inserter.Insert(treeRange, file, Comment);
         }
     }
 }
