3788c8498c87bb368680a3585fc03a2893b0fd01
george.lemeshko <heorhii.lemeshko@jetbrains.com>
2025-12-15 19:19:31 +0100

    [rider|unity][RIDER-91824] Add IReSharperHostCoreSharedFeatureZone requirement to ZoneMarker
    Space-RevId: ce068a16c79a3a55380432b2d7b3870f729da363
    
    GitOrigin-RevId: 8737b7f6d03284fcc4e46e9659e714165cab4991

--- resharper/resharper-unity/src/Unity.Rider/Common/CSharp/ZoneMarker.cs
+++ resharper/resharper-unity/src/Unity.Rider/Common/CSharp/ZoneMarker.cs
@@ -1,10 +1,12 @@
 using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.RdBackend.Common.Env;
 using JetBrains.ReSharper.Psi.CSharp;
 
 namespace JetBrains.ReSharper.Plugins.Unity.Rider.Common.CSharp
 {
     [ZoneMarker]
-    public class ZoneMarker : IRequire<ILanguageCSharpZone>
+    public class ZoneMarker : IRequire<ILanguageCSharpZone>,   
+        IRequire<IReSharperHostCoreSharedFeatureZone>
     {
     }
 }
