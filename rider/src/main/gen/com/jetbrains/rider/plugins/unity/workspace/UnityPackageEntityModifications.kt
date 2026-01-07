8f786a6085b148b5cf88ca09cb5c359c77ee9830
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2026-01-07 16:36:10 +0100

    fix RIDER-133173 MissingTypeMetadataException: Metadata for the com.jetbrains.rider.plugins.unity.workspace.UnityWorkspacePackageUpdater$RiderUnityPackageEntitySource was not collected. Please run the generator for the class
    (cherry picked from commit 8738d2cd9b4d74d9171888434abb1ac010c2cb5b)
    
    IJ-CR-187153
    
    GitOrigin-RevId: 2c6dd3f8ad88754aad6f57e2718a99d18bc69ccf

--- rider/src/main/gen/com/jetbrains/rider/plugins/unity/workspace/UnityPackageEntityModifications.kt
+++ rider/src/main/gen/com/jetbrains/rider/plugins/unity/workspace/UnityPackageEntityModifications.kt
@@ -2,39 +2,42 @@
 
 package com.jetbrains.rider.plugins.unity.workspace
 
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.workspace.storage.*
+import com.jetbrains.rdclient.util.idea.toVirtualFile
 import com.jetbrains.rider.plugins.unity.model.frontendBackend.UnityPackage
+import com.jetbrains.rider.plugins.unity.model.frontendBackend.UnityPackageSource
 
 @GeneratedCodeApiVersion(3)
 interface UnityPackageEntityBuilder : WorkspaceEntityBuilder<UnityPackageEntity> {
-  override var entitySource: EntitySource
-  var descriptor: UnityPackage
+    override var entitySource: EntitySource
+    var descriptor: UnityPackage
 }
 
 internal object UnityPackageEntityType : EntityType<UnityPackageEntity, UnityPackageEntityBuilder>() {
-  override val entityClass: Class<UnityPackageEntity> get() = UnityPackageEntity::class.java
-  operator fun invoke(
-    descriptor: UnityPackage,
-    entitySource: EntitySource,
-    init: (UnityPackageEntityBuilder.() -> Unit)? = null,
-  ): UnityPackageEntityBuilder {
-    val builder = builder()
-    builder.descriptor = descriptor
-    builder.entitySource = entitySource
-    init?.invoke(builder)
-    return builder
-  }
+    override val entityClass: Class<UnityPackageEntity> get() = UnityPackageEntity::class.java
+    operator fun invoke(
+        descriptor: UnityPackage,
+        entitySource: EntitySource,
+        init: (UnityPackageEntityBuilder.() -> Unit)? = null,
+    ): UnityPackageEntityBuilder {
+        val builder = builder()
+        builder.descriptor = descriptor
+        builder.entitySource = entitySource
+        init?.invoke(builder)
+        return builder
+    }
 }
 
 fun MutableEntityStorage.modifyUnityPackageEntity(
-  entity: UnityPackageEntity,
-  modification: UnityPackageEntityBuilder.() -> Unit,
+    entity: UnityPackageEntity,
+    modification: UnityPackageEntityBuilder.() -> Unit,
 ): UnityPackageEntity = modifyEntity(UnityPackageEntityBuilder::class.java, entity, modification)
 
 @JvmOverloads
 @JvmName("createUnityPackageEntity")
 fun UnityPackageEntity(
-  descriptor: UnityPackage,
-  entitySource: EntitySource,
-  init: (UnityPackageEntityBuilder.() -> Unit)? = null,
+    descriptor: UnityPackage,
+    entitySource: EntitySource,
+    init: (UnityPackageEntityBuilder.() -> Unit)? = null,
 ): UnityPackageEntityBuilder = UnityPackageEntityType(descriptor, entitySource, init)
--- rider/src/main/gen/com/jetbrains/rider/plugins/unity/workspace/impl/UnityPackageEntityImpl.kt
+++ rider/src/main/gen/com/jetbrains/rider/plugins/unity/workspace/impl/UnityPackageEntityImpl.kt
@@ -1,5 +1,8 @@
+@file:Suppress("UnstableApiUsage")
+
 package com.jetbrains.rider.plugins.unity.workspace.impl
 
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.workspace.storage.*
 import com.intellij.platform.workspace.storage.impl.ModifiableWorkspaceEntityBase
 import com.intellij.platform.workspace.storage.impl.WorkspaceEntityBase
@@ -7,7 +10,9 @@ import com.intellij.platform.workspace.storage.impl.WorkspaceEntityData
 import com.intellij.platform.workspace.storage.instrumentation.EntityStorageInstrumentation
 import com.intellij.platform.workspace.storage.instrumentation.EntityStorageInstrumentationApi
 import com.intellij.platform.workspace.storage.metadata.model.EntityMetadata
+import com.jetbrains.rdclient.util.idea.toVirtualFile
 import com.jetbrains.rider.plugins.unity.model.frontendBackend.UnityPackage
+import com.jetbrains.rider.plugins.unity.model.frontendBackend.UnityPackageSource
 import com.jetbrains.rider.plugins.unity.workspace.UnityPackageEntity
 import com.jetbrains.rider.plugins.unity.workspace.UnityPackageEntityBuilder
 
@@ -15,168 +20,167 @@ import com.jetbrains.rider.plugins.unity.workspace.UnityPackageEntityBuilder
 @GeneratedCodeImplVersion(7)
 @OptIn(WorkspaceEntityInternalApi::class)
 internal class UnityPackageEntityImpl(private val dataSource: UnityPackageEntityData) : UnityPackageEntity,
-                                                                                        WorkspaceEntityBase(dataSource) {
+    WorkspaceEntityBase(dataSource) {
 
-  private companion object {
+    private companion object {
 
-    private val connections = listOf<ConnectionId>()
+        private val connections = listOf<ConnectionId>()
 
-  }
-
-  override val descriptor: UnityPackage
-    get() {
-      readField("descriptor")
-      return dataSource.descriptor
     }
 
-  override val entitySource: EntitySource
-    get() {
-      readField("entitySource")
-      return dataSource.entitySource
-    }
-
-  override fun connectionIdList(): List<ConnectionId> {
-    return connections
-  }
-
-
-  internal class Builder(result: UnityPackageEntityData?) :
-    ModifiableWorkspaceEntityBase<UnityPackageEntity, UnityPackageEntityData>(result), UnityPackageEntityBuilder {
-    internal constructor() : this(UnityPackageEntityData())
-
-    override fun applyToBuilder(builder: MutableEntityStorage) {
-      if (this.diff != null) {
-        if (existsInBuilder(builder)) {
-          this.diff = builder
-          return
+    override val descriptor: UnityPackage
+        get() {
+            readField("descriptor")
+            return dataSource.descriptor
         }
-        else {
-          error("Entity UnityPackageEntity is already created in a different builder")
-        }
-      }
-      this.diff = builder
-      addToBuilder()
-      this.id = getEntityData().createEntityId()
-// After adding entity data to the builder, we need to unbind it and move the control over entity data to builder
-// Builder may switch to snapshot at any moment and lock entity data to modification
-      this.currentEntityData = null
-// Process linked entities that are connected without a builder
-      processLinkedEntities(builder)
-      checkInitialization() // TODO uncomment and check failed tests
-    }
 
-    private fun checkInitialization() {
-      val _diff = diff
-      if (!getEntityData().isEntitySourceInitialized()) {
-        error("Field WorkspaceEntity#entitySource should be initialized")
-      }
-      if (!getEntityData().isDescriptorInitialized()) {
-        error("Field UnityPackageEntity#descriptor should be initialized")
-      }
-    }
+    override val entitySource: EntitySource
+        get() {
+            readField("entitySource")
+            return dataSource.entitySource
+        }
 
     override fun connectionIdList(): List<ConnectionId> {
-      return connections
-    }
-
-    // Relabeling code, move information from dataSource to this builder
-    override fun relabel(dataSource: WorkspaceEntity, parents: Set<WorkspaceEntity>?) {
-      dataSource as UnityPackageEntity
-      if (this.entitySource != dataSource.entitySource) this.entitySource = dataSource.entitySource
-      if (this.descriptor != dataSource.descriptor) this.descriptor = dataSource.descriptor
-      updateChildToParentReferences(parents)
+        return connections
     }
 
 
-    override var entitySource: EntitySource
-      get() = getEntityData().entitySource
-      set(value) {
-        checkModificationAllowed()
-        getEntityData(true).entitySource = value
-        changedProperty.add("entitySource")
+    internal class Builder(result: UnityPackageEntityData?) :
+        ModifiableWorkspaceEntityBase<UnityPackageEntity, UnityPackageEntityData>(result), UnityPackageEntityBuilder {
+        internal constructor() : this(UnityPackageEntityData())
 
-      }
-    override var descriptor: UnityPackage
-      get() = getEntityData().descriptor
-      set(value) {
-        checkModificationAllowed()
-        getEntityData(true).descriptor = value
-        changedProperty.add("descriptor")
+        override fun applyToBuilder(builder: MutableEntityStorage) {
+            if (this.diff != null) {
+                if (existsInBuilder(builder)) {
+                    this.diff = builder
+                    return
+                } else {
+                    error("Entity UnityPackageEntity is already created in a different builder")
+                }
+            }
+            this.diff = builder
+            addToBuilder()
+            this.id = getEntityData().createEntityId()
+// After adding entity data to the builder, we need to unbind it and move the control over entity data to builder
+// Builder may switch to snapshot at any moment and lock entity data to modification
+            this.currentEntityData = null
+// Process linked entities that are connected without a builder
+            processLinkedEntities(builder)
+            checkInitialization() // TODO uncomment and check failed tests
+        }
 
-      }
+        private fun checkInitialization() {
+            val _diff = diff
+            if (!getEntityData().isEntitySourceInitialized()) {
+                error("Field WorkspaceEntity#entitySource should be initialized")
+            }
+            if (!getEntityData().isDescriptorInitialized()) {
+                error("Field UnityPackageEntity#descriptor should be initialized")
+            }
+        }
 
-    override fun getEntityClass(): Class<UnityPackageEntity> = UnityPackageEntity::class.java
-  }
+        override fun connectionIdList(): List<ConnectionId> {
+            return connections
+        }
+
+        // Relabeling code, move information from dataSource to this builder
+        override fun relabel(dataSource: WorkspaceEntity, parents: Set<WorkspaceEntity>?) {
+            dataSource as UnityPackageEntity
+            if (this.entitySource != dataSource.entitySource) this.entitySource = dataSource.entitySource
+            if (this.descriptor != dataSource.descriptor) this.descriptor = dataSource.descriptor
+            updateChildToParentReferences(parents)
+        }
+
+
+        override var entitySource: EntitySource
+            get() = getEntityData().entitySource
+            set(value) {
+                checkModificationAllowed()
+                getEntityData(true).entitySource = value
+                changedProperty.add("entitySource")
+
+            }
+        override var descriptor: UnityPackage
+            get() = getEntityData().descriptor
+            set(value) {
+                checkModificationAllowed()
+                getEntityData(true).descriptor = value
+                changedProperty.add("descriptor")
+
+            }
+
+        override fun getEntityClass(): Class<UnityPackageEntity> = UnityPackageEntity::class.java
+    }
 
 }
 
 @OptIn(WorkspaceEntityInternalApi::class)
 internal class UnityPackageEntityData : WorkspaceEntityData<UnityPackageEntity>() {
-  lateinit var descriptor: UnityPackage
+    lateinit var descriptor: UnityPackage
 
-  internal fun isDescriptorInitialized(): Boolean = ::descriptor.isInitialized
+    internal fun isDescriptorInitialized(): Boolean = ::descriptor.isInitialized
 
-  override fun wrapAsModifiable(diff: MutableEntityStorage): WorkspaceEntityBuilder<UnityPackageEntity> {
-    val modifiable = UnityPackageEntityImpl.Builder(null)
-    modifiable.diff = diff
-    modifiable.id = createEntityId()
-    return modifiable
-  }
-
-  @OptIn(EntityStorageInstrumentationApi::class)
-  override fun createEntity(snapshot: EntityStorageInstrumentation): UnityPackageEntity {
-    val entityId = createEntityId()
-    return snapshot.initializeEntity(entityId) {
-      val entity = UnityPackageEntityImpl(this)
-      entity.snapshot = snapshot
-      entity.id = entityId
-      entity
+    override fun wrapAsModifiable(diff: MutableEntityStorage): WorkspaceEntityBuilder<UnityPackageEntity> {
+        val modifiable = UnityPackageEntityImpl.Builder(null)
+        modifiable.diff = diff
+        modifiable.id = createEntityId()
+        return modifiable
     }
-  }
 
-  override fun getMetadata(): EntityMetadata {
-    return MetadataStorageImpl.getMetadataByTypeFqn("com.jetbrains.rider.plugins.unity.workspace.UnityPackageEntity") as EntityMetadata
-  }
+    @OptIn(EntityStorageInstrumentationApi::class)
+    override fun createEntity(snapshot: EntityStorageInstrumentation): UnityPackageEntity {
+        val entityId = createEntityId()
+        return snapshot.initializeEntity(entityId) {
+            val entity = UnityPackageEntityImpl(this)
+            entity.snapshot = snapshot
+            entity.id = entityId
+            entity
+        }
+    }
 
-  override fun getEntityInterface(): Class<out WorkspaceEntity> {
-    return UnityPackageEntity::class.java
-  }
+    override fun getMetadata(): EntityMetadata {
+        return MetadataStorageImpl.getMetadataByTypeFqn("com.jetbrains.rider.plugins.unity.workspace.UnityPackageEntity") as EntityMetadata
+    }
 
-  override fun createDetachedEntity(parents: List<WorkspaceEntityBuilder<*>>): WorkspaceEntityBuilder<*> {
-    return UnityPackageEntity(descriptor, entitySource)
-  }
+    override fun getEntityInterface(): Class<out WorkspaceEntity> {
+        return UnityPackageEntity::class.java
+    }
 
-  override fun getRequiredParents(): List<Class<out WorkspaceEntity>> {
-    val res = mutableListOf<Class<out WorkspaceEntity>>()
-    return res
-  }
+    override fun createDetachedEntity(parents: List<WorkspaceEntityBuilder<*>>): WorkspaceEntityBuilder<*> {
+        return UnityPackageEntity(descriptor, entitySource)
+    }
 
-  override fun equals(other: Any?): Boolean {
-    if (other == null) return false
-    if (this.javaClass != other.javaClass) return false
-    other as UnityPackageEntityData
-    if (this.entitySource != other.entitySource) return false
-    if (this.descriptor != other.descriptor) return false
-    return true
-  }
+    override fun getRequiredParents(): List<Class<out WorkspaceEntity>> {
+        val res = mutableListOf<Class<out WorkspaceEntity>>()
+        return res
+    }
 
-  override fun equalsIgnoringEntitySource(other: Any?): Boolean {
-    if (other == null) return false
-    if (this.javaClass != other.javaClass) return false
-    other as UnityPackageEntityData
-    if (this.descriptor != other.descriptor) return false
-    return true
-  }
+    override fun equals(other: Any?): Boolean {
+        if (other == null) return false
+        if (this.javaClass != other.javaClass) return false
+        other as UnityPackageEntityData
+        if (this.entitySource != other.entitySource) return false
+        if (this.descriptor != other.descriptor) return false
+        return true
+    }
 
-  override fun hashCode(): Int {
-    var result = entitySource.hashCode()
-    result = 31 * result + descriptor.hashCode()
-    return result
-  }
+    override fun equalsIgnoringEntitySource(other: Any?): Boolean {
+        if (other == null) return false
+        if (this.javaClass != other.javaClass) return false
+        other as UnityPackageEntityData
+        if (this.descriptor != other.descriptor) return false
+        return true
+    }
 
-  override fun hashCodeIgnoringEntitySource(): Int {
-    var result = javaClass.hashCode()
-    result = 31 * result + descriptor.hashCode()
-    return result
-  }
+    override fun hashCode(): Int {
+        var result = entitySource.hashCode()
+        result = 31 * result + descriptor.hashCode()
+        return result
+    }
+
+    override fun hashCodeIgnoringEntitySource(): Int {
+        var result = javaClass.hashCode()
+        result = 31 * result + descriptor.hashCode()
+        return result
+    }
 }
