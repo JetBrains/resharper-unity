3a8517e460cba079e05fb2be9c2cab6221a6e7fc
george.lemeshko <heorhii.lemeshko@jetbrains.com>
2026-02-12 17:18:19 +0100

    RIDER-123067 profiler: refactor actions to use `ToggleAction` and clean up related logic
    - Converted `ToggleUnityProfilerAutoFetchAction`, `ToggleUnityProfilerGutterMarksAction`, and `ToggleUnityProfilerIntegrationAction` to `ToggleAction` for simplification and consistency.
    - Removed redundant `update` methods, improving code clarity.
    - Updated localized strings to reflect unified action behavior.
    
    GitOrigin-RevId: 278bf86572018985f1659f32b237449cee274e78

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/profiler/actions/ToggleUnityProfilerAutoFetchAction.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/profiler/actions/ToggleUnityProfilerAutoFetchAction.kt
@@ -1,6 +1,5 @@
 package com.jetbrains.rider.plugins.unity.profiler.actions
 
-import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.ToggleAction
@@ -11,17 +10,10 @@ import com.jetbrains.rider.plugins.unity.ui.UnityUIBundle
 class ToggleUnityProfilerAutoFetchAction(private val settings: UnityProfilerIntegrationSettingsModel) : ToggleAction(
     UnityUIBundle.message("unity.profiler.integration.auto.fetch"),
     null,
-    AllIcons.General.RefreshAuto
+    null
 ) {
     override fun isSelected(e: AnActionEvent): Boolean = settings.fetchingMode.valueOrNull == FetchingMode.Auto
-    override fun setSelected(e: AnActionEvent, state: Boolean) {
+    override fun setSelected(e: AnActionEvent, state: Boolean): Unit =
         settings.fetchingMode.set(if (state) FetchingMode.Auto else FetchingMode.Manual)
-    }
-
-    override fun update(e: AnActionEvent) {
-        super.update(e)
-        e.presentation.isEnabled = settings.isIntegrationEnabled.valueOrNull ?: false
-    }
-
     override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
 }
