b98fcf0da89316455853ccf6be37d02ecf4e4ba5
george.lemeshko <heorhii.lemeshko@jetbrains.com>
2026-02-12 17:18:19 +0100

    RIDER-123067 profiler: refactor actions to use `ToggleAction` and clean up related logic
    - Converted `ToggleUnityProfilerAutoFetchAction`, `ToggleUnityProfilerGutterMarksAction`, and `ToggleUnityProfilerIntegrationAction` to `ToggleAction` for simplification and consistency.
    - Removed redundant `update` methods, improving code clarity.
    - Updated localized strings to reflect unified action behavior.
    
    GitOrigin-RevId: d436f6183d13f01bbf0ea45bbdb22461bd036f2c

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/profiler/actions/ToggleUnityProfilerGutterMarksAction.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/profiler/actions/ToggleUnityProfilerGutterMarksAction.kt
@@ -1,34 +1,23 @@
 package com.jetbrains.rider.plugins.unity.profiler.actions
 
-import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.actionSystem.ToggleAction
+import com.jetbrains.rd.util.reactive.valueOrDefault
 import com.jetbrains.rider.plugins.unity.model.frontendBackend.ProfilerGutterMarkRenderSettings
 import com.jetbrains.rider.plugins.unity.profiler.UnityProfilerIntegrationSettingsModel
 import com.jetbrains.rider.plugins.unity.ui.UnityUIBundle
 
-class ToggleUnityProfilerGutterMarksAction(private val settings: UnityProfilerIntegrationSettingsModel) : DumbAwareAction() {
-    override fun actionPerformed(e: AnActionEvent) {
-        val currentValue = settings.gutterMarksRenderSettings.valueOrNull ?: ProfilerGutterMarkRenderSettings.Default
-        if (currentValue == ProfilerGutterMarkRenderSettings.Hidden) {
-            settings.gutterMarksRenderSettings.set(ProfilerGutterMarkRenderSettings.Default)
-        } else {
-            settings.gutterMarksRenderSettings.set(ProfilerGutterMarkRenderSettings.Hidden)
-        }
-    }
+class ToggleUnityProfilerGutterMarksAction(private val settings: UnityProfilerIntegrationSettingsModel) : ToggleAction(
+    UnityUIBundle.message("unity.profiler.integration.gutter.marks.enable"),
+    null,
+    null
+) {
+    override fun isSelected(e: AnActionEvent): Boolean =
+        settings.gutterMarksRenderSettings.valueOrDefault(ProfilerGutterMarkRenderSettings.Default) != ProfilerGutterMarkRenderSettings.Hidden
 
-    override fun update(e: AnActionEvent) {
-        val current = settings.gutterMarksRenderSettings.valueOrNull ?: ProfilerGutterMarkRenderSettings.Default
-        val isHidden = current == ProfilerGutterMarkRenderSettings.Hidden
-        e.presentation.text = if (isHidden) {
-            UnityUIBundle.message("unity.profiler.integration.gutter.marks.enable")
-        } else {
-            UnityUIBundle.message("unity.profiler.integration.gutter.marks.disable")
-        }
-        e.presentation.icon = if (isHidden) AllIcons.Actions.Show else AllIcons.Actions.ToggleVisibility
-        e.presentation.isEnabled = settings.isIntegrationEnabled.valueOrNull ?: false
-    }
+    override fun setSelected(e: AnActionEvent, state: Boolean): Unit =
+        settings.gutterMarksRenderSettings.set(if (state) ProfilerGutterMarkRenderSettings.Default else ProfilerGutterMarkRenderSettings.Hidden)
 
     override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
 }
