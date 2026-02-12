07961036479b60b73560986196eac8dc0e58a9a6
Alexander Karaev <alexander.karaev@jetbrains.com>
2026-02-12 17:18:43 +0200

    cleanup [rider-cpp]: rename `CppSyntax*` to `RiderCppSyntax*`
    GitOrigin-RevId: ac0037bed3150eb23258f37349de91d974b06b29

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/ideaInterop/fileTypes/shaderLab/ShaderLabSyntaxHighlighterProvider.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/ideaInterop/fileTypes/shaderLab/ShaderLabSyntaxHighlighterProvider.kt
@@ -6,16 +6,16 @@ import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory
 import com.intellij.openapi.fileTypes.SyntaxHighlighterProvider
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
-import com.jetbrains.rider.cpp.fileType.CppSyntaxHighlighter
+import com.jetbrains.rider.cpp.fileType.RiderCppSyntaxHighlighter
 
 class ShaderLabSyntaxHighlighterProvider : SyntaxHighlighterProvider, SyntaxHighlighterFactory() {
     override fun getSyntaxHighlighter(project: Project?, file: VirtualFile?): SyntaxHighlighter {
-        return CppSyntaxHighlighter()
+        return RiderCppSyntaxHighlighter()
     }
 
     override fun create(fileType: FileType, project: Project?, file: VirtualFile?): SyntaxHighlighter? {
         if (fileType !is ShaderLabFileType) return null
-        return CppSyntaxHighlighter()
+        return RiderCppSyntaxHighlighter()
     }
 }
 
