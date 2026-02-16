436c069773478e20e43d6a9c4d9cfe15576e8c45
george.lemeshko <heorhii.lemeshko@jetbrains.com>
2026-02-16 14:31:42 +0100

    RIDER-123067 profiler: refactor text field binding and tree update logic for improved UI responsiveness and maintainability
    fix RIDER-135804
    
    GitOrigin-RevId: 17998103f2a1cf0006971e81f46af3590b9df2be

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/profiler/toolWindow/UnityProfilerFilterField.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/profiler/toolWindow/UnityProfilerFilterField.kt
@@ -1,29 +1,21 @@
 package com.jetbrains.rider.plugins.unity.profiler.toolWindow
 
-import com.intellij.ui.DocumentAdapter
 import com.intellij.ui.SearchTextField
 import com.jetbrains.rd.util.lifetime.Lifetime
 import com.jetbrains.rd.util.lifetime.intersect
+import com.jetbrains.rider.plugins.unity.profiler.utils.bindTextFieldToProperty
 import com.jetbrains.rider.plugins.unity.profiler.viewModels.UnityProfilerTreeViewModel
-import javax.swing.event.DocumentEvent
 
 class UnityProfilerFilterField(
-    private val viewModel: UnityProfilerTreeViewModel,
+    viewModel: UnityProfilerTreeViewModel,
     lifetime: Lifetime
 ) : SearchTextField(false) {
     init {
-        addDocumentListener(object : DocumentAdapter() {
-            override fun textChanged(e: DocumentEvent) {
-                if (text != viewModel.filterText.value) {
-                    viewModel.setFilter(text, false)
-                }
-            }
-        })
-
-        viewModel.filterText.advise(viewModel.lifetime.intersect(lifetime)) {
-            if (text != it) {
-                text = it
-            }
-        }
+        bindTextFieldToProperty(
+            textComponent = textEditor,
+            property = viewModel.filterText,
+            lifetime = viewModel.lifetime.intersect(lifetime),
+            onTextChanged = { text -> viewModel.setFilter(text, false) }
+        )
     }
 }
---
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/profiler/utils/PropertyTextFieldBinding.kt
@@ -0,0 +1,50 @@
+package com.jetbrains.rider.plugins.unity.profiler.utils
+
+import com.intellij.ui.DocumentAdapter
+import com.jetbrains.rd.util.lifetime.Lifetime
+import com.jetbrains.rd.util.reactive.IProperty
+import javax.swing.event.DocumentEvent
+import javax.swing.text.JTextComponent
+
+/**
+ * Creates a bidirectional binding between a [JTextComponent] and an [IProperty].
+ *
+ * This utility handles the common pattern of synchronizing a text field with a reactive property,
+ * preventing circular update loops through an internal guard flag.
+ *
+ * @param T the type of text component (e.g., JTextField, SearchTextField)
+ * @param textComponent the UI text component to bind
+ * @param property the reactive property to bind to
+ * @param lifetime controls the lifetime of the binding subscription
+ * @param onTextChanged optional callback invoked when text changes from user input (not from property updates)
+ */
+fun <T : JTextComponent> bindTextFieldToProperty(
+    textComponent: T,
+    property: IProperty<String>,
+    lifetime: Lifetime,
+    onTextChanged: ((String) -> Unit)? = null
+) {
+    var updatingFromProperty = false
+
+    textComponent.document.addDocumentListener(object : DocumentAdapter() {
+        override fun textChanged(e: DocumentEvent) {
+            if (!updatingFromProperty) {
+                val text = textComponent.text
+                if (text != property.value) {
+                    onTextChanged?.invoke(text)
+                }
+            }
+        }
+    })
+
+    property.advise(lifetime) { newValue ->
+        if (textComponent.text != newValue) {
+            updatingFromProperty = true
+            try {
+                textComponent.text = newValue
+            } finally {
+                updatingFromProperty = false
+            }
+        }
+    }
+}
