9ea06b2f839a5e673323793b81846d4f5273c96a
Alexey Korovin <Alexey.Korovin@jetbrains.com>
2026-02-27 19:34:04 +0100

    RIDER-136325 Apply hack with using both APIs for different modes to Unity Texture Visualizer as well
    GitOrigin-RevId: a9dfa5467920699b153a7568ead0a609230711e8

--- rider/src/textureVisualizer/frontend/src/intellij/rider/plugins/unity/debugger/textureVisualizer/frontend/UnityTextureLinkProvider.kt
+++ rider/src/textureVisualizer/frontend/src/intellij/rider/plugins/unity/debugger/textureVisualizer/frontend/UnityTextureLinkProvider.kt
@@ -1,20 +1,57 @@
 package intellij.rider.plugins.unity.debugger.textureVisualizer.frontend
 
+import com.intellij.frontend.FrontendApplicationInfo
+import com.intellij.frontend.FrontendType
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.project.Project
 import com.intellij.platform.debugger.impl.shared.proxy.XDebugManagerProxy
 import com.intellij.platform.debugger.impl.shared.proxy.XDebugSessionProxy
 import com.intellij.xdebugger.frame.XDebuggerTreeNodeHyperlink
 import com.intellij.xdebugger.frame.XValue
+import com.intellij.xdebugger.impl.collection.visualizer.XDebuggerNodeLinkActionProvider
+import com.intellij.xdebugger.impl.frame.XDebugView
+import com.intellij.xdebugger.impl.ui.tree.nodes.XValueNodeImpl
 import com.jetbrains.rider.debugger.shared.RiderDebuggerLinkProvider
 import intellij.rider.plugins.unity.debugger.textureVisualizer.RiderTextureDataApi
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+internal class UnityTextureXDebuggerNodeLinkProvider : XDebuggerNodeLinkActionProvider {
+    override suspend fun CoroutineScope.provideHyperlink(
+        project: Project,
+        node: XValueNodeImpl,
+    ): XDebuggerTreeNodeHyperlink? {
+        //TODO(Korovin): RIDER-136325 [Split Debugger, RemDev] Collection visualizers don't work
+        if (FrontendApplicationInfo.getFrontendType() is FrontendType.Monolith)
+            return null
+
+        val session = withContext(Dispatchers.EDT) {
+            XDebugView.getSessionProxy(node.tree)
+        } ?: return null
+        return hyperlink(node.valueContainer, session, this)
+    }
+}
 
 class UnityTextureLinkProvider : RiderDebuggerLinkProvider {
     override suspend fun getAdditionalLink(scope: CoroutineScope, session: XDebugSessionProxy, value: XValue): XDebuggerTreeNodeHyperlink? {
-        if (!XDebugManagerProxy.getInstance().hasBackendCounterpart(value))
+        //TODO(Korovin): RIDER-136325 [Split Debugger, RemDev] Collection visualizers don't work
+        if (FrontendApplicationInfo.getFrontendType() is FrontendType.Remote)
             return null
-        return XDebugManagerProxy.getInstance().withId(value, session) { valueId ->
-            val accessorId = RiderTextureDataApi.getInstance().findTextureAccessor(valueId).await() ?: return@withId null
-            UnityTextureHyperLink(scope, session, accessorId)
-        }
+
+        return hyperlink(value, session, scope)
+    }
+}
+
+private suspend fun hyperlink(
+    value: XValue,
+    session: XDebugSessionProxy,
+    scope: CoroutineScope
+): UnityTextureHyperLink? {
+    if (!XDebugManagerProxy.getInstance().hasBackendCounterpart(value))
+        return null
+    return XDebugManagerProxy.getInstance().withId(value, session) { valueId ->
+        val accessorId = RiderTextureDataApi.getInstance().findTextureAccessor(valueId).await() ?: return@withId null
+        UnityTextureHyperLink(scope, session, accessorId)
     }
 }
