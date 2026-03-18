3507394661328c2d9e2af54ce413da56e3cea4d7
george.lemeshko <heorhii.lemeshko@jetbrains.com>
2026-03-18 12:34:19 -0500

    RIDER-136332 unity-profiler: window activation fix
    (cherry picked from commit 48833b8b431f475c6ba1cc148e353b7bffc94562)
    
    IJ-CR-196799
    
    GitOrigin-RevId: 10975dffe0d58a4bb3ef52c36eb2d9c10acae86c

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/profiler/toolWindow/UnityProfilerToolWindowStartupActivity.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/profiler/toolWindow/UnityProfilerToolWindowStartupActivity.kt
@@ -3,19 +3,24 @@ package com.jetbrains.rider.plugins.unity.profiler.toolWindow
 import com.intellij.openapi.application.EDT
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
-import com.jetbrains.rider.plugins.unity.isUnityProject
+import com.jetbrains.rider.plugins.unity.UnityProjectDiscoverer
+import com.jetbrains.rider.plugins.unity.UnityProjectLifetimeService
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.launch
 
 /**
  * Makes Unity Profiler tool window available when a Unity project is loaded.
  */
 internal class UnityProfilerToolWindowStartupActivity : ProjectActivity {
     override suspend fun execute(project: Project) {
-        if (!project.isUnityProject.value) return
-        
-        withContext(Dispatchers.EDT) {
-            UnityProfilerToolWindowFactory.makeAvailable(project)
+        val lifetime = UnityProjectLifetimeService.getLifetime(project)
+
+        UnityProjectDiscoverer.getInstance(project).isUnityProject.advise(lifetime) { isUnity ->
+            if (isUnity) {
+                lifetime.coroutineScope.launch(Dispatchers.EDT) {
+                    UnityProfilerToolWindowFactory.makeAvailable(project)
+                }
+            }
         }
     }
 }
