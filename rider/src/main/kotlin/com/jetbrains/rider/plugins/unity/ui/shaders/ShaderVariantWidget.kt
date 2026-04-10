8ffecb81ab843ff691ae5fef1cdd4cb72fd1f793
george.lemeshko <heorhii.lemeshko@jetbrains.com>
2026-04-10 16:50:54 +0200

    RIDER-136053 unity: fix ShaderVariantWidget background width calculation
    GitOrigin-RevId: 5aa8c890152854525ba29d0fd199331880d5ed93

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/ui/shaders/ShaderVariantWidget.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/unity/ui/shaders/ShaderVariantWidget.kt
@@ -5,15 +5,12 @@ import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.actionSystem.Presentation
-import com.intellij.openapi.actionSystem.impl.ActionButton
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.markup.InspectionWidgetActionProvider
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.rd.createLifetime
 import com.intellij.ui.GotItTooltip
 import com.intellij.ui.awt.RelativePoint
-import com.intellij.util.ui.JBUI
 import com.jetbrains.rd.ide.editor.getExtensionSafe
 import com.jetbrains.rdclient.document.textControlModel
 import com.jetbrains.rider.editors.resolveContextWidget.RiderResolveContextWidget
@@ -21,17 +18,7 @@ import com.jetbrains.rider.editors.resolveContextWidget.WidgetAction
 import com.jetbrains.rider.plugins.unity.UnityBundle
 import com.jetbrains.rider.plugins.unity.ideaInterop.fileTypes.shaderLab.ShaderLabFileType
 import com.jetbrains.rider.plugins.unity.model.frontendBackend.RdShaderVariantExtension
-import java.awt.Dimension
-import java.awt.Graphics
-import java.awt.Graphics2D
-import java.awt.Insets
-import java.awt.Rectangle
-import java.awt.RenderingHints
 import java.net.URL
-import javax.swing.JComponent
-import javax.swing.JLabel
-import javax.swing.plaf.basic.BasicHTML
-import javax.swing.text.View
 
 class ShaderVariantWidgetActionProvider : InspectionWidgetActionProvider {
     override fun createAction(editor: Editor): AnAction? {
@@ -51,43 +38,6 @@ class ShaderVariantWidgetAction : WidgetAction("ShaderVariantsService"){
 
         e.presentation.text = shaderVariantWidget.text.value
     }
-
-    override fun createCustomComponent(presentation: Presentation, place: String): JComponent {
-        val component = object : ActionButton(this, presentation, place, JBUI.size(18)) {
-            override fun getInsets(): Insets = JBUI.insets(2, 4)
-            override fun paintComponent(g: Graphics?) {
-                val view = BasicHTML.createHTMLView(JLabel(), presentation.text)
-                // Enable anti-aliasing for smoother text
-                (g as Graphics2D).setRenderingHint(
-                    RenderingHints.KEY_TEXT_ANTIALIASING,
-                    RenderingHints.VALUE_TEXT_ANTIALIAS_ON
-                )
-
-                // Hover
-                val look = buttonLook
-                look.paintBackground(g, this)
-                look.paintBorder(g, this)
-
-                // Centering inside the Rect
-                val viewWidth = view.getPreferredSpan(View.X_AXIS).toInt()
-                val viewHeight = view.getPreferredSpan(View.Y_AXIS).toInt()
-                val x = (size.width - viewWidth) / 2
-                val y = (size.height - viewHeight) / 2
-
-                // Paint the HTML view within the component's bounding box
-                view.paint(g, Rectangle(x, y, viewWidth, viewHeight))
-            }
-
-            override fun getPreferredSize(): Dimension? {
-                val view = BasicHTML.createHTMLView(JLabel(), presentation.text)
-                val width = view.getPreferredSpan(View.X_AXIS).toInt()
-                val height = view.getPreferredSpan(View.Y_AXIS).toInt()
-                return Dimension(width, height)
-            }
-        }
-
-        return component
-    }
 }
 
 class ShaderVariantWidget(project: Project, editor: Editor, shaderVariant: RdShaderVariantExtension) : AbstractShaderWidget(project,
@@ -99,8 +49,11 @@ class ShaderVariantWidget(project: Project, editor: Editor, shaderVariant: RdSha
 
         shaderVariant.info.advise(lifetime) { info ->
             val newText = when {
-                info.suppressedCount > 0 -> UnityBundle.message("shaderVariant.widget.text.with.suppressed", info.enabledCount,
-                                                                info.suppressedCount, info.availableCount)
+                info.suppressedCount > 0 -> {
+                    val strikethrough = info.suppressedCount.toString().map { "$it\u0336" }.joinToString("")
+                    UnityBundle.message("shaderVariant.widget.text.with.suppressed", info.enabledCount,
+                                        strikethrough, info.availableCount)
+                }
                 else -> UnityBundle.message("shaderVariant.widget.text", info.enabledCount, info.availableCount)
             }
             text.set(newText)
