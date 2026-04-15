bd2d7523f2ef5976f39f5837815b2b626273cebc
george.lemeshko <heorhii.lemeshko@jetbrains.com>
2026-04-15 11:24:35 +0200

    RIDER-131013 revert AssetIndexingSupport to ApartmentForNotifications.Primary
    AssetIndexingSupport needs reactive IProperty<bool> for .Change.Advise
    and .SetValue writes, so the apartment-agnostic GetValue() pattern
    cannot be applied without a larger refactor. Mta() causes STA apartment
    assertion failures during test teardown. Revert to Primary(shellLocks).
    
    GitOrigin-RevId: 4818178cc4df56d9bbb5384b30b8b71368f456cf

--- resharper/resharper-unity/src/Unity/Core/Psi/Modules/AssetIndexingSupport.cs
+++ resharper/resharper-unity/src/Unity/Core/Psi/Modules/AssetIndexingSupport.cs
@@ -1,5 +1,6 @@
 using JetBrains.Application.Parts;
 using JetBrains.Application.Settings;
+using JetBrains.Application.Threading;
 using JetBrains.DataFlow;
 using JetBrains.Lifetimes;
 using JetBrains.ProjectModel;
@@ -13,12 +14,12 @@ namespace JetBrains.ReSharper.Plugins.Unity.Core.Psi.Modules
     {
         public readonly IProperty<bool> IsEnabled;
 
-        public AssetIndexingSupport(Lifetime lifetime,
+        public AssetIndexingSupport(Lifetime lifetime, IShellLocks shellLocks,
                                     SolutionWideWritableContextBoundSettingsStore settingsStore)
         {
             // If this property is written to, the changes are saved to solution level settings, specifically .sln.DotSettings.user
             IsEnabled = settingsStore.BoundSettingsStore.GetValueProperty2(lifetime,
-                (UnitySettings key) => key.IsAssetIndexingEnabled, ApartmentForNotifications.Mta());
+                (UnitySettings key) => key.IsAssetIndexingEnabled, ApartmentForNotifications.Primary(shellLocks));
         }
     }
 }
