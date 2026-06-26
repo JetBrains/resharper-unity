b6c121f5ec46399a51cb97e1351576c5242e65ce
Ivan Šachov <ivan.shakhov@jetbrains.com>
2026-06-26 11:45:14 +0200

    fix RIDER-138950 statistics
    (cherry picked from commit 3fd12432a5336680f3a18b6fef4d176af25d4cdc)
    
    IJ-MR-210451
    
    GitOrigin-RevId: 0c646afeb1ad46045d81a44ddec6bf74d57d331f

--- resharper/resharper-unity/src/Unity/Core/Feature/Services/UsageStatistics/UnityProjectTechnologyAnalyticsProvider.cs
+++ resharper/resharper-unity/src/Unity/Core/Feature/Services/UsageStatistics/UnityProjectTechnologyAnalyticsProvider.cs
@@ -1,13 +1,15 @@
+#nullable enable
 using System.Collections.Generic;
 using JetBrains.Application.Parts;
 using JetBrains.IDE.UsageStatistics;
 using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Plugins.Unity.Core.ProjectModel;
+using JetBrains.ReSharper.Plugins.Unity.UnityEditorIntegration;
 
 namespace JetBrains.ReSharper.Plugins.Unity.Core.Feature.Services.UsageStatistics
 {
     [SolutionComponent(Instantiation.DemandAnyThreadSafe)]
-    public class UnityProjectTechnologyAnalyticsProvider(UnitySolutionTracker unitySolutionTracker) : IProjectTechnologyAnalyticsProvider
+    public class UnityProjectTechnologyAnalyticsProvider(UnitySolutionTracker unitySolutionTracker, IUnityVersion unityVersion) : IProjectTechnologyAnalyticsProvider
     {
         public IEnumerable<string> GetProjectTechnology(IProject project)
         {
@@ -16,6 +18,10 @@ namespace JetBrains.ReSharper.Plugins.Unity.Core.Feature.Services.UsageStatistic
                 yield return "Unity";
                 yield return "GameDev";
             }
+            
+            var appPath = unityVersion.GetActualAppPathForSolution();
+            if (UnityInstallationFinder.IsTuanjie(appPath))
+                yield return "Tuanjie";
         }
     }
 }
