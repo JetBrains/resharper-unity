043c4d72a80dc5fe4a5035fe676057f35bebc08e
Kirill Bochkarev <kirill.bochkarev@jetbrains.com>
2026-08-11 17:53:59 +0000

    IJPL-15937 Workspace Model: hide entity implementation details, regen IntelliJ v23
    - make EntityId internal
    - move code from gen to ModifiableWorkspaceEntityBase
    - entityIdEquals for ArtifactBridge
    - @WorkspaceEntityInternalApi WorkspaceEntityBase
    
    
    Merge-request: IJ-MR-217660
    Merged-by: Kirill Bochkarev <kirill.bochkarev@jetbrains.com>
    
    GitOrigin-RevId: 0f7de3371ee310908c1ff2d7bafae05f74257afb

--- rider/src/main/gen/com/jetbrains/rider/plugins/unity/workspace/impl/UnityPackageEntityImpl.kt
+++ rider/src/main/gen/com/jetbrains/rider/plugins/unity/workspace/impl/UnityPackageEntityImpl.kt
@@ -6,14 +6,12 @@ import com.intellij.platform.workspace.storage.ConnectionId
 import com.intellij.platform.workspace.storage.EntitySource
 import com.intellij.platform.workspace.storage.GeneratedCodeApiVersion
 import com.intellij.platform.workspace.storage.GeneratedCodeImplVersion
-import com.intellij.platform.workspace.storage.MutableEntityStorage
 import com.intellij.platform.workspace.storage.WorkspaceEntity
 import com.intellij.platform.workspace.storage.WorkspaceEntityBuilder
 import com.intellij.platform.workspace.storage.WorkspaceEntityInternalApi
 import com.intellij.platform.workspace.storage.impl.ModifiableWorkspaceEntityBase
 import com.intellij.platform.workspace.storage.impl.WorkspaceEntityBase
 import com.intellij.platform.workspace.storage.impl.WorkspaceEntityData
-import com.intellij.platform.workspace.storage.instrumentation.EntityStorageInstrumentation
 import com.intellij.platform.workspace.storage.instrumentation.EntityStorageInstrumentationApi
 import com.intellij.platform.workspace.storage.metadata.model.EntityMetadata
 import com.jetbrains.rider.plugins.unity.model.frontendBackend.UnityPackage
@@ -45,27 +43,7 @@ internal class UnityPackageEntityImpl(private val dataSource: UnityPackageEntity
         ModifiableWorkspaceEntityBase<UnityPackageEntity, UnityPackageEntityData>(result), UnityPackageEntityBuilder {
         internal constructor() : this(UnityPackageEntityData())
 
-        override fun applyToBuilder(builder: MutableEntityStorage) {
-            if (this.diff != null) {
-                if (existsInBuilder(builder)) {
-                    this.diff = builder
-                    return
-                } else {
-                    error("Entity UnityPackageEntity is already created in a different builder")
-                }
-            }
-            this.diff = builder
-            addToBuilder()
-            this.id = getEntityData().createEntityId()
-// After adding entity data to the builder, we need to unbind it and move the control over entity data to builder
-// Builder may switch to snapshot at any moment and lock entity data to modification
-            this.currentEntityData = null
-// Process linked entities that are connected without a builder
-            processLinkedEntities(builder)
-            checkInitialization()
-        }
-
-        private fun checkInitialization() {
+        override fun checkInitialization() {
             val _diff = diff
             if (!getEntityData().isEntitySourceInitialized()) {
                 error("Field WorkspaceEntity#entitySource should be initialized")
@@ -110,23 +88,8 @@ internal class UnityPackageEntityImpl(private val dataSource: UnityPackageEntity
 internal class UnityPackageEntityData : WorkspaceEntityData<UnityPackageEntity>() {
     lateinit var descriptor: UnityPackage
     internal fun isDescriptorInitialized(): Boolean = ::descriptor.isInitialized
-    override fun wrapAsModifiable(diff: MutableEntityStorage): WorkspaceEntityBuilder<UnityPackageEntity> {
-        val modifiable = UnityPackageEntityImpl.Builder(null)
-        modifiable.diff = diff
-        modifiable.id = createEntityId()
-        return modifiable
-    }
-
-    override fun createEntity(snapshot: EntityStorageInstrumentation): UnityPackageEntity {
-        val entityId = createEntityId()
-        return snapshot.initializeEntity(entityId) {
-            val entity = UnityPackageEntityImpl(this)
-            entity.snapshot = snapshot
-            entity.id = entityId
-            entity
-        }
-    }
-
+    override fun newInstance(): UnityPackageEntity = UnityPackageEntityImpl(this)
+    override fun newBuilderInstance(): ModifiableWorkspaceEntityBase<UnityPackageEntity, *> = UnityPackageEntityImpl.Builder(null)
     override fun getMetadata(): EntityMetadata {
         return MetadataStorageImpl.getMetadataByTypeFqn("com.jetbrains.rider.plugins.unity.workspace.UnityPackageEntity") as EntityMetadata
     }
