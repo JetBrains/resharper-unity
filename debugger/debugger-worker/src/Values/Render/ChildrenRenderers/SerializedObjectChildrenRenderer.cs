f638f48f3168141cb34602f55ea84f1b5402f785
Daniil Korostelev <daniil.korostelev@jetbrains.com>
2026-08-25 08:11:42 +0200

    RIDER-142154 Unity: remove a TODO
    (unrelated to previous commit, just saving some PR noise)
    
    GitOrigin-RevId: 54571ba544691df947d7f7b94aa97041f4afa9d0

--- debugger/debugger-worker/src/Values/Render/ChildrenRenderers/SerializedObjectChildrenRenderer.cs
+++ debugger/debugger-worker/src/Values/Render/ChildrenRenderers/SerializedObjectChildrenRenderer.cs
@@ -114,9 +114,6 @@ namespace JetBrains.Debugger.Worker.Plugins.Unity.Values.Render.ChildrenRenderer
                 // show all child properties of the SerializedObject. But empirically, there is only one direct child of
                 // SerializedObject, called "Base", with a depth of -1. We'll avoid the unnecessary method invocations,
                 // unless it turns out to be an actual issue.
-
-                // TODO: this is kinda annoying UI-wise, since all SerializedObject will always have a single "Base"
-                //  entry that the user will have to expand further to get more properties. should we skip it?
             }
 
             private bool TryInvokeGetIterator(IObjectValueRole<TValue> serializedObjectRole,
